1a1ff6ba7bc825155be3fe33e30ba8493ebd568f
madmalkav <madmalkav@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-08-28 17:19:50 +0000

    Added database host param for the batch files (lithium)

--- datapack_development/tools/full_install.bat
+++ datapack_development/tools/full_install.bat
@@ -11,6 +11,7 @@ REM set mysqlBinPath=C:\Program Files\MySQL\MySQL Server 4.1\bin
 set user=root
 set pass=
 set DBname=l2jdb
+set DBHost=localhost
 
 REM ############################################
 
@@ -19,53 +20,53 @@ set mysqlPath="%mysqlBinPath%\mysql"
 
 echo.
 echo Making a backup of the original database.
-%mysqldumpPath% --add-drop-table -u %user% --password=%pass% %DBname% > l2jdb_backup.sql
+%mysqldumpPath% --add-drop-table -h %DBHost% -u %user% --password=%pass% %DBname% > l2jdb_backup.sql
 echo.
 echo Deleting tables for new content.
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < full_install.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < full_install.sql
 echo.
 echo Installling new content.
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/accounts.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/armor.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/boxaccess.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/boxes.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards_skills.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/char_templates.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_friends.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_hennas.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_macroses.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_quests.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_recipebook.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_shortcuts.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/character_skills.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/characters.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/clan_data.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/clan_wars.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/class_list.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/droplist.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/etcitem.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/henna.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/henna_trees.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/items.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/locations.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/lvlupgain.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/mapregion.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_areas_list.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_buylists.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_lease.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_shopids.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchants.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/minions.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/npc.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/npcskills.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/pets.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/siege_clans.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_learn.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_spellbooks.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_trees.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/spawnlist.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/teleport.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/weapon.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/seven_signs.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/accounts.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/armor.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/boxaccess.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/boxes.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards_skills.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/char_templates.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_friends.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_hennas.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_macroses.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_quests.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_recipebook.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_shortcuts.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_skills.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/characters.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/clan_data.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/clan_wars.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/class_list.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/droplist.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/etcitem.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/henna.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/henna_trees.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/items.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/locations.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/lvlupgain.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/mapregion.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_areas_list.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_buylists.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_lease.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_shopids.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchants.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/minions.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/npc.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/npcskills.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/pets.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/siege_clans.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_learn.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_spellbooks.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_trees.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/spawnlist.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/teleport.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/weapon.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/seven_signs.sql
 pause
--- datapack_development/tools/upgrade.bat
+++ datapack_development/tools/upgrade.bat
@@ -12,6 +12,7 @@ REM set mysqlBinPath=C:\Program Files\MySQL\MySQL Server 4.1\bin
 set user=root
 set pass=
 set DBname=l2jdb
+set DBHost=localhost
 
 REM ############################################
 
@@ -20,34 +21,34 @@ set mysqlPath="%mysqlBinPath%\mysql"
 
 echo.
 echo Making a backup of the original database.
-%mysqldumpPath% --add-drop-table -u %user% --password=%pass% %DBname% > l2jdb_backup.sql
+%mysqldumpPath% --add-drop-table -h %DBHost% -u %user% --password=%pass% %DBname% > l2jdb_backup.sql
 echo.
 echo Deleting table for new content.
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < upgrade.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < upgrade.sql
 echo.
 echo Installing new content.
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/armor.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/char_templates.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/class_list.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/droplist.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/etcitem.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/henna_trees.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/henna.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/locations.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/lvlupgain.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/mapregion.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_areas_list.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_buylists.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_shopids.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/merchants.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/minions.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/npc.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/npcskills.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_learn.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_trees.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/skill_spellbooks.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/spawnlist.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/teleport.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/weapon.sql
-%mysqlPath% -u %user% --password=%pass% -D %DBname% < ../sql/seven_signs.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/armor.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/char_templates.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/class_list.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/droplist.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/etcitem.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/henna_trees.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/henna.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/locations.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/lvlupgain.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/mapregion.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_areas_list.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_buylists.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchant_shopids.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/merchants.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/minions.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/npc.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/npcskills.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_learn.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_trees.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/skill_spellbooks.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/spawnlist.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/teleport.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/weapon.sql
+%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/seven_signs.sql
 pause
