6d5cbfa527a15d8b810464a7e656d79afa6ac14c
mr <mr@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-09-09 15:59:49 +0000

    quests: c3 tutorial added dark elf support

--- datapack_development/data/jscript/quests/999_C3Tutorial/__init__.py
+++ datapack_development/data/jscript/quests/999_C3Tutorial/__init__.py
@@ -5,6 +5,7 @@ from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
+BLOOD_OF_JUNDIN_ID = 1070
 LICENSE_OF_MINER_ID = 1498
 VOUCHER_OF_FLAME_ID = 1496
 SOULSHOOT_NOVICE_ID = 5789
@@ -16,6 +17,17 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
+    if event == "7129_02" :
+#      st.showRadar(2,28384,11056,-4233)
+      htmltext = "7129-03.htm"
+      if st.getQuestItemsCount(BLOOD_OF_JUNDIN_ID) and int(st.get("onlyone")) == 0 :
+        st.addExpAndSp(0,50)
+        st.takeItems(BLOOD_OF_JUNDIN_ID,1)
+        st.giveItems(SOULSHOOT_NOVICE_ID,200)
+        st.set("cond","0")
+        st.set("onlyone","1")
+        st.setState(COMPLETED)
+        st.playSound("ItemSound.quest_finish")
     if event == "7528_02" :
 #      st.showRadar(2,108567,-173994,-406)
       htmltext = "7573-03.htm"
@@ -50,6 +62,18 @@ class Quest (JQuest) :
      st.set("onlyone","0")
      st.set("id","0")
    if int(st.get("onlyone")) == 0 and int(st.get("cond")) == 0 and st.getPlayer().getLevel() < 10 :
+#darkelf
+     if st.getPlayer().getRace().ordinal() == 2 :
+     	if npcId == 7129 :
+          htmltext = "7129-01.htm"
+	if npcId == 7131 :
+	  if st.getPlayer().getClassId().getId() == 0x26 :
+            htmltext = "7131-01.htm"
+	  else:
+            htmltext = "7530-01.htm"
+          st.set("cond","1")
+          st.setState(STARTED)
+          st.playSound("ItemSound.quest_tutorial")
 #orc
      if st.getPlayer().getRace().ordinal() == 3 :
      	if npcId == 7573 :
@@ -91,6 +115,19 @@ class Quest (JQuest) :
           st.set("cond","2")
       else :
           htmltext = "7575-02.htm"
+#darkelf
+   elif npcId == 7131 and int(st.get("cond"))==1 and st.getQuestItemsCount(BLOOD_OF_JUNDIN_ID)==0 :
+      if st.getQuestItemsCount(BLUE_GEM_ID) :
+          st.takeItems(BLUE_GEM_ID,st.getQuestItemsCount(BLUE_GEM_ID))
+          st.giveItems(BLOOD_OF_JUNDIN_ID,1)
+          st.giveItems(SOULSHOOT_NOVICE_ID,200)
+          htmltext = "7131-03.htm"
+          st.set("cond","2")
+      else :
+	  if st.getPlayer().getClassId().getId() == 0x26 :
+            htmltext = "7131-02.htm"
+	  else:
+            htmltext = "7530-02.htm"
 
 #dwarf
    elif npcId == 7530 and int(st.get("cond"))==2 and st.getQuestItemsCount(LICENSE_OF_MINER_ID) :
@@ -110,6 +147,15 @@ class Quest (JQuest) :
       htmltext = "7573-02.htm"
    elif npcId == 7573 and  int(st.get("cond"))==3 :
       htmltext = "7573-04.htm"
+#darkelf
+   elif npcId == 7131 and int(st.get("cond"))==2 and st.getQuestItemsCount(BLOOD_OF_JUNDIN_ID) :
+      htmltext = "7131-04.htm"
+   elif npcId == 7131 and  int(st.get("cond"))==1 :
+      htmltext = "7131-01.htm"
+   elif npcId == 7129 and  int(st.get("cond"))==2 and st.getQuestItemsCount(BLOOD_OF_JUNDIN_ID) :
+      htmltext = "7129-02.htm"
+   elif npcId == 7129 and  int(st.get("cond"))==3 :
+      htmltext = "7129-04.htm"
    return htmltext
 
  def onKill (self,npcId,st):
@@ -129,11 +175,15 @@ COMPLETED   = State('Completed', QUEST)
 
 
 QUEST.setInitialState(CREATED)
+QUEST.addStartNpc(7129)
+QUEST.addStartNpc(7131)
 QUEST.addStartNpc(7528)
 QUEST.addStartNpc(7530)
 QUEST.addStartNpc(7573)
 QUEST.addStartNpc(7575)
 
+STARTED.addTalkId(7129)
+STARTED.addTalkId(7131)
 STARTED.addTalkId(7528)
 STARTED.addTalkId(7530)
 STARTED.addTalkId(7573)
@@ -142,5 +192,6 @@ STARTED.addTalkId(7575)
 STARTED.addKillId(5198)
 
 STARTED.addQuestDrop(5198,BLUE_CRYSTAL,1)
+STARTED.addQuestDrop(7131,BLOOD_OF_JUNDIN_ID,1)
 STARTED.addQuestDrop(7530,LICENSE_OF_MINER_ID,1)
 STARTED.addQuestDrop(7575,VOUCHER_OF_FLAME_ID,1)
