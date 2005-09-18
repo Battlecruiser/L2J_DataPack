225c458466646d6e38e6b44a2978292027ca1e75
balancer <balancer@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-09-18 10:41:12 +0000

    Added new field, needed to use with calculationg of breackage items and little optimization.

--- datapack_development/sql/items.sql
+++ datapack_development/sql/items.sql
@@ -2,16 +2,19 @@
 -- Table structure for items
 -- ----------------------------
 CREATE TABLE items (
-  owner_id decimal(10) , -- object id of the player or clan,owner of this item
-  object_id decimal(11) ,-- object id of the item
-  item_id decimal(6) ,   -- item id
-  count decimal(10) ,
-  enchant_level decimal(2) ,
-  loc varchar(10) ,      -- inventory,paperdoll,npc,clan warehouse,pet,and so on
-  loc_data decimal(10) , -- depending on location: equiped slot,npc id,pet id,etc
-  price_sell decimal(10) ,
-  price_buy decimal(10) ,
-  PRIMARY KEY  (object_id),
-  KEY `owner_id` (`owner_id`),
-  KEY `loc` (`loc`)
-) ;
+  `owner_id` INT, -- object id of the player or clan,owner of this item
+  `object_id` INT,-- object id of the item
+  `item_id` INT,   -- item id
+  `count` INT,
+  `enchant_level` INT,
+  `loc` VARCHAR(10),      -- inventory,paperdoll,npc,clan warehouse,pet,and so on
+  `loc_data` INT, -- depending on location: equiped slot,npc id,pet id,etc
+  `price_sell` INT,
+  `price_buy` INT,
+  `time_of_use` INT, -- time of item use, for calculate of breackages
+  PRIMARY KEY (`object_id`),
+  KEY `key_owner_id` (`owner_id`),
+  KEY `key_loc` (`loc`),
+  KEY `key_item_id` (`item_id`),
+  KEY `key_time_of_use` (`time_of_use`)
+);
