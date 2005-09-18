8d37adf331b7a72cf8cb65d66326e5b6037e89bf
L2Unleashed <L2Unleashed@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-09-18 03:01:05 +0000

    Fixed error in character_skills.sql table design

--- datapack_development/sql/character_skills.sql
+++ datapack_development/sql/character_skills.sql
@@ -3,7 +3,7 @@
 -- ---------------------------
 CREATE TABLE character_skills (
   char_obj_id decimal(11) ,
-  skill_id decimal(3) ,
+  skill_id decimal(4) ,
   skill_level varchar(5) ,
   skill_name varchar(24),
   PRIMARY KEY  (char_obj_id,skill_id)
