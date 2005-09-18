2c9c24de8e1bacce072efb508c8b0e2ae91ab977
balancer <balancer@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-09-18 19:24:43 +0000

    Fully worked interactive version. Simple type ".password" in game.

--- datapack_development/data/jbforth/users/password.f
+++ datapack_development/data/jbforth/users/password.f
@@ -2,43 +2,40 @@
 
 \ called as ".password new-password new-password"
 
-: (password-change)
+: bypass_change_user_password
 	over <> if
 		drop
-		"Passwords not match" .
+		"Passwords not match" show
 		exit
    	then
 
-	?dup "" = if
-		"Password can not be empty!"
+	dup is-null if
+		drop
+		"Password can not be empty!" show
 		exit
 	then	
   	
 	>password_hash >slashes
+
 	"update `accounts` set `password`='" swap s+
 	"' where `login` LIKE '" s+
 	player@ "AccountName" p@ >slashes s+ 
 	"';" s+
+
 	update ?dup if
-		"Error while update: " . .
+		"Error while update: " swap s+ show
 		exit
 	then
 
 	drop
-	"Password changed successfully" .
-;
-
-: bypass_change_user_password
-	next-word
-	next-word
-	(password-change)
-	tail drop \ drop tail of command. Antihack.
+	"Password changed successfully" show
 ;
 
 : user_password
-	"<font color="LEVEL">Enter you new password and confirm it:</font>"
-	"<table width=250><tr><td><edit var="password" width=80 height=15></td>" s+
-	"<td><edit var="password2" width=80 height=15></td>" s+
-	"<td><button value="Check" action="bypass -h jbf_change_user_password $password $password2" width=40 height=15 back="sek.cbui94" fore="sek.cbui92"></td></tr></table>" s+
+	'<font color="LEVEL">Enter you new password and confirm it:</font>'
+	'<table width=250><tr><td><edit var="password1" width=80 height=15></td>' s+
+	'<td><edit var="password2" width=80 height=15></td>' s+
+	'<td><button value="Change" action="bypass -h jbf_change_user_password $password1 $password2" width=50 height=15 back="sek.cbui94" fore="sek.cbui92"></td></tr></table>' s+
 	show
+	tail drop \ drop tail of command. Antihack.
 ;
