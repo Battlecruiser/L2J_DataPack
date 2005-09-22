f2cd6414c34f5922d70217a6f81dc1f69c4eb2d4
eloquence <eloquence@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-09-22 05:35:01 +0000

    Fix Ticket #117

--- datapack_development/sql/accounts.sql
+++ datapack_development/sql/accounts.sql
@@ -4,7 +4,7 @@
 CREATE TABLE `accounts` (
   `login` VARCHAR(45) NOT NULL default '',
   `password` VARCHAR(45) ,
-  `lastactive` INT,
+  `lastactive` DECIMAL(20),
   `access_level` INT,
   `lastIP` VARCHAR(20),
   PRIMARY KEY (`login`)
