70666527ad7438439639325f3ed7cd8da50278d9
mr <mr@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-10-30 18:55:41 +0000

    added new table for store character effected skills+ some fix

---
+++ datapack_development/sql/character_skills_save.sql
@@ -0,0 +1,11 @@
+-- ---------------------------
+-- Table structure for character_skills
+-- ---------------------------
+CREATE TABLE character_skills_save (
+  char_obj_id INT NOT NULL default 0,
+  skill_id INT NOT NULL default 0,
+  skill_level INT NOT NULL default 0,
+  effect_count INT NOT NULL default 0,
+  effect_cur_time INT NOT NULL default 0,
+  PRIMARY KEY  (char_obj_id,skill_id)
+) ;
