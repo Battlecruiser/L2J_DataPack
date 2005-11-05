bddc9df08de916cbba82bc93472c9b8577db84e3
warrax <warrax@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-11-05 12:21:44 +0000

    #303

--- datapack_development/sql/updates/051104-[1447].sql
+++ datapack_development/sql/updates/051104-[1447].sql
@@ -1,2 +1,2 @@
 -- needed only if your charater tables doesn't contains 'wantspeace' already
-alter table characters add column wantspeace decimal(1,0) DEFAULT 0;
+alter table `characters` add column `wantspeace` decimal(1,0) DEFAULT 0;
