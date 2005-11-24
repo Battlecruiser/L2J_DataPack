a7c176330c5b3044fc8cbf2338ba00575abeaf62
DrLecter <DrLecter@users.noreply.github.com>
2005-11-24 23:09:13 +0000

    Quest 102: little cleanup, restricted rewards checker to elven classes only

--- datapack_development/data/jscript/quests/102_FungusFever/__init__.py
+++ datapack_development/data/jscript/quests/102_FungusFever/__init__.py
@@ -1,5 +1,4 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 102: Fungus Fever"
+# Made by Mr. Have fun! Version 0.2
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -24,7 +23,6 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-        st.set("id","0")
         htmltext = "7284-02.htm"
         st.giveItems(ALBERRYUS_LETTER_ID,1)
         st.set("cond","1")
@@ -39,23 +37,18 @@ class Quest (JQuest) :
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
      st.set("onlyone","0")
-     st.set("id","0")
    if npcId == 7284 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getPlayer().getRace().ordinal() != 1 :
-            htmltext = "7284-00.htm"
-          elif st.getPlayer().getLevel() >= 12 :
-            htmltext = "7284-07.htm"
-            return htmltext
-          else:
-            htmltext = "7284-08.htm"
-            st.exitQuest(1)
-        else:
-          htmltext = "7284-08.htm"
-          st.exitQuest(1)
+      if st.getPlayer().getRace().ordinal() != 1 :
+         htmltext = "7284-00.htm"
+         st.exitQuest(1)
+      elif st.getPlayer().getLevel() >= 12 :
+         htmltext = "7284-07.htm"
+         return htmltext
+      else:
+         htmltext = "7284-08.htm"
+         st.exitQuest(1)
    elif npcId == 7284 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
         htmltext = "<html><head><body>This quest have already been completed.</body></html>"
    elif npcId == 7284 and int(st.get("cond"))==1 and st.getQuestItemsCount(ALBERRYUS_LETTER_ID)==1 :
@@ -64,11 +57,9 @@ class Quest (JQuest) :
         htmltext = "7284-09.htm"
    elif npcId == 7156 and int(st.get("cond"))==1 and st.getQuestItemsCount(ALBERRYUS_LETTER_ID)==1 :
         if st.getQuestItemsCount(ALBERRYUS_LETTER_ID)>0 :
-          if int(st.get("id")) != 102 :
-            st.set("id","102")
-            st.giveItems(EVERGREEN_AMULET_ID,1)
-            st.takeItems(ALBERRYUS_LETTER_ID,1)
-            htmltext = "7156-03.htm"
+           st.giveItems(EVERGREEN_AMULET_ID,1)
+           st.takeItems(ALBERRYUS_LETTER_ID,1)
+           htmltext = "7156-03.htm"
    elif npcId == 7156 and int(st.get("cond"))==1 and st.getQuestItemsCount(EVERGREEN_AMULET_ID)>0 and st.getQuestItemsCount(DRYAD_TEARS_ID)<10 :
         htmltext = "7156-04.htm"
    elif npcId == 7156 and int(st.get("cond"))==1 and st.getQuestItemsCount(ALBERRYUS_LIST_ID)>0 :
@@ -113,28 +104,17 @@ class Quest (JQuest) :
         st.playSound("ItemSound.quest_finish")
         htmltext = "7284-06.htm"
         st.set("onlyone","1")
-        if st.getPlayer().getClassId().getId() == 0x00 or st.getPlayer().getClassId().getId() == 0x12 or st.getPlayer().getClassId().getId() == 0x1f or st.getPlayer().getClassId().getId() == 0x07 or st.getPlayer().getClassId().getId() == 0x01 or st.getPlayer().getClassId().getId() == 0x13 or st.getPlayer().getClassId().getId() == 0x16 or st.getPlayer().getClassId().getId() == 0x23 or st.getPlayer().getClassId().getId() == 0x04 or st.getPlayer().getClassId().getId() == 0x20 or st.getPlayer().getClassId().getId() == 0x2c or st.getPlayer().getClassId().getId() == 0x2f or st.getPlayer().getClassId().getId() == 0x35 or st.getPlayer().getClassId().getId() == 0x36 or st.getPlayer().getClassId().getId() == 0x38 or st.getPlayer().getClassId().getId() == 0x2d :
+        if st.getPlayer().getClassId().getId() in range(18,25) :
           st.giveItems(SWORD_OF_SENTINEL_ID,1)
         else:
           st.giveItems(STAFF_OF_SENTINEL_ID,1)
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 13 :
-        st.set("id","0")
-        if st.getQuestItemsCount(EVERGREEN_AMULET_ID)>0 and st.getQuestItemsCount(DRYAD_TEARS_ID)<10 :
-          if st.getRandom(10)<3 :
-            st.giveItems(DRYAD_TEARS_ID,1)
-            if st.getQuestItemsCount(DRYAD_TEARS_ID) == 10 :
-              st.playSound("ItemSound.quest_middle")
-            else:
-              st.playSound("ItemSound.quest_itemget")
-   elif npcId == 19 :
-        st.set("id","0")
-        if st.getQuestItemsCount(EVERGREEN_AMULET_ID)>0 and st.getQuestItemsCount(DRYAD_TEARS_ID)<10 :
-          if st.getRandom(10)<3 :
+   if npcId in [13,19] :
+      if st.getQuestItemsCount(EVERGREEN_AMULET_ID)>0 and st.getQuestItemsCount(DRYAD_TEARS_ID)<10 :
+         if st.getRandom(10)<3 :
             st.giveItems(DRYAD_TEARS_ID,1)
             if st.getQuestItemsCount(DRYAD_TEARS_ID) == 10 :
               st.playSound("ItemSound.quest_middle")
@@ -144,15 +124,11 @@ class Quest (JQuest) :
 
 QUEST       = Quest(102,"102_FungusFever","Fungus Fever")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
-
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7284)
-
-STARTING.addTalkId(7284)
 
 STARTED.addTalkId(7156)
 STARTED.addTalkId(7217)
@@ -174,3 +150,5 @@ STARTED.addQuestDrop(7156,COBS_MEDICINE3_ID,1)
 STARTED.addQuestDrop(7156,COBS_MEDICINE4_ID,1)
 STARTED.addQuestDrop(7156,COBS_MEDICINE5_ID,1)
 STARTED.addQuestDrop(7284,ALBERRYUS_LIST_ID,1)
+
+print "importing quests: 102: Fungus Fever"
