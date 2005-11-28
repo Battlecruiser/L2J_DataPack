54d3c2fed36af98d493934d1aef658d0014d2d79
TIBlue <TIBlue@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-11-28 19:17:05 +0000

    - Removed castle_guard (replaced with Merc System)

--- datapack_development/tools/full_install.sql
+++ datapack_development/tools/full_install.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS boxes;
 DROP TABLE IF EXISTS castle;
 DROP TABLE IF EXISTS castle_door;
 DROP TABLE IF EXISTS castle_doorupgrade;
-DROP TABLE IF EXISTS castle_guards;
-DROP TABLE IF EXISTS castle_guards_skills;
 DROP TABLE IF EXISTS castle_siege_guards;
 DROP TABLE IF EXISTS char_templates;
 DROP TABLE IF EXISTS character_friends;
