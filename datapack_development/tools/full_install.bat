60baae98da0687ca0179b9b5b2cd9fc2946f1106
TIBlue <TIBlue@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-11-28 19:17:05 +0000

    - Removed castle_guard (replaced with Merc System)

--- datapack_development/tools/full_install.bat
+++ datapack_development/tools/full_install.bat
@@ -30,8 +30,6 @@ echo Installling new content.
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_door.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_doorupgrade.sql
-%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards.sql
-%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_guards_skills.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/castle_siege_guards.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/char_templates.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/character_friends.sql
