7d0534606734b04e749c603f0148215a32b39fb0
DrLecter <DrLecter@users.noreply.github.com>
2005-12-01 21:25:30 +0000

    mising file for rev 866

--- datapack_development/data/jscript/quests/101_SwordOfSolidarityQuest/__init__.py
+++ datapack_development/data/jscript/quests/101_SwordOfSolidarityQuest/__init__.py
@@ -1,5 +1,5 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 101: Sword Of Solidarity Quest"
+# Made by Mr. Have fun! Version 0.2
+
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -20,7 +20,6 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-        st.set("id","0")
         htmltext = "7008-04.htm"
         st.set("cond","1")
         st.setState(STARTED)
@@ -39,25 +38,19 @@ class Quest (JQuest) :
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
      st.set("onlyone","0")
-     st.set("id","0")
    if npcId == 7008 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getPlayer().getRace().ordinal() != 0 :
-            htmltext = "7008-00.htm"
-          elif st.getPlayer().getLevel() >= 9 :
-            htmltext = "7008-02.htm"
-            return htmltext
-          else:
-            htmltext = "7008-08.htm"
-            st.exitQuest(1)
-        else:
-          htmltext = "7008-08.htm"
-          st.exitQuest(1)
+      if st.getPlayer().getRace().ordinal() != 0 :
+        htmltext = "7008-00.htm"
+      elif st.getPlayer().getLevel() >= 9 :
+        htmltext = "7008-02.htm"
+        return htmltext
+      else:
+        htmltext = "7008-08.htm"
+        st.exitQuest(1)
    elif npcId == 7008 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-      htmltext = "<html><head><body>This quest have already been completed.</body></html>"
+        htmltext = "<html><head><body>This quest have already been completed.</body></html>"
    elif npcId == 7008 and int(st.get("cond"))==1 and (st.getQuestItemsCount(ROIENS_LETTER_ID)==1) and int(st.get("onlyone"))==0 :
         htmltext = "7008-05.htm"
    elif npcId == 7008 and int(st.get("cond"))==1 and int(st.get("onlyone"))==0 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID)==0 :
@@ -73,11 +66,9 @@ class Quest (JQuest) :
           htmltext = "7008-10.htm"
    elif npcId == 7008 and int(st.get("cond"))==1 and int(st.get("onlyone"))==0 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
         if st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
-          if int(st.get("id")) != 101 :
-            st.set("id","101")
-            htmltext = "7008-06.htm"
-            st.takeItems(ALLTRANS_NOTE_ID,st.getQuestItemsCount(ALLTRANS_NOTE_ID))
-            st.giveItems(BROKEN_SWORD_HANDLE_ID,1)
+           htmltext = "7008-06.htm"
+           st.takeItems(ALLTRANS_NOTE_ID,st.getQuestItemsCount(ALLTRANS_NOTE_ID))
+           st.giveItems(BROKEN_SWORD_HANDLE_ID,1)
    elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(ROIENS_LETTER_ID)>0 :
         htmltext = "7283-01.htm"
    elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(HOWTOGO_RUINS_ID)>0 :
@@ -106,49 +97,21 @@ class Quest (JQuest) :
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 362 :
-        st.set("id","0")
-        if st.getQuestItemsCount(HOWTOGO_RUINS_ID)>0 :
-          if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 :
+   if npcId in [361,362] :
+      if st.getQuestItemsCount(HOWTOGO_RUINS_ID) :
+         if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 :
             if st.getRandom(5) == 0 :
-              st.giveItems(BROKEN_BLADE_TOP_ID,1)
-              if st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 1 :
-                st.playSound("ItemSound.quest_middle")
-              else:
-                st.playSound("ItemSound.quest_itemget")
-          else:
-            if st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
-              if st.getRandom(5) == 0 :
-                st.giveItems(BROKEN_BLADE_BOTTOM_ID,1)
-                if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 1 :
-                  st.playSound("ItemSound.quest_middle")
-                else:
-                  st.playSound("ItemSound.quest_itemget")
-   elif npcId == 361 :
-        st.set("id","0")
-        if st.getQuestItemsCount(HOWTOGO_RUINS_ID)>0 :
-          if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 :
+               st.giveItems(BROKEN_BLADE_TOP_ID,1)
+               st.playSound("ItemSound.quest_middle")
+         elif st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
             if st.getRandom(5) == 0 :
-              st.giveItems(BROKEN_BLADE_TOP_ID,1)
-              if st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 1 :
-                st.playSound("ItemSound.quest_middle")
-              else:
-                st.playSound("ItemSound.quest_itemget")
-          else:
-            if st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
-              if st.getRandom(5) == 0 :
-                st.giveItems(BROKEN_BLADE_BOTTOM_ID,1)
-                if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 1 :
-                  st.playSound("ItemSound.quest_middle")
-                else:
-                  st.playSound("ItemSound.quest_itemget")
+               st.giveItems(BROKEN_BLADE_BOTTOM_ID,1)
+               st.playSound("ItemSound.quest_middle")
    return
 
 QUEST       = Quest(101,"101_SwordOfSolidarityQuest","Sword Of Solidarity Quest")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
@@ -156,7 +119,7 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7008)
 
-STARTING.addTalkId(7008)
+CREATED.addTalkId(7008)
 
 STARTED.addTalkId(7008)
 STARTED.addTalkId(7283)
@@ -172,3 +135,5 @@ STARTED.addQuestDrop(362,BROKEN_BLADE_BOTTOM_ID,1)
 STARTED.addQuestDrop(361,BROKEN_BLADE_BOTTOM_ID,1)
 STARTED.addQuestDrop(7008,ROIENS_LETTER_ID,1)
 STARTED.addQuestDrop(7008,BROKEN_SWORD_HANDLE_ID,1)
+
+print "importing quests: 101: Sword Of Solidarity Quest"
