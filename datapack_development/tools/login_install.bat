dd2cfbd48e8334b4d8360a9e6a47e6dff3bdf650
Rolarga <Rolarga@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-01 01:11:54 +0000

    changed login and full install bat.. so full install now added also gamservers.. and login .. only add gameservers... actually for agelian :)

--- datapack_development/tools/login_install.bat
+++ datapack_development/tools/login_install.bat
@@ -18,11 +18,10 @@ set mysqlPath="%mysqlBinPath%\mysql"
 
 echo.
 echo Making a backup of the original database.
-%mysqldumpPath% --add-drop-table -h %DBHost% -u %user% --password=%pass% %DBname% > l2jdb_backup.sql
+%mysqldumpPath% --add-drop-table -h %DBHost% -u %user% --password=%pass% %DBname% > login_backup.sql
 echo.
 echo Deleting tables for new content.
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < login_install.sql
 echo.
 echo Installling new content.
-%mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/accounts.sql
 %mysqlPath% -h %DBHost% -u %user% --password=%pass% -D %DBname% < ../sql/gameservers.sql
