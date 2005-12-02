3d420025a3e5b8cbdd616dc482826160b3ae26e7
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-02 17:00:58 +0000

    Quest 101: cleanup and support for all progress levels (players in the middle of the quest may need to restart it in order to complete)

--- datapack_development/data/jscript/quests/101_SwordOfSolidarityQuest/__init__.py
+++ datapack_development/data/jscript/quests/101_SwordOfSolidarityQuest/__init__.py
@@ -1,4 +1,4 @@
-# Made by Mr. Have fun! Version 0.2
+# Made by Mr. Have fun! - Version 0.3 by DrLecter
 
 import sys
 from net.sf.l2j.gameserver.model.quest import State
@@ -19,21 +19,26 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-        htmltext = "7008-04.htm"
+    if event == "7008-04.htm" :
         st.set("cond","1")
         st.setState(STARTED)
         st.playSound("ItemSound.quest_accept")
         st.giveItems(ROIENS_LETTER_ID,1)
-    elif event == "101_1" :
-          htmltext = "7283-02.htm"
-          st.takeItems(ROIENS_LETTER_ID,st.getQuestItemsCount(ROIENS_LETTER_ID))
-          st.giveItems(HOWTOGO_RUINS_ID,1)
+    elif event == "7283-02.htm" :
+        st.set("cond","2")
+        st.takeItems(ROIENS_LETTER_ID,st.getQuestItemsCount(ROIENS_LETTER_ID))
+        st.giveItems(HOWTOGO_RUINS_ID,1)
+    elif event == "7283-07.htm" :
+        st.takeItems(BROKEN_SWORD_HANDLE_ID,-1)
+        st.giveItems(SWORD_OF_SOLIDARITY_ID,1)
+        st.set("cond","0")
+        st.setState(COMPLETED)
+        st.playSound("ItemSound.quest_finish")
+        st.set("onlyone","1")        
     return htmltext
 
 
- def onTalk (Self,npc,st):
-
+ def onTalk (Self,npc,st) :
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
@@ -51,49 +56,40 @@ class Quest (JQuest) :
         st.exitQuest(1)
    elif npcId == 7008 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
         htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7008 and int(st.get("cond"))==1 and (st.getQuestItemsCount(ROIENS_LETTER_ID)==1) and int(st.get("onlyone"))==0 :
+   elif npcId == 7008 and int(st.get("cond"))==1 and (st.getQuestItemsCount(ROIENS_LETTER_ID)==1) :
         htmltext = "7008-05.htm"
-   elif npcId == 7008 and int(st.get("cond"))==1 and int(st.get("onlyone"))==0 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID)==0 :
+   elif npcId == 7008 and int(st.get("cond"))>=2 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID)==0 :
         if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) :
           htmltext = "7008-12.htm"
-        if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
+        if (st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) + st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID)) <= 1 :
           htmltext = "7008-11.htm"
-        if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) :
-          htmltext = "7008-11.htm"
-        if st.getQuestItemsCount(BROKEN_SWORD_HANDLE_ID)>0 :
+        if st.getQuestItemsCount(BROKEN_SWORD_HANDLE_ID) > 0 :
           htmltext = "7008-07.htm"
-        if st.getQuestItemsCount(HOWTOGO_RUINS_ID) == 1 and int(st.get("onlyone")) == 0 :
+        if st.getQuestItemsCount(HOWTOGO_RUINS_ID) == 1 :
           htmltext = "7008-10.htm"
-   elif npcId == 7008 and int(st.get("cond"))==1 and int(st.get("onlyone"))==0 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
-        if st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
-           htmltext = "7008-06.htm"
-           st.takeItems(ALLTRANS_NOTE_ID,st.getQuestItemsCount(ALLTRANS_NOTE_ID))
-           st.giveItems(BROKEN_SWORD_HANDLE_ID,1)
+   elif npcId == 7008 and int(st.get("cond"))==4 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
+        htmltext = "7008-06.htm"
+        st.set("cond","5")
+        st.takeItems(ALLTRANS_NOTE_ID,st.getQuestItemsCount(ALLTRANS_NOTE_ID))
+        st.giveItems(BROKEN_SWORD_HANDLE_ID,1)
    elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(ROIENS_LETTER_ID)>0 :
         htmltext = "7283-01.htm"
-   elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(HOWTOGO_RUINS_ID)>0 :
-        if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
+   elif npcId == 7283 and int(st.get("cond"))>=2 and st.getQuestItemsCount(ROIENS_LETTER_ID)==0 and st.getQuestItemsCount(HOWTOGO_RUINS_ID)>0 :
+        if (st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) + st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID)) == 1 :
           htmltext = "7283-08.htm"
-        if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) :
-          htmltext = "7283-08.htm"
-        if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) == 0 and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) == 0 :
+        if (st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) + st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID)) == 0 :
           htmltext = "7283-03.htm"
         if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) :
           htmltext = "7283-04.htm"
+          st.set("cond","4")
           st.takeItems(HOWTOGO_RUINS_ID,st.getQuestItemsCount(HOWTOGO_RUINS_ID))
           st.takeItems(BROKEN_BLADE_TOP_ID,st.getQuestItemsCount(BROKEN_BLADE_TOP_ID))
           st.takeItems(BROKEN_BLADE_BOTTOM_ID,st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID))
           st.giveItems(ALLTRANS_NOTE_ID,1)
-   elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(ALLTRANS_NOTE_ID) and st.getQuestItemsCount(HOWTOGO_RUINS_ID)==0 :
+   elif npcId == 7283 and int(st.get("cond"))==4 and st.getQuestItemsCount(ALLTRANS_NOTE_ID) :
         htmltext = "7283-05.htm"
-   elif npcId == 7283 and int(st.get("cond"))==1 and st.getQuestItemsCount(BROKEN_SWORD_HANDLE_ID)>0 and int(st.get("onlyone"))==0 :
-        st.takeItems(BROKEN_SWORD_HANDLE_ID,st.getQuestItemsCount(BROKEN_SWORD_HANDLE_ID))
-        st.giveItems(SWORD_OF_SOLIDARITY_ID,1)
-        st.set("cond","0")
-        st.setState(COMPLETED)
-        st.playSound("ItemSound.quest_finish")
+   elif npcId == 7283 and int(st.get("cond"))==5 and st.getQuestItemsCount(BROKEN_SWORD_HANDLE_ID) :
         htmltext = "7283-06.htm"
-        st.set("onlyone","1")
    return htmltext
 
  def onKill (self,npc,st):
@@ -108,6 +104,8 @@ class Quest (JQuest) :
             if st.getRandom(5) == 0 :
                st.giveItems(BROKEN_BLADE_BOTTOM_ID,1)
                st.playSound("ItemSound.quest_middle")
+      if st.getQuestItemsCount(BROKEN_BLADE_TOP_ID) and st.getQuestItemsCount(BROKEN_BLADE_BOTTOM_ID) :
+         st.set("cond","3")
    return
 
 QUEST       = Quest(101,"101_SwordOfSolidarityQuest","Sword Of Solidarity Quest")
