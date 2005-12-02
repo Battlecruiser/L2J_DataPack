51d1ab47f0e33c0555e2ffdab00a8782f4d8fa89
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-02 18:20:36 +0000

    cleanup + progress + repeatable + fix #364

--- datapack_development/data/jscript/quests/303_CollectArrowheads/__init__.py
+++ datapack_development/data/jscript/quests/303_CollectArrowheads/__init__.py
@@ -1,5 +1,5 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 303: Collect Arrowheads"
+# Made by Mr. Have fun! - Version 0.3 by DrLecter
+
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -15,7 +15,6 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-        st.set("id","0")
         st.set("cond","1")
         st.setState(STARTED)
         st.playSound("ItemSound.quest_accept")
@@ -24,67 +23,53 @@ class Quest (JQuest) :
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
    if npcId == 7029 and int(st.get("cond"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getPlayer().getLevel() >= 10 :
-            htmltext = "7029-03.htm"
-            return htmltext
-          else:
-            htmltext = "7029-02.htm"
-            st.exitQuest(1)
-        else:
-          htmltext = "7029-02.htm"
-          st.exitQuest(1)
-   elif npcId == 7029 and int(st.get("cond"))!=0 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)<10 :
+     if st.getPlayer().getLevel() >= 10 :
+        htmltext = "7029-03.htm"
+        return htmltext
+     else:
+        htmltext = "7029-02.htm"
+        st.exitQuest(1)
+   elif npcId == 7029 and int(st.get("cond"))==1 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)<10 :
         htmltext = "7029-05.htm"
-   elif npcId == 7029 and int(st.get("cond"))!=0 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)>=10 :
-        if int(st.get("id")) != 303 :
-          st.set("id","303")
-          st.takeItems(ORCISH_ARROWHEAD_ID,st.getQuestItemsCount(ORCISH_ARROWHEAD_ID))
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          st.giveItems(ADENA_ID,1000)
-          st.addExpAndSp(1200,0)
-          htmltext = "7029-06.htm"
+   elif npcId == 7029 and int(st.get("cond"))==2 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)>=10 :
+        st.takeItems(ORCISH_ARROWHEAD_ID,-1)
+        st.playSound("ItemSound.quest_finish")
+        st.giveItems(ADENA_ID,1000)
+        st.addExpAndSp(1200,0)
+        htmltext = "7029-06.htm"
+        st.exitQuest(1)
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
    if npcId == 361 :
-        st.set("id","0")
-        if int(st.get("cond")) != 0 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)<10 and st.getRandom(100)<40 :
-          st.giveItems(ORCISH_ARROWHEAD_ID,1)
-          if st.getQuestItemsCount(ORCISH_ARROWHEAD_ID) == 10 :
+      if int(st.get("cond")) == 1 and st.getQuestItemsCount(ORCISH_ARROWHEAD_ID)<10 and st.getRandom(100)<40 :
+         st.giveItems(ORCISH_ARROWHEAD_ID,1)
+         if st.getQuestItemsCount(ORCISH_ARROWHEAD_ID) == 10 :
+            st.set("cond","2") 
             st.playSound("ItemSound.quest_middle")
-          else:
+         else:
             st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(303,"303_CollectArrowheads","Collect Arrowheads")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7029)
 
-STARTING.addTalkId(7029)
-
+CREATED.addTalkId(7029)
 STARTED.addTalkId(7029)
 
 STARTED.addKillId(361)
-
 STARTED.addQuestDrop(361,ORCISH_ARROWHEAD_ID,1)
+
+print "importing quests: 303: Collect Arrowheads"
