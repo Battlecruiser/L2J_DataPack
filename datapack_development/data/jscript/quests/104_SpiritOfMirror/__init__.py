a23b42d65a6ca3d999ce27ea41216bbbfc374e2d
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-02 17:39:34 +0000

    Quest 104: cleanup and support for all progress levels (players in the middle of the quest may need to restart it in order to complete)

--- datapack_development/data/jscript/quests/104_SpiritOfMirror/__init__.py
+++ datapack_development/data/jscript/quests/104_SpiritOfMirror/__init__.py
@@ -1,5 +1,5 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 104: Spirit Of Mirror"
+# Made by Mr. Have fun! - Version 0.3 by DrLecter
+
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -17,94 +17,81 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7017-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
       st.giveItems(GALLINS_OAK_WAND_ID,1)
       st.giveItems(GALLINS_OAK_WAND_ID,1)
       st.giveItems(GALLINS_OAK_WAND_ID,1)
-      htmltext = "7017-03.htm"
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
      st.set("onlyone","0")
-     st.set("id","0")
    if npcId == 7017 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-      if int(st.get("cond"))<15 :
-        if st.getPlayer().getRace().ordinal() != 0 :
-          htmltext = "7017-00.htm"
-        elif st.getPlayer().getLevel() >= 10 :
-          htmltext = "7017-02.htm"
-          return htmltext
-        else:
-          htmltext = "7017-06.htm"
-          st.exitQuest(1)
-      else:
+     if st.getPlayer().getRace().ordinal() != 0 :
+        htmltext = "7017-00.htm"
+     elif st.getPlayer().getLevel() >= 10 :
+        htmltext = "7017-02.htm"
+        return htmltext
+     else:
         htmltext = "7017-06.htm"
         st.exitQuest(1)
    elif npcId == 7017 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
       htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7017 and int(st.get("cond")) and st.getQuestItemsCount(GALLINS_OAK_WAND_ID)>=1 and (st.getQuestItemsCount(WAND_SPIRITBOUND1_ID)==1 and st.getQuestItemsCount(WAND_SPIRITBOUND2_ID)==1 and st.getQuestItemsCount(WAND_SPIRITBOUND3_ID)==1)==0 :
+   elif npcId == 7017 and int(st.get("cond")) and st.getQuestItemsCount(GALLINS_OAK_WAND_ID)>=1 and (st.getQuestItemsCount(WAND_SPIRITBOUND1_ID) and st.getQuestItemsCount(WAND_SPIRITBOUND2_ID) and st.getQuestItemsCount(WAND_SPIRITBOUND3_ID))==0 :
       htmltext = "7017-04.htm"
-   elif npcId == 7017 and int(st.get("cond")) and st.getQuestItemsCount(WAND_SPIRITBOUND1_ID)==1 and st.getQuestItemsCount(WAND_SPIRITBOUND2_ID)==1 and st.getQuestItemsCount(WAND_SPIRITBOUND3_ID)==1 and int(st.get("onlyone"))==0 :
-      if int(st.get("id")) != 104 :
-        st.set("id","104")
-        st.takeItems(WAND_SPIRITBOUND1_ID,1)
-        st.takeItems(WAND_SPIRITBOUND2_ID,1)
-        st.takeItems(WAND_SPIRITBOUND3_ID,1)
-        st.giveItems(WAND_OF_ADEPT_ID,1)
-        htmltext = "7017-05.htm"
-        st.set("cond","0")
-        st.setState(COMPLETED)
-        st.playSound("ItemSound.quest_finish")
-        st.set("onlyone","1")
+   elif npcId == 7017 and int(st.get("cond"))==3 and st.getQuestItemsCount(WAND_SPIRITBOUND1_ID) and st.getQuestItemsCount(WAND_SPIRITBOUND2_ID) and st.getQuestItemsCount(WAND_SPIRITBOUND3_ID) :
+      st.takeItems(WAND_SPIRITBOUND1_ID,1)
+      st.takeItems(WAND_SPIRITBOUND2_ID,1)
+      st.takeItems(WAND_SPIRITBOUND3_ID,1)
+      st.giveItems(WAND_OF_ADEPT_ID,1)
+      htmltext = "7017-05.htm"
+      st.set("cond","0")
+      st.setState(COMPLETED)
+      st.playSound("ItemSound.quest_finish")
+      st.set("onlyone","1")
    elif npcId == 7045 and int(st.get("cond")) :
       htmltext = "7045-01.htm"
+      st.set("cond","2")
    elif npcId == 7043 and int(st.get("cond")) :
       htmltext = "7043-01.htm"
+      st.set("cond","2")
    elif npcId == 7041 and int(st.get("cond")) :
       htmltext = "7041-01.htm"
+      st.set("cond","2")
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
    if npcId == 5003 :
-      st.set("id","0")
       if int(st.get("cond")) >= 1 and st.getQuestItemsCount(GALLINS_OAK_WAND_ID) > 0 and st.getQuestItemsCount(WAND_SPIRITBOUND1_ID) == 0 :
         st.takeItems(GALLINS_OAK_WAND_ID,1)
         st.giveItems(WAND_SPIRITBOUND1_ID,1)
-        st.playSound("ItemSound.quest_middle")
-        st.set("cond","2")
+        st.playSound("ItemSound.quest_itemget")
    elif npcId == 5004 :
-      st.set("id","0")
       if int(st.get("cond")) >= 1 and st.getQuestItemsCount(GALLINS_OAK_WAND_ID) > 0 and st.getQuestItemsCount(WAND_SPIRITBOUND2_ID) == 0 :
         st.takeItems(GALLINS_OAK_WAND_ID,1)
         st.giveItems(WAND_SPIRITBOUND2_ID,1)
-        st.playSound("ItemSound.quest_middle")
-        st.set("cond","2")
+        st.playSound("ItemSound.quest_itemget")
    elif npcId == 5005 :
-      st.set("id","0")
       if int(st.get("cond")) >= 1 and st.getQuestItemsCount(GALLINS_OAK_WAND_ID) > 0 and st.getQuestItemsCount(WAND_SPIRITBOUND3_ID) == 0 :
         st.takeItems(GALLINS_OAK_WAND_ID,1)
         st.giveItems(WAND_SPIRITBOUND3_ID,1)
-        st.playSound("ItemSound.quest_middle")
-        st.set("cond","2")
+        st.playSound("ItemSound.quest_itemget")
+   if st.getQuestItemsCount(GALLINS_OAK_WAND_ID) == 0 and (st.getQuestItemsCount(WAND_SPIRITBOUND3_ID) + st.getQuestItemsCount(WAND_SPIRITBOUND2_ID) + st.getQuestItemsCount(WAND_SPIRITBOUND3_ID)) ==3 :
+      st.set("cond","3")
+      st.playSound("ItemSound.quest_middle")
    return
 
 QUEST       = Quest(104,"104_SpiritOfMirror","Spirit Of Mirror")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
@@ -112,7 +99,7 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7017)
 
-STARTING.addTalkId(7017)
+CREATED.addTalkId(7017)
 
 STARTED.addTalkId(7017)
 STARTED.addTalkId(7041)
@@ -129,3 +116,5 @@ STARTED.addQuestDrop(5005,WAND_SPIRITBOUND3_ID,1)
 STARTED.addQuestDrop(7017,GALLINS_OAK_WAND_ID,1)
 STARTED.addQuestDrop(7017,GALLINS_OAK_WAND_ID,1)
 STARTED.addQuestDrop(7017,GALLINS_OAK_WAND_ID,1)
+
+print "importing quests: 104: Spirit Of Mirror"
