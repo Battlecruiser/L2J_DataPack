49f30a1e13f0ed776288de42c6d8a05306130e82
warrax <warrax@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-04 08:49:36 +0000

    added class_index as primary key

--- datapack_development/sql/character_skills.sql
+++ datapack_development/sql/character_skills.sql
@@ -7,5 +7,5 @@ CREATE TABLE character_skills (
   skill_level varchar(5) ,
   skill_name varchar(24),
   `class_index` INT(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY  (char_obj_id,skill_id)
+  PRIMARY KEY  (char_obj_id,skill_id,`class_index`)
 ) ;
--- datapack_development/sql/updates/051205-[1768].sql
+++ datapack_development/sql/updates/051205-[1768].sql
@@ -1,7 +1,7 @@
 -- Needed only if your character tables are needed to be preserved.
 ALTER TABLE `character_hennas` ADD `class_index` int(1) NOT NULL default '0', DROP PRIMARY KEY, ADD PRIMARY KEY (`char_obj_id`,`slot`,`class_index`);
 
-ALTER TABLE `character_quests` ADD `class_index` int(1) NOT NULL default '0';
+ALTER TABLE `character_quests` ADD `class_index` int(1) NOT NULL default '0', DROP PRIMARY KEY, ADD PRIMARY KEY (`char_id`,`name`,`var`,`class_index`);
 
 ALTER TABLE `character_shortcuts` CHANGE `unknown` `class_index` int(1) NOT NULL default '0', DROP PRIMARY KEY, ADD PRIMARY KEY (`char_obj_id`,`slot`,`page`,`class_index`);
 
