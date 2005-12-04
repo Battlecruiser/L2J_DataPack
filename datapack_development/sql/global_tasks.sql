f7362b9996b5d2af452cb9de23cddc97b1ff79fd
zeratul <zeratul@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-04 07:29:00 +0000

    Added update for global_tasks sql in update.bat and added IF NOT EXISTS to tables to stop error if table already exists

--- datapack_development/sql/global_tasks.sql
+++ datapack_development/sql/global_tasks.sql
@@ -2,7 +2,7 @@
 -- Table structure for table `global_tasks`
 --
 
-CREATE TABLE global_tasks (
+CREATE TABLE IF NOT EXISTS global_tasks (
   task_name char(50) NOT NULL default '',
   last_activation decimal(20,0) NOT NULL default 0,
   PRIMARY KEY  (task_name)
