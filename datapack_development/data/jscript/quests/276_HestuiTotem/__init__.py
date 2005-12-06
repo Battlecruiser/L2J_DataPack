0b985ed90990db819f3b519f7370f4884a2c08dd
DrLecter <DrLecter@users.noreply.github.com>
2005-12-06 23:54:31 +0000

    Quest 276: cleanup + progress + repeatable (tnx kmarty)

--- datapack_development/data/jscript/quests/276_HestuiTotem/__init__.py
+++ datapack_development/data/jscript/quests/276_HestuiTotem/__init__.py
@@ -1,5 +1,4 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 276: Hestui Totem"
+# Maked by Mr. Have fun! - Version 0.3 by kmarty
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -16,109 +15,72 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-      st.set("id","0")
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
       htmltext = "7571-03.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
    if npcId == 7571 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getRace().ordinal() != 3 :
-          htmltext = "7571-00.htm"
-          st.exitQuest(1)
-        elif st.getPlayer().getLevel() < 15 :
-          htmltext = "7571-01.htm"
-          st.exitQuest(1)
-        else:
-          htmltext = "7571-02.htm"
-          return htmltext
-      else:
-        htmltext = "7571-02.htm"
+      if st.getPlayer().getRace().ordinal() != 3 :
+        htmltext = "7571-00.htm"
         st.exitQuest(1)
+      elif st.getPlayer().getLevel() < 15 :
+        htmltext = "7571-01.htm"
+        st.exitQuest(1)
+      else:
+         htmltext = "7571-02.htm"
    elif npcId == 7571 and int(st.get("cond")) :
-      if st.getQuestItemsCount(KASHA_CRYSTAL_ID) < 1 :
+      if st.getQuestItemsCount(KASHA_CRYSTAL_ID) == 0 :
         htmltext = "7571-04.htm"
       else:
-        if int(st.get("id")) != 276 :
-          st.set("id","276")
-          htmltext = "7571-05.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          n = st.getQuestItemsCount(KASHA_CRYSTAL_ID)
-          st.takeItems(KASHA_CRYSTAL_ID,n)
-          st.takeItems(KASHA_PARASITE_ID,st.getQuestItemsCount(KASHA_PARASITE_ID))
-          st.giveItems(HESTUIS_TOTEM_ID,1)
+        htmltext = "7571-05.htm"
+        st.exitQuest(1)
+        st.playSound("ItemSound.quest_finish")
+        st.takeItems(KASHA_CRYSTAL_ID,-1)
+        st.takeItems(KASHA_PARASITE_ID,-1)
+        st.giveItems(HESTUIS_TOTEM_ID,1)
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
    if npcId == 479 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(KASHA_CRYSTAL_ID) < 1 :
-        n0 = st.getQuestItemsCount(KASHA_PARASITE_ID)
-        n = st.getRandom(100)
-        n2 = 1
-        if n >= 79 :
-          st.getPcSpawn().addSpawn(5044)
-          st.takeItems(KASHA_PARASITE_ID,n0)
-          n2 = 0
-        elif n >= 69 :
-          if n0 <= 20 :
-            st.getPcSpawn().addSpawn(5044)
-            st.takeItems(KASHA_PARASITE_ID,n0)
-            n2 = 0
-        elif n >= 59 :
-          if n0 <= 15 :
-            st.getPcSpawn().addSpawn(5044)
-            st.takeItems(KASHA_PARASITE_ID,n0)
-            n2 = 0
-        elif n >= 49 :
-          if n0 <= 10 :
-            st.getPcSpawn().addSpawn(5044)
-            st.takeItems(KASHA_PARASITE_ID,n0)
-            n2 = 0
-        elif n >= 39 :
-          if n0 <= 2 :
-            st.getPcSpawn().addSpawn(5044)
-            st.takeItems(KASHA_PARASITE_ID,n0)
-            n2 = 0
-        if n2 :
-          st.giveItems(KASHA_PARASITE_ID,1)
-          st.playSound("ItemSound.quest_itemget")
+      if int(st.get("cond"))==1 and st.getQuestItemsCount(KASHA_CRYSTAL_ID) == 0 :
+        count = st.getQuestItemsCount(KASHA_PARASITE_ID)
+        random = st.getRandom(100)
+        if (count >= 20 and random < 70) or \
+           (count >= 15 and random < 60) or \
+           (count >= 10 and random < 50) or \
+           (count >= 2  and random < 40) or \
+           (count >  0  and random < 30) :
+                st.getPcSpawn().addSpawn(5044)
+                st.takeItems(KASHA_PARASITE_ID,count)
+        else :
+                st.giveItems(KASHA_PARASITE_ID,1)
+                st.playSound("ItemSound.quest_itemget")
    elif npcId == 5044 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(KASHA_CRYSTAL_ID) < 1 :
+      if int(st.get("cond"))==1 and st.getQuestItemsCount(KASHA_CRYSTAL_ID) == 0 :
         st.giveItems(KASHA_CRYSTAL_ID,1)
         st.playSound("ItemSound.quest_middle")
+        st.set("cond","2")
    return
 
 QUEST       = Quest(276,"276_HestuiTotem","Hestui Totem")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
 
 QUEST.setInitialState(CREATED)
+
 QUEST.addStartNpc(7571)
-
-STARTING.addTalkId(7571)
-
+CREATED.addTalkId(7571)
 STARTED.addTalkId(7571)
 
 STARTED.addKillId(479)
@@ -126,3 +88,5 @@ STARTED.addKillId(5044)
 
 STARTED.addQuestDrop(5044,KASHA_CRYSTAL_ID,1)
 STARTED.addQuestDrop(479,KASHA_PARASITE_ID,1)
+
+print "importing quests: 276: Hestui Totem"
