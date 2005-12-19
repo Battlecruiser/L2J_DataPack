cba2184db85b32337bda06cf63b6b70af960ae7f
TempyIncursion <TempyIncursion@5288da4f-0efc-0310-bd40-8d490a2659fa>
2005-12-19 02:30:04 +0000

    Update to global_tasks.sql and some henna price additions with more to follow.

--- datapack_development/sql/global_tasks.sql
+++ datapack_development/sql/global_tasks.sql
@@ -2,8 +2,13 @@
 -- Table structure for table `global_tasks`
 --
 
-CREATE TABLE IF NOT EXISTS global_tasks (
-  task_name char(50) NOT NULL default '',
-  last_activation decimal(20,0) NOT NULL default 0,
-  PRIMARY KEY  (task_name)
+CREATE TABLE global_tasks (
+  id int(11) NOT NULL auto_increment,
+  task varchar(50) NOT NULL default '',
+  type varchar(50) NOT NULL default '',
+  last_activation int(20) NOT NULL default 0,
+  param1 varchar(100) NOT NULL default '',
+  param2 varchar(100) NOT NULL default '',
+  param3 varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`id`)
 );
