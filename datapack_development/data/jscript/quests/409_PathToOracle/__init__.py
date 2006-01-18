9039cfeeb663b46f1d47f9e78a6cb75bb6b7dd3d
DrLecter <DrLecter@users.noreply.github.com>
2006-01-18 06:44:20 +0000

    Quest 409: Added missing texts.

--- datapack_development/data/jscript/quests/409_PathToOracle/__init__.py
+++ datapack_development/data/jscript/quests/409_PathToOracle/__init__.py
@@ -1,5 +1,4 @@
-# Maked by Mr. Have fun! Version 0.2.1 cheked & fix by Ryo Saeba
-print "importing quests: 409: Path To Oracle"
+# Made by Mr. Have fun! Version 0.2.1 cheked & fix by Ryo Saeba
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -36,26 +35,27 @@ class Quest (JQuest) :
             htmltext = "7293-03.htm"
         elif st.getPlayer().getLevel() >= 19 and st.getPlayer().getClassId().getId() == 0x19 and st.getQuestItemsCount(LEAF_OF_ORACLE_ID) == 1 :
             htmltext = "7293-04.htm"
+    elif event == "7424-08.htm" :
+        if int(st.get("cond")) :
+           st.getPcSpawn().addSpawn(5032)
+           st.getPcSpawn().addSpawn(5033)
+           st.getPcSpawn().addSpawn(5034)
+           st.set("cond","2")
     elif event == "7424_1" :
-          if int(st.get("cond")) :
-            st.getPcSpawn().addSpawn(5032)
-            st.getPcSpawn().addSpawn(5033)
-            st.getPcSpawn().addSpawn(5034)
-            st.set("cond","2")
+        htmltext=""
     elif event == "7428_1" :
-          if int(st.get("cond")) :
-            htmltext = "7428-02.htm"
+        if int(st.get("cond")) :
+           htmltext = "7428-02.htm"
     elif event == "7428_2" :
-            if int(st.get("cond")) :
-              htmltext = "7428-03.htm"
+        if int(st.get("cond")) :
+           htmltext = "7428-03.htm"
     elif event == "7428_3" :
-            if int(st.get("cond")) :
-              st.getPcSpawn().addSpawn(5035)
+        if int(st.get("cond")) :
+           st.getPcSpawn().addSpawn(5035)
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
@@ -65,14 +65,11 @@ class Quest (JQuest) :
      st.set("onlyone","0")
      st.set("id","0")
    if npcId == 7293 and int(st.get("cond"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getQuestItemsCount(LEAF_OF_ORACLE_ID) == 0 :
-            htmltext = "7293-01.htm"
-            return htmltext
-          else:
-            htmltext = "7293-04.htm"
-        else:
-          htmltext = "7293-04.htm"
+      if st.getQuestItemsCount(LEAF_OF_ORACLE_ID) == 0 :
+         htmltext = "7293-01.htm"
+         return htmltext
+      else:
+         htmltext = "7293-04.htm"
    elif npcId == 7293 and int(st.get("cond")) and st.getQuestItemsCount(CRYSTAL_MEDALLION_ID) :
     if st.getQuestItemsCount(MONEY_OF_SWINDLER_ID) == 0 and st.getQuestItemsCount(DAIRY_OF_ALLANA_ID) == 0 and st.getQuestItemsCount(LIZARD_CAPTAIN_ORDER_ID) == 0 and st.getQuestItemsCount(HALF_OF_DAIRY_ID) == 0 :
         if int(st.get("cond")) :
@@ -132,7 +129,6 @@ class Quest (JQuest) :
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
    if npcId == 5032 :
         st.set("id","0")
@@ -178,3 +174,4 @@ STARTED.addQuestDrop(7293,CRYSTAL_MEDALLION_ID,1)
 STARTED.addQuestDrop(7424,HALF_OF_DAIRY_ID,1)
 STARTED.addQuestDrop(5035,TAMATOS_NECKLACE_ID,1)
 
+print "importing quests: 409: Path To Oracle"
