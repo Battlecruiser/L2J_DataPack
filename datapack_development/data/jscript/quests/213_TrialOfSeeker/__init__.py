e54c2a9424427899a2357c7a7187c20e075dd7ca
DrLecter <DrLecter@users.noreply.github.com>
2006-01-24 00:55:06 +0000

    Quest 213: some fixes and code cleanup.

--- datapack_development/data/jscript/quests/213_TrialOfSeeker/__init__.py
+++ datapack_development/data/jscript/quests/213_TrialOfSeeker/__init__.py
@@ -1,37 +1,30 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 213: Trial Of Seeker"
+# Made by Mr. Have fun!
+# Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-DUFNERS_LETTER_ID = 2647
-TERYS_ORDER1_ID = 2648
-TERYS_ORDER2_ID = 2649
-TERYS_LETTER_ID = 2650
-VIKTORS_LETTER_ID = 2651
-HAWKEYES_LETTER_ID = 2652
-MYSTERIOUS_RUNESTONE_ID = 2653
-TERYS_REPORT_ID = 2672
-MARK_OF_SEEKER_ID = 2673
-OL_MAHUM_RUNESTONE_ID = 2654
-TUREK_RUNESTONE_ID = 2655
-ANT_RUNESTONE_ID = 2656
-TURAK_BUGBEAR_RUNESTONE_ID = 2657
-TERYS_BOX_ID = 2658
-TERYS_ORDER3_ID = 2666
-ANALYSIS_RESULT_ID = 2665
-LIST_OF_HOST_ID = 2667
-ABYSS_RUNESTONE1_ID = 2668
-ABYSS_RUNESTONE2_ID = 2669
-ABYSS_RUNESTONE3_ID = 2670
-ABYSS_RUNESTONE4_ID = 2671
-VIKTORS_REQUEST_ID = 2659
-MEDUSAS_SCALES_ID = 2660
-SILENS_RUNESTONE_ID = 2661
-ANALYSIS_REQUEST_ID = 2662
-MARINAS_LETTER_ID = 2663
-EXPERIMENT_TOOLS_ID = 2664
+DUFNERS_LETTER_ID,  TERYS_ORDER1_ID,    TERYS_ORDER2_ID,           TERYS_LETTER_ID,      \
+VIKTORS_LETTER_ID,  HAWKEYES_LETTER_ID, MYSTERIOUS_RUNESTONE_ID,   OL_MAHUM_RUNESTONE_ID,\
+TUREK_RUNESTONE_ID, ANT_RUNESTONE_ID,   TURAK_BUGBEAR_RUNESTONE_ID,TERYS_BOX_ID,         \
+VIKTORS_REQUEST_ID, MEDUSAS_SCALES_ID,  SILENS_RUNESTONE_ID,       ANALYSIS_REQUEST_ID,  \
+MARINAS_LETTER_ID,  EXPERIMENT_TOOLS_ID,ANALYSIS_RESULT_ID,        TERYS_ORDER3_ID,      \
+LIST_OF_HOST_ID,    ABYSS_RUNESTONE1_ID,ABYSS_RUNESTONE2_ID,       ABYSS_RUNESTONE3_ID,  \
+ABYSS_RUNESTONE4_ID,TERYS_REPORT_ID,    MARK_OF_SEEKER_ID = range(2647,2674)
+
+DROPLIST={
+198:[TERYS_ORDER1_ID,MYSTERIOUS_RUNESTONE_ID,   10,1],
+211:[TERYS_ORDER2_ID,OL_MAHUM_RUNESTONE_ID,     25,1],
+495:[TERYS_ORDER2_ID,TUREK_RUNESTONE_ID,        25,1],
+80 :[TERYS_ORDER2_ID,ANT_RUNESTONE_ID,          25,1],
+249:[TERYS_ORDER2_ID,TURAK_BUGBEAR_RUNESTONE_ID,25,1],
+234:[LIST_OF_HOST_ID,ABYSS_RUNESTONE1_ID,       25,1],
+270:[LIST_OF_HOST_ID,ABYSS_RUNESTONE2_ID,       25,1],
+88 :[LIST_OF_HOST_ID,ABYSS_RUNESTONE3_ID,       25,1],
+580:[LIST_OF_HOST_ID,ABYSS_RUNESTONE4_ID,       25,1],
+158:[VIKTORS_REQUEST_ID,MEDUSAS_SCALES_ID,      30,10]
+}
 
 class Quest (JQuest) :
 
@@ -39,27 +32,19 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      htmltext = "7106-05.htm"
+    if event == "7106-05.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
       st.giveItems(DUFNERS_LETTER_ID,1)
-    elif event == "7106_1" :
-      htmltext = "7106-04.htm"
-    elif event == "7064_1" :
-      htmltext = "7064-02.htm"
-    elif event == "7064_2" :
-      htmltext = "7064-03.htm"
+    elif event == "7064-03.htm" :
       st.giveItems(TERYS_ORDER1_ID,1)
       st.takeItems(DUFNERS_LETTER_ID,1)
-    elif event == "7064_3" :
-      htmltext = "7064-06.htm"
+    elif event == "7064-06.htm" :
       st.takeItems(MYSTERIOUS_RUNESTONE_ID,1)
       st.giveItems(TERYS_ORDER2_ID,1)
       st.takeItems(TERYS_ORDER1_ID,1)
-    elif event == "7064_4" :
-      htmltext = "7064-10.htm"
+    elif event == "7064-10.htm" :
       st.takeItems(OL_MAHUM_RUNESTONE_ID,1)
       st.takeItems(TUREK_RUNESTONE_ID,1)
       st.takeItems(ANT_RUNESTONE_ID,1)
@@ -67,74 +52,48 @@ class Quest (JQuest) :
       st.takeItems(TERYS_ORDER2_ID,1)
       st.giveItems(TERYS_LETTER_ID,1)
       st.giveItems(TERYS_BOX_ID,1)
-    elif event == "7064_5" :
-      htmltext = "7064-16.htm"
-    elif event == "7064_6" :
+    elif event == "7064-18.htm" :
       if st.getPlayer().getLevel()<36 :
         htmltext = "7064-17.htm"
         st.giveItems(TERYS_ORDER3_ID,1)
         st.takeItems(ANALYSIS_RESULT_ID,1)
       else:
-        htmltext = "7064-18.htm"
         st.giveItems(LIST_OF_HOST_ID,1)
         st.takeItems(ANALYSIS_RESULT_ID,1)
-    elif event == "7684_1" :
-      htmltext = "7684-02.htm"
-    elif event == "7684_2" :
-      htmltext = "7684-03.htm"
-    elif event == "7684_3" :
-      htmltext = "7684-04.htm"
-    elif event == "7684_4" :
-      htmltext = "7684-05.htm"
+    elif event == "7684-05.htm" :
       st.giveItems(VIKTORS_LETTER_ID,1)
       st.takeItems(TERYS_LETTER_ID,1)
-    elif event == "7684_5" :
-      htmltext = "7684-06.htm"
-    elif event == "7684_6" :
-      htmltext = "7684-07.htm"
-    elif event == "7684_7" :
-      htmltext = "7684-08.htm"
-    elif event == "7684_8" :
-      htmltext = "7684-09.htm"
-    elif event == "7684_9" :
-      htmltext = "7684-10.htm"
-    elif event == "7684_10" :
-      htmltext = "7684-11.htm"
+    elif event == "7684-11.htm" :
       st.giveItems(VIKTORS_REQUEST_ID,1)
       st.takeItems(TERYS_LETTER_ID,1)
       st.takeItems(TERYS_BOX_ID,1)
       st.takeItems(HAWKEYES_LETTER_ID,1)
       st.takeItems(VIKTORS_LETTER_ID,st.getQuestItemsCount(VIKTORS_LETTER_ID))
-    elif event == "7684_11" :
-      htmltext = "7684-15.htm"
+    elif event == "7684-15.htm" :
       st.takeItems(VIKTORS_REQUEST_ID,1)
       st.takeItems(MEDUSAS_SCALES_ID,st.getQuestItemsCount(MEDUSAS_SCALES_ID))
       st.giveItems(SILENS_RUNESTONE_ID,1)
       st.giveItems(ANALYSIS_REQUEST_ID,1)
-    elif event == "7715_1" :
-      htmltext = "7715-02.htm"
+    elif event == "7715-02.htm" :
       st.takeItems(SILENS_RUNESTONE_ID,1)
       st.takeItems(ANALYSIS_REQUEST_ID,1)
       st.giveItems(MARINAS_LETTER_ID,1)
-    elif event == "7715_2" :
-      htmltext = "7715-05.htm"
+    elif event == "7715-05.htm" :
       st.takeItems(EXPERIMENT_TOOLS_ID,1)
       st.giveItems(ANALYSIS_RESULT_ID,1)
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
      st.set("onlyone","0")
      st.set("id","0")
    if npcId == 7106 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-     if st.getPlayer().getClassId().getId() == 0x07 or st.getPlayer().getClassId().getId() == 0x16 or st.getPlayer().getClassId().getId() == 0x23 :
+     if st.getPlayer().getClassId().getId() in [ 0x07, 0x16, 0x23 ] :
        if st.getPlayer().getLevel() >= 35 :
          htmltext = "7106-03.htm"
        else:
@@ -151,8 +110,6 @@ class Quest (JQuest) :
           elif st.getQuestItemsCount(DUFNERS_LETTER_ID) == 0 and st.getQuestItemsCount(TERYS_REPORT_ID) == 0 :
             htmltext = "7106-07.htm"
           elif st.getQuestItemsCount(DUFNERS_LETTER_ID) == 0 and st.getQuestItemsCount(TERYS_REPORT_ID) == 1 :
-            if st.getGameTicks() != int(st.get("id")) :
-              st.set("id",str(st.getGameTicks()))
               st.addExpAndSp(27000,3100)
               htmltext = "7106-08.htm"
               st.set("cond","0")
@@ -236,48 +193,16 @@ class Quest (JQuest) :
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 198 :
-    if int(st.get("cond")) and st.getQuestItemsCount(TERYS_ORDER1_ID) == 1 and st.getQuestItemsCount(MYSTERIOUS_RUNESTONE_ID) == 0 :
-      if st.getRandom(100) < 10 :
-        st.giveItems(MYSTERIOUS_RUNESTONE_ID,1)
-   elif npcId == 211 :
-    if int(st.get("cond")) and st.getQuestItemsCount(TERYS_ORDER2_ID) == 1 and st.getQuestItemsCount(OL_MAHUM_RUNESTONE_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(OL_MAHUM_RUNESTONE_ID,1)
-   elif npcId == 495 :
-    if int(st.get("cond")) and st.getQuestItemsCount(TERYS_ORDER2_ID) == 1 and st.getQuestItemsCount(TUREK_RUNESTONE_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(TUREK_RUNESTONE_ID,1)
-   elif npcId == 80 :
-    if int(st.get("cond")) and st.getQuestItemsCount(TERYS_ORDER2_ID) == 1 and st.getQuestItemsCount(ANT_RUNESTONE_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(ANT_RUNESTONE_ID,1)
-   elif npcId == 249 :
-    if int(st.get("cond")) and st.getQuestItemsCount(TERYS_ORDER2_ID) == 1 and st.getQuestItemsCount(TURAK_BUGBEAR_RUNESTONE_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(TURAK_BUGBEAR_RUNESTONE_ID,1)
-   elif npcId == 234 :
-    if int(st.get("cond")) and st.getQuestItemsCount(LIST_OF_HOST_ID) == 1 and st.getQuestItemsCount(ABYSS_RUNESTONE1_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(ABYSS_RUNESTONE1_ID,1)
-   elif npcId == 270 :
-    if int(st.get("cond")) and st.getQuestItemsCount(LIST_OF_HOST_ID) == 1 and st.getQuestItemsCount(ABYSS_RUNESTONE2_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(ABYSS_RUNESTONE2_ID,1)
-   elif npcId == 88 :
-    if int(st.get("cond")) and st.getQuestItemsCount(LIST_OF_HOST_ID) == 1 and st.getQuestItemsCount(ABYSS_RUNESTONE3_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(ABYSS_RUNESTONE3_ID,1)
-   elif npcId == 580 :
-    if int(st.get("cond")) and st.getQuestItemsCount(LIST_OF_HOST_ID) == 1 and st.getQuestItemsCount(ABYSS_RUNESTONE4_ID) == 0 :
-      if st.getRandom(100) < 25 :
-        st.giveItems(ABYSS_RUNESTONE4_ID,1)
-   elif npcId == 158 :
-    if int(st.get("cond")) and st.getQuestItemsCount(VIKTORS_REQUEST_ID) == 1 and st.getQuestItemsCount(MEDUSAS_SCALES_ID)<10 :
-      if st.getRandom(100) < 30 :
-        st.giveItems(MEDUSAS_SCALES_ID,1)
+   required,item,chance,maxqty=DROPLIST[npcId]
+   count = st.getQuestItemsCount(item)
+   if st.getQuestItemsCount(required) and count < maxqty :
+      if st.getRandom(100) < chance :
+        st.giveItems(item,1)
+        if count == maxqty :
+           st.playSound("Itemsound.quest_middle")
+        else :
+           st.playSound("Itemsound.quest_itemget")
    return
 
 QUEST       = Quest(213,"213_TrialOfSeeker","Trial Of Seeker")
@@ -290,7 +215,9 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7106)
 
+CREATED.addTalkId(7106)
 STARTING.addTalkId(7106)
+COMPLETED.addTalkId(7106)
 
 STARTED.addTalkId(7064)
 STARTED.addTalkId(7106)
@@ -335,3 +262,5 @@ STARTED.addQuestDrop(7684,SILENS_RUNESTONE_ID,1)
 STARTED.addQuestDrop(7684,ANALYSIS_REQUEST_ID,1)
 STARTED.addQuestDrop(7526,EXPERIMENT_TOOLS_ID,1)
 STARTED.addQuestDrop(7715,MARINAS_LETTER_ID,1)
+
+print "importing quests: 213: Trial Of Seeker"
