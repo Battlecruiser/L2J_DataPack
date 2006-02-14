d18fea3061fad21b0beca7fc6b57a47b333eea12
DrLecter <DrLecter@users.noreply.github.com>
2006-02-14 15:57:43 +0000

    Quest 166: Minor code cleanup.

--- datapack_development/data/jscript/quests/166_DarkMass/__init__.py
+++ datapack_development/data/jscript/quests/166_DarkMass/__init__.py
@@ -1,5 +1,4 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 166: Dark Mass"
+# Made by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -17,77 +16,73 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
-      htmltext = "7130-04.htm"
-      st.giveItems(UNDRES_LETTER_ID,1)
-      st.set("cond","1")
-      st.setState(STARTED)
-      st.playSound("ItemSound.quest_accept")
+    if event == "7130-04.htm" :
+       st.giveItems(UNDRES_LETTER_ID,1)
+       st.set("cond","1")
+       st.setState(STARTED)
+       st.playSound("ItemSound.quest_accept")
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7130 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-      if int(st.get("cond"))<15 :
-        if st.getPlayer().getRace().ordinal() != 2 and st.getPlayer().getRace().ordinal() != 0 :
-          htmltext = "7130-00.htm"
-        elif st.getPlayer().getLevel() >= 2 :
-          htmltext = "7130-03.htm"
-          return htmltext
-        else:
-          htmltext = "7130-02.htm"
-          st.exitQuest(1)
-      else:
+     if st.getPlayer().getRace().ordinal() == 2 :
+        if st.getPlayer().getLevel() >= 2 :
+           htmltext = "7130-03.htm"
+           return htmltext
+        else :
+           htmltext = "7130-00.htm"
+           st.exitQuest(1)
+     else:
         htmltext = "7130-02.htm"
         st.exitQuest(1)
-   elif npcId == 7130 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
+   elif id == COMPLETED :
       htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7130 and int(st.get("cond"))==1 and st.getQuestItemsCount(UNDRES_LETTER_ID)==1 and (st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)<1 or st.getQuestItemsCount(DREVIANT_WINE_ID)<1 or st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)<1) :
-      htmltext = "7130-05.htm"
-   elif npcId == 7135 and int(st.get("cond"))==1 and st.getQuestItemsCount(UNDRES_LETTER_ID)==1 and st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)==0 :
-      st.giveItems(CEREMONIAL_DAGGER_ID,1)
-      htmltext = "7135-01.htm"
-   elif npcId == 7135 and int(st.get("cond"))==1 and st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)==1 :
-      htmltext = "7135-02.htm"
-   elif npcId == 7139 and int(st.get("cond"))==1 and st.getQuestItemsCount(UNDRES_LETTER_ID)==1 and st.getQuestItemsCount(DREVIANT_WINE_ID)==0 :
-      st.giveItems(DREVIANT_WINE_ID,1)
-      htmltext = "7139-01.htm"
-   elif npcId == 7139 and int(st.get("cond"))==1 and st.getQuestItemsCount(DREVIANT_WINE_ID)==1 :
-      htmltext = "7139-02.htm"
-   elif npcId == 7143 and int(st.get("cond"))==1 and st.getQuestItemsCount(UNDRES_LETTER_ID)==1 and st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)==0 :
-      st.giveItems(GARMIELS_SCRIPTURE_ID,1)
-      htmltext = "7143-01.htm"
-   elif npcId == 7143 and int(st.get("cond"))==1 and st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)==1 :
-      htmltext = "7143-02.htm"
-   elif npcId == 7130 and int(st.get("cond"))==1 and st.getQuestItemsCount(UNDRES_LETTER_ID)==1 and st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)==1 and st.getQuestItemsCount(DREVIANT_WINE_ID)==1 and st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)==1 and int(st.get("onlyone"))==0 :
-      if int(st.get("id")) != 166 :
-        st.set("id","166")
-        htmltext = "7130-06.htm"
-        st.takeItems(CEREMONIAL_DAGGER_ID,1)
-        st.takeItems(DREVIANT_WINE_ID,1)
-        st.takeItems(GARMIELS_SCRIPTURE_ID,1)
-        st.takeItems(UNDRES_LETTER_ID,1)
-        st.giveItems(ADENA_ID,250)
-        st.addExpAndSp(500,0)
-        st.set("cond","0")
-        st.setState(COMPLETED)
-        st.playSound("ItemSound.quest_finish")
-        st.set("onlyone","1")
+   elif npcId == 7130 :
+      if int(st.get("cond")) and st.getQuestItemsCount(UNDRES_LETTER_ID):
+         if (st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)+st.getQuestItemsCount(DREVIANT_WINE_ID)+st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)==0) :
+            htmltext = "7130-05.htm"
+         elif st.getQuestItemsCount(CEREMONIAL_DAGGER_ID)==st.getQuestItemsCount(DREVIANT_WINE_ID)==st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID)==1 :
+            htmltext = "7130-06.htm"
+            st.takeItems(CEREMONIAL_DAGGER_ID,1)
+            st.takeItems(DREVIANT_WINE_ID,1)
+            st.takeItems(GARMIELS_SCRIPTURE_ID,1)
+            st.takeItems(UNDRES_LETTER_ID,1)
+            st.giveItems(ADENA_ID,250)
+            st.addExpAndSp(500,0)
+            st.set("cond","0")
+            st.setState(COMPLETED)
+            st.playSound("ItemSound.quest_finish")
+   elif npcId == 7135 :
+      if st.getQuestItemsCount(UNDRES_LETTER_ID) :
+         if not st.getQuestItemsCount(CEREMONIAL_DAGGER_ID) :
+            st.giveItems(CEREMONIAL_DAGGER_ID,1)
+            htmltext = "7135-01.htm"
+         else :
+            htmltext = "7135-02.htm"
+   elif npcId == 7139 :
+      if st.getQuestItemsCount(UNDRES_LETTER_ID) :
+         if not st.getQuestItemsCount(DREVIANT_WINE_ID) :
+            st.giveItems(DREVIANT_WINE_ID,1)
+            htmltext = "7139-01.htm"
+         else :
+            htmltext = "7139-02.htm"
+   elif npcId == 7143 :
+      if st.getQuestItemsCount(UNDRES_LETTER_ID) :
+         if not st.getQuestItemsCount(GARMIELS_SCRIPTURE_ID) :
+            st.giveItems(GARMIELS_SCRIPTURE_ID,1)
+            htmltext = "7143-01.htm"
+         else :
+            htmltext = "7143-02.htm"
    return htmltext
 
 QUEST       = Quest(166,"166_DarkMass","Dark Mass")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
@@ -95,15 +90,18 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7130)
 
+CREATED.addTalkId(7130)
 STARTING.addTalkId(7130)
+COMPLETED.addTalkId(7130)
 
 STARTED.addTalkId(7130)
 STARTED.addTalkId(7135)
 STARTED.addTalkId(7139)
 STARTED.addTalkId(7143)
 
-
 STARTED.addQuestDrop(7135,CEREMONIAL_DAGGER_ID,1)
 STARTED.addQuestDrop(7139,DREVIANT_WINE_ID,1)
 STARTED.addQuestDrop(7143,GARMIELS_SCRIPTURE_ID,1)
 STARTED.addQuestDrop(7130,UNDRES_LETTER_ID,1)
+
+print "importing quests: 166: Dark Mass"
