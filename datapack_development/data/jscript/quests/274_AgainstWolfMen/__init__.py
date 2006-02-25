27d70ee4df99d9bc4ed3783900c1d353d32e698a
DrLecter <DrLecter@users.noreply.github.com>
2006-02-25 02:06:19 +0000

    Quest 274: Repeatable + cleanup

--- datapack_development/data/jscript/quests/274_AgainstWolfMen/__init__.py
+++ datapack_development/data/jscript/quests/274_AgainstWolfMen/__init__.py
@@ -1,15 +1,15 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 274: Against Wolf Men"
+# Maked by Mr. - Version 0.3 by DrLecter
 import sys
+from net.sf.l2j import Config
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-MARAKU_WEREWOLF_HEAD_ID = 1477
-NECKLACE_OF_VALOR_ID = 1507
-NECKLACE_OF_COURAGE_ID = 1506
-ADENA_ID = 57
-MARAKU_WOLFMEN_TOTEM_ID = 1501
+MARAKU_WEREWOLF_HEAD = 1477
+NECKLACE_OF_VALOR = 1507
+NECKLACE_OF_COURAGE = 1506
+ADENA = 57
+MARAKU_WOLFMEN_TOTEM = 1501
 
 class Quest (JQuest) :
 
@@ -17,105 +17,76 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7569-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "7569-03.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7569 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getRace().ordinal() != 3 :
-          htmltext = "7569-00.htm"
-          st.exitQuest(1)
-        elif st.getPlayer().getLevel() < 9 :
-          htmltext = "7569-01.htm"
-          st.exitQuest(1)
-        elif st.getQuestItemsCount(NECKLACE_OF_VALOR_ID) or st.getQuestItemsCount(NECKLACE_OF_COURAGE_ID) :
-          htmltext = "7569-02.htm"
-          return htmltext
-        else:
-          htmltext = "7569-07.htm"
-      else:
-        htmltext = "7569-07.htm"
-        st.exitQuest(1)
-   elif npcId == 7569 and int(st.get("cond")) :
-      if int(st.get("id")) != 274 :
-        st.set("id","274")
-        if st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID) < 40 :
-          htmltext = "7569-04.htm"
-        elif st.getQuestItemsCount(MARAKU_WOLFMEN_TOTEM_ID) < 1 :
-          htmltext = "7569-05.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          st.giveItems(ADENA_ID,st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID)*30)
-          st.takeItems(MARAKU_WEREWOLF_HEAD_ID,st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID))
-        else:
-          htmltext = "7569-06.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          st.giveItems(ADENA_ID,st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID)*30)
-          st.takeItems(MARAKU_WEREWOLF_HEAD_ID,st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID))
+   if int(st.get("cond"))==0 :
+     if st.getPlayer().getRace().ordinal() == 3 :
+       if st.getPlayer().getLevel() > 8 :
+         if st.getQuestItemsCount(NECKLACE_OF_VALOR) or st.getQuestItemsCount(NECKLACE_OF_COURAGE) :
+           htmltext = "7569-02.htm"
+         else :
+           htmltext = "7569-07.htm"
+           st.exitQuest(1)
+       else :
+         htmltext = "7569-01.htm"
+         st.exitQuest(1)
+     else :
+       htmltext = "7569-00.htm"
+       st.exitQuest(1)
+   else :
+     if st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD) < 40 :
+       htmltext = "7569-04.htm"
+     else :
+       if st.getQuestItemsCount(MARAKU_WOLFMEN_TOTEM) :
+         htmltext = "7569-06.htm"
+       else :
+         htmltext = "7569-05.htm"
+       exitQuest(1)
+       st.playSound("ItemSound.quest_finish")
+       st.giveItems(ADENA,1200*int(Config.RATE_DROP_ADENA))
+       st.takeItems(MARAKU_WEREWOLF_HEAD,-1)
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 363 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID) < 40 :
-        if st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID) < 39 :
-          st.playSound("ItemSound.quest_itemget")
-        else:
-          st.playSound("ItemSound.quest_middle")
-        st.giveItems(MARAKU_WEREWOLF_HEAD_ID,1)
-        n = st.getRandom(100)
-        if n <= 5 :
-          st.giveItems(MARAKU_WOLFMEN_TOTEM_ID,1)
-   elif npcId == 364 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID) < 40 :
-        if st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD_ID) < 39 :
-          st.playSound("ItemSound.quest_itemget")
-        else:
-          st.playSound("ItemSound.quest_middle")
-        st.giveItems(MARAKU_WEREWOLF_HEAD_ID,1)
-        n = st.getRandom(100)
-        if n <= 5 :
-          st.giveItems(MARAKU_WOLFMEN_TOTEM_ID,1)
+   count=st.getQuestItemsCount(MARAKU_WEREWOLF_HEAD)
+   if count < 40 :
+     if count < 39 :
+       st.playSound("ItemSound.quest_itemget")
+     else:
+       st.playSound("ItemSound.quest_middle")
+       st.set("cond","2")
+     st.giveItems(MARAKU_WEREWOLF_HEAD,1)
+     if st.getRandom(100) <= 5 :
+       st.giveItems(MARAKU_WOLFMEN_TOTEM,1)
    return
 
 QUEST       = Quest(274,"274_AgainstWolfMen","Against Wolf Men")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7569)
 
+CREATED.addTalkId(7569)
 STARTING.addTalkId(7569)
-
 STARTED.addTalkId(7569)
+COMPLETED.addTalkId(7569)
 
 STARTED.addKillId(363)
 STARTED.addKillId(364)
 
-STARTED.addQuestDrop(363,MARAKU_WEREWOLF_HEAD_ID,1)
-STARTED.addQuestDrop(364,MARAKU_WEREWOLF_HEAD_ID,1)
+STARTED.addQuestDrop(363,MARAKU_WEREWOLF_HEAD,1)
+STARTED.addQuestDrop(364,MARAKU_WEREWOLF_HEAD,1)
+
+print "importing quests: 274: Against Wolf Men"
