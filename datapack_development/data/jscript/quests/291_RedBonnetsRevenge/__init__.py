6dfe7f80f9f758e7e612c9aa4ac7b20f90a1b6c4
DrLecter <DrLecter@users.noreply.github.com>
2006-02-26 05:21:18 +0000

    Quests 275,276,291,292,293: Repeatable + cleaup

--- datapack_development/data/jscript/quests/291_RedBonnetsRevenge/__init__.py
+++ datapack_development/data/jscript/quests/291_RedBonnetsRevenge/__init__.py
@@ -1,15 +1,12 @@
 # Maked by Mr. Have fun! Version 0.2
-print "importing quests: 291: Red Bonnets Revenge"
+
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-BLACK_WOLF_PELT_ID = 1482
-GRANDMAS_PEARL_ID = 1502
-GRANDMAS_MIRROR_ID = 1503
-GRANDMAS_NECKLACE_ID = 1504
-GRANDMAS_HAIRPIN_ID = 1505
+BLACK_WOLF_PELT = 1482
+GRANDMAS_PEARL,GRANDMAS_MIRROR,GRANDMAS_NECKLACE,GRANDMAS_HAIRPIN = range(1502,1506)
 
 class Quest (JQuest) :
 
@@ -17,74 +14,56 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7553-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "7553-03.htm"
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7553 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getLevel() < 4 :
+   if int(st.get("cond"))==0 :
+      if st.getPlayer().getLevel() < 4 :
           htmltext = "7553-01.htm"
           st.exitQuest(1)
-        else:
-          htmltext = "7553-02.htm"
-          return htmltext
       else:
-        htmltext = "7553-01.htm"
-        st.exitQuest(1)
-   elif npcId == 7553 and int(st.get("cond")) :
-      if st.getQuestItemsCount(BLACK_WOLF_PELT_ID) < 40 :
+          htmltext = "7553-02.htm"
+   else :
+      if st.getQuestItemsCount(BLACK_WOLF_PELT) < 40 :
         htmltext = "7553-04.htm"
       else:
-        if int(st.get("id")) != 291 :
-          st.set("id","291")
           htmltext = "7553-05.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
+          st.exitQuest(1)
           st.playSound("ItemSound.quest_finish")
-          st.takeItems(BLACK_WOLF_PELT_ID,st.getQuestItemsCount(BLACK_WOLF_PELT_ID))
+          st.takeItems(BLACK_WOLF_PELT,-1)
           n = st.getRandom(100)
           if n <= 2 :
-            st.giveItems(GRANDMAS_PEARL_ID,1)
+            st.giveItems(GRANDMAS_PEARL,1)
           elif n <= 20 :
-            st.giveItems(GRANDMAS_MIRROR_ID,1)
+            st.giveItems(GRANDMAS_MIRROR,1)
           elif n <= 45 :
-            st.giveItems(GRANDMAS_NECKLACE_ID,1)
-          else:
-            st.giveItems(GRANDMAS_HAIRPIN_ID,1)
+            st.giveItems(GRANDMAS_NECKLACE,1)
+          else :
+            st.giveItems(GRANDMAS_HAIRPIN,1)
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 317 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(BLACK_WOLF_PELT_ID) < 40 :
-        if st.getQuestItemsCount(BLACK_WOLF_PELT_ID) < 39 :
-          st.playSound("ItemSound.quest_itemget")
-        else:
-          st.playSound("ItemSound.quest_middle")
-        st.giveItems(BLACK_WOLF_PELT_ID,1)
+   if st.getQuestItemsCount(BLACK_WOLF_PELT) < 40 :
+     if st.getQuestItemsCount(BLACK_WOLF_PELT) < 39 :
+       st.playSound("ItemSound.quest_itemget")
+     else:
+       st.playSound("ItemSound.quest_middle")
+       st.set("cond","2")
+     st.giveItems(BLACK_WOLF_PELT,1)
    return
 
 QUEST       = Quest(291,"291_RedBonnetsRevenge","Red Bonnets Revenge")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
@@ -92,10 +71,13 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7553)
 
+CREATED.addTalkId(7553)
 STARTING.addTalkId(7553)
-
 STARTED.addTalkId(7553)
+COMPLETED.addTalkId(7553)
 
 STARTED.addKillId(317)
 
-STARTED.addQuestDrop(317,BLACK_WOLF_PELT_ID,1)
+STARTED.addQuestDrop(317,BLACK_WOLF_PELT,1)
+
+print "importing quests: 291: Red Bonnets Revenge"
--- datapack_development/data/jscript/quests/293_HiddenVein/__init__.py
+++ datapack_development/data/jscript/quests/293_HiddenVein/__init__.py
@@ -1,14 +1,13 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 293: Hidden Vein"
+# Made by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-ORIHARUKON_ORE_1_ID = 1488
-TORN_MAP_FRAGMENT_ID = 1489
-HIDDEN_VEIN_MAP_ID = 1490
-ADENA_ID = 57
+CHRYSOLITE_ORE = 1488
+TORN_MAP_FRAGMENT = 1489
+HIDDEN_VEIN_MAP = 1490
+ADENA = 57
 
 class Quest (JQuest) :
 
@@ -16,105 +15,68 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7535-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "7535-03.htm"
-    elif event == "7535_1" :
-          htmltext = "7535-06.htm"
-          st.takeItems(TORN_MAP_FRAGMENT_ID,st.getQuestItemsCount(TORN_MAP_FRAGMENT_ID))
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-    elif event == "7535_2" :
-          htmltext = "7535-07.htm"
-    elif event == "7539_1" and st.getQuestItemsCount(TORN_MAP_FRAGMENT_ID) < 4 :
-          htmltext = "7539-02.htm"
-    elif event == "7539_1" and st.getQuestItemsCount(TORN_MAP_FRAGMENT_ID) >= 4 :
-          htmltext = "7539-03.htm"
-          st.giveItems(HIDDEN_VEIN_MAP_ID,1)
-          st.takeItems(TORN_MAP_FRAGMENT_ID,4)
+    elif event == "7535-06.htm" :
+      st.takeItems(TORN_MAP_FRAGMENT,-1)
+      st.exitQuest(1)
+      st.playSound("ItemSound.quest_finish")
+    elif event == "7539-02.htm" :
+      if st.getQuestItemsCount(TORN_MAP_FRAGMENT) >=4 :
+        htmltext = "7539-03.htm"
+        st.giveItems(HIDDEN_VEIN_MAP,1)
+        st.takeItems(TORN_MAP_FRAGMENT,4)
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7535 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getRace().ordinal() != 4 :
-          htmltext = "7535-00.htm"
-          st.exitQuest(1)
-        elif st.getPlayer().getLevel() >= 6 :
-          htmltext = "7535-02.htm"
-          return htmltext
-        else:
-          htmltext = "7535-01.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7535-01.htm"
-        st.exitQuest(1)
-   elif npcId == 7535 and int(st.get("cond"))==1 and st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)<1 and st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)<1 :
-      htmltext = "7535-04.htm"
-   elif npcId == 7535 and int(st.get("cond"))==1 and st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)<1 and st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)>=1 :
-      htmltext = "7535-08.htm"
-      st.giveItems(ADENA_ID,st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)*1000)
-      st.takeItems(HIDDEN_VEIN_MAP_ID,st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID))
-   elif npcId == 7535 and int(st.get("cond"))==1 and st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)>=1 and st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)<1 :
-      htmltext = "7535-05.htm"
-      st.giveItems(ADENA_ID,st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)*10)
-      st.takeItems(ORIHARUKON_ORE_1_ID,st.getQuestItemsCount(ORIHARUKON_ORE_1_ID))
-   elif npcId == 7535 and int(st.get("cond"))==1 and st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)>=1 and st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)>=1 :
-      htmltext = "7535-09.htm"
-      st.giveItems(ADENA_ID,st.getQuestItemsCount(ORIHARUKON_ORE_1_ID)*10+st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID)*1000)
-      st.takeItems(HIDDEN_VEIN_MAP_ID,st.getQuestItemsCount(HIDDEN_VEIN_MAP_ID))
-      st.takeItems(ORIHARUKON_ORE_1_ID,st.getQuestItemsCount(ORIHARUKON_ORE_1_ID))
-   elif npcId == 7539 and int(st.get("cond"))==1 :
+   if npcId == 7535 :
+     if int(st.get("cond"))==0 :
+       if st.getPlayer().getRace().ordinal() != 4 :
+         htmltext = "7535-00.htm"
+         st.exitQuest(1)
+       elif st.getPlayer().getLevel() >= 6 :
+         htmltext = "7535-02.htm"
+         return htmltext
+       else:
+         htmltext = "7535-01.htm"
+         st.exitQuest(1)
+     else :
+       if st.getQuestItemsCount(CHRYSOLITE_ORE)==0 :
+         if st.getQuestItemsCount(HIDDEN_VEIN_MAP)==0 :
+           htmltext = "7535-04.htm"
+         else :
+           htmltext = "7535-08.htm"
+           st.giveItems(ADENA,st.getQuestItemsCount(HIDDEN_VEIN_MAP)*1000)
+           st.takeItems(HIDDEN_VEIN_MAP,-1)
+       else :
+         if st.getQuestItemsCount(HIDDEN_VEIN_MAP)==0 :
+           htmltext = "7535-05.htm"
+           st.giveItems(ADENA,st.getQuestItemsCount(CHRYSOLITE_ORE)*10)
+           st.takeItems(CHRYSOLITE_ORE,-1)
+         else :
+           htmltext = "7535-09.htm"
+           st.giveItems(ADENA,st.getQuestItemsCount(CHRYSOLITE_ORE)*10+st.getQuestItemsCount(HIDDEN_VEIN_MAP)*1000)
+           st.takeItems(HIDDEN_VEIN_MAP,-1)
+           st.takeItems(CHRYSOLITE_ORE,-1)
+   elif npcId == 7539 :
       htmltext = "7539-01.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 446 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        n = st.getRandom(100)
-        if n > 50 :
-          st.giveItems(ORIHARUKON_ORE_1_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-        elif n < 5 :
-          st.giveItems(TORN_MAP_FRAGMENT_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-   elif npcId == 448 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        n = st.getRandom(100)
-        if n > 50 :
-          st.giveItems(ORIHARUKON_ORE_1_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-        elif n < 5 :
-          st.giveItems(TORN_MAP_FRAGMENT_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-   elif npcId == 447 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        n = st.getRandom(100)
-        if n > 50 :
-          st.giveItems(ORIHARUKON_ORE_1_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-        elif n < 5 :
-          st.giveItems(TORN_MAP_FRAGMENT_ID,1)
-          st.playSound("ItemSound.quest_itemget")
+   n = st.getRandom(100)
+   if n > 50 :
+     st.giveItems(CHRYSOLITE_ORE,1)
+     st.playSound("ItemSound.quest_itemget")
+   elif n < 5 :
+     st.giveItems(TORN_MAP_FRAGMENT,1)
+     st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(293,"293_HiddenVein","Hidden Vein")
@@ -123,23 +85,22 @@ STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
-
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7535)
 
+CREATED.addTalkId(7535)
 STARTING.addTalkId(7535)
-
 STARTED.addTalkId(7535)
+COMPLETED.addTalkId(7535)
+
 STARTED.addTalkId(7539)
 
 STARTED.addKillId(446)
 STARTED.addKillId(447)
 STARTED.addKillId(448)
 
-STARTED.addQuestDrop(7539,HIDDEN_VEIN_MAP_ID,1)
-STARTED.addQuestDrop(446,ORIHARUKON_ORE_1_ID,1)
-STARTED.addQuestDrop(448,ORIHARUKON_ORE_1_ID,1)
-STARTED.addQuestDrop(447,ORIHARUKON_ORE_1_ID,1)
-STARTED.addQuestDrop(446,TORN_MAP_FRAGMENT_ID,1)
-STARTED.addQuestDrop(448,TORN_MAP_FRAGMENT_ID,1)
-STARTED.addQuestDrop(447,TORN_MAP_FRAGMENT_ID,1)
+STARTED.addQuestDrop(7539,HIDDEN_VEIN_MAP,1)
+STARTED.addQuestDrop(446,CHRYSOLITE_ORE,1)
+STARTED.addQuestDrop(447,TORN_MAP_FRAGMENT,1)
+
+print "importing quests: 293: Hidden Vein"
