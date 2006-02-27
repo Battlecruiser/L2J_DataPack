77e30af0dfc3d0a5af30b11ed941c0f9b2482dee
mr <mr@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-02-27 12:43:19 +0000

    quests: fix 350

--- datapack_development/data/jscript/quests/350_EnhanceYourWeapon/__init__.py
+++ datapack_development/data/jscript/quests/350_EnhanceYourWeapon/__init__.py
@@ -6,6 +6,8 @@ from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 NPC=[7115,7856,7194]
+True=1
+False=0
 
 RED_SOUL_CRYSTAL0_ID,RED_SOUL_CRYSTAL1_ID,RED_SOUL_CRYSTAL2_ID,RED_SOUL_CRYSTAL3_ID,\
 RED_SOUL_CRYSTAL4_ID,RED_SOUL_CRYSTAL5_ID,RED_SOUL_CRYSTAL6_ID,RED_SOUL_CRYSTAL7_ID,\
@@ -19,7 +21,7 @@ BLUE_SOUL_CRYSTAL10_ID,RED_SOUL_CRYSTALX_ID,GREEN_SOUL_CRYSTALX_ID,BLUE_SOUL_CRY
 
 def check(st) :
     for i in range(4629,4665) :
-       if st.getQuestItemsCount(i) :
+       if st.getQuestItemsCount(i)>0 :
          return True
     return False
 
@@ -29,13 +31,13 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event ["7115-04.htm","7856-04.htm","7194-04.htm"] :
+    if event in ["7115-04.htm","7856-04.htm","7194-04.htm"] :
         st.set("cond","1")
         st.setState(STARTED)
         st.playSound("ItemSound.quest_accept")
-    elif event ["7115-09.htm","7856-09.htm","7194-09.htm"] :
+    elif event in ["7115-09.htm","7856-09.htm","7194-09.htm"] :
         st.giveItems(RED_SOUL_CRYSTAL0_ID,1)
-    elif event ["7115-10.htm","7856-10.htm","7194-10.htm"] :
+    elif event in ["7115-10.htm","7856-10.htm","7194-10.htm"] :
         st.giveItems(GREEN_SOUL_CRYSTAL0_ID,1)
     elif event in ["7115-11.htm","7856-11.htm","7194-11.htm"] :
         st.giveItems(BLUE_SOUL_CRYSTAL0_ID,1)
@@ -51,11 +53,11 @@ class Quest (JQuest) :
      st.setState(STARTING)
      st.set("cond","0")
    if int(st.get("cond")) == 0:   
-     htmltext = npcId+"-01.htm"
+     htmltext = str(npcId)+"-01.htm"
    elif check(st) :
-     htmltext = npcId+"-03.htm"
+     htmltext = str(npcId)+"-03.htm"
    elif st.getQuestItemsCount(RED_SOUL_CRYSTAL0_ID) == st.getQuestItemsCount(GREEN_SOUL_CRYSTAL0_ID) == st.getQuestItemsCount(BLUE_SOUL_CRYSTAL0_ID) == 0 :
-     htmltext = npcId+"-21.htm"
+     htmltext = str(npcId)+"-21.htm"
    return htmltext
 
 QUEST       = Quest(350,"350_EnhanceYourWeapon","Enhance Your Weapon")
