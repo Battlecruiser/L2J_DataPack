edc909c25d74118daf6981c27d281690c8187a6b
DrLecter <DrLecter@users.noreply.github.com>
2006-02-27 23:31:12 +0000

    Quest 295: Repeatable + cleanup

--- datapack_development/data/jscript/quests/295_DreamsOfFlight/__init__.py
+++ datapack_development/data/jscript/quests/295_DreamsOfFlight/__init__.py
@@ -1,13 +1,12 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 295: Dreams Of Flight"
+# Made by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-FLOATING_STONE_ID = 1492
-RING_OF_FIREFLY_ID = 1509
-ADENA_ID = 57
+FLOATING_STONE = 1492
+RING_OF_FIREFLY = 1509
+ADENA = 57
 
 class Quest (JQuest) :
 
@@ -15,79 +14,51 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7536-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "7536-03.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7536 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getLevel() >= 11 :
-          htmltext = "7536-02.htm"
-          return htmltext
-        else:
-          htmltext = "7536-01.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7536-01.htm"
-        st.exitQuest(1)
-   elif npcId == 7536 and int(st.get("cond"))==1 and st.getQuestItemsCount(FLOATING_STONE_ID)<50 :
-      htmltext = "7536-04.htm"
-   elif npcId == 7536 and int(st.get("cond"))==1 and st.getQuestItemsCount(FLOATING_STONE_ID)>=50 :
-      if int(st.get("id")) != 295 :
-        st.set("id","295")
-        if st.getQuestItemsCount(RING_OF_FIREFLY_ID) < 1 :
+   if int(st.get("cond"))==0 :
+     if st.getPlayer().getLevel() >= 11 :
+       htmltext = "7536-02.htm"
+     else:
+       htmltext = "7536-01.htm"
+       st.exitQuest(1)
+   else:
+     if st.getQuestItemsCount(FLOATING_STONE)<50 :
+       htmltext = "7536-04.htm"
+     else :
+       if st.getQuestItemsCount(RING_OF_FIREFLY)==0 :
           htmltext = "7536-05.htm"
-          st.giveItems(RING_OF_FIREFLY_ID,1)
-          st.addExpAndSp(0,60)
-          st.takeItems(FLOATING_STONE_ID,st.getQuestItemsCount(FLOATING_STONE_ID))
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-        elif st.getQuestItemsCount(RING_OF_FIREFLY_ID) >= 1 :
+          st.giveItems(RING_OF_FIREFLY,1)
+       else :
           htmltext = "7536-06.htm"
-          st.giveItems(ADENA_ID,2400)
-          st.addExpAndSp(0,60)
-          st.takeItems(FLOATING_STONE_ID,st.getQuestItemsCount(FLOATING_STONE_ID))
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
+          st.giveItems(ADENA,2400)
+       st.addExpAndSp(0,60)
+       st.playSound("ItemSound.quest_finish")
+       st.takeItems(FLOATING_STONE,-1)
+       st.exitQuest(1)
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 153 :
-      st.set("id","0")
-      if int(st.get("cond")) and st.getQuestItemsCount(FLOATING_STONE_ID) < 50 :
-        if st.getRandom(100) > 25 :
-          if st.getQuestItemsCount(FLOATING_STONE_ID) == 49 :
-            st.giveItems(FLOATING_STONE_ID,1)
-            st.playSound("ItemSound.quest_middle")
-          else:
-            st.giveItems(FLOATING_STONE_ID,1)
-            st.playSound("ItemSound.quest_itemget")
-        else:
-          if st.getQuestItemsCount(FLOATING_STONE_ID) >= 48 :
-            st.giveItems(FLOATING_STONE_ID,50-st.getQuestItemsCount(FLOATING_STONE_ID))
-            st.playSound("ItemSound.quest_middle")
-          else:
-            st.giveItems(FLOATING_STONE_ID,2)
-            st.playSound("ItemSound.quest_itemget")
+   count=st.getQuestItemsCount(FLOATING_STONE)
+   if count < 50 :
+     if st.getRandom(100) < 25 and count < 49 :
+       st.giveItems(FLOATING_STONE,2)
+     else:
+       st.giveItems(FLOATING_STONE,1)
+     if count == 49 :
+         st.playSound("ItemSound.quest_middle")
+         st.set("cond","2")
+     else:
+         st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(295,"295_DreamsOfFlight","Dreams Of Flight")
@@ -100,10 +71,12 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7536)
 
+CREATED.addTalkId(7536)
 STARTING.addTalkId(7536)
-
 STARTED.addTalkId(7536)
+COMPLETED.addTalkId(7536)
 
 STARTED.addKillId(153)
 
-STARTED.addQuestDrop(153,FLOATING_STONE_ID,1)
+STARTED.addQuestDrop(153,FLOATING_STONE,1)
+print "importing quests: 295: Dreams Of Flight"
