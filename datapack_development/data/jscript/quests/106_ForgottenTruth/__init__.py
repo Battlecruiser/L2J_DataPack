9b8ee3f6f059c3c48a3a8353a88937be909142e1
DrLecter <DrLecter@users.noreply.github.com>
2006-02-28 16:35:51 +0000

    Quests 106,160,161: Cleanup (tnx H1GHL4ND3R)

--- datapack_development/data/jscript/quests/106_ForgottenTruth/__init__.py
+++ datapack_development/data/jscript/quests/106_ForgottenTruth/__init__.py
@@ -1,16 +1,15 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 106: Forgotten Truth"
+# Made by Mr. Have fun! Version 0.2
+# Version 0.3 by H1GHL4ND3R
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-ONYX_TALISMAN1_ID = 984
-ONYX_TALISMAN2_ID = 985
-ANCIENT_SCROLL_ID = 986
-ANCIENT_CLAY_TABLET_ID = 987
-KARTAS_TRANSLATION_ID = 988
-ELDRITCH_DAGGER_ID = 989
+ONYX_TALISMAN1,      ONYX_TALISMAN2,     ANCIENT_SCROLL,  \
+ANCIENT_CLAY_TABLET, KARTAS_TRANSLATION, ELDRITCH_DAGGER  \
+= range(984,990)
+
+ORC = 5070
 
 class Quest (JQuest) :
 
@@ -18,107 +17,104 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-        st.set("id","0")
-        htmltext = "7358-05.htm"
-        st.giveItems(ONYX_TALISMAN1_ID,1)
+    if event == "7358-05.htm" :
+        st.giveItems(ONYX_TALISMAN1,1)
         st.set("cond","1")
         st.setState(STARTED)
         st.playSound("ItemSound.quest_accept")
-    elif event == "7358_1" :
-            htmltext = "7358-04.htm"
-            return htmltext
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
-   if id == CREATED :
-     st.setState(STARTING)
+   if id == CREATED :                                      # Check if is starting the quest
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7358 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-        if int(st.get("cond")) < 15 :
-          if st.getPlayer().getRace().ordinal() != 2 :
-            htmltext = "7358-00.htm"
-            st.exitQuest(1)
-          elif st.getPlayer().getLevel() >= 10 :
-            htmltext = "7358-03.htm"
-          else:
-            htmltext = "7358-02.htm"
-            st.exitQuest(1)
-        else:
-          htmltext = "7358-02.htm"
-          st.exitQuest(1)
-   elif npcId == 7358 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-        htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7358 and int(st.get("cond")) :
-        if (st.getQuestItemsCount(ONYX_TALISMAN1_ID) or st.getQuestItemsCount(ONYX_TALISMAN2_ID)) and st.getQuestItemsCount(KARTAS_TRANSLATION_ID) == 0 :
-          htmltext = "7358-06.htm"
-        elif st.getQuestItemsCount(KARTAS_TRANSLATION_ID) and int(st.get("onlyone")) == 0 :
-          if int(st.get("id")) != 106 :
-            st.set("id","106")
-            htmltext = "7358-07.htm"
-            st.takeItems(KARTAS_TRANSLATION_ID,1)
-            st.giveItems(ELDRITCH_DAGGER_ID,1)
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-            st.set("onlyone","1")
-   elif npcId == 7133 and int(st.get("cond")) :
-        if st.getQuestItemsCount(ONYX_TALISMAN1_ID) :
-          htmltext = "7133-01.htm"
-          st.takeItems(ONYX_TALISMAN1_ID,1)
-          st.giveItems(ONYX_TALISMAN2_ID,1)
-        elif st.getQuestItemsCount(ONYX_TALISMAN2_ID) and st.getQuestItemsCount(ANCIENT_SCROLL_ID) == 0 or st.getQuestItemsCount(ANCIENT_CLAY_TABLET_ID) == 0 :
-          htmltext = "7133-02.htm"
-        elif st.getQuestItemsCount(ANCIENT_SCROLL_ID) and st.getQuestItemsCount(ANCIENT_CLAY_TABLET_ID) :
-          htmltext = "7133-03.htm"
-          st.takeItems(ONYX_TALISMAN2_ID,1)
-          st.takeItems(ANCIENT_SCROLL_ID,1)
-          st.takeItems(ANCIENT_CLAY_TABLET_ID,1)
-          st.giveItems(KARTAS_TRANSLATION_ID,1)
-        elif st.getQuestItemsCount(KARTAS_TRANSLATION_ID) :
-          htmltext = "7133-04.htm"
+     if st.getPlayer().getRace().ordinal() == 2 :
+       if st.getPlayer().getLevel() >= 10 :
+         htmltext = "7358-03.htm"
+       else:
+         htmltext = "7358-02.htm"
+         st.exitQuest(1)
+     else :
+       htmltext = "7358-00.htm"
+       st.exitQuest(1)
+   elif id == COMPLETED :                                  # Check if the quest is already made
+     htmltext = "<html><head><body>This quest have already been completed.</body></html>"
+   else :                                                  # The quest itself
+     try :
+       cond = int(st.get("cond"))
+     except :
+       cond = None
+     if cond == 1 :
+       if npcId == 7358 :
+         htmltext = "7358-06.htm"
+       elif npcId == 7133 and st.getQuestItemsCount(ONYX_TALISMAN1) :
+         htmltext = "7133-01.htm"
+         st.takeItems(ONYX_TALISMAN1,1)
+         st.giveItems(ONYX_TALISMAN2,1)
+         st.set("cond","2")
+     elif cond == 2 :
+       if npcId == 7358 :
+         htmltext = "7358-06.htm"
+       elif npcId == 7133 :
+         htmltext = "7133-02.htm"
+     elif cond == 3 :
+       if npcId == 7358 :
+         htmltext = "7358-06.htm"
+       elif npcId == 7133 and st.getQuestItemsCount(ANCIENT_SCROLL) and st.getQuestItemsCount(ANCIENT_CLAY_TABLET) :
+         htmltext = "7133-03.htm"
+         st.takeItems(ONYX_TALISMAN2,1)
+         st.takeItems(ANCIENT_SCROLL,1)
+         st.takeItems(ANCIENT_CLAY_TABLET,1)
+         st.giveItems(KARTAS_TRANSLATION,1)
+         st.set("cond","4")
+     elif cond == 4 :
+       if npcId == 7358 and st.getQuestItemsCount(KARTAS_TRANSLATION) :
+         htmltext = "7358-07.htm"
+         st.takeItems(KARTAS_TRANSLATION,1)
+         st.giveItems(ELDRITCH_DAGGER,1)
+         st.unset("cond")
+         st.setState(COMPLETED)
+         st.playSound("ItemSound.quest_finish")
+       elif npcId == 7133 :
+         htmltext = "7133-04.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 5070 :
-        st.set("id","0")
-        if int(st.get("cond")) and st.getQuestItemsCount(ONYX_TALISMAN2_ID) and st.getRandom(100) < 20 :
-          if st.getQuestItemsCount(ANCIENT_SCROLL_ID) == 0 :
-            st.giveItems(ANCIENT_SCROLL_ID,1)
-            st.playSound("ItemSound.quest_middle")
-          elif st.getQuestItemsCount(ANCIENT_CLAY_TABLET_ID) == 0 :
-            st.giveItems(ANCIENT_CLAY_TABLET_ID,1)
-            st.playSound("ItemSound.quest_middle")
+   if int(st.get("cond")) == 2 :
+     if st.getRandom(100) < 20 :
+       if st.getQuestItemsCount(ANCIENT_SCROLL) == 0 :
+         st.giveItems(ANCIENT_SCROLL,1)
+         st.playSound("Itemsound.quest_itemget")
+       elif st.getQuestItemsCount(ANCIENT_CLAY_TABLET) == 0 :
+         st.giveItems(ANCIENT_CLAY_TABLET,1)
+         st.playSound("ItemSound.quest_middle")
+         st.set("cond","3")
    return
 
 QUEST       = Quest(106,"106_ForgottenTruth","Forgotten Truth")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7358)
 
+CREATED.addTalkId(7358)
 STARTING.addTalkId(7358)
+COMPLETED.addTalkId(7358)
 
 STARTED.addTalkId(7133)
 STARTED.addTalkId(7358)
 
 STARTED.addKillId(5070)
 
-STARTED.addQuestDrop(7133,KARTAS_TRANSLATION_ID,1)
-STARTED.addQuestDrop(7358,ONYX_TALISMAN1_ID,1)
-STARTED.addQuestDrop(7133,ONYX_TALISMAN2_ID,1)
-STARTED.addQuestDrop(5070,ANCIENT_SCROLL_ID,1)
-STARTED.addQuestDrop(5070,ANCIENT_CLAY_TABLET_ID,1)
+STARTED.addQuestDrop(7133,KARTAS_TRANSLATION,1)
+STARTED.addQuestDrop(7358,ONYX_TALISMAN1,1)
+STARTED.addQuestDrop(7133,ONYX_TALISMAN2,1)
+STARTED.addQuestDrop(5070,ANCIENT_SCROLL,1)
+STARTED.addQuestDrop(5070,ANCIENT_CLAY_TABLET,1)
+
+print "importing quests: 106: Forgotten Truth"
