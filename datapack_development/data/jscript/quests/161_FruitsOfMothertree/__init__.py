feedda0b787379c8472b71f8bfe762e45077fcbe
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-02-28 16:35:51 +0000

    Quests 106,160,161: Cleanup (tnx H1GHL4ND3R)

--- datapack_development/data/jscript/quests/161_FruitsOfMothertree/__init__.py
+++ datapack_development/data/jscript/quests/161_FruitsOfMothertree/__init__.py
@@ -1,13 +1,13 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 161: Fruits Of Mothertree"
+# Made by Mr. Have fun!
+# Version 0.3 by H1GHL4ND3R
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-ANDELLRIAS_LETTER_ID = 1036
-MOTHERTREE_FRUIT_ID = 1037
-ADENA_ID = 57
+ANDELLRIAS_LETTER = 1036
+MOTHERTREE_FRUIT = 1037
+ADENA = 57
 
 class Quest (JQuest) :
 
@@ -15,79 +15,71 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
-      htmltext = "7362-04.htm"
-      st.giveItems(ANDELLRIAS_LETTER_ID,1)
+    if event == "7362-04.htm" :
       st.set("cond","1")
       st.setState(STARTED)
+      st.giveItems(ANDELLRIAS_LETTER,1)
       st.playSound("ItemSound.quest_accept")
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
-     st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7362 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-      if int(st.get("cond"))<15 :
-        if st.getPlayer().getRace().ordinal() != 1 :
-          htmltext = "7362-00.htm"
-        elif st.getPlayer().getLevel() >= 3 :
-          htmltext = "7362-03.htm"
-          return htmltext
-        else:
-          htmltext = "7362-02.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7362-02.htm"
-        st.exitQuest(1)
-   elif npcId == 7362 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-      htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7362 and int(st.get("cond")) :
-      if st.getQuestItemsCount(ANDELLRIAS_LETTER_ID) == 1 and st.getQuestItemsCount(MOTHERTREE_FRUIT_ID) == 0 :
-        htmltext = "7362-05.htm"
-      elif st.getQuestItemsCount(MOTHERTREE_FRUIT_ID) == 1 and int(st.get("onlyone")) == 0 :
-          htmltext = "7362-06.htm"
-          st.giveItems(ADENA_ID,500)
-          st.addExpAndSp(1000,0)
-          st.takeItems(MOTHERTREE_FRUIT_ID,1)
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          st.set("onlyone","1")
-   elif npcId == 7371 and int(st.get("cond"))==1 :
-      if st.getQuestItemsCount(ANDELLRIAS_LETTER_ID) == 1 :
-        if int(st.get("id")) != 161 :
-          st.set("id","161")
-          htmltext = "7371-01.htm"
-          st.giveItems(MOTHERTREE_FRUIT_ID,1)
-          st.takeItems(ANDELLRIAS_LETTER_ID,1)
-      elif st.getQuestItemsCount(MOTHERTREE_FRUIT_ID) == 1 :
-        htmltext = "7371-02.htm"
+     if st.getPlayer().getRace().ordinal() != 1 :
+       htmltext = "7362-00.htm"
+     elif st.getPlayer().getLevel() >= 3 :
+       htmltext = "7362-03.htm"
+       st.set("cond","0")
+     else:
+       htmltext = "7362-02.htm"
+       st.exitQuest(1)
+   elif id == COMPLETED :
+     htmltext = "<html><head><body>This quest have already been completed.</body></html>"
+   else :
+     try :
+       cond = int(st.get("cond"))
+     except :
+       cond = None
+     if cond == 1 :
+       if npcId == 7362 :
+         htmltext = "7362-05.htm"
+       elif npcId == 7371 and st.getQuestItemsCount(ANDELLRIAS_LETTER) :
+         htmltext = "7371-01.htm"
+         st.takeItems(ANDELLRIAS_LETTER,1)
+         st.giveItems(MOTHERTREE_FRUIT,1)
+         st.set("cond", "2")
+     elif cond == 2 :
+       if npcId == 7362 and st.getQuestItemsCount(MOTHERTREE_FRUIT) :
+         htmltext = "7362-06.htm"
+         st.giveItems(ADENA,500)
+         st.addExpAndSp(1000,0)
+         st.takeItems(MOTHERTREE_FRUIT,1)
+         st.unset("cond")
+         st.setState(COMPLETED)
+         st.playSound("ItemSound.quest_finish")
+       elif npcId == 7371 and st.getQuestItemsCount(MOTHERTREE_FRUIT) :
+         htmltext = "7371-02.htm"
    return htmltext
 
 QUEST       = Quest(161,"161_FruitsOfMothertree","Fruits Of Mothertree")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7362)
 
+CREATED.addTalkId(7362)
 STARTING.addTalkId(7362)
+COMPLETED.addTalkId(7362)
 
 STARTED.addTalkId(7362)
 STARTED.addTalkId(7371)
 
+STARTED.addQuestDrop(7371,MOTHERTREE_FRUIT,1)
+STARTED.addQuestDrop(7362,ANDELLRIAS_LETTER,1)
 
-STARTED.addQuestDrop(7371,MOTHERTREE_FRUIT_ID,1)
-STARTED.addQuestDrop(7362,ANDELLRIAS_LETTER_ID,1)
+print "importing quests: 161: Fruits Of Mothertree"
