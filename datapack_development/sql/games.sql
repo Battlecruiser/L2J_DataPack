9ddadc9e1de57e63ff6e010a7f13eb66bb2422e2
mr <mr@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-03-09 10:08:07 +0000

    sql: added games.sql

---
+++ datapack_development/sql/games.sql
@@ -0,0 +1,19 @@
+-- ---------------------------
+-- Table structure for games
+-- ---------------------------
+DROP TABLE IF EXISTS games;
+CREATE TABLE games (
+  id INT NOT NULL default 0,
+  idnr INT NOT NULL default 0,
+  number1 INT NOT NULL default 0,
+  number2 INT NOT NULL default 0,
+  prize  INT NOT NULL default 0,
+  newprize  INT NOT NULL default 0,
+  prize1  INT NOT NULL default 0,
+  prize2  INT NOT NULL default 0,
+  prize3  INT NOT NULL default 0,
+  enddate decimal(20,0) NOT NULL default 0,
+  finished INT NOT NULL default 0,
+  PRIMARY KEY (`id`,`idnr`)
+);
+
