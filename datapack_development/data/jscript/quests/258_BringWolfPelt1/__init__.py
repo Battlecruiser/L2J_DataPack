ae111fb3663e0b6a42d2556f14e23eeca0b87a9d
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-03-11 05:18:12 +0000

    Quests 257, 258, 265: Repeatable + cleanup

--- datapack_development/data/jscript/quests/258_BringWolfPelt1/__init__.py
+++ datapack_development/data/jscript/quests/258_BringWolfPelt1/__init__.py
@@ -1,16 +1,15 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 258: Bring Wolf Pelt1"
+# Made by Mr. Have fun! - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-WOLF_PELT_ID = 702
-LEATHER_TUNIC_ID = 429
-LEATHER_CAP_ID = 42
-CLOTH_CAP_ID = 41
-HOSE_ID = 462
-LEATHER_SHIELD_ID = 18
+WOLF_PELT = 702
+LEATHER_TUNIC = 429
+LEATHER_CAP = 42
+CLOTH_CAP = 41
+HOSE = 462
+LEATHER_SHIELD = 18
 
 class Quest (JQuest) :
 
@@ -26,89 +25,67 @@ class Quest (JQuest) :
       htmltext = "7001-03.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7001 and int(st.get("cond"))==0 :
-      if int(st.get("cond"))<15 :
-        if st.getPlayer().getLevel() >= 3 :
-          htmltext = "7001-02.htm"
-          return htmltext
-        else:
-          htmltext = "7001-01.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7001-01.htm"
-        st.exitQuest(1)
-   elif npcId == 7001 and (int(st.get("cond"))==1) and (st.getQuestItemsCount(WOLF_PELT_ID)>=0) and (st.getQuestItemsCount(WOLF_PELT_ID)<40) :
-      htmltext = "7001-05.htm"
-   elif npcId == 7001 and (int(st.get("cond"))==1) and (st.getQuestItemsCount(WOLF_PELT_ID)>=40) :
-        if int(st.get("id")) != 258 :
-          st.set("id","258")
-          st.takeItems(WOLF_PELT_ID,40)
-          n = st.getRandom(16)
-          if n == 0 :
-            st.giveItems(LEATHER_TUNIC_ID,1)
-            st.playSound("ItemSound.quest_jackpot")
-          elif n < 6 :
-            st.giveItems(LEATHER_CAP_ID,1)
-          elif n < 9 :
-            st.giveItems(CLOTH_CAP_ID,1)
-          elif n < 13 :
-            st.giveItems(HOSE_ID,1)
-          else:
-            st.giveItems(LEATHER_SHIELD_ID,1)
-          htmltext = "7001-06.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
+   if int(st.get("cond"))==0 :
+     if st.getPlayer().getLevel() >= 3 :
+       htmltext = "7001-02.htm"
+     else:
+       htmltext = "7001-01.htm"
+       st.exitQuest(1)
+   else :
+     if st.getQuestItemsCount(WOLF_PELT) < 40 :
+       htmltext = "7001-05.htm"
+     else :
+       st.takeItems(WOLF_PELT,-1)
+       n = st.getRandom(16)
+       if n == 0 :
+         st.giveItems(LEATHER_TUNIC,1)
+         st.playSound("ItemSound.quest_jackpot")
+       elif n < 6 :
+         st.giveItems(LEATHER_CAP,1)
+       elif n < 9 :
+         st.giveItems(CLOTH_CAP,1)
+       elif n < 13 :
+         st.giveItems(HOSE,1)
+       else:
+         st.giveItems(LEATHER_SHIELD,1)
+       htmltext = "7001-06.htm"
+       st.exitQuest(1)
+       st.playSound("ItemSound.quest_finish")
    return htmltext
 
  def onKill (self,npc,st):
-
-   npcId = npc.getNpcId()
-   if npcId == 120 :
-        st.set("id","0")
-        if st.getQuestItemsCount(WOLF_PELT_ID)<40 and int(st.get("cond")) :
-          st.giveItems(WOLF_PELT_ID,1)
-          if st.getQuestItemsCount(WOLF_PELT_ID) == 40 :
-            st.playSound("ItemSound.quest_middle")
-          else:
-            st.playSound("ItemSound.quest_itemget")
-   elif npcId == 442 :
-        st.set("id","0")
-        if st.getQuestItemsCount(WOLF_PELT_ID)<40 and int(st.get("cond")) :
-          st.giveItems(WOLF_PELT_ID,1)
-          if st.getQuestItemsCount(WOLF_PELT_ID) == 40 :
-            st.playSound("ItemSound.quest_middle")
-          else:
-            st.playSound("ItemSound.quest_itemget")
+   count = st.getQuestItemsCount(WOLF_PELT)
+   if count<40 :
+     st.giveItems(WOLF_PELT,1)
+     if count == 39 :
+       st.playSound("ItemSound.quest_middle")
+       st.set("cond","2")
+     else:
+       st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(258,"258_BringWolfPelt1","Bring Wolf Pelt1")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7001)
 
+CREATED.addTalkId(7001)
 STARTING.addTalkId(7001)
-
 STARTED.addTalkId(7001)
+COMPLETED.addTalkId(7001)
 
 STARTED.addKillId(120)
 STARTED.addKillId(442)
 
-STARTED.addQuestDrop(120,WOLF_PELT_ID,1)
-STARTED.addQuestDrop(442,WOLF_PELT_ID,1)
+STARTED.addQuestDrop(120,WOLF_PELT,1)
+
+print "importing quests: 258: Bring Wolf Pelt1"
--- datapack_development/data/jscript/quests/265_ChainsOfSlavery/__init__.py
+++ datapack_development/data/jscript/quests/265_ChainsOfSlavery/__init__.py
@@ -1,12 +1,11 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 265: Chains Of Slavery"
+# Made by Mr. Have fun! - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-IMP_SHACKLES_ID = 1368
-ADENA_ID = 57
+IMP_SHACKLES = 1368
+ADENA = 57
 
 class Quest (JQuest) :
 
@@ -14,90 +13,65 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-        st.set("id","0")
-        st.set("cond","1")
-        st.setState(STARTED)
-        st.playSound("ItemSound.quest_accept")
-        htmltext = "7357-03.htm"
-    elif event == "7357_1" :
-            htmltext = "7357-06.htm"
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-    elif event == "7357_2" :
-            htmltext = "7357-07.htm"
+    if event == "7357-03.htm" :
+      st.set("cond","1")
+      st.setState(STARTED)
+      st.playSound("ItemSound.quest_accept")
+    elif event == "7357-06.htm" :
+      st.exitQuest(1)
+      st.playSound("ItemSound.quest_finish")
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
-   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
-     st.setState(STARTING)
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7357 and int(st.get("cond"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getPlayer().getLevel() >= 5 and st.getPlayer().getRace().ordinal() == 2 :
-            htmltext = "7357-02.htm"
-            return htmltext
-          elif st.getPlayer().getRace().ordinal() != 2 :
-            htmltext = "7357-00.htm"
-            st.exitQuest(1)
-          elif st.getPlayer().getLevel()<5 :
-            htmltext = "7357-01.htm"
-            st.exitQuest(1)
-        else:
+   if int(st.get("cond"))==0 :
+     if st.getPlayer().getRace().ordinal() != 2 :
+       htmltext = "7357-00.htm"
+       st.exitQuest(1)
+     else :
+       if st.getPlayer().getLevel()<5 :
           htmltext = "7357-01.htm"
           st.exitQuest(1)
-   elif npcId == 7357 and int(st.get("cond"))==1 :
-        if st.getQuestItemsCount(IMP_SHACKLES_ID)>0 :
-          if int(st.get("id")) != 265 :
-            st.set("id","265")
-            st.giveItems(ADENA_ID,13*st.getQuestItemsCount(IMP_SHACKLES_ID))
-            st.takeItems(IMP_SHACKLES_ID,st.getQuestItemsCount(IMP_SHACKLES_ID))
-            htmltext = "7357-05.htm"
-        else:
-          htmltext = "7357-04.htm"
+       else:
+          htmltext = "7357-02.htm"
+   else :
+     count=st.getQuestItemsCount(IMP_SHACKLES)
+     if count :
+       st.giveItems(ADENA,13*count)
+       st.takeItems(IMP_SHACKLES,-1)
+       htmltext = "7357-05.htm"
+     else:
+       htmltext = "7357-04.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 4 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        if st.getRandom(10)<5 :
-          st.giveItems(IMP_SHACKLES_ID,1)
-          st.playSound("ItemSound.quest_itemget")
-   elif npcId == 5 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        if st.getRandom(10)<6 :
-          st.giveItems(IMP_SHACKLES_ID,1)
-          st.playSound("ItemSound.quest_itemget")
+   chance=5+(npcId^4)
+   if st.getRandom(10)<chance :
+     st.giveItems(IMP_SHACKLES,1)
+     st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(265,"265_ChainsOfSlavery","Chains Of Slavery")
 CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
+STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
 
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7357)
 
+CREATED.addTalkId(7357)
 STARTING.addTalkId(7357)
-
 STARTED.addTalkId(7357)
+COMPLETED.addTalkId(7357)
 
 STARTED.addKillId(4)
 STARTED.addKillId(5)
 
-STARTED.addQuestDrop(4,IMP_SHACKLES_ID,1)
-STARTED.addQuestDrop(5,IMP_SHACKLES_ID,1)
+STARTED.addQuestDrop(4,IMP_SHACKLES,1)
+
+print "importing quests: 265: Chains Of Slavery"
