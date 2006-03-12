bb1400bfc4d9d453ebbad9d7160b7e28e98d275c
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-03-12 03:47:34 +0000

    Quests 259,326,328,329,331: Cleanup + some repeatable.

--- datapack_development/data/jscript/quests/259_RanchersPlea/__init__.py
+++ datapack_development/data/jscript/quests/259_RanchersPlea/__init__.py
@@ -1,14 +1,13 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 259: Ranchers Plea"
+# Made by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-GIANT_SPIDER_SKIN_ID = 1495
-ADENA_ID = 57
-HEALING_POTION_ID = 1061
-WOODEN_ARROW_ID = 17
+GIANT_SPIDER_SKIN = 1495
+ADENA = 57
+HEALING_POTION = 1061
+WOODEN_ARROW = 17
 
 class Quest (JQuest) :
 
@@ -16,90 +15,56 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
-      st.set("id","0")
+    if event == "7497-03.htm" :
       st.set("cond","1")
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "7497-03.htm"
-    elif event == "7497_1" :
-          htmltext = "7497-06.htm"
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-    elif event == "7497_2" :
-          htmltext = "7497-07.htm"
-    elif event == "7405_1" :
-          htmltext = "7405-03.htm"
-    elif event == "7405_2" :
-          htmltext = "7405-04.htm"
-          st.giveItems(HEALING_POTION_ID,1)
-          st.takeItems(GIANT_SPIDER_SKIN_ID,10)
-    elif event == "7405_3" :
-          htmltext = "7405-05.htm"
-          st.giveItems(WOODEN_ARROW_ID,50)
-          st.takeItems(GIANT_SPIDER_SKIN_ID,10)
-    elif event == "7405_4" :
-          if st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID) >= 10 :
-            htmltext = "7405-06.htm"
-          elif st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID) < 10 :
-            htmltext = "7405-07.htm"
+    elif event == "7497-06.htm" :
+      st.exitQuest(1)
+      st.playSound("ItemSound.quest_finish")
+    elif event == "7405-04.htm" :
+      st.giveItems(HEALING_POTION,1)
+      st.takeItems(GIANT_SPIDER_SKIN,10)
+    elif event == "7405-05.htm" :
+      st.giveItems(WOODEN_ARROW,50)
+      st.takeItems(GIANT_SPIDER_SKIN,10)
+    elif event == "7405-07.htm" :
+      if st.getQuestItemsCount(GIANT_SPIDER_SKIN) >= 10 :
+        htmltext = "7405-06.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
-   if id == CREATED :
-     st.setState(STARTING)
+   if id != STARTED :
      st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7497 and int(st.get("cond"))==0 :
-      if int(st.get("cond")) < 15 :
-        if st.getPlayer().getLevel() >= 15 :
-          htmltext = "7497-02.htm"
-          return htmltext
-        else:
-          htmltext = "7497-01.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7497-01.htm"
-        st.exitQuest(1)
-   elif npcId == 7497 and int(st.get("cond"))==1 and st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID)<1 :
-      htmltext = "7497-04.htm"
-   elif npcId == 7497 and int(st.get("cond"))==1 and st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID)>=1 :
-      if int(st.get("id")) != 259 :
-        st.set("id","259")
-        htmltext = "7497-05.htm"
-      st.giveItems(ADENA_ID,st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID)*25)
-      st.takeItems(GIANT_SPIDER_SKIN_ID,st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID))
-   elif npcId == 7405 and int(st.get("cond"))==1 and st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID)<10 :
-      htmltext = "7405-01.htm"
-   elif npcId == 7405 and int(st.get("cond"))==1 and st.getQuestItemsCount(GIANT_SPIDER_SKIN_ID)>=10 :
-      htmltext = "7405-02.htm"
+   if int(st.get("cond"))==0 :
+     if st.getPlayer().getLevel() >= 15 :
+       htmltext = "7497-02.htm"
+     else:
+       htmltext = "7497-01.htm"
+       st.exitQuest(1)
+   else :
+     count=st.getQuestItemsCount(GIANT_SPIDER_SKIN)
+     if npcId == 7497 :
+       if count == 0 :
+         htmltext = "7497-04.htm"
+       else :
+         htmltext = "7497-05.htm"
+         st.giveItems(ADENA,count*25)
+         st.takeItems(GIANT_SPIDER_SKIN,-1)
+     else :
+       if count < 10 :
+         htmltext = "7405-01.htm"
+       else :
+         htmltext = "7405-02.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 103 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        st.giveItems(GIANT_SPIDER_SKIN_ID,1)
-        st.playSound("ItemSound.quest_itemget")
-   elif npcId == 106 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        st.giveItems(GIANT_SPIDER_SKIN_ID,1)
-        st.playSound("ItemSound.quest_itemget")
-   elif npcId == 108 :
-      st.set("id","0")
-      if int(st.get("cond")) :
-        st.giveItems(GIANT_SPIDER_SKIN_ID,1)
-        st.playSound("ItemSound.quest_itemget")
+   st.giveItems(GIANT_SPIDER_SKIN,1)
+   st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(259,"259_RanchersPlea","Ranchers Plea")
@@ -108,11 +73,12 @@ STARTING     = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
 
-
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7497)
 
+CREATED.addTalkId(7497)
 STARTING.addTalkId(7497)
+COMPLETED.addTalkId(7497)
 
 STARTED.addTalkId(7405)
 STARTED.addTalkId(7497)
@@ -121,6 +87,6 @@ STARTED.addKillId(103)
 STARTED.addKillId(106)
 STARTED.addKillId(108)
 
-STARTED.addQuestDrop(103,GIANT_SPIDER_SKIN_ID,1)
-STARTED.addQuestDrop(106,GIANT_SPIDER_SKIN_ID,1)
-STARTED.addQuestDrop(108,GIANT_SPIDER_SKIN_ID,1)
+STARTED.addQuestDrop(103,GIANT_SPIDER_SKIN,1)
+
+print "importing quests: 259: Ranchers Plea"
--- datapack_development/data/jscript/quests/328_SenseForBusiness/__init__.py
+++ datapack_development/data/jscript/quests/328_SenseForBusiness/__init__.py
@@ -4,10 +4,10 @@ from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-MONSTER_EYE_CARCASS_ID = 1347
-MONSTER_EYE_LENS_ID = 1366
-BASILISK_GIZZARD_ID = 1348
-ADENA_ID = 57
+MONSTER_EYE_CARCASS = 1347
+MONSTER_EYE_LENS = 1366
+BASILISK_GIZZARD = 1348
+ADENA = 57
 
 class Quest (JQuest) :
 
@@ -30,19 +30,22 @@ class Quest (JQuest) :
    id = st.getState()
    if id == CREATED :
      st.set("cond","0")
-   if npcId == 7436 and int(st.get("cond"))==0 :
+   if int(st.get("cond"))==0 :
      if st.getPlayer().getLevel() >= 21 :
         htmltext = "7436-02.htm"
         return htmltext
      else:
         htmltext = "7436-01.htm"
         st.exitQuest(1)
-   elif npcId == 7436 and int(st.get("cond")) :
-     if st.getQuestItemsCount(MONSTER_EYE_CARCASS_ID)+st.getQuestItemsCount(MONSTER_EYE_LENS_ID)+st.getQuestItemsCount(BASILISK_GIZZARD_ID) > 0 :
-        st.giveItems(ADENA_ID,30*st.getQuestItemsCount(MONSTER_EYE_CARCASS_ID)+2000*st.getQuestItemsCount(MONSTER_EYE_LENS_ID)+75*st.getQuestItemsCount(BASILISK_GIZZARD_ID))
-        st.takeItems(MONSTER_EYE_CARCASS_ID,-1)
-        st.takeItems(MONSTER_EYE_LENS_ID,-1)
-        st.takeItems(BASILISK_GIZZARD_ID,-1)
+   else :
+     carcass=st.getQuestItemsCount(MONSTER_EYE_CARCASS)
+     lenses=st.getQuestItemsCount(MONSTER_EYE_LENS)
+     gizzard=st.getQuestItemsCount(BASILISK_GIZZARD)
+     if carcass+lenses+gizzard > 0 :
+        st.giveItems(ADENA,30*carcass+2000*lenses+75*gizzard)
+        st.takeItems(MONSTER_EYE_CARCASS,-1)
+        st.takeItems(MONSTER_EYE_LENS,-1)
+        st.takeItems(BASILISK_GIZZARD,-1)
         htmltext = "7436-05.htm"
      else:
         htmltext = "7436-04.htm"
@@ -55,37 +58,37 @@ class Quest (JQuest) :
      if n<51 :
       st.playSound("ItemSound.quest_itemget")
       if n<50 :
-         st.giveItems(MONSTER_EYE_CARCASS_ID,1)
+         st.giveItems(MONSTER_EYE_CARCASS,1)
       else :
-         st.giveItems(MONSTER_EYE_LENS_ID,1)
+         st.giveItems(MONSTER_EYE_LENS,1)
    elif npcId == 59 :
      if n<55 :
       st.playSound("ItemSound.quest_itemget")
       if n<54 :
-         st.giveItems(MONSTER_EYE_CARCASS_ID,1)
+         st.giveItems(MONSTER_EYE_CARCASS,1)
       else :
-         st.giveItems(MONSTER_EYE_LENS_ID,1)
+         st.giveItems(MONSTER_EYE_LENS,1)
    elif npcId == 67 :
      if n<69 :
       st.playSound("ItemSound.quest_itemget")
       if n<67 :
-         st.giveItems(MONSTER_EYE_CARCASS_ID,1)
+         st.giveItems(MONSTER_EYE_CARCASS,1)
       else :
-         st.giveItems(MONSTER_EYE_LENS_ID,1)
+         st.giveItems(MONSTER_EYE_LENS,1)
    elif npcId == 68 :
      if n<74 :
       st.playSound("ItemSound.quest_itemget")
       if n<72 :
-         st.giveItems(MONSTER_EYE_CARCASS_ID,1)
+         st.giveItems(MONSTER_EYE_CARCASS,1)
       else :
-         st.giveItems(MONSTER_EYE_LENS_ID,1)
+         st.giveItems(MONSTER_EYE_LENS,1)
    elif npcId == 70 :
       if n<50 :
-         st.giveItems(BASILISK_GIZZARD_ID,1)
+         st.giveItems(BASILISK_GIZZARD,1)
          st.playSound("ItemSound.quest_itemget")
    elif npcId == 72 :
       if n<53 :
-         st.giveItems(BASILISK_GIZZARD_ID,1)
+         st.giveItems(BASILISK_GIZZARD,1)
          st.playSound("ItemSound.quest_itemget")
    return
 
@@ -103,8 +106,8 @@ STARTED.addTalkId(7436)
 for i in [ 0,4,12,13,15,17 ] :
     STARTED.addKillId(55+i)
 
-STARTED.addQuestDrop(55,MONSTER_EYE_CARCASS_ID,1)
-STARTED.addQuestDrop(55,MONSTER_EYE_LENS_ID,1)
-STARTED.addQuestDrop(70,BASILISK_GIZZARD_ID,1)
+STARTED.addQuestDrop(55,MONSTER_EYE_CARCASS,1)
+STARTED.addQuestDrop(55,MONSTER_EYE_LENS,1)
+STARTED.addQuestDrop(70,BASILISK_GIZZARD,1)
 
 print "importing quests: 328: Sense For Business"
