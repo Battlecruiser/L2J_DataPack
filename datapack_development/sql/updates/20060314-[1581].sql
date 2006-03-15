851496db847b57b422c37cfc38d99100c9c244f8
DrLecter <DrLecter@users.noreply.github.com>
2006-03-15 13:05:55 +0000

    SQL update: table name fix. (tnx nhur)

--- datapack_development/sql/updates/20060314-[1581].sql
+++ datapack_development/sql/updates/20060314-[1581].sql
@@ -1,2 +1,2 @@
--- increace lhand cloumn field size from decimal 3 to decimal 4
-ALTER TABLE `NPC` MODIFY `lhand` DECIMAL(4); 
+-- increase lhand column field size from decimal 3 to decimal 4
+ALTER TABLE `npc` MODIFY `lhand` DECIMAL(4); 
