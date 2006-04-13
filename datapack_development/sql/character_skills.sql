c5ab3c3f02ad57db0a1a07483d05ffc12718975d
DrLecter <DrLecter@users.noreply.github.com>
2006-04-13 16:50:30 +0000

    Some SQL fixes (tnx kriau, Eragon)

--- datapack_development/sql/character_skills.sql
+++ datapack_development/sql/character_skills.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS character_skills (
   char_obj_id INT NOT NULL default 0,
   skill_id INT NOT NULL default 0,
   skill_level varchar(5) ,
-  skill_name varchar(24),
+  skill_name varchar(26),
   `class_index` INT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY  (char_obj_id,skill_id,`class_index`)
 ) ;
