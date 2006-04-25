79dc35eb58d74c73a8a705e9d10f91a28125b073
NewAge <NewAge@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-04-25 12:55:26 +0000

    - Seven Signs sql update for mr. Lbaldi :) - Small fix on the 2 newly added lvl 78 skills

--- datapack_development/sql/seven_signs_status.sql
+++ datapack_development/sql/seven_signs_status.sql
@@ -1,29 +1,30 @@
-DROP TABLE IF EXISTS `seven_signs_status`;
-CREATE TABLE `seven_signs_status` (
-  `current_cycle` INT NOT NULL default '1',
-  `festival_cycle` INT NOT NULL default '1',
-  `active_period` INT NOT NULL default '1',
-  `date` INT NOT NULL default '1',
-  `previous_winner` INT NOT NULL default '0',
-  `dawn_stone_score` INT NOT NULL default '0',
-  `dawn_festival_score` INT NOT NULL default '0',
-  `dusk_stone_score` INT NOT NULL default '0',
-  `dusk_festival_score` INT NOT NULL default '0',
-  `avarice_owner` INT NOT NULL default '0',
-  `gnosis_owner` INT NOT NULL default '0',
-  `strife_owner` INT NOT NULL default '0',
-  `avarice_dawn_score` INT NOT NULL default '0',
-  `gnosis_dawn_score` INT NOT NULL default '0',
-  `strife_dawn_score` INT NOT NULL default '0',
-  `avarice_dusk_score` INT NOT NULL default '0',
-  `gnosis_dusk_score` INT NOT NULL default '0',
-  `strife_dusk_score` INT NOT NULL default '0',
-  `accumulated_bonus0` INT NOT NULL default '0',
-  `accumulated_bonus1` INT NOT NULL default '0',
-  `accumulated_bonus2` INT NOT NULL default '0',
-  `accumulated_bonus3` INT NOT NULL default '0',
-  `accumulated_bonus4` INT NOT NULL default '0'
+CREATE TABLE IF NOT EXISTS `seven_signs_status` (
+  `id` int(11) NOT NULL default '0',
+  `current_cycle` int(10) NOT NULL default '1',
+  `festival_cycle` int(10) NOT NULL default '1',
+  `active_period` int(10) NOT NULL default '1',
+  `date` int(10) NOT NULL default '1',
+  `previous_winner` int(10) NOT NULL default '0',
+  `dawn_stone_score` int(10) NOT NULL default '0',
+  `dawn_festival_score` int(10) NOT NULL default '0',
+  `dusk_stone_score` int(10) NOT NULL default '0',
+  `dusk_festival_score` int(10) NOT NULL default '0',
+  `avarice_owner` int(10) NOT NULL default '0',
+  `gnosis_owner` int(10) NOT NULL default '0',
+  `strife_owner` int(10) NOT NULL default '0',
+  `avarice_dawn_score` int(10) NOT NULL default '0',
+  `gnosis_dawn_score` int(10) NOT NULL default '0',
+  `strife_dawn_score` int(10) NOT NULL default '0',
+  `avarice_dusk_score` int(10) NOT NULL default '0',
+  `gnosis_dusk_score` int(10) NOT NULL default '0',
+  `strife_dusk_score` int(10) NOT NULL default '0',
+  `accumulated_bonus0` int(10) NOT NULL default '0',
+  `accumulated_bonus1` int(10) NOT NULL default '0',
+  `accumulated_bonus2` int(10) NOT NULL default '0',
+  `accumulated_bonus3` int(10) NOT NULL default '0',
+  `accumulated_bonus4` int(10) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
 );
 
 INSERT INTO `seven_signs_status` VALUES
-(1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+(0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
