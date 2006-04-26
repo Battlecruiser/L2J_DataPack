b1140109097f0d47344a7c14cc5406091e8a0600
Fulminus <Fulminus@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-04-26 17:33:42 +0000

    character_friends update. Just commiting godson's comment from l2j rev 1842

--- datapack_development/sql/character_friends.sql
+++ datapack_development/sql/character_friends.sql
@@ -3,6 +3,7 @@
 -- ---------------------------- 
 CREATE TABLE IF NOT EXISTS `character_friends` ( 
   `char_id` INT NOT NULL default 0,
+  `friend_id` INT(11) NOT NULL DEFAULT 0,
   `friend_name` VARCHAR(35) NOT NULL DEFAULT '',
   PRIMARY KEY  (`char_id`,`friend_name`) 
 );
