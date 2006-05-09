e5222c9b9141d47e89a2bd0dcb1fd64f70c8ed92
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-05-09 23:11:25 +0000

    Quests: more rewards updates and cleanup on 159. (tnx Aikimaniac for fixing spree) New code on 159 could cause problems on players in the middle of the quest.

--- datapack_development/data/jscript/quests/157_RecoverSmuggled/__init__.py
+++ datapack_development/data/jscript/quests/157_RecoverSmuggled/__init__.py
@@ -1,12 +1,11 @@
-# Maked by Mr. Have fun! Version 0.2
-print "importing quests: 157: Recover Smuggled"
+# Maked by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-ADAMANTITE_ORE_ID = 1024
-ADENA_ID = 57
+ADAMANTITE_ORE = 1024
+BUCKLER = 20
 
 class Quest (JQuest) :
 
@@ -15,64 +14,47 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-        st.set("id","0")
-        st.set("cond","1")
-        st.setState(STARTED)
-        st.playSound("ItemSound.quest_accept")
-        htmltext = "7005-05.htm"
+       st.set("cond","1")
+       st.setState(STARTED)
+       st.playSound("ItemSound.quest_accept")
+       htmltext = "7005-05.htm"
     elif event == "157_1" :
-          htmltext = "7005-04.htm"
-          return htmltext
+       htmltext = "7005-04.htm"
     return htmltext
 
-
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
-   if id == CREATED :
-     st.setState(STARTING)
-     st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7005 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-        if int(st.get("cond"))<15 :
-          if st.getPlayer().getLevel() >= 5 :
-            htmltext = "7005-03.htm"
-          else:
-            htmltext = "7005-02.htm"
-            st.exitQuest(1)
-        else:
-          htmltext = "7005-02.htm"
-          st.exitQuest(1)
-   elif npcId == 7005 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-      htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7005 and int(st.get("cond"))!=0 and st.getQuestItemsCount(ADAMANTITE_ORE_ID)<20 :
+   cond = st.getInt("cond")
+   if id == COMPLETED :
+     htmltext = "<html><head><body>This quest have already been completed.</body></html>"
+   elif cond == 0 :
+     if st.getPlayer().getLevel() >= 5 :
+        htmltext = "7005-03.htm"
+     else :
+        htmltext = "7005-02.htm"
+        st.exitQuest(1)
+   elif cond :
+     if st.getQuestItemsCount(ADAMANTITE_ORE)>=20 :
+        st.takeItems(ADAMANTITE_ORE,-1)
+        st.setState(COMPLETED)
+        st.playSound("ItemSound.quest_finish")
+        st.giveItems(BUCKLER,1)
+        htmltext = "7005-07.htm"
+     else :
         htmltext = "7005-06.htm"
-   elif npcId == 7005 and int(st.get("cond"))!=0 and st.getQuestItemsCount(ADAMANTITE_ORE_ID)>=20 and int(st.get("onlyone"))==0 :
-        if int(st.get("id")) != 157 :
-          st.set("id","157")
-          st.takeItems(ADAMANTITE_ORE_ID,st.getQuestItemsCount(ADAMANTITE_ORE_ID))
-          st.set("cond","0")
-          st.setState(COMPLETED)
-          st.playSound("ItemSound.quest_finish")
-          st.set("onlyone","1")
-          st.giveItems(ADENA_ID,1500)
-          htmltext = "7005-07.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 121 :
-        st.set("id","0")
-        if int(st.get("cond")) != 0 and st.getQuestItemsCount(ADAMANTITE_ORE_ID)<20 and st.getRandom(10)<4 :
-            st.giveItems(ADAMANTITE_ORE_ID,1)
-            if st.getQuestItemsCount(ADAMANTITE_ORE_ID) == 20 :
-              st.playSound("ItemSound.quest_middle")
-            else:
-              st.playSound("ItemSound.quest_itemget")
+   if int(st.get("cond"))==1 and st.getQuestItemsCount(ADAMANTITE_ORE)<20 and st.getRandom(10)<4 :
+      st.giveItems(ADAMANTITE_ORE,1)
+      if st.getQuestItemsCount(ADAMANTITE_ORE) == 20 :
+         st.playSound("ItemSound.quest_middle")
+         st.set("cond","2")
+      else:
+         st.playSound("ItemSound.quest_itemget")
    return
 
 QUEST       = Quest(157,"157_RecoverSmuggled","Recover Smuggled")
@@ -85,10 +67,13 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7005)
 
+CREATED.addTalkId(7005)
 STARTING.addTalkId(7005)
-
 STARTED.addTalkId(7005)
+COMPLETED.addTalkId(7005)
 
 STARTED.addKillId(121)
 
 STARTED.addQuestDrop(121,ADAMANTITE_ORE_ID,1)
+
+print "importing quests: 157: Recover Smuggled"
