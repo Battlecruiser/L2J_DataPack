ecff4117b3e49c6ad121201481da558e72fcbecf
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-05-12 22:32:37 +0000

    Quests: yet another reward update + some cleanup on 170. (tnx Aikimaniac)

--- datapack_development/data/jscript/quests/170_DangerousAllure/__init__.py
+++ datapack_development/data/jscript/quests/170_DangerousAllure/__init__.py
@@ -1,12 +1,10 @@
 # Maked by Mr. Have fun! Version 0.2
-print "importing quests: 170: Dangerous Allure"
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-NIGHTMARE_CRYSTAL_ID = 1046
-PIECE_BONE_BREASTPLATE_ID = 25
+NIGHTMARE_CRYSTAL = 1046
 
 class Quest (JQuest) :
 
@@ -15,62 +13,46 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "1" :
-        st.set("id","0")
-        htmltext = "7305-04.htm"
-        st.set("cond","1")
-        st.setState(STARTED)
-        st.playSound("ItemSound.quest_accept")
+       htmltext = "7305-04.htm"
+       st.set("cond","1")
+       st.setState(STARTED)
+       st.playSound("ItemSound.quest_accept")
     return htmltext
 
 
  def onTalk (Self,npc,st):
-
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
-   if id == CREATED :
-     st.setState(STARTING)
-     st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
-   if npcId == 7305 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-      if int(st.get("cond"))<15 :
-        if st.getPlayer().getRace().ordinal() != 2 :
-          htmltext = "7305-00.htm"
-        elif st.getPlayer().getLevel() >= 21 :
-          htmltext = "7305-03.htm"
-          return htmltext
-        else:
-          htmltext = "7305-02.htm"
-          st.exitQuest(1)
-      else:
-        htmltext = "7305-02.htm"
-        st.exitQuest(1)
-   elif npcId == 7305 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
+   cond=st.getInt("cond")
+   if id == COMPLETED :
       htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7305 and int(st.get("cond")) :
-      if st.getQuestItemsCount(NIGHTMARE_CRYSTAL_ID)<1 :
-        htmltext = "7305-05.htm"
-      elif st.getQuestItemsCount(NIGHTMARE_CRYSTAL_ID) >= 1 and int(st.get("onlyone")) == 0 :
-          if int(st.get("id")) != 170 :
-            st.set("id","170")
-            htmltext = "7305-06.htm"
-            st.giveItems(PIECE_BONE_BREASTPLATE_ID,1)
-            st.takeItems(NIGHTMARE_CRYSTAL_ID,st.getQuestItemsCount(NIGHTMARE_CRYSTAL_ID))
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-            st.set("onlyone","1")
+   elif cond == 0 :
+      if st.getPlayer().getRace().ordinal() <> 2 :
+         htmltext = "7305-00.htm"
+         st.exitQuest(1)
+      elif st.getPlayer().getLevel() > 20 :
+         htmltext = "7305-03.htm"
+      else:
+         htmltext = "7305-02.htm"
+         st.exitQuest(1)
+   elif cond :
+      if st.getQuestItemsCount(NIGHTMARE_CRYSTAL) :
+         htmltext = "7305-06.htm"
+         st.giveItems(57,102680)
+         st.takeItems(NIGHTMARE_CRYSTAL,-1)
+         st.setState(COMPLETED)
+         st.playSound("ItemSound.quest_finish")
+      else :
+         htmltext = "7305-05.htm"
    return htmltext
 
  def onKill (self,npc,st):
-
    npcId = npc.getNpcId()
-   if npcId == 5022 :
-        st.set("id","0")
-        if int(st.get("cond")) == 1 and st.getQuestItemsCount(NIGHTMARE_CRYSTAL_ID) == 0 :
-          st.giveItems(NIGHTMARE_CRYSTAL_ID,1)
-          st.playSound("ItemSound.quest_middle")
+   if int(st.get("cond")) == 1 :
+      st.giveItems(NIGHTMARE_CRYSTAL,1)
+      st.playSound("ItemSound.quest_middle")
+      st.set("cond","2")
    return
 
 QUEST       = Quest(170,"170_DangerousAllure","Dangerous Allure")
@@ -83,10 +65,12 @@ COMPLETED   = State('Completed', QUEST)
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(7305)
 
+CREATED.addTalkId(7305)
 STARTING.addTalkId(7305)
-
 STARTED.addTalkId(7305)
+COMPLETED.addTalkId(7305)
 
 STARTED.addKillId(5022)
+STARTED.addQuestDrop(5022,NIGHTMARE_CRYSTAL,1)
 
-STARTED.addQuestDrop(5022,NIGHTMARE_CRYSTAL_ID,1)
+print "importing quests: 170: Dangerous Allure"
