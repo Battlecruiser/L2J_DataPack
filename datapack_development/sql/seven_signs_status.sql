921c51ee9ecca63171eeef76de644423677dc7c8
TempyIncursion <TempyIncursion@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-05-15 20:27:37 +0000

    Added class_index field to character_subclass table, and reintroduced id field for seven_signs_status.

--- datapack_development/sql/seven_signs_status.sql
+++ datapack_development/sql/seven_signs_status.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS `seven_signs_status` (
+  `id` int(3) NOT NULL default '0',
   `current_cycle` int(10) NOT NULL default '1',
   `festival_cycle` int(10) NOT NULL default '1',
   `active_period` int(10) NOT NULL default '1',
@@ -22,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `seven_signs_status` (
   `accumulated_bonus2` int(10) NOT NULL default '0',
   `accumulated_bonus3` int(10) NOT NULL default '0',
   `accumulated_bonus4` int(10) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`, `current_cycle`)
 );
 
 INSERT IGNORE INTO `seven_signs_status` VALUES
