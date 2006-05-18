fc3f24f208848e457395b843d1996bd7280a7085
g0dson <g0dson@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-05-18 10:43:30 +0000

    Needed for java rev [1962]

--- datapack_development/sql/raidboss_spawnlist.sql
+++ datapack_development/sql/raidboss_spawnlist.sql
@@ -10,5 +10,7 @@ CREATE TABLE IF NOT EXISTS raidboss_spawnlist (
   heading INT NOT NULL default 0,
   respawn_delay INT NOT NULL default 0,
   respawn_time BIGINT NOT NULL default 0,
+  currentHp decimal(8,0) default NULL,
+  currentMp decimal(8,0) default NULL,
   PRIMARY KEY  (boss_id, loc_x, loc_y, loc_z)
 );
