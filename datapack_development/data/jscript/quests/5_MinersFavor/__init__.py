722b4e13c10b076845e060e9afc56a620a39eb6a
mtrix <mtrix@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-06-03 23:54:37 +0000

    doh.... [1;9] + 414 by StiviS2 sorry mate.. was my fault..

--- datapack_development/data/jscript/quests/5_MinersFavor/__init__.py
+++ datapack_development/data/jscript/quests/5_MinersFavor/__init__.py
@@ -1,37 +1,50 @@
 # Maked by Mr. Have fun! Version 0.2
-print "importing quests: 5: Miners Favor"
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-BOLTERS_LIST_ID = 1547
-MINING_BOOTS_ID = 1548
-MINERS_PICK_ID = 1549
-BOOMBOOM_POWDER_ID = 1550
-REDSTONE_BEER_ID = 1551
-BOLTERS_SMELLY_SOCKS_ID = 1552
-NECKLACE_ID = 906
+#NPCs 
+BOLTER = 7554 
+SHARI  = 7517 
+GARITA = 7518 
+REED   = 7520 
+BRUNON = 7526 
 
+#ITEMS 
+BOLTERS_LIST         = 1547 
+MINING_BOOTS         = 1548 
+MINERS_PICK          = 1549 
+BOOMBOOM_POWDER      = 1550 
+REDSTONE_BEER        = 1551 
+BOLTERS_SMELLY_SOCKS = 1552 
+ 
+#REWARD 
+NECKLACE = 906 
+ 
 class Quest (JQuest) :
 
  def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
  def onEvent (self,event,st) :
-    htmltext = event
-    if event == "1" :
-          st.set("id","0")
-          htmltext = "7554-03.htm"
-          st.giveItems(BOLTERS_LIST_ID,1)
-          st.giveItems(BOLTERS_SMELLY_SOCKS_ID,1)
-          st.set("cond","1")
-          st.setState(STARTED)
-          st.playSound("ItemSound.quest_accept")
-    elif event == "7526_1" :
-          htmltext = "7526-02.htm"
-          st.takeItems(BOLTERS_SMELLY_SOCKS_ID,1)
-          st.giveItems(MINERS_PICK_ID,1)
-    return htmltext
+   htmltext = event 
+   if event == "7554-03.htm" : 
+     st.giveItems(BOLTERS_LIST,1) 
+     st.giveItems(BOLTERS_SMELLY_SOCKS,1) 
+     st.set("cond","1") 
+     st.set("id","1") 
+     st.setState(STARTED) 
+     st.playSound("ItemSound.quest_accept") 
+   elif event == "7526-02.htm" : 
+     st.takeItems(BOLTERS_SMELLY_SOCKS,-1) 
+     st.giveItems(MINERS_PICK,1) 
+     if st.getQuestItemsCount(BOLTERS_LIST) and (st.getQuestItemsCount(MINING_BOOTS) + st.getQuestItemsCount(MINERS_PICK) + st.getQuestItemsCount(BOOMBOOM_POWDER) + st.getQuestItemsCount(REDSTONE_BEER) >= 4) : 
+       st.set("cond","2") 
+       st.set("id","2") 
+       st.playSound("ItemSound.quest_middle") 
+     else: 
+       st.playSound("ItemSound.quest_itemget") 
+   return htmltext 
 
 
  def onTalk (Self,npc,st):
@@ -39,83 +52,92 @@ class Quest (JQuest) :
    npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
+ 
    if id == CREATED :
      st.setState(STARTING)
      st.set("cond","0")
      st.set("onlyone","0")
      st.set("id","0")
-   if npcId == 7554 and int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-          if int(st.get("cond")) < 15 :
-            if st.getPlayer().getLevel() >= 2 :
-              htmltext = "7554-02.htm"
-              return htmltext
-            else:
-              htmltext = "7554-01.htm"
-              st.exitQuest(1)
-          else:
-            htmltext = "7554-01.htm"
-            st.exitQuest(1)
-   elif npcId == 7554 and int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-      htmltext = "<html><head><body>This quest have already been completed.</body></html>"
-   elif npcId == 7554 and int(st.get("cond"))==1 and st.getQuestItemsCount(BOLTERS_LIST_ID) and (st.getQuestItemsCount(MINING_BOOTS_ID)+st.getQuestItemsCount(MINERS_PICK_ID)+st.getQuestItemsCount(BOOMBOOM_POWDER_ID)+st.getQuestItemsCount(REDSTONE_BEER_ID)<4) :
-          htmltext = "7554-04.htm"
-   elif npcId == 7554 and int(st.get("cond"))==1 and st.getQuestItemsCount(BOLTERS_LIST_ID) and (st.getQuestItemsCount(MINING_BOOTS_ID)+st.getQuestItemsCount(MINERS_PICK_ID)+st.getQuestItemsCount(BOOMBOOM_POWDER_ID)+st.getQuestItemsCount(REDSTONE_BEER_ID)>=4) and int(st.get("onlyone"))==0 :
-          if int(st.get("id")) != 5 :
-            st.set("id","5")
-            htmltext = "7554-06.htm"
-            st.takeItems(MINING_BOOTS_ID,1)
-            st.takeItems(MINERS_PICK_ID,1)
-            st.takeItems(BOOMBOOM_POWDER_ID,1)
-            st.takeItems(REDSTONE_BEER_ID,1)
-            st.takeItems(BOLTERS_LIST_ID,1)
-            st.giveItems(NECKLACE_ID,1)
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-            st.set("onlyone","1")
-   elif npcId == 7517 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(BOOMBOOM_POWDER_ID)==0 :
-          htmltext = "7517-01.htm"
-          st.giveItems(BOOMBOOM_POWDER_ID,1)
-   elif npcId == 7517 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(BOOMBOOM_POWDER_ID) :
-          htmltext = "7517-02.htm"
-   elif npcId == 7518 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(MINING_BOOTS_ID)==0 :
-          htmltext = "7518-01.htm"
-          st.giveItems(MINING_BOOTS_ID,1)
-   elif npcId == 7518 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(MINING_BOOTS_ID) :
-          htmltext = "7518-02.htm"
-   elif npcId == 7520 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(REDSTONE_BEER_ID)==0 :
-          htmltext = "7520-01.htm"
-          st.giveItems(REDSTONE_BEER_ID,1)
-   elif npcId == 7520 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(REDSTONE_BEER_ID) :
-          htmltext = "7520-02.htm"
-   elif npcId == 7526 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(MINERS_PICK_ID)==0 :
-          htmltext = "7526-01.htm"
-   elif npcId == 7526 and int(st.get("cond")) and st.getQuestItemsCount(BOLTERS_LIST_ID) and st.getQuestItemsCount(MINERS_PICK_ID) :
-          htmltext = "7526-03.htm"
+ 
+   cond    = st.getInt("cond") 
+   onlyone = st.getInt("onlyone") 
+ 
+   if npcId == BOLTER and cond == 0 : 
+     if onlyone == 1 : 
+       htmltext = "<html><head><body>This quest have already been completed.</body></html>" 
+     elif st.getPlayer().getLevel() >= 2 and st.getPlayer().getLevel() <= 5 : 
+       htmltext = "7554-02.htm" 
+     else: 
+       htmltext = "7554-01.htm" 
+       st.exitQuest(1) 
+   elif npcId == BOLTER and cond == 1 : 
+     htmltext = "7554-04.htm" 
+   elif npcId == BOLTER and cond == 2 : 
+     htmltext = "7554-06.htm" 
+     st.takeItems(MINING_BOOTS,-1) 
+     st.takeItems(MINERS_PICK,-1) 
+     st.takeItems(BOOMBOOM_POWDER,-1) 
+     st.takeItems(REDSTONE_BEER,-1) 
+     st.takeItems(BOLTERS_LIST,-1) 
+     st.giveItems(NECKLACE,1) 
+     st.set("cond","0") 
+     st.set("onlyone","1") 
+     st.setState(COMPLETED) 
+     st.playSound("ItemSound.quest_finish") 
+   elif npcId == SHARI and cond == 1 and st.getQuestItemsCount(BOLTERS_LIST) : 
+     if st.getQuestItemsCount(BOOMBOOM_POWDER) == 0 : 
+       htmltext = "7517-01.htm" 
+       st.giveItems(BOOMBOOM_POWDER,1) 
+       st.playSound("ItemSound.quest_itemget") 
+     else: 
+       htmltext = "7517-02.htm" 
+   elif npcId == GARITA and cond == 1 and st.getQuestItemsCount(BOLTERS_LIST) : 
+     if st.getQuestItemsCount(MINING_BOOTS) == 0 : 
+       htmltext = "7518-01.htm" 
+       st.giveItems(MINING_BOOTS,1) 
+       st.playSound("ItemSound.quest_itemget") 
+     else: 
+       htmltext = "7518-02.htm" 
+   elif npcId == REED and cond == 1 and st.getQuestItemsCount(BOLTERS_LIST) : 
+     if st.getQuestItemsCount(REDSTONE_BEER) == 0 : 
+       htmltext = "7520-01.htm" 
+       st.giveItems(REDSTONE_BEER,1) 
+       st.playSound("ItemSound.quest_itemget") 
+     else: 
+       htmltext = "7520-02.htm" 
+   elif npcId == BRUNON and cond == 1 and st.getQuestItemsCount(BOLTERS_LIST) : 
+     if st.getQuestItemsCount(MINERS_PICK) == 0 : 
+       htmltext = "7526-01.htm" 
+     else: 
+       htmltext = "7526-03.htm" 
+   if st.getQuestItemsCount(BOLTERS_LIST) and (st.getQuestItemsCount(MINING_BOOTS) + st.getQuestItemsCount(MINERS_PICK) + st.getQuestItemsCount(BOOMBOOM_POWDER) + st.getQuestItemsCount(REDSTONE_BEER) >= 4) : 
+     st.set("cond","2") 
+     st.set("id","2") 
+     st.playSound("ItemSound.quest_middle") 
    return htmltext
 
-QUEST       = Quest(5,"5_MinersFavor","Miners Favor")
-CREATED     = State('Start', QUEST)
-STARTING     = State('Starting', QUEST)
-STARTED     = State('Started', QUEST)
-COMPLETED   = State('Completed', QUEST)
-
+QUEST     = Quest(5,"5_MinersFavor","Miner's Favor") 
+CREATED   = State('Start',     QUEST) 
+STARTING  = State('Starting',  QUEST) 
+STARTED   = State('Started',   QUEST) 
+COMPLETED = State('Completed', QUEST) 
 
 QUEST.setInitialState(CREATED)
-QUEST.addStartNpc(7554)
+QUEST.addStartNpc(BOLTER) 
 
-STARTING.addTalkId(7554)
+STARTING.addTalkId(BOLTER) 
 
-STARTED.addTalkId(7517)
-STARTED.addTalkId(7518)
-STARTED.addTalkId(7520)
-STARTED.addTalkId(7526)
-STARTED.addTalkId(7554)
+STARTED.addTalkId(SHARI) 
+STARTED.addTalkId(GARITA) 
+STARTED.addTalkId(REED) 
+STARTED.addTalkId(BRUNON) 
+STARTED.addTalkId(BOLTER) 
 
+STARTED.addQuestDrop(BOLTER,MINING_BOOTS,1) 
+STARTED.addQuestDrop(BOLTER,MINERS_PICK,1) 
+STARTED.addQuestDrop(BOLTER,BOOMBOOM_POWDER,1) 
+STARTED.addQuestDrop(BOLTER,REDSTONE_BEER,1) 
+STARTED.addQuestDrop(BOLTER,BOLTERS_LIST,1) 
+STARTED.addQuestDrop(BOLTER,BOLTERS_SMELLY_SOCKS,1) 
 
-STARTED.addQuestDrop(7518,MINING_BOOTS_ID,1)
-STARTED.addQuestDrop(7526,MINERS_PICK_ID,1)
-STARTED.addQuestDrop(7517,BOOMBOOM_POWDER_ID,1)
-STARTED.addQuestDrop(7520,REDSTONE_BEER_ID,1)
-STARTED.addQuestDrop(7554,BOLTERS_LIST_ID,1)
-STARTED.addQuestDrop(7554,BOLTERS_SMELLY_SOCKS_ID,1)
+print "importing quests: 5: Miner's Favor" 
--- datapack_development/data/jscript/quests/9_IntoTheCityOfHumans/__init__.py
+++ datapack_development/data/jscript/quests/9_IntoTheCityOfHumans/__init__.py
@@ -5,71 +5,80 @@ from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-CENTURION_PETUKAI_ID = 7583
-SEER_TANAPI_ID = 7571
-GATEKEEPER_TAMIL_ID = 7576
-ADENA_ID = 57
-SCROLL_OF_ESCAPE_GIRAN_ID = 7559
-MARK_OF_TRAVELER_ID = 7570
+#NPCs 
+PETUKAI = 7583 
+TANAPI  = 7571 
+TAMIL   = 7576 
 
+#REWARDS 
+ADENA = 57 
+SCROLL_OF_ESCAPE_GIRAN = 7559 
+MARK_OF_TRAVELER = 7570 
+ 
 class Quest (JQuest) :
 
-    def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr) 
 
-    def onEvent (self,event,st) :
-        htmltext = event
-        if event == "1" :
-            st.set("cond","1")
-            st.setState(STARTED)
-            st.playSound("ItemSound.quest_accept")
-            htmltext = "7583-03.htm"
-        elif event == "2" :
-            st.set("cond","2")
-            htmltext = "7571-02.htm"
-        elif event == "3" :
-            st.giveItems(SCROLL_OF_ESCAPE_GIRAN_ID,1)
-            st.giveItems(MARK_OF_TRAVELER_ID, 1)
-            htmltext = "7576-02.htm"
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-        return htmltext
+ def onEvent (self,event,st) : 
+   htmltext = event 
+   if event == "7583-03.htm" : 
+     st.set("cond","1") 
+     st.set("id","1") 
+     st.setState(STARTED) 
+     st.playSound("ItemSound.quest_accept") 
+   elif event == "7571-02.htm" : 
+     st.set("cond","2") 
+     st.set("id","2") 
+     st.playSound("ItemSound.quest_middle") 
+   elif event == "7576-02.htm" : 
+     st.giveItems(SCROLL_OF_ESCAPE_GIRAN,1) 
+     st.giveItems(MARK_OF_TRAVELER, 1) 
+     st.set("cond","0") 
+     st.setState(COMPLETED) 
+     st.playSound("ItemSound.quest_finish") 
+   return htmltext 
 
-    def onTalk (Self,npc,st):
-        npcId = npc.getNpcId()
-        htmltext = "<html><head><body>I have nothing to say you</body></html>"
-        id = st.getState()
-        if id == CREATED :
-            st.set("cond","0")
-            if st.getPlayer().getRace().ordinal() == 3 :
-                htmltext = "7583-02.htm"
-            else :
-                htmltext = "7583-01.htm"
-                st.exitQuest(1)
-        elif npc == 7583 and id == COMPLETED :
-            htmltext = "<html><head><body>I can't supply you with another Giran Scroll of Escape. Sorry traveller.</body></html>"
-        elif npc == 7583 and int(st.get("cond"))==1 :
-            htmltext = "7583-04.htm"
-        elif npcId == 7571 and int(st.get("cond")) :
-            htmltext = "7571-01.htm"
-        elif npcId == 7576 and int(st.get("cond"))==2 :
-            htmltext = "7576-01.htm"
+ def onTalk (Self,npc,st): 
+   htmltext = "<html><head><body>I have nothing to say you</body></html>" 
+   npcId = npc.getNpcId() 
+   cond  = st.getInt("cond") 
+   id    = st.getState() 
 
-        return htmltext
+   if id == CREATED : 
+     st.set("cond","0") 
+     if st.getPlayer().getRace().ordinal() == 3 : 
+       if st.getPlayer().getLevel() >= 3 and st.getPlayer().getLevel() <= 10: 
+         htmltext = "7583-02.htm" 
+       else: 
+         htmltext = "<html><head><body>Quest for characters level between 3 and 10.</body></html>" 
+         st.exitQuest(1) 
+     else : 
+       htmltext = "7583-01.htm" 
+       st.exitQuest(1) 
+   elif npc == PETUKAI and id == COMPLETED : 
+     htmltext = "<html><head><body>I can't supply you with another Giran Scroll of Escape. Sorry traveller.</body></html>" 
+   elif npc == PETUKAI and cond == 1 : 
+     htmltext = "7583-04.htm" 
+   elif npcId == TANAPI and cond : 
+     htmltext = "7571-01.htm" 
+   elif npcId == TAMIL and cond == 2 : 
+     htmltext = "7576-01.htm" 
 
-QUEST       = Quest(9,"9_IntoTheCityOfHumans","Into The City Of Humans")
-CREATED     = State('Start', QUEST)
-STARTED     = State('Started', QUEST)
-COMPLETED   = State('Completed', QUEST)
+   return htmltext 
 
+QUEST     = Quest(9,"9_IntoTheCityOfHumans","Into the City of Humans") 
+CREATED   = State('Start',     QUEST) 
+STARTED   = State('Started',   QUEST) 
+COMPLETED = State('Completed', QUEST) 
+ 
 QUEST.setInitialState(CREATED)
-QUEST.addStartNpc(7583)
+QUEST.addStartNpc(PETUKAI) 
 
-CREATED.addTalkId(7583)
-COMPLETED.addTalkId(7576)
+CREATED.addTalkId(PETUKAI) 
+COMPLETED.addTalkId(PETUKAI) 
 
-STARTED.addTalkId(7583)
-STARTED.addTalkId(7571)
-STARTED.addTalkId(7576)
+STARTED.addTalkId(PETUKAI) 
+STARTED.addTalkId(TANAPI) 
+STARTED.addTalkId(TAMIL) 
 
-print "importing quests: 9: Into the City of Humans"
+print "importing quests: 9: Into the City of Humans" 
