c83d4b9776877b06f7a22d0c3303e8c50f7393de
mtrix <mtrix@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-06-03 23:54:37 +0000

    doh.... [1;9] + 414 by StiviS2 sorry mate.. was my fault..

--- datapack_development/data/jscript/quests/7_ATripBegins/__init__.py
+++ datapack_development/data/jscript/quests/7_ATripBegins/__init__.py
@@ -5,85 +5,95 @@ from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-GATEKEEPER_MIRABEL_ID = 7146
-TRADER_ARIEL_ID = 7148
-ARIELS_RECOMMENDATION_ID = 7572
-HIERARCH_ASTERIOS_ID = 7154
-ADENA_ID = 57
-SCROLL_OF_ESCAPE_GIRAN_ID = 7559
-MARK_OF_TRAVELER_ID = 7570
+#NPCs 
+MIRABEL  = 7146 
+ARIEL    = 7148 
+ASTERIOS = 7154 
 
+#ITEM 
+ARIELS_RECOMMENDATION = 7572 
+ 
+#REWARDS 
+ADENA                  = 57 
+SCROLL_OF_ESCAPE_GIRAN = 7559 
+MARK_OF_TRAVELER       = 7570 
+ 
 class Quest (JQuest) :
 
-    def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr) 
 
-    def onEvent (self,event,st) :
-        htmltext = event
-        if event == "1" :
-            st.set("cond","1")
-            st.setState(STARTED)
-            st.playSound("ItemSound.quest_accept")
-            htmltext = "7146-03.htm"
-        elif event == "2" :
-            st.set("cond","2")
-            st.giveItems(ARIELS_RECOMMENDATION_ID,1)
-            htmltext = "7148-02.htm"
-        elif event == "3" :
-            st.set("cond","3")
-            st.takeItems(ARIELS_RECOMMENDATION_ID,1)
-            htmltext = "7154-02.htm"
-        elif event == "4" :
-            st.giveItems(SCROLL_OF_ESCAPE_GIRAN_ID,1)
-            st.giveItems(MARK_OF_TRAVELER_ID, 1)
-            htmltext = "7146-06.htm"
-            st.set("cond","0")
-            st.setState(COMPLETED)
-            st.playSound("ItemSound.quest_finish")
-        return htmltext
+ def onEvent (self,event,st) : 
+   htmltext = event 
+   if event == "7146-03.htm" : 
+     st.set("cond","1") 
+     st.setState(STARTED) 
+     st.playSound("ItemSound.quest_accept") 
+   elif event == "7148-02.htm" : 
+     st.giveItems(ARIELS_RECOMMENDATION,1) 
+     st.set("cond","2") 
+     st.set("id","2") 
+     st.playSound("ItemSound.quest_middle") 
+   elif event == "7154-02.htm" : 
+     st.takeItems(ARIELS_RECOMMENDATION,-1) 
+     st.set("cond","3") 
+     st.set("id","3") 
+     st.playSound("ItemSound.quest_middle") 
+   elif event == "7146-06.htm" : 
+     st.giveItems(SCROLL_OF_ESCAPE_GIRAN,1) 
+     st.giveItems(MARK_OF_TRAVELER, 1) 
+     st.set("cond","0") 
+     st.setState(COMPLETED) 
+     st.playSound("ItemSound.quest_finish") 
+   return htmltext 
 
-    def onTalk (Self,npc,st):
-        npcId = npc.getNpcId()
-        htmltext = "<html><head><body>I have nothing to say you</body></html>"
-        id = st.getState()
-        if id == CREATED :
-            st.set("cond","0")
-            if st.getPlayer().getRace().ordinal() == 1 :
-                htmltext = "7146-02.htm"
-            else :
-                htmltext = "7146-01.htm"
-                st.exitQuest(1)
-        elif npcId == 7146 and id == COMPLETED :
-            htmltext = "<html><head><body>I can't supply you with another Giran Scroll of Escape. Sorry traveller.</body></html>"
-        elif npcId == 7146 and int(st.get("cond"))==1 :
-            htmltext = "7146-04.htm"
-        elif npcId == 7148 and int(st.get("cond")) :
-            if st.getQuestItemsCount(ARIELS_RECOMMENDATION_ID) == 0 :
-                htmltext = "7148-01.htm"
-            elif st.getQuestItemsCount(ARIELS_RECOMMENDATION_ID) > 0 :
-                htmltext = "7148-03.htm"
-        elif npcId == 7154 and int(st.get("cond"))==2 :
-            if st.getQuestItemsCount(ARIELS_RECOMMENDATION_ID) > 0 :
-                htmltext = "7154-01.htm"
-        elif npcId == 7146 and int(st.get("cond"))==3 :
-            htmltext = "7146-05.htm"
+ def onTalk (Self,npc,st): 
+   htmltext = "<html><head><body>I have nothing to say you</body></html>" 
+   npcId = npc.getNpcId() 
+   cond  = st.getInt("cond") 
+   id    = st.getState() 
 
-        return htmltext
+   if id == CREATED : 
+     st.set("cond","0") 
+     if st.getPlayer().getRace().ordinal() == 1 : 
+       if st.getPlayer().getLevel() >= 3 and st.getPlayer().getLevel() <= 10 : 
+         htmltext = "7146-02.htm" 
+       else : 
+         htmltext = "<html><head><body>Quest for characters level between 3 and 10.</body></html>" 
+         st.exitQuest(1) 
+     else : 
+       htmltext = "7146-01.htm" 
+       st.exitQuest(1) 
+   elif npcId == MIRABEL and id == COMPLETED : 
+     htmltext = "<html><head><body>I can't supply you with another Giran Scroll of Escape. Sorry traveller.</body></html>" 
+   elif npcId == MIRABEL and cond == 1 : 
+     htmltext = "7146-04.htm" 
+   elif npcId == ARIEL and cond : 
+     if st.getQuestItemsCount(ARIELS_RECOMMENDATION) == 0 : 
+       htmltext = "7148-01.htm" 
+     else : 
+       htmltext = "7148-03.htm" 
+   elif npcId == ASTERIOS and cond == 2 and st.getQuestItemsCount(ARIELS_RECOMMENDATION) > 0 : 
+     htmltext = "7154-01.htm" 
+   elif npcId == MIRABEL and cond == 3 : 
+     htmltext = "7146-05.htm" 
 
-QUEST       = Quest(7,"7_ATripBegins","A Trip Begins")
-CREATED     = State('Start', QUEST)
-STARTED     = State('Started', QUEST)
-COMPLETED   = State('Completed', QUEST)
+   return htmltext 
 
+QUEST     = Quest(7,"7_ATripBegins","A Trip Begins") 
+CREATED   = State('Start',     QUEST) 
+STARTED   = State('Started',   QUEST) 
+COMPLETED = State('Completed', QUEST) 
+ 
 QUEST.setInitialState(CREATED)
-QUEST.addStartNpc(7146)
+QUEST.addStartNpc(MIRABEL) 
 
-CREATED.addTalkId(7146)
-COMPLETED.addTalkId(7146)
+CREATED.addTalkId(MIRABEL) 
+COMPLETED.addTalkId(MIRABEL) 
 
-STARTED.addTalkId(7146)
-STARTED.addTalkId(7148)
-STARTED.addTalkId(7154)
+STARTED.addTalkId(MIRABEL) 
+STARTED.addTalkId(ARIEL) 
+STARTED.addTalkId(ASTERIOS) 
 
-STARTED.addQuestDrop(7148,ARIELS_RECOMMENDATION_ID,1)
+STARTED.addQuestDrop(MIRABEL,ARIELS_RECOMMENDATION,1) 
 
-print "importing quests: 7: A Trip Begins"
+print "importing quests: 7: A Trip Begins" 
