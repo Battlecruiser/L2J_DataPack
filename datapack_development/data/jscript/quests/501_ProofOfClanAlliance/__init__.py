22fa3532f20f0c6075c0a0a6c981f2eee9f0192b
mtrix <mtrix@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-07-03 10:16:27 +0000

    fix for previous fix.. sorry about that

--- datapack_development/data/jscript/quests/501_ProofOfClanAlliance/__init__.py
+++ datapack_development/data/jscript/quests/501_ProofOfClanAlliance/__init__.py
@@ -194,6 +194,7 @@ class Quest (JQuest) :
               st.getQuestItemsCount(HERB_OF_VANOR) and \
               st.getQuestItemsCount(HERB_OF_OEL_MAHUM) and \
               st.getQuestItemsCount(BLOOD_OF_EVA) and \
+              st.getQuestItemsCount(ANTIDOTE_RECIPE) and \
               int(st.get("chest_game"))== 3 :
              st.takeItems(ANTIDOTE_RECIPE,1)
              st.takeItems(HERB_OF_HARIT,1)
