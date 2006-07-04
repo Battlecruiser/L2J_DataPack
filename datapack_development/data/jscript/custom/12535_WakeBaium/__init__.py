49362851f325f8f02589392b7df630981817c5f4
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-07-04 19:55:45 +0000

    Custom 12535: Not tested. Hopefully fixes Baium exploit.

--- datapack_development/data/jscript/custom/12535_WakeBaium/__init__.py
+++ datapack_development/data/jscript/custom/12535_WakeBaium/__init__.py
@@ -17,13 +17,21 @@ class Quest (JQuest):
   def onTalk (self,npc,st):
     npcId = npc.getNpcId()
     if npcId == 12535 :
-      st.getPcSpawn().addSpawn(12372)
-      npc.reduceCurrentHp(9999999, npc)
-      st.exitQuest(1)
+      if st.getInt("ok"):
+        if not npc.isBusy():
+           npc.setBusy(True)
+           npc.setBusyMessage("Attending another player's request")
+           st.getPcSpawn().addSpawn(12372)
+           npc.reduceCurrentHp(9999999, npc)
+        st.exitQuest(1)
+      else:
+        st.exitQuest(1)
+        return "Conditions are not right to wake up Baium"
     elif npcId == 12571 :
       if st.getQuestItemsCount(4295) > 0 :   # bloody fabric
         st.takeItems(4295,1)
         st.getPlayer().teleToLocation(113100,14500,10077)
+        st.set("ok","1")
       else :
         return '<html><head><body>Angelic Vortex:<br>You do not have enough items</body></html>'      
     return
