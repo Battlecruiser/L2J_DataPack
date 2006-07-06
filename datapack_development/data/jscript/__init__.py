bc88d38a6d9bed033b414836130afba7740d283b
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-07-06 14:43:32 +0000

    Multisell: Luxury Shop. (tnx Snumsmumrik)

--- datapack_development/data/jscript/__init__.py
+++ datapack_development/data/jscript/__init__.py
@@ -1,13 +1,11 @@
 __all__ = [
 'quests',
-'luxury_shop',
 'blacksmith',
 'custom',
 'teleports',
 'village_master'
 ]
 import quests
-import luxury_shop
 import blacksmith
 import custom
 import teleports
