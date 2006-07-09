039fbdf89c22e103895b02e783ce23b06707e5a5
Lbaldi <Lbaldi@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-07-09 22:59:06 +0000

    little fix

--- datapack_development/sql/helper_buff_list.sql
+++ datapack_development/sql/helper_buff_list.sql
@@ -18,18 +18,18 @@ PRIMARY KEY  (`id`)
 #----------------------------
 
 insert  into helper_buff_list values 
-(0, 4322, 'WindWalk', 1, 8, 24, 'false'),
-(1, 4323, 'Shield', 1, 11, 23, 'false'), 
+(0, 4322, 'WindWalk', 1, 8, 25, 'false'),
+(1, 4323, 'Shield', 1, 11, 24, 'false'), 
 (2, 4338, 'Life Cubic', 1, 16, 19, 'false'), 
-(3, 4324, 'Bless the Body', 1, 12, 22, 'false'), 
-(4, 4325, 'Vampiric Rage', 1, 13, 21, 'false'), 
-(5, 4326, 'Regeneration', 1, 14, 20, 'false'), 
-(6, 4327, 'Haste', 1, 15, 19, 'false'), 
-(7, 4322, 'WindWalk', 1, 8, 24, 'true'),
-(8, 4323, 'Shield', 1, 11, 23, 'true'), 
+(3, 4324, 'Bless the Body', 1, 12, 23, 'false'), 
+(4, 4325, 'Vampiric Rage', 1, 13, 22, 'false'), 
+(5, 4326, 'Regeneration', 1, 14, 21, 'false'), 
+(6, 4327, 'Haste', 1, 15, 20, 'false'), 
+(7, 4322, 'WindWalk', 1, 8, 25, 'true'),
+(8, 4323, 'Shield', 1, 11, 24, 'true'), 
 (9, 4338, 'Life Cubic', 1, 16, 19, 'true'), 
-(10, 4328, 'Bless the Soul', 1, 12, 22, 'true'), 
-(11, 4329, 'Acumen', 1, 13, 21, 'true'), 
-(12, 4330, 'Concentration', 1, 14, 20, 'true'), 
-(13, 4331, 'Empower', 1, 15, 19, 'true');
+(10, 4328, 'Bless the Soul', 1, 12, 23, 'true'), 
+(11, 4329, 'Acumen', 1, 13, 22, 'true'), 
+(12, 4330, 'Concentration', 1, 14, 21, 'true'), 
+(13, 4331, 'Empower', 1, 15, 20, 'true');
 
