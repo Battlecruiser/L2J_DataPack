961c16a50abbe33a56eb8b27691cc477d442d592
Fulminus <Fulminus@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-07-19 02:50:12 +0000

    Quest 234 (new version).  Requires server revision [2225]

--- datapack_development/data/jscript/quests/234_FatesWhisper/__init__.py
+++ datapack_development/data/jscript/quests/234_FatesWhisper/__init__.py
@@ -17,15 +17,11 @@ RED_PIPETTE_KNIFE = 4673
 STAR_OF_DESTINY = 5011
 CRYSTAL_B = 1460
 
-#Leorin, Cliff, Ferris, Zenkin, Kaspar, Kernon's Chest, Golkonda's Chest, Hallate's Chest
-NPC=[8002,7182,7847,7178,7833,8028,8029,8030]
+#Leorin, Cliff, Ferris, Zenkin, Kaspar, Kernon's Chest, Golkonda's Chest, Hallate's Chest, Cabrio's "Coffer of the Dead"
+NPC=[8002,7182,7847,7178,7833,8028,8029,8030,8027]
 
 #mobId=[cond,spawn,dropId,rate]
 DROPLIST={
-10035: [1,0,REIRIAS_SOUL_ORB,100], 				#Shilen’s Messenger Cabrio
-10054: [2,8028,0,0], 							#Demon Kernon
-10126: [2,8029,0,0], 							#Golkonda, the Longhorn General
-10220: [2,8030,0,0], 							#Death Lord Hallate
 12372: [7,0,RED_PIPETTE_KNIFE,10]				#Baium...NOTE: rate may need adjustment
 }
 
@@ -52,7 +48,11 @@ class Quest (JQuest) :
 		if event == "1" :
 			st.set("cond","1")
 			st.setState(STARTED)
-			htmltext = "8002-04.htm"
+			htmltext = "8002-03.htm"
+		# talking with cliff...last dialog to get the Infernium Varnish
+		elif event == "7182_01" :
+			htmltext = "7182-01c.htm"
+			st.giveItems(INFERNIUM_VARNISH,1)
 		# open Kernon's Chest
 		elif event == "8028_01" :
 			htmltext = "8028-02.htm"
@@ -65,6 +65,15 @@ class Quest (JQuest) :
 		elif event == "8030_01" :
 			htmltext = "8030-02.htm"
 			st.giveItems(HALLATES_INFERNIUM_SCEPTER,1)
+		# dialog with Zenkin
+		elif event == "7178_01" :
+			st.set("cond","6")
+			htmltext = "7178-01a.htm"
+		# dialog with Kaspar - go to baium
+		elif event == "7833_01a" :
+			htmltext = "7833-01b.htm"
+			st.giveItems(PIPETTE_KNIFE,1)
+			st.set("cond","7")
 		## FINAL ITEM EXCHANGE SECTION
 		elif event == "showWeapons" :
 			htmltext = ""
@@ -82,25 +91,22 @@ class Quest (JQuest) :
 			htmltext = "<html><body>Maestro Reorin:<br>Please choose which weapon you wish to give me to melt, from the below list:<br>" + htmltext + "</body></html>"
 		elif event.startswith("selectBGrade_"):
 			# store the object id of the selected weapon for the trade
-			st.set("giveWeapObjectId", event.replace("selectBGrade_", ""))
-			# now show the A Grade weapon list
-			htmltext = "8002-AGradeList.htm"
-		elif event.startswith("selectAGrade_"):
-			aGradeItemId = int(event.replace("selectAGrade_", ""))
-			bGradeObjId = st.getInt("giveWeapObjectId")
+			bGradeObjId = int(event.replace("selectBGrade_", ""))
 			# to avoid exploitation, check if the stored objectId still corresponds to an existing item
 			# and if that item is still not equipped
 			Item = st.getPlayer().getInventory().getItemByObjectId(bGradeObjId)
 			if Item and not Item.isEquipped() :
-				htmltext = "8002-12.htm"
-				st.getPlayer().destroyItem("quest_234", bGradeObjId, 1, st.getPlayer(), 0)
-				st.giveItems(aGradeItemId,1)
-				st.giveItems(STAR_OF_DESTINY,1)
-				st.setState(COMPLETED)
-				st.unset("cond")
-				st.unset("giveWeapObjectId")
+				# now show the A Grade weapon list
+				htmltext = "8002-AGradeList.htm"
 			else :
 				htmltext = "<html><body>Maestro Reorin:<br>Are you trying to cheat me?!  What happenned to the weapon you were about to give me for the neutralization of Infernum's evil aura?</body></html>"
+		elif event.startswith("selectAGrade_"):
+			aGradeItemId = int(event.replace("selectAGrade_", ""))
+			htmltext = "8002-12.htm"
+			st.giveItems(aGradeItemId,1)
+			st.giveItems(STAR_OF_DESTINY,1)
+			st.setState(COMPLETED)
+			st.unset("cond")
 		return htmltext
 
 	def onTalk(self,npc,st):
@@ -123,14 +129,14 @@ class Quest (JQuest) :
 			cond =st.getInt("cond")
 			if npcId == NPC[0] :
 				if cond == 1 and not st.getQuestItemsCount(REIRIAS_SOUL_ORB) :  # waiting for the orb
-					htmltext = "8002-04.htm"
+					htmltext = "8002-04b.htm"
 				elif cond == 1 :	#got the orb!  Go to the next step (infernium scepter pieces)
 					st.takeItems(REIRIAS_SOUL_ORB,1)
 					htmltext = "8002-05.htm"
 					st.set("cond","2")
 				# waiting for infernium scepter pieces
 				elif cond == 2 and (st.getQuestItemsCount(KERMONS_INFERNIUM_SCEPTER)+st.getQuestItemsCount(GOLCONDAS_INFERNIUM_SCEPTER)+st.getQuestItemsCount(HALLATES_INFERNIUM_SCEPTER) < 3) :
-					htmltext = "8002-05a.htm"
+					htmltext = "8002-05c.htm"
 				elif cond == 2 :	#got the infernium scepter pieces!  Go to the next step (infernium Varnish)
 					st.takeItems(KERMONS_INFERNIUM_SCEPTER,1)
 					st.takeItems(GOLCONDAS_INFERNIUM_SCEPTER,1)
@@ -139,20 +145,20 @@ class Quest (JQuest) :
 					st.set("cond","3")
 				# waiting for infernium varnish
 				elif cond == 3 and not st.getQuestItemsCount(INFERNIUM_VARNISH) :
-					htmltext = "8002-06a.htm"
+					htmltext = "8002-06b.htm"
 				elif cond == 3 :	#got the infernium varnish!  Go to the next step (Leorin's Hammer)
 					st.takeItems(INFERNIUM_VARNISH,1)
 					htmltext = "8002-07.htm"
 					st.set("cond","4")
 				# waiting for Leorin's Hammer
 				elif cond == 4 and not st.getQuestItemsCount(REORINS_HAMMER) :
-					htmltext = "8002-07a.htm"
+					htmltext = "8002-07b.htm"
 				elif cond == 4 :	# got Leorin's Hammer!  Go to the next step (Leorin's Mold)
 					st.takeItems(REORINS_HAMMER,1)
 					htmltext = "8002-08.htm"
 					st.set("cond","5")
 				elif cond < 8 :	 	# waiting for Leorin's Mold
-					htmltext = "8002-08a.htm"
+					htmltext = "8002-08b.htm"
 				elif cond == 8 :	# got Leorin's Mold!  Go to the next step (B Crystals)
 					st.takeItems(REORINS_MOLD,1)
 					htmltext = "8002-09.htm"
@@ -171,7 +177,6 @@ class Quest (JQuest) :
 			# came to take the varnish
 			elif npcId == NPC[1] and cond==3 and not st.getQuestItemsCount(INFERNIUM_VARNISH) :
 				htmltext = "7182-01.htm"
-				st.giveItems(INFERNIUM_VARNISH,1)
 			# you already got the varnish...why are you back?
 			elif npcId == NPC[1] and (cond>=3 or st.getQuestItemsCount(INFERNIUM_VARNISH)) :
 				htmltext = "7182-02.htm"
@@ -187,7 +192,6 @@ class Quest (JQuest) :
 			# go to take mold
 			elif npcId == NPC[3] and cond==5 :
 				htmltext = "7178-01.htm"	# go to Magister Kaspar
-				st.set("cond","6")
 			# I already told you I don't have it!  
 			elif npcId == NPC[3] and cond>5 :
 				htmltext = "7178-02.htm"	# go to Magister Kaspar
@@ -196,8 +200,6 @@ class Quest (JQuest) :
 				# first visit: You have neither plain nor blooded knife.
 				if cond==6 :
 					htmltext = "7833-01.htm"	# go to Magister Hanellin,etc. Get Baium's Blood with the pipette
-					st.giveItems(PIPETTE_KNIFE,1)
-					st.set("cond","7")
 				# revisit before getting the blood: remind "go get the blood"
 				if cond==7 and st.getQuestItemsCount(PIPETTE_KNIFE) and not st.getQuestItemsCount(RED_PIPETTE_KNIFE) :
 					htmltext = "7833-02.htm"	# go to Magister Hanellin,etc. Get Baium's Blood with the pipette
@@ -211,6 +213,11 @@ class Quest (JQuest) :
 				if st.getInt("cond")>7 :
 					htmltext = "7833-04.htm"	# Have you given the mold to Leorin, yet?
 			## CHESTS FROM RAIDBOSSES
+			elif cond==1 :
+				if npcId ==NPC[8] and st.getQuestItemsCount(REIRIAS_SOUL_ORB)==0 :
+					htmltext = "8027-01.htm"
+					st.giveItems(REIRIAS_SOUL_ORB,1)
+					st.playSound("Itemsound.quest_itemget")
 			elif cond==2 :
 				# Kernon's Chest
 				if npcId == NPC[5] and st.getQuestItemsCount(KERMONS_INFERNIUM_SCEPTER)==0 :
