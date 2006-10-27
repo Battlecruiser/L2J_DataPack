a86d5aaf39eccd0ed32118093b00ac5f6158b34f
Scafu <Scafu@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-10-27 23:22:09 +0000

    HTML/SQL: Missing merchants

--- datapack_development/sql/merchant_shopids.sql
+++ datapack_development/sql/merchant_shopids.sql
@@ -174,6 +174,8 @@ INSERT INTO merchant_shopids VALUES (159,'31414');
 INSERT INTO merchant_shopids VALUES (160,'31418');
 INSERT INTO merchant_shopids VALUES (161,'31415');
 INSERT INTO merchant_shopids VALUES (162,'31423');
+INSERT INTO merchant_shopids VALUES (163,'31433');
+INSERT INTO merchant_shopids VALUES (164,'31440');
 INSERT INTO merchant_shopids VALUES (5800,'30890');
 INSERT INTO merchant_shopids VALUES (5801,'30890');
 INSERT INTO merchant_shopids VALUES (5802,'30891');
