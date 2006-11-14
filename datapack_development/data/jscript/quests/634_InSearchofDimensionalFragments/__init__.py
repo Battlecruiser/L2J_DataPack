59338b2477a285723e9901baf382f76dda050a31
DrLecter <DrLecter@users.noreply.github.com>
2006-11-14 23:11:55 +0000

    Quest 634 should now match retail

--- datapack_development/data/jscript/quests/634_InSearchofDimensionalFragments/__init__.py
+++ datapack_development/data/jscript/quests/634_InSearchofDimensionalFragments/__init__.py
@@ -11,16 +11,13 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
+    if event == "2a.htm" :
       st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
-      htmltext = "4.htm"
       st.set("cond","1")
-    elif event == "2" :
-          htmltext = "5.htm"
-          st.playSound("ItemSound.quest_finish")
-          st.exitQuest(1)
-          
+    elif event == "5.htm" :
+      st.playSound("ItemSound.quest_finish")
+      st.exitQuest(1)
     return htmltext
 
  def onTalk (Self,npc,st):
@@ -28,22 +25,20 @@ class Quest (JQuest) :
    htmltext = "<html><head><body>I have nothing to say to you</body></html>"
    id = st.getState()
    if id == CREATED :
-       st.set("cond","0")
-       htmltext = "4.htm"
+      if st.getPlayer().getLevel() < 20 :
+         st.exitQuest(1)
+         htmltext="1.htm"
+      else:
+         htmltext="2.htm"
    elif id == STARTED :
-       st.set("cond","1")
-       htmltext = "4.htm"
-       
+      htmltext = "4.htm"
    return htmltext
 
  def onKill (self,npc,st):
-    npcId = npc.getNpcId()
-    if npcId in range(21208,21256) :
-         if st.getRandom(10)<6 :
-           st.giveItems(DIMENSION_FRAGMENT_ID,1)
-           st.playSound("ItemSound.quest_itemget")
-
-    return
+   if st.getRandom(10)<6 :
+      st.giveItems(DIMENSION_FRAGMENT_ID,1)
+      st.playSound("ItemSound.quest_itemget")
+   return
 
 QUEST       = Quest(634, "634_InSearchofDimensionalFragments", "In Search of Dimensional Fragments",True)
 CREATED     = State('Start', QUEST)
