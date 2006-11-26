60fb07044abb8579b9b5f780383dab25946be760
HeXa <HeXa@5288da4f-0efc-0310-bd40-8d490a2659fa>
2006-11-26 12:34:44 +0000

    SQL changes for saving reuse time, requires core update: 263 [thanks Yesod]

--- datapack_development/sql/character_skills_save.sql
+++ datapack_development/sql/character_skills_save.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS character_skills_save (
   skill_level INT NOT NULL default 0,
   effect_count INT NOT NULL default 0,
   effect_cur_time INT NOT NULL default 0,
+  reuse_delay INT(8) NOT NULL DEFAULT 0,
+  restore_type INT(1) NOT NULL DEFAULT 0,
   `class_index` INT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY  (char_obj_id,skill_id,`class_index`)
 ) ;
