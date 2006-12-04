28f3c652af17b126c7017a0620cf43f172085e01
DrLecter <DrLecter@users.noreply.github.com>
2006-12-04 22:47:36 +0000

    Quest 171: missing text added and onKill rewritten. Former drop method were producing odd results.

--- datapack_development/data/jscript/quests/171_ActsOfEvil/__init__.py
+++ datapack_development/data/jscript/quests/171_ActsOfEvil/__init__.py
@@ -9,14 +9,17 @@ WEAPON_TRADE_CONTRACT,ATTACK_DIRECTIVES,CERTIFICATE,CARGOBOX,OL_MAHUM_HEAD = ran
 
 ADENA = 57
 
-CHANCE = 400000
-CHANCE2 = 300000
-CHANCE21 = 300000
-CHANCE22 = 300000
-CHANCE23 = 300000
-CHANCE24 = 200000
-CHANCE25 = 200000
-CHANCE3 = 150000
+CHANCE1  = 50
+CHANCE11 = 10
+CHANCE2  = 100
+CHANCE21 = 20
+CHANCE22 = 20
+CHANCE23 = 20
+CHANCE24 = 10
+CHANCE25 = 10
+CHANCE3  = 50
+
+ALVAH,ARODIN,TYRA,ROLENTO,NETI,BURAI=30381,30207,30420,30437,30425,30617
 
 class Quest (JQuest) :
 
@@ -24,23 +27,25 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
      htmltext = event
-     if event == "30381-02.htm" :
+     cond = st.getInt("cond")
+     if st.getState() <> COMPLETED :
+       if event == "30381-02.htm" and cond == 0 :
          st.setState(STARTED)
          st.set("cond","1")
          st.playSound("ItemSound.quest_accept")
-     elif event == "30207-02.htm" :
+       elif event == "30207-02.htm" and cond == 1 :
          st.set("cond","2")
-     elif event == "30381-04.htm" :
+       elif event == "30381-04.htm" and cond == 4:
          st.set("cond","5")
-     elif event == "30381-07.htm" :
+       elif event == "30381-07.htm" and cond == 6:
          st.set("cond","7")
          st.takeItems(WEAPON_TRADE_CONTRACT,-1)
          st.playSound("ItemSound.quest_middle")
-     elif event == "30437-03.htm" :
+       elif event == "30437-03.htm" and cond == 8:
          st.giveItems(CARGOBOX,1)
          st.giveItems(CERTIFICATE,1)
          st.set("cond","9")
-     elif event == "30617-04.htm" :
+       elif event == "30617-04.htm" and cond == 9:
          st.takeItems(CERTIFICATE,-1)
          st.takeItems(ATTACK_DIRECTIVES,-1)
          st.takeItems(CARGOBOX,-1)
@@ -53,7 +58,7 @@ class Quest (JQuest) :
      id = st.getState()
      level = st.getPlayer().getLevel()
      cond = st.getInt("cond")
-     if npcId==30381 :
+     if npcId==ALVAH :
          if id == CREATED :
             if level > 26:
                htmltext = "30381-01.htm"
@@ -88,7 +93,7 @@ class Quest (JQuest) :
              st.giveItems(ADENA,90000)
              st.playSound("ItemSound.quest_finish")
              st.setState(COMPLETED)
-     elif npcId==30207 :
+     elif npcId==ARODIN :
          if cond==1 :
              htmltext = "30207-01.htm"
          elif cond==2 :
@@ -102,90 +107,118 @@ class Quest (JQuest) :
                  htmltext = "30207-01a.htm"
          elif cond==4 :
              htmltext = "30207-03a.htm"
-     elif npcId==30420 :
-         if cond==2 and st.getQuestItemsCount(BLADE_MOLD)>=20 :
-            st.takeItems(BLADE_MOLD,-1)
-            st.giveItems(TYRAS_BILL,1)
-            htmltext = "30420-01.htm"
-            st.set("cond","3")
-            #mmh.. we're lacking a some text here in case you dont have 20 molds?
-         if cond==3 :
+     elif npcId==TYRA :
+         if cond==2 :
+            if st.getQuestItemsCount(BLADE_MOLD)>=20 :
+               st.takeItems(BLADE_MOLD,-1)
+               st.giveItems(TYRAS_BILL,1)
+               htmltext = "30420-01.htm"
+               st.set("cond","3")
+            else :
+               htmltext = "30420-01b.htm"
+         elif cond==3 :
              htmltext = "30420-01a.htm"
-     elif npcId==30425 :
+         elif cond > 3 :
+             htmltext = "30420-02.htm"
+     elif npcId==NETI :
          if cond==7 :
              htmltext = "30425-01.htm"
              st.set("cond","8")
          elif cond==8 :
              htmltext = "30425-02.htm"
-     elif npcId==30437 :
+     elif npcId==ROLENTO :
          if cond==8 :
              htmltext = "30437-01.htm"
          elif cond==9 :
              htmltext = "30437-03a.htm"
-     elif npcId==30617 :
+     elif npcId==BURAI :
          if cond==9 and st.getQuestItemsCount(CERTIFICATE) and st.getQuestItemsCount(CARGOBOX) and st.getQuestItemsCount(ATTACK_DIRECTIVES) :
              htmltext = "30617-01.htm"
          if cond==10 :
              if st.getQuestItemsCount(OL_MAHUM_HEAD)>=30 :
-                 htmltext = "30617-05.htm"
-                 st.giveItems(ADENA,8000)
-                 st.takeItems(OL_MAHUM_HEAD,-1)
-                 st.set("cond","11")
-                 st.playSound("ItemSound.quest_itemget")
+                htmltext = "30617-05.htm"
+                st.giveItems(ADENA,8000)
+                st.takeItems(OL_MAHUM_HEAD,-1)
+                st.set("cond","11")
+                st.playSound("ItemSound.quest_itemget")
              else :
-                 htmltext = "30617-04a.htm"
+                htmltext = "30617-04a.htm"
      return htmltext
 
  def onKill (self,npc,st):
      npcId = npc.getNpcId()
      cond = st.getInt("cond")
-     if cond==2 :
-         if npcId in range(20496,20500) and st.getQuestItemsCount(BLADE_MOLD)<20 :
-             st.getPcSpawn().addSpawn(27190)
-         elif npcId==27190 :
-             st.dropQuestItems(BLADE_MOLD,1,20,CHANCE,1)
-     elif cond==5 and npcId==20062 :
-         st.dropQuestItems(RANGERS_REPORT1,1,1,CHANCE2,0)
-         st.dropQuestItems(RANGERS_REPORT2,1,1,CHANCE21,0)
-         st.dropQuestItems(RANGERS_REPORT3,1,1,CHANCE22,0)
-         st.dropQuestItems(RANGERS_REPORT4,1,1,CHANCE23,0)
+     chance=st.getRandom(100)
+     if cond==2 and npcId in range(20496,20500) :
+        blades = st.getQuestItemsCount(BLADE_MOLD)
+        if chance < CHANCE11 :
+           st.getPcSpawn().addSpawn(27190)
+        if chance < CHANCE1 and blades < 20 :
+           st.giveItems(BLADE_MOLD,1)
+           if count == 19 :
+              st.playSound("ItemSound.quest_middle")
+           else :
+              st.playSound("ItemSound.quest_itemget")
+     elif cond==5 and npcId == 20062 :
+         if not st.getQuestItemsCount(RANGERS_REPORT1) and chance < CHANCE2:
+            st.giveItems(RANGERS_REPORT1)
+            st.playSound("ItemSound.quest_itemget")
+         elif not st.getQuestItemsCount(RANGERS_REPORT2) and chance < CHANCE21:
+            st.giveItems(RANGERS_REPORT2)
+            st.playSound("ItemSound.quest_itemget")
+         elif not st.getQuestItemsCount(RANGERS_REPORT3) and chance < CHANCE22:
+            st.giveItems(RANGERS_REPORT3)
+            st.playSound("ItemSound.quest_itemget")
+         elif not st.getQuestItemsCount(RANGERS_REPORT4) and chance < CHANCE23:
+            st.giveItems(RANGERS_REPORT4)
+            st.playSound("ItemSound.quest_itemget")
      elif cond==6 and npcId==20438 :
-         st.dropQuestItems(WEAPON_TRADE_CONTRACT,1,1,CHANCE24,0)
-         st.dropQuestItems(ATTACK_DIRECTIVES,1,1,CHANCE25,0)
+         if not st.getQuestItemsCount(WEAPON_TRADE_CONTRACT) and chance < CHANCE24:
+            st.giveItems(WEAPON_TRADE_CONTRACT)
+            st.playSound("ItemSound.quest_itemget")
+         elif not st.getQuestItemsCount(ATTACK_DIRECTIVES) and chance < CHANCE25:
+            st.giveItems(ATTACK_DIRECTIVES)
+            st.playSound("ItemSound.quest_itemget")
      elif cond==10 and npcId==20066 :
-         st.dropQuestItems(OL_MAHUM_HEAD,1,30,CHANCE3,1)
+         heads=st.getQuestItemsCount(OL_MAHUM_HEAD)
+         if heads < 30 and chance < CHANCE3 :
+            st.giveItems(OL_MAHUM_HEAD,1)
+            if count == 29 :
+               st.playSound("ItemSound.quest_middle")
+            else :
+               st.playSound("ItemSound.quest_itemget")
      return
 
 QUEST       = Quest(171,"171_ActsOfEvil","Acts of Evil")
 CREATED     = State('Start', QUEST)
 STARTED     = State('Started', QUEST)
-COMPLETED     = State('Completed', QUEST)
+COMPLETED   = State('Completed', QUEST)
 
 QUEST.setInitialState(CREATED)
-QUEST.addStartNpc(30381)
+QUEST.addStartNpc(ALVAH)
 
-CREATED.addTalkId(30381)
-STARTED.addTalkId(30381)
-COMPLETED.addTalkId(30381)
+CREATED.addTalkId(ALVAH)
+STARTED.addTalkId(ALVAH)
+COMPLETED.addTalkId(ALVAH)
 
-STARTED.addTalkId(30207)
-STARTED.addTalkId(30420)
-STARTED.addTalkId(30425)
-STARTED.addTalkId(30437)
-STARTED.addTalkId(30617)
+STARTED.addTalkId(ARODIN)
+STARTED.addTalkId(TYRA)
+STARTED.addTalkId(ROLENTO)
+STARTED.addTalkId(NETI)
+STARTED.addTalkId(BURAI)
 
-STARTED.addQuestDrop(30381,RANGERS_REPORT1,1)
-STARTED.addQuestDrop(30381,RANGERS_REPORT2,1)
-STARTED.addQuestDrop(30381,RANGERS_REPORT3,1)
-STARTED.addQuestDrop(30381,RANGERS_REPORT4,1)
-STARTED.addQuestDrop(30381,OL_MAHUM_HEAD,1)
-STARTED.addQuestDrop(30381,CARGOBOX,1)
-STARTED.addQuestDrop(30381,TYRAS_BILL,1)
-STARTED.addQuestDrop(30381,CERTIFICATE,1)
-STARTED.addQuestDrop(30381,BLADE_MOLD,1)
-STARTED.addQuestDrop(30381,WEAPON_TRADE_CONTRACT,1)
+STARTED.addQuestDrop(ALVAH,RANGERS_REPORT1,1)
+STARTED.addQuestDrop(ALVAH,RANGERS_REPORT2,1)
+STARTED.addQuestDrop(ALVAH,RANGERS_REPORT3,1)
+STARTED.addQuestDrop(ALVAH,RANGERS_REPORT4,1)
+STARTED.addQuestDrop(ALVAH,OL_MAHUM_HEAD,1)
+STARTED.addQuestDrop(ALVAH,CARGOBOX,1)
+STARTED.addQuestDrop(ALVAH,TYRAS_BILL,1)
+STARTED.addQuestDrop(ALVAH,CERTIFICATE,1)
+STARTED.addQuestDrop(ALVAH,BLADE_MOLD,1)
+STARTED.addQuestDrop(ALVAH,WEAPON_TRADE_CONTRACT,1)
 
-for i in range(20494,20500)+[20062,20066,27190,20438] :
+for i in range(20494,20500)+[20062,20066,20438] :
     STARTED.addKillId(i)
 
 print "importing quests: 171: Acts of Evil"
