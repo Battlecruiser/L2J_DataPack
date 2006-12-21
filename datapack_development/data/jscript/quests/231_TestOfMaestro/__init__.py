65cc13134639e0316045aa20f3e366e98b27e80a
DrLecter <DrLecter@users.noreply.github.com>
2006-12-21 12:09:50 +0000

    Quest 231: Some code improvement. tnx sombre for remind me of that quest.

--- datapack_development/data/jscript/quests/231_TestOfMaestro/__init__.py
+++ datapack_development/data/jscript/quests/231_TestOfMaestro/__init__.py
@@ -1,4 +1,4 @@
-# Maked by Mr. Have fun! Version 0.2
+# Made by Mr. - Version 0.3 by DrLecter
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
@@ -6,36 +6,36 @@ from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 
 #item definition
-RECOMMENDATION_OF_BALANKI_ID = 2864
-RECOMMENDATION_OF_FILAUR_ID = 2865
-RECOMMENDATION_OF_ARIN_ID = 2866
-MARK_OF_MAESTRO_ID = 2867
-LETTER_OF_SOLDER_DETACHMENT_ID = 2868
-PAINT_OF_KAMURU_ID = 2869
-NECKLACE_OF_KAMURU_ID = 2870
-PAINT_OF_TELEPORT_DEVICE_ID = 2871
-TELEPORT_DEVICE_ID = 2872
-ARCHITECTURE_OF_KRUMA_ID = 2873
-REPORT_OF_KRUMA_ID = 2874
-INGREDIENTS_OF_ANTIDOTE_ID = 2875
-WEIRD_BEES_NEEDLE_ID = 2876
-MARSH_SPIDERS_WEB_ID = 2877
-BLOOD_OF_LEECH_ID = 2878
-BROKEN_TELEPORT_DEVICE_ID = 2916
+RECOMMENDATION_OF_BALANKI = 2864
+RECOMMENDATION_OF_FILAUR = 2865
+RECOMMENDATION_OF_ARIN = 2866
+MARK_OF_MAESTRO = 2867
+LETTER_OF_SOLDER_DETACHMENT = 2868
+PAINT_OF_KAMURU = 2869
+NECKLACE_OF_KAMURU = 2870
+PAINT_OF_TELEPORT_DEVICE = 2871
+TELEPORT_DEVICE = 2872
+ARCHITECTURE_OF_KRUMA = 2873
+REPORT_OF_KRUMA = 2874
+INGREDIENTS_OF_ANTIDOTE = 2875
+WEIRD_BEES_NEEDLE = 2876
+MARSH_SPIDERS_WEB = 2877
+BLOOD_OF_LEECH = 2878
+BROKEN_TELEPORT_DEVICE = 2916
 
-#This Handels all Mob Drop Data.  npcId:[condition,maxcount,item]
+#This handle all Mob-Drop Data.  npcId:[condition,maxcount,item]
 DROPLIST={
-20225:[13,10,BLOOD_OF_LEECH_ID],
-20229:[13,10,WEIRD_BEES_NEEDLE_ID],
-20233:[13,10,MARSH_SPIDERS_WEB_ID],
-27133:[4,1,NECKLACE_OF_KAMURU_ID]
+20225:[13,10,BLOOD_OF_LEECH],
+20229:[13,10,WEIRD_BEES_NEEDLE],
+20233:[13,10,MARSH_SPIDERS_WEB],
+27133:[4,1,NECKLACE_OF_KAMURU]
 }
 
-#if you have all three recommendation it sets a end cond
+#if you have all three recommendation, it sets final cond
 def recommendationCount(st):
   count=0
-  for device in [RECOMMENDATION_OF_ARIN_ID,RECOMMENDATION_OF_FILAUR_ID,RECOMMENDATION_OF_BALANKI_ID]:
-    count+=st.getQuestItemsCount(device)
+  for item in [RECOMMENDATION_OF_ARIN,RECOMMENDATION_OF_FILAUR,RECOMMENDATION_OF_BALANKI]:
+    count+=st.getQuestItemsCount(item)
   if count == 3:
     st.set("cond","17")
 
@@ -46,53 +46,56 @@ class Quest (JQuest) :
 
  def onEvent (self,event,st) :
     htmltext = event
-    if event == "1" :
+    cond = st.getInt("cond")
+    id=st.getState()
+    if id <> COMPLETED :
+       if event == "1" and cond == 0 :
           htmltext = "30531-04.htm"
           st.setState(STARTED)
           st.playSound("ItemSound.quest_accept")
           st.set("cond","1")
-    elif event == "30533_1" :
+       elif event == "30533_1" and cond == 1:
           htmltext = "30533-02.htm"
           st.set("cond","2")
-    elif event == "30671_1" :
+       elif event == "30671_1" and cond == 2:
           htmltext = "30671-02.htm"
-          st.giveItems(PAINT_OF_KAMURU_ID,1)
+          st.giveItems(PAINT_OF_KAMURU,1)
           st.set("cond","3")
-    elif event == "30556_1" :
+       elif event == "30556_1" :
           htmltext = "30556-02.htm"
-    elif event == "30556_2" :
+       elif event == "30556_2" :
           htmltext = "30556-03.htm"
-    elif event == "30556_3" :
+       elif event == "30556_3" and cond == 8 :
           htmltext = "30556-05.htm"
-          st.takeItems(PAINT_OF_TELEPORT_DEVICE_ID,1)
+          st.takeItems(PAINT_OF_TELEPORT_DEVICE,1)
           st.getPlayer().teleToLocation(140352,-194133,-2028);
-          st.giveItems(BROKEN_TELEPORT_DEVICE_ID,1)
+          st.giveItems(BROKEN_TELEPORT_DEVICE,1)
           st.set("cond","9")
-    elif event == "30556_4" :
+       elif event == "30556_4" :
           htmltext = "30556-04.htm"
-    elif event == "30673_1" :
+       elif event == "30673_1" and cond == 14 :
           htmltext = "30673-04.htm"
-          st.giveItems(REPORT_OF_KRUMA_ID,1)
-          st.takeItems(WEIRD_BEES_NEEDLE_ID,st.getQuestItemsCount(WEIRD_BEES_NEEDLE_ID))
-          st.takeItems(MARSH_SPIDERS_WEB_ID,st.getQuestItemsCount(MARSH_SPIDERS_WEB_ID))
-          st.takeItems(BLOOD_OF_LEECH_ID,st.getQuestItemsCount(BLOOD_OF_LEECH_ID))
-          st.takeItems(INGREDIENTS_OF_ANTIDOTE_ID,st.getQuestItemsCount(INGREDIENTS_OF_ANTIDOTE_ID))
+          st.giveItems(REPORT_OF_KRUMA,1)
+          st.takeItems(WEIRD_BEES_NEEDLE,-1)
+          st.takeItems(MARSH_SPIDERS_WEB,-1)
+          st.takeItems(BLOOD_OF_LEECH,-1)
+          st.takeItems(INGREDIENTS_OF_ANTIDOTE,-1)
           st.set("cond","15")
     return htmltext
 
 
  def onTalk (self,npc,st):
-   npcId = npc.getNpcId()
+   npcId = npc.getNpcId()
    htmltext = "<html><head><body>I have nothing to say you</body></html>"
    id = st.getState()
    if id == CREATED :
      st.setState(STARTING)
-     st.set("cond","0")
-     st.set("onlyone","0")
-     st.set("id","0")
+   cond = st.getInt("cond")
    if npcId == 30531:
-     if int(st.get("cond"))==0 and int(st.get("onlyone"))==0 :
-        if st.getPlayer().getClassId().getId() == 0x38 and st.getPlayer().getLevel() >38 :
+     if id == COMPLETED :
+       htmltext = "<html><head><body>This quest has already been completed.</body></html>"
+     elif cond==0 :
+        if st.getPlayer().getClassId().getId() == 0x38 and st.getPlayer().getLevel() > 38 :
           htmltext = "30531-03.htm"
         elif st.getPlayer().getClassId().getId() == 0x38 :
           htmltext = "30531-01.htm"
@@ -100,105 +103,102 @@ class Quest (JQuest) :
         else:
           htmltext = "30531-02.htm"
           st.exitQuest(1)
-     elif int(st.get("cond"))==0 and int(st.get("onlyone"))==1 :
-       htmltext = "<html><head><body>This quest has already been completed.</body></html>"
-     elif int(st.get("cond"))>0 and int(st.get("cond"))<17 :
+     elif cond>0 and cond<17 :
        htmltext = "30531-05.htm"
-     elif int(st.get("cond"))==17 :
+     elif cond==17 :
        st.addExpAndSp(154499,37500)
        htmltext = "30531-06.htm"
-       st.giveItems(MARK_OF_MAESTRO_ID,1)
-       st.takeItems(RECOMMENDATION_OF_BALANKI_ID,1)
-       st.takeItems(RECOMMENDATION_OF_FILAUR_ID,1)
-       st.takeItems(RECOMMENDATION_OF_ARIN_ID,1)
-       st.set("cond","0")
+       st.giveItems(MARK_OF_MAESTRO,1)
+       st.takeItems(RECOMMENDATION_OF_BALANKI,1)
+       st.takeItems(RECOMMENDATION_OF_FILAUR,1)
+       st.takeItems(RECOMMENDATION_OF_ARIN,1)
+       st.unset("cond")
        st.setState(COMPLETED)
        st.playSound("ItemSound.quest_finish")
-       st.set("onlyone","1")
    elif npcId == 30533:
-     if (int(st.get("cond"))==1 or int(st.get("cond"))==11 or int(st.get("cond"))==16) and st.getQuestItemsCount(RECOMMENDATION_OF_BALANKI_ID)==0:
+     if cond in [1,11,16] and not st.getQuestItemsCount(RECOMMENDATION_OF_BALANKI):
        htmltext = "30533-01.htm"
-     elif int(st.get("cond"))==2:
+     elif cond==2:
        htmltext = "30533-03.htm"
-     elif int(st.get("cond"))==6 :
+     elif cond==6 :
        htmltext = "30533-04.htm"
-       st.giveItems(RECOMMENDATION_OF_BALANKI_ID,1)
-       st.takeItems(LETTER_OF_SOLDER_DETACHMENT_ID,1)
+       st.giveItems(RECOMMENDATION_OF_BALANKI,1)
+       st.takeItems(LETTER_OF_SOLDER_DETACHMENT,1)
        st.set("cond","7")
        recommendationCount(st)
-     elif int(st.get("cond"))==7 or int(st.get("cond"))==17 :
+     elif cond in [7,17] :
        htmltext = "30533-05.htm"
    elif npcId == 30671:
-     if int(st.get("cond"))==2 :
+     if cond==2 :
        htmltext = "30671-01.htm"
-     elif int(st.get("cond"))==3:
+     elif cond==3:
        htmltext = "30671-03.htm"
-     elif int(st.get("cond"))==5 :
+     elif cond==5 :
        htmltext = "30671-04.htm"
-       st.giveItems(LETTER_OF_SOLDER_DETACHMENT_ID,1)
-       st.takeItems(NECKLACE_OF_KAMURU_ID,1)
-       st.takeItems(PAINT_OF_KAMURU_ID,1)
+       st.giveItems(LETTER_OF_SOLDER_DETACHMENT,1)
+       st.takeItems(NECKLACE_OF_KAMURU,1)
+       st.takeItems(PAINT_OF_KAMURU,1)
        st.set("cond","6")
-     elif int(st.get("cond"))==6 :
+     elif cond==6 :
        htmltext = "30671-05.htm"
-   elif npcId == 30672 and int(st.get("cond"))==3 :
+   elif npcId == 30672 and cond==3 :
        htmltext = "30672-01.htm"
-   elif npcId == 30675 and int(st.get("cond"))==3:
+   elif npcId == 30675 and cond==3:
        st.set("cond","4")
        htmltext="30675-01.htm"
    elif npcId == 30536:
-     if (int(st.get("cond"))==1 or int(st.get("cond"))==7 or int(st.get("cond"))==16) and st.getQuestItemsCount(RECOMMENDATION_OF_ARIN_ID)==0:
+     if cond in [1,7,16] and not st.getQuestItemsCount(RECOMMENDATION_OF_ARIN) :
        htmltext = "30536-01.htm"
-       st.giveItems(PAINT_OF_TELEPORT_DEVICE_ID,1)
+       st.giveItems(PAINT_OF_TELEPORT_DEVICE,1)
        st.set("cond","8")
-     elif int(st.get("cond"))==8 :
+     elif cond==8 :
        htmltext = "30536-02.htm"
-     elif int(st.get("cond"))==10:
+     elif cond==10:
        htmltext = "30536-03.htm"
-       st.giveItems(RECOMMENDATION_OF_ARIN_ID,1)
-       st.takeItems(TELEPORT_DEVICE_ID,5)
+       st.giveItems(RECOMMENDATION_OF_ARIN,1)
+       st.takeItems(TELEPORT_DEVICE,5)
        st.set("cond","11")
        recommendationCount(st)
-     elif int(st.get("cond"))==11 or int(st.get("cond"))==17:
+     elif cond in [11,17]:
        htmltext = "30536-04.htm"
    elif npcId==30556:
-     if int(st.get("cond"))==8:
+     if cond==8:
        htmltext = "30556-01.htm"
-     elif int(st.get("cond"))==9:
+     elif cond==9:
        htmltext = "30556-06.htm"
-       st.giveItems(TELEPORT_DEVICE_ID,5)
-       st.takeItems(BROKEN_TELEPORT_DEVICE_ID,1)
+       st.giveItems(TELEPORT_DEVICE,5)
+       st.takeItems(BROKEN_TELEPORT_DEVICE,1)
        st.set("cond","10")
-     elif int(st.get("cond"))==10 :
+     elif cond==10 :
        htmltext = "30556-07.htm"
    elif npcId==30535:  
-     if (int(st.get("cond"))==1 or int(st.get("cond"))==7 or int(st.get("cond"))==11) and st.getQuestItemsCount(RECOMMENDATION_OF_FILAUR_ID)==0 :
+     if cond in [1,7,11] and not st.getQuestItemsCount(RECOMMENDATION_OF_FILAUR) :
        htmltext = "30535-01.htm"
-       st.giveItems(ARCHITECTURE_OF_KRUMA_ID,1)
+       st.giveItems(ARCHITECTURE_OF_KRUMA,1)
        st.set("cond","12")
-     elif int(st.get("cond"))==12 :
+     elif cond==12 :
        htmltext = "30535-02.htm"
-     elif int(st.get("cond"))==15 :
+     elif cond==15 :
        htmltext = "30535-03.htm"
-       st.giveItems(RECOMMENDATION_OF_FILAUR_ID,1)
-       st.takeItems(REPORT_OF_KRUMA_ID,1)
+       st.giveItems(RECOMMENDATION_OF_FILAUR,1)
+       st.takeItems(REPORT_OF_KRUMA,1)
        st.set("cond","16")
        recommendationCount(st)
-     elif int(st.get("cond"))>15:
+     elif cond>15:
        htmltext = "30535-04.htm"
    elif npcId == 30673:
-     if int(st.get("cond"))==12 :
+     if cond==12 :
        htmltext = "30673-01.htm"
-       st.giveItems(INGREDIENTS_OF_ANTIDOTE_ID,1)
-       st.takeItems(ARCHITECTURE_OF_KRUMA_ID,1)
+       st.giveItems(INGREDIENTS_OF_ANTIDOTE,1)
+       st.takeItems(ARCHITECTURE_OF_KRUMA,1)
        st.set("cond","13")
-     elif int(st.get("cond"))==14 :
-       htmltext = "30673-03.htm"
-     elif int(st.get("cond"))==15:
-       htmltext = "30673-05.htm"
-     elif int(st.get("cond"))==13 :
+     elif cond==13 :
        htmltext = "30673-02.htm"
-   elif npcId==30532 and int(st.get("cond")) :
+     elif cond==14 :
+       htmltext = "30673-03.htm"
+     elif cond==15:
+       htmltext = "30673-05.htm"
+   elif npcId==30532 and cond :
       htmltext = "30532-01.htm"
    return htmltext
 
@@ -206,15 +206,16 @@ class Quest (JQuest) :
    npcId = npc.getNpcId()
    condition,maxcount,item=DROPLIST[npcId]
    count=st.getQuestItemsCount(item)
-   if int(st.get("cond")) == condition and count < maxcount :
+   cond = st.getInt("cond")
+   if cond == condition and count < maxcount :
         st.giveItems(item,1)
         if count == maxcount-1 :
           st.playSound("Itemsound.quest_middle")
           itemcount=0
-          for id in [WEIRD_BEES_NEEDLE_ID,MARSH_SPIDERS_WEB_ID,BLOOD_OF_LEECH_ID]:
+          for id in [WEIRD_BEES_NEEDLE,MARSH_SPIDERS_WEB,BLOOD_OF_LEECH]:
            itemcount+=st.getQuestItemsCount(id)
           if npcId==27133 or itemcount>29:          
-            st.set("cond",str(int(st.get("cond"))+1))
+            st.set("cond",str(cond+1))
         else:
           st.playSound("Itemsound.quest_itemget")
    return
@@ -224,7 +225,8 @@ CREATED     = State('Start', QUEST)
 STARTING    = State('Starting', QUEST)
 STARTED     = State('Started', QUEST)
 COMPLETED   = State('Completed', QUEST)
-
+
+
 QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(30531)
 
@@ -240,4 +242,4 @@ for mobId in [20225,20229,20233,27133]:
 for item in range(2864,2867)+range(2868,2879)+[2916]:
   STARTED.addQuestDrop(30531,item,1)
 
-print "importing quests: 231: Test Of Maestro"
+print "importing quests: 231: Test Of Maestro"
