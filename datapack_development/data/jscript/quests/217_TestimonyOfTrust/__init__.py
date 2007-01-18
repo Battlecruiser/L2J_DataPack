0547ec797683ea27d9cd05661663574c00a7e32a
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-01-18 17:35:55 +0000

    Quest 217: Fix for problem discussed at http://forum.l2jdp.com/viewtopic.php?t=5526

--- datapack_development/data/jscript/quests/217_TestimonyOfTrust/__init__.py
+++ datapack_development/data/jscript/quests/217_TestimonyOfTrust/__init__.py
@@ -256,14 +256,16 @@ class Quest (JQuest) :
        st.set("id",str(int(st.get("id"))+1))
        if st.getRandom(100)<(int(st.get("id"))*33) :
          st.playSound("Itemsound.quest_before_battle")
-         st.getPcSpawn().addSpawn(27120,9410,50301,-3713)   ### FIXME ### Temp fix for spawn
+         st.getPcSpawn().addSpawn(27120,9410,50301,-3713,600000)   ### FIXME ### Temp fix for spawn
+         st.addRadar(9410,50301,-3713)
          return "Luell Of Zephyr Winds has spawned at X=9410 Y=50301 Z=-3713"
 #         st.getPcSpawn().addSpawn(27120)                   # The original spawn code
      elif npcId in [ 20013,20019 ] and st.getQuestItemsCount(SEED_OF_VERDURE_ID) == 0 :
        st.set("id",str(int(st.get("id"))+1))
        if st.getRandom(100)<(int(st.get("id"))*33) :
          st.playSound("Itemsound.quest_before_battle")
-         st.getPcSpawn().addSpawn(27121,16895,47210,-3673)  ### FIXME ### Temp fix for spawn
+         st.getPcSpawn().addSpawn(27121,16895,47210,-3673,600000)  ### FIXME ### Temp fix for spawn
+         st.addRadar(16895,47210,-3673)
          return "Actea Of Verdant Wilds has spawned at X=16895 Y=47210 Z=-3673"
 #         st.getPcSpawn().addSpawn(27121)                   # The original spawn code
    elif cond == 14 :                                       # Condition 14 get 10 Parasite of lota
