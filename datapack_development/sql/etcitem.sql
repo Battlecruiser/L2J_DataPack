b507d14362c80baccfbd6e7c1db870c655ff01b0
HeXa <HeXa@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-01-26 08:39:54 +0000

    fix for #703 [thx kriau]

--- datapack_development/sql/etcitem.sql
+++ datapack_development/sql/etcitem.sql
@@ -6068,7 +6068,7 @@ INSERT INTO `etcitem` (`item_id`,`name`,`crystallizable`,`item_type`,`weight`,`c
  (7809,'Yellow Colored Lure - For Beginners','false','lure',5,'stackable','liquid','none',0,36,0,'false','C4Item','none'),
  (7832,'Apprentice Adventurer''s Weapon Exchange Coupon','false','none',1,'normal','paper','none',0,0,0,'false','C4Item','none'),
  (7833,'Adventurer''s Weapon Exchange Coupon','false','none',1,'normal','paper','none',0,0,0,'false','C4Item','none'),
- (7835,'Amulet: Seal of Disease','false','none',120,'normal','paper','none',0,11600,0,'true','C4Item','none');
+ (7835,'Amulet: Seal of Disease','false','spellbook',120,'normal','paper','none',0,11600,0,'true','C4Item','none');
 
 COMMIT;
 
