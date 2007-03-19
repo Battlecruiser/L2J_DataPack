5d78856bc623c3523a7e2c7b58483d477503aec6
DrLecter <DrLecter@users.noreply.github.com>
2007-03-19 19:03:15 +0000

    Newbie Travel Token implementation (teleports only). I'll add items later.

--- datapack_development/data/jscript/teleports/__init__.py
+++ datapack_development/data/jscript/teleports/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
 '1102_toivortex_blue',
 '1102_toivortex_red',
 '1103_OracleTeleport',
+'1104_NewbieTravelToken',
 '1630_PaganTeleporters',
 '2000_NoblesseTeleport',
 '2211_HuntingGroundsTeleport'
