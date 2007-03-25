5850d73ed0cd9c48c196d4d73aa74fe896fcc35f
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-03-25 00:12:40 +0000

    Varka/Ketra rewards support, more to come :D

--- datapack_development/data/jscript/custom/__init__.py
+++ datapack_development/data/jscript/custom/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
 '3995_echo',
 '12535_WakeBaium',
-'7000_HeroItems'
+'7000_HeroItems',
+'6050_KetraOrcSupport',
+'6051_VarkaSilenosSupport',
 ]
 print ""
 print "importing custom data ..."
