8344ba2b03e5356c2142b1a015ad6fcd405fcce4
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-03-25 00:12:40 +0000

    Varka/Ketra rewards support, more to come :D

---
+++ datapack_development/data/jscript/custom/6050_KetraOrcSupport/__init__.py
@@ -0,0 +1,38 @@
+# Created by Emperorc
+import sys
+from net.sf.l2j.gameserver.model.quest import State
+from net.sf.l2j.gameserver.model.quest import QuestState
+from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
+
+qn = "6050_KetraOrcSupport"
+
+class Quest (JQuest) :
+
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
+ 
+ def onEvent (self,event,st):
+    return
+ 
+ def onTalk (Self,npc,st):
+     npcId = npc.getNpcId()
+     htmltext = "<html><head><body>I have nothing to say to you.</body></html>"
+     Alevel = st.getPlayer().getAllianceWithVarkaKetra()
+     if Alevel == 2 :
+        htmltext = "1.htm"
+     elif Alevel == 3 or Alevel == 4 :
+         htmltext = "2.htm"
+     elif Alevel == 5 :
+         htmltext = "3.htm"
+     else :
+         htmltext = "no.htm"
+     return htmltext
+
+QUEST       = Quest(6050, qn, "custom")
+CREATED     = State('Start', QUEST)
+COMPLETED   = State('Completed',   QUEST)
+
+QUEST.setInitialState(CREATED)
+QUEST.addStartNpc(31375)
+CREATED.addTalkId(31375)
+
+print "importing quests: 6050: Ketra Orc Support"
