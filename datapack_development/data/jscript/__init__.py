8d6e8dd2dc74e189707ddc071427326f87cf234f
DrLecter <DrLecter@users.noreply.github.com>
2007-04-09 14:56:28 +0000

    Multisell based blacksmiths. Jython scripts were removed, and the last missing detail would be exchange for C5 items, I'll generate that later. Requires core updated to [L724] in order to work properly. Thanks to Fulminus for the implementation, and worldonlinehosting, ratonofx, ThePhoenixBird, Censor and others who contributed.

--- datapack_development/data/jscript/__init__.py
+++ datapack_development/data/jscript/__init__.py
@@ -1,13 +1,11 @@
 __all__ = [
 'quests',
-'blacksmith',
 'custom',
 'teleports',
 'village_master',
 'ai'
 ]
 import quests
-import blacksmith
 import custom
 import teleports
 import village_master
