0787b0bb3b23749043e9f8dd3978f10e9e0adbb1
DrLecter <DrLecter@users.noreply.github.com>
2007-06-07 05:22:54 +0000

    Quest 618_IntoTheFlame. by Vice. Valakas mechanics are missing yet. And my changes aren't tested but should be safe. As usual, please report any problem.

--- datapack_development/data/jscript/quests/__init__.py
+++ datapack_development/data/jscript/quests/__init__.py
@@ -227,6 +227,7 @@ __all__ = [
 '615_MagicalPowerOfFirePart1',
 '616_MagicalPowerOfFirePart2',
 '617_GatherTheFlames',
+'618_IntoTheFlame',
 '619_RelicsOfTheOldEmpire',
 '621_EggDelivery',
 '622_DeliveryOfSpecialLiquor',
