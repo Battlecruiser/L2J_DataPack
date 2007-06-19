ca8877b6803e6f81c726ad70027da01fea57359d
DrLecter <DrLecter@users.noreply.github.com>
2007-06-19 13:15:10 +0000

    Jscript: fix for #1250.

--- datapack_development/data/jscript/village_master/30026_bitz_occupation_change/__init__.py
+++ datapack_development/data/jscript/village_master/30026_bitz_occupation_change/__init__.py
@@ -1,16 +1,14 @@
-#
-# Created by DraX on 2005.08.08
-#
+# This script is part of the Official L2J Datapack Project
+# visit us at http://www.l2jdp.com/
+# Created by DraX on 08.08.2005
 # Updated by ElgarL on 28.09.2005
-#
-
-print "importing village master data: Talking Island Village ...done"
+# Updated by DrLecter on 19.06.2007
 
 import sys
-
 from net.sf.l2j.gameserver.model.quest        import State
 from net.sf.l2j.gameserver.model.quest        import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
+
 qn="30026_bitz_occupation_change"
 GRAND_MASTER_BITZ = 30026
 
@@ -19,69 +17,38 @@ class Quest (JQuest) :
  def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
  def onEvent (self,event,st):
-   
-   htmltext = "No Quest"
-   
-   if event == "30026-01.htm":
+   if event in ["30026-01.htm","30026-02.htm","30026-03.htm","30026-04.htm","30026-05.htm","30026-06.htm","30026-07.htm"]:
      htmltext = event
-
-   if event == "30026-02.htm":
-     htmltext = event
-
-   if event == "30026-03.htm":
-     htmltext = event
-
-   if event == "30026-04.htm":
-     htmltext = event
-
-   if event == "30026-05.htm":
-     htmltext = event
-
-   if event == "30026-06.htm":
-     htmltext = event
-
-   if event == "30026-07.htm":
-     htmltext = event
-
+   else :
+     htmltext = "No Quest"
+     st.exitQuest(1)
    return htmltext
 
- 
  def onTalk (Self,npc,player):
    st = player.getQuestState(qn)
    npcId = npc.getNpcId()
-   
-   Race    = st.getPlayer().getRace()
-   pcId = st.getPlayer().getClassId().getId()
-   
-   # Humans got accepted
-   if npcId == GRAND_MASTER_BITZ and Race in [Race.human]:
-#fighter
+   Race  = st.getPlayer().getRace()
+   pcId  = st.getPlayer().getClassId().getId()
+   # Human fighters get accepted
+   if npcId == GRAND_MASTER_BITZ and Race in [Race.human] and pcId in range(0x0a)+range(88,94) :
+     #fighter
      if pcId == 0x00:
        htmltext = "30026-01.htm"
-#warrior, knight, rogue
-     if pcId == 0x01 or pcId == 0x04 or pcId == 0x07:
+     #warrior, knight, rogue
+     elif pcId in [1, 4, 7] :
        htmltext = "30026-08.htm"
-#warlord, paladin, treasureHunter
-     if pcId == 0x03 or pcId == 0x05 or pcId == 0x08:
+     #warlord, paladin, treasureHunter, adventurer, hell knight, dreadnought
+     elif pcId in [3, 5, 8, 93, 91, 89 ] :
        htmltext = "30026-09.htm"
-#gladiator, darkAvenger, hawkeye
-     if pcId == 0x02 or pcId == 0x06 or pcId == 0x09:
+     #gladiator, darkAvenger, hawkeye,  sagitarius, phoenix knight, duelist
+     elif pcId in [2, 6, 9, 92, 90, 88 ]:
        htmltext = "30026-09.htm"
-#mage, wizard, cleric]:
-     if pcId == 0x0a or pcId == 0x0b or pcId == 0x0f:
-       htmltext = "30026-10.htm"
-#sorceror, necromancer, warlock, bishop, prophet
-     if pcId == 0x0c or pcId == 0x0d or pcId == 0x0e or pcId == 0x10 or pcId == 0x11:
-       htmltext = "30026-10.htm"
-       
      st.setState(STARTED)
-     return htmltext
-
-   # All other Races must be out
-   if npcId == GRAND_MASTER_BITZ and Race in [Race.dwarf, Race.darkelf, Race.elf, Race.orc]:
-     st.setState(COMPLETED)
+   # All other Races and classes must be out
+   else :
      st.exitQuest(1)
-     return "30026-10.htm"
+     htmltext = "30026-10.htm"
+   return htmltext
 
 QUEST     = Quest(30026,qn,"village_master")
 CREATED   = State('Start',     QUEST)
@@ -89,7 +56,7 @@ STARTED   = State('Started',   QUEST)
 COMPLETED = State('Completed', QUEST)
 
 QUEST.setInitialState(CREATED)
+QUEST.addStartNpc(GRAND_MASTER_BITZ)
+QUEST.addTalkId(GRAND_MASTER_BITZ)
 
-QUEST.addStartNpc(30026)
-
-QUEST.addTalkId(30026)
+print "importing village master data: Talking Island Village ...done"
