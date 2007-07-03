ff4e11432946402fae0471e52b92ad590bb62f76
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-07-03 06:46:25 +0000

    Echo crystal dialogs should now match what I saw the last time I were there. And for the little detail of we not having these links in the first NPC dialogs, honestly I feel it more organized the way it is now and we'd rather focus in some other pending stuff, essentially more important.

--- datapack_development/data/jscript/custom/3995_echo/__init__.py
+++ datapack_development/data/jscript/custom/3995_echo/__init__.py
@@ -1,80 +1,70 @@
-### ---------------------------------------------------------------------------
-### <history>
-###    Elektra@ElektraL2.com
-### </history>
-### ---------------------------------------------------------------------------
+# Formerly based on Elektra's script
+# updated by DrLecter for the Official L2J Datapack Project.
+# Visit http://forum.l2jdp.com/ for more details.
 
-### Settings
 qn = "3995_echo"
-NPC         = [31042,31043]
+
+NPCS        = [31042,31043]
+ADENA       = 57
+COST        = 200
+
 QuestId     = 3995
 QuestName   = "echo"
 QuestDesc   = "custom"
-InitialHtml = "1.htm"
 
-### Items - Format [name, giveItemId, giveItemQty, takeItem1Id, takeItem1Qty, takeItem2Id, takeItem2Qty]
-Items       = [
-["Theme of Travel",      4411, 1, 57, 200, 4410, 1],
-["Theme of Festival",    4415, 1, 57, 200, 4421, 1],
-["Theme of Lonely",      4414, 1, 57, 200, 4420, 1],
-["Theme of Love",        4413, 1, 57, 200, 4408, 1],
-["Theme of Battle",      4412, 1, 57, 200, 4409, 1],
-["Theme of Comedy",      4417, 1, 57, 200, 4419, 1],
-["Theme of Celebration", 4416, 1, 57, 200, 4418, 1]
-]
+#score:[crystal,msg_ok,msg_noadena,msg_noscore]
+LIST={
+4410:[4411,"01","02","03"],
+4409:[4412,"04","05","06"],
+4408:[4413,"07","08","09"],
+4420:[4414,"10","11","12"],
+4421:[4415,"13","14","15"],
+4419:[4417,"16","05","06"],
+4418:[4416,"17","05","06"]
+}
 
-### ---------------------------------------------------------------------------
-### DO NOT MODIFY BELOW THIS LINE
-### ---------------------------------------------------------------------------
 
-print "importing " + QuestDesc + ": " + str(QuestId) + ": " + QuestName + ": " + str(len(Items)) + " item(s)",
+print "importing " + QuestDesc + ": " + str(QuestId) + ": " + QuestName + ": " + str(len(LIST)) + " crystals",
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
-### doRequestedEvent
-def do_RequestedEvent(event, st, giveItemId, giveItemQty, takeItem1Id, takeItem1Qty, takeItem2Id, takeItem2Qty) :
-    if st.getQuestItemsCount(takeItem1Id) >= takeItem1Qty and st.getQuestItemsCount(takeItem2Id) >= takeItem2Qty :
-        st.takeItems(takeItem1Id, takeItem1Qty)
-        st.giveItems(giveItemId, giveItemQty)
-        return "Echo Crystal Created"
-    else :
-        return "You do not have enough items."
-
-### main code
 class Quest (JQuest) :
 
  def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
- def onEvent (self,event,st) :
-    htmltext = event
-    if event == "0" :
-        return InitialHtml
-    for item in Items :
-        if event == str(item[1]):
-            htmltext = do_RequestedEvent(event, st, item[1], item[2], item[3], item[4], item[5], item[6])
-    if htmltext != event :
-      st.setState(COMPLETED)
-      st.exitQuest(1)
+ def onAdvEvent (self,event,npc,player) :
+    st = player.getQuestState(qn)
+    if not (st and event.isdigit()): return
+    score=int(event)
+    if score in LIST.keys() :
+      crystal,ok,noadena,noscore=LIST[score]
+      npcId = str(npc.getNpcId())
+      if not st.getQuestItemsCount(score) :
+         htmltext=npcId+"-"+noscore+".htm"
+      elif st.getQuestItemsCount(ADENA) < COST :
+         htmltext=npcId+"-"+noadena+".htm"
+      else :
+         st.takeItems(ADENA,COST)
+         st.giveItems(crystal,1)
+         htmltext=npcId+"-"+ok+".htm"
+    else :
+      htmltext=""
+    st.exitQuest(1)
     return htmltext
 
  def onTalk (Self,npc,player):
-   htmltext = "<html><head><body>I have nothing to say with you</body></html>"
-   return InitialHtml
+   return "1.htm"
 
-### Quest class and state definition
+
 QUEST       = Quest(QuestId,qn,QuestDesc)
 CREATED     = State('Start',     QUEST)
-COMPLETED   = State('Completed', QUEST)
 
-### Quest initialization
 QUEST.setInitialState(CREATED)
 
-for item in NPC:
-### Quest NPC starter initialization
-   QUEST.addStartNpc(item)
-### Quest NPC initialization
-   QUEST.addTalkId(item)
+for npc in NPCS:
+   QUEST.addStartNpc(npc)
+   QUEST.addTalkId(npc)
 
 print "...done"
