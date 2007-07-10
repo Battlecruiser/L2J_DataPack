f841312855076e3420460cf333ff849bfbb17042
Fulminus <Fulminus@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-07-10 20:41:48 +0000

    DP Support for Wedding System [L1003].  Thanks Sami, evil33t, and all others.

--- datapack_development/tools/database_installer.sh
+++ datapack_development/tools/database_installer.sh
@@ -302,6 +302,7 @@ $MYG < ../sql/merchant_lease.sql &> /dev/null
 $MYG < ../sql/merchant_shopids.sql &> /dev/null
 $MYG < ../sql/merchants.sql &> /dev/null
 $MYG < ../sql/minions.sql &> /dev/null
+$MYG < ../sql/mods_wedding.sql &> /dev/null
 $MYG < ../sql/npc.sql &> /dev/null
 $MYG < ../sql/olympiad_nobles.sql&> /dev/null
 $MYG < ../sql/pets.sql &> /dev/null
