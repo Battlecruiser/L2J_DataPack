8d88994e0fb38aba490ef10052f4832e2e54632a
Fulminus <Fulminus@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-10-23 04:48:47 +0000

    Ermm..I can never commit without a follow-up fix, can I?...

--- datapack_development/sql/merchant_shopids.sql
+++ datapack_development/sql/merchant_shopids.sql
@@ -274,8 +274,8 @@ INSERT INTO merchant_shopids VALUES (417,'31579');
 INSERT INTO merchant_shopids VALUES (418,'31616'); 
 INSERT INTO merchant_shopids VALUES (419,'31696'); 
 INSERT INTO merchant_shopids VALUES (420,'31697');
-INSERT INTO merchant_shopids VALUES (421,'31989'),
-INSERT INTO merchant_shopids VALUES (423,'32105');;
+INSERT INTO merchant_shopids VALUES (421,'31989');
+INSERT INTO merchant_shopids VALUES (423,'32105');
 
 -- GM Shops
 
