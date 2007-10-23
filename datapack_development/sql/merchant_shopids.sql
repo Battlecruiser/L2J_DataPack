a5a53bd1c803e037a57a6e727ba156cd11d384dc
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-10-23 22:35:16 +0000

    Finishing up support for Ketra/Varka. Thanks Kerberos_20 and Emperorc

--- datapack_development/sql/merchant_shopids.sql
+++ datapack_development/sql/merchant_shopids.sql
@@ -231,6 +231,8 @@ INSERT INTO merchant_shopids VALUES (362,'30138');
 INSERT INTO merchant_shopids VALUES (363,'30560');
 INSERT INTO merchant_shopids VALUES (364,'31424');
 INSERT INTO merchant_shopids VALUES (365,'32106');
+INSERT INTO merchant_shopids VALUES (366,'31380');
+INSERT INTO merchant_shopids VALUES (367,'31373');
 INSERT INTO merchant_shopids VALUES (5600,'30892');
 INSERT INTO merchant_shopids VALUES (5601,'30893');
 INSERT INTO merchant_shopids VALUES (5710,'31067');
