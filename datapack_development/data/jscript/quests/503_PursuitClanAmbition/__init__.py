edacf50ee3804a1271d4587ffc2ba6f6a78aa5f1
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-10-25 19:25:54 +0000

    JSCRIPT: Fix for quest 503 (thx kerberos_20)

--- datapack_development/data/jscript/quests/503_PursuitClanAmbition/__init__.py
+++ datapack_development/data/jscript/quests/503_PursuitClanAmbition/__init__.py
@@ -287,11 +287,11 @@ class Quest (JQuest) :
     elif event == "30766-04.htm":
       st.set("cond","9")
       spawnedNpc=st.addSpawn(30766,160622,21230,-3710,90000)
-      npc.broadcastPacket(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"Blood and Honour."))
+      spawnedNpc.broadcastPacket()(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"Blood and Honour."))
       spawnedNpc=st.addSpawn(30759,160665,21209,-3710,90000)
-      npc.broadcastPacket(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"Ambition and Power"))
+      spawnedNpc.broadcastPacket()(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"Ambition and Power"))
       spawnedNpc=st.addSpawn(30758,160665,21291,-3710,90000)
-      npc.broadcastPacket(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"War and Death"))
+      spawnedNpc.broadcastPacket()(CreatureSay(spawnedNpc.getObjectId(),0,spawnedNpc.getName(),"War and Death"))
     elif event == "30766-08.htm":
       st.takeItems(Scepter_Judgement,-1)
       exit503(0,st)
