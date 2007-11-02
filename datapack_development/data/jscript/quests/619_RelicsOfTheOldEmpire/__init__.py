4275816b1d7a4269b5ac57055bb46c4897bfb26f
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-11-02 08:50:53 +0000

    lil fixing for quest drop Entrance to four sepulchers, no limit is present on retail

--- datapack_development/data/jscript/quests/619_RelicsOfTheOldEmpire/__init__.py
+++ datapack_development/data/jscript/quests/619_RelicsOfTheOldEmpire/__init__.py
@@ -77,13 +77,12 @@ class Quest (JQuest) :
      st = partyMember.getQuestState(qn)
      if st :
        if st.getState() == STARTED :
-         entr = st.getQuestItemsCount(ENTRANCE)
          numItems, chance = divmod(100*Config.RATE_DROP_QUEST,100)
          if st.getRandom(100) < chance :
             numItems += 1
          st.giveItems(RELICS,int(numItems))
          st.playSound("ItemSound.quest_itemget")
-         if not entr and st.getRandom(100) < (5*Config.RATE_DROP_QUEST) :
+         if st.getRandom(100) < (5*Config.RATE_DROP_QUEST) :
              st.giveItems(ENTRANCE,1)
              st.playSound("ItemSound.quest_middle")
      return
