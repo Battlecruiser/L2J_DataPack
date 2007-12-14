2940262b71213fd8d42e94dc3cfbdd4300061f46
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2007-12-14 21:06:52 +0000

    fix for #1594

--- datapack_development/data/jscript/teleports/1630_PaganTeleporters/__init__.py
+++ datapack_development/data/jscript/teleports/1630_PaganTeleporters/__init__.py
@@ -20,15 +20,15 @@ class Quest (JQuest):
     npcId = npc.getNpcId()
     htmltext = "You have been teleported."
     if player.getLevel() < 73 :
-       htmltext = "<html>Teleport available only for characters with Pagans Mark and level 73 or above.</body></html>"
+       htmltext = "<html><body>Teleport available only for characters with Pagans Mark and level 73 or above.</body></html>"
     elif npcId == 32034 and st.getQuestItemsCount(8064) :
           st.takeItems(8064,1)
-          htmltext = "<html><body>As you pass through the gates your mark fades. Make a note to return to <font color='LEVEL'>Priest Flauron</font> to inquire about this!</body></html>"
+          htmltext = "FadedMark.htm"
           st.giveItems(8065,1)
           player.teleToLocation(-16324,-37147,-10724)
     elif npcId in [32034,32036]:
        if not st.getQuestItemsCount(8067) :
-          htmltext = '<html>Teleport available only for characters with Pagans Mark and level 73 or above.</body></html>'
+          htmltext = '<html><body>Teleport available only for characters with Pagans Mark and level 73 or above.</body></html>'
        else:
           if npcId == 32034 :
              player.teleToLocation(-16324,-37147,-10724)
