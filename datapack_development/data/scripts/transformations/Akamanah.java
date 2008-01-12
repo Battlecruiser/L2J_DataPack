d1f01fefbab2fba13b102e71d094d8108a1c26aa
yellowperil <yellowperil@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-01-12 06:24:23 +0000

    Cursed Weapons:  DP side for [L1521] (thanks Baorc & evil33t)
    Bug Corrected:
    When cursed weapon is equipped by a player and then is dropped on the ground, the cursed weapon does not lose all of the previous levels that the previous owner had done.
    - This has been corrected so it does not retain the levels set by the previous owner.
    Kamael Cursed Weapon Transformation:
    When a player equips the cursed weapons they transform into the associated transformation. A players name will display to the player as his own name, but to others it will display the name associated with the cursed weapon. In the community board the player will display as the players true name, also in ALL chat the player will display the name of the associated cursed weapon, party is not allowed, shout and trade are disabled for cursed weapon users, and clan chats should be displaying the true characters name.

--- datapack_development/data/scripts/transformations/Akamanah.java
+++ datapack_development/data/scripts/transformations/Akamanah.java
@@ -11,9 +11,6 @@ import net.sf.l2j.gameserver.instancemanager.TransformationManager;
  */
 public class Akamanah extends L2Transformation
 {
-	private String _realName;
-    private String _realTitle;
-    
     public Akamanah()
     {
         // id, duration (secs), colRadius, colHeight
@@ -22,19 +19,16 @@ public class Akamanah extends L2Transformation
     
     public void onTransform()
     {
-        // Store real values
-        _realName = this.getPlayer().getName();
-        _realTitle = this.getPlayer().getTitle();
-        
-        // remove title and name
-        this.getPlayer().setTitle("");
-        this.getPlayer().setName("");
+        // Set charachter name to transformed name
+	this.getPlayer().getAppearance().getVisibleName();
+	this.getPlayer().getAppearance().getVisibleTitle();
     }
     
     public void onUntransform()
     {
-    	this.getPlayer().setTitle(_realTitle);
-        this.getPlayer().setName(_realName);
+	// set character back to true name.
+	this.getPlayer().getAppearance().getVisibleName();
+	this.getPlayer().getAppearance().getVisibleTitle();
     }
     
     public static void main(String[] args)
