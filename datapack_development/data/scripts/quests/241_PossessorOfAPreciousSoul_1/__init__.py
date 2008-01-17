223955d98890148c6b55cce670d5d01619e219a4
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-01-17 00:50:39 +0000

    JYTHON: Fix for #1708 (thx Zeus for reporting) and clean up of this quest series while I was in there. Plus 2 minor typo fixes in quest htmls.

--- datapack_development/data/scripts/quests/241_PossessorOfAPreciousSoul_1/__init__.py
+++ datapack_development/data/scripts/quests/241_PossessorOfAPreciousSoul_1/__init__.py
@@ -42,97 +42,98 @@ class Quest (JQuest) :
 
  def __init__(self,id,name,descr):
      JQuest.__init__(self,id,name,descr)
-     self.questItemIds = [LEGEND_OF_SEVENTEEN, MALRUK_SUCCUBUS_CLAW, ECHO_CRYSTAL, POETRY_BOOK, CRIMSON_MOSS, RAHORAKTIS_MEDICINE, VIRGILS_LETTER]
+     self.questItemIds = [LEGEND_OF_SEVENTEEN, MALRUK_SUCCUBUS_CLAW, ECHO_CRYSTAL, POETRY_BOOK, CRIMSON_MOSS, RAHORAKTIS_MEDICINE]
 
  def onEvent (self,event,st) :
    htmltext = event
    cond = st.getInt("cond")
+   if not st.getPlayer().isSubClassActive() : return
    if event == "31739-4.htm" :
-     if cond == 0 and st.getPlayer().isSubClassActive() :
+     if cond == 0 :
        st.setState(State.STARTED)
        st.set("cond","1")
        st.playSound("ItemSound.quest_accept")
-   if event == "30753-2.htm" :
-     if cond == 1 and st.getPlayer().isSubClassActive() :
+   elif event == "30753-2.htm" :
+     if cond == 1 :
        st.set("cond","2")
        st.playSound("ItemSound.quest_middle")
-   if event == "30754-2.htm" :
-     if cond == 2 and st.getPlayer().isSubClassActive() :
+   elif event == "30754-2.htm" :
+     if cond == 2 :
        st.set("cond","3")
        st.playSound("ItemSound.quest_middle")
-   if event == "31739-8.htm" :
-     if cond == 4 and st.getPlayer().isSubClassActive() :
+   elif event == "31739-8.htm" :
+     if cond == 4 :
        st.set("cond","5")
        st.takeItems(LEGEND_OF_SEVENTEEN,1)
        st.playSound("ItemSound.quest_middle")
-   if event == "31042-2.htm" :
-     if cond == 5 and st.getPlayer().isSubClassActive() :
+   elif event == "31042-2.htm" :
+     if cond == 5 :
        st.set("cond","6")
        st.playSound("ItemSound.quest_middle")
-   if event == "31042-5.htm" :
-     if cond == 7 and st.getPlayer().isSubClassActive() :
+   elif event == "31042-5.htm" :
+     if cond == 7 :
        st.set("cond","8")
        st.takeItems(MALRUK_SUCCUBUS_CLAW,10)
        st.giveItems(ECHO_CRYSTAL,1)
        st.playSound("ItemSound.quest_middle")
-   if event == "31739-12.htm" :
-     if cond == 8 and st.getPlayer().isSubClassActive() :
+   elif event == "31739-12.htm" :
+     if cond == 8 :
        st.set("cond","9")
        st.takeItems(ECHO_CRYSTAL,1)
        st.playSound("ItemSound.quest_accept")
-   if event == "30692-2.htm" :
-     if cond == 9 and st.getPlayer().isSubClassActive() :
+   elif event == "30692-2.htm" :
+     if cond == 9 :
        st.set("cond","10")
        st.giveItems(POETRY_BOOK,1)
        st.playSound("ItemSound.quest_accept")
-   if event == "31739-15.htm" :
-     if cond == 10 and st.getPlayer().isSubClassActive() :
+   elif event == "31739-15.htm" :
+     if cond == 10 :
        st.set("cond","11")
        st.takeItems(POETRY_BOOK,1)
        st.playSound("ItemSound.quest_accept")
-   if event == "31742-2.htm" :
-     if cond == 11 and st.getPlayer().isSubClassActive() :
+   elif event == "31742-2.htm" :
+     if cond == 11 :
        st.set("cond","12")
        st.playSound("ItemSound.quest_accept")
-   if event == "31744-2.htm" :
-     if cond == 12 and st.getPlayer().isSubClassActive() :
+   elif event == "31744-2.htm" :
+     if cond == 12 :
        st.set("cond","13")
        st.playSound("ItemSound.quest_accept")
-   if event == "31336-2.htm" :
-     if cond == 13 and st.getPlayer().isSubClassActive() :
+   elif event == "31336-2.htm" :
+     if cond == 13 :
        st.set("cond","14")
        st.playSound("ItemSound.quest_accept")
-   if event == "31336-5.htm" :
-     if cond == 15 and st.getPlayer().isSubClassActive() :
+   elif event == "31336-5.htm" :
+     if cond == 15:
        st.set("cond","16")
        st.takeItems(CRIMSON_MOSS,5)
        st.giveItems(RAHORAKTIS_MEDICINE,1)
        st.playSound("ItemSound.quest_accept")
-   if event == "31743-2.htm" :
-     if cond == 16 and st.getPlayer().isSubClassActive() :
+   elif event == "31743-2.htm" :
+     if cond == 16 :
        st.set("cond","17")
        st.takeItems(RAHORAKTIS_MEDICINE,1)
        st.playSound("ItemSound.quest_accept")
-   if event == "31742-5.htm" :
-     if cond == 17 and st.getPlayer().isSubClassActive() :
+   elif event == "31742-5.htm" :
+     if cond == 17 :
        st.set("cond","18")
        st.playSound("ItemSound.quest_accept")
-   if event == "31740-2.htm" :
-     if cond == 18 and st.getPlayer().isSubClassActive() :
+   elif event == "31740-2.htm" :
+     if cond == 18 :
        st.set("cond","19")
        st.playSound("ItemSound.quest_accept")
-   if event == "31272-2.htm" :
-     if cond == 19 and st.getPlayer().isSubClassActive() :
+   elif event == "31272-2.htm" :
+     if cond == 19 :
        st.set("cond","20")
        st.playSound("ItemSound.quest_accept")
-   if event == "31272-5.htm" :
-     if cond == 20 and st.getPlayer().isSubClassActive() :
+   elif event == "31272-5.htm" :
+     if cond == 20 :
        st.takeItems(LUNARGENT,5)
        st.takeItems(HELLFIRE_OIL,1)
        st.set("cond","21")
        st.playSound("ItemSound.quest_accept")
-   if event == "31740-5.htm" :
-     if cond == 21 and st.getPlayer().isSubClassActive() :
+   elif event == "31740-5.htm" :
+     if cond == 21 :
        st.giveItems(VIRGILS_LETTER,1)
        st.addExpAndSp(263043,0)
        st.set("cond","0")
