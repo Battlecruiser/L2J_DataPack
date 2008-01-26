f91716641f7f58c826cf5546a2f9351dfa0f7fd1
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-01-26 12:35:02 +0000

    better giran zone, tnx Morkavah Bolts in grocery, tnx Zhyla and removing deprecated zone.sql

--- datapack_development/tools/database_installer.sh
+++ datapack_development/tools/database_installer.sh
@@ -340,7 +340,6 @@ $MYG < ../sql/teleport.sql &> /dev/null
 $MYG < ../sql/topic.sql &> /dev/null
 $MYG < ../sql/walker_routes.sql &> /dev/null
 $MYG < ../sql/weapon.sql &> /dev/null
-$MYG < ../sql/zone.sql &> /dev/null
 $MYG < ../sql/zone_vertices.sql &> /dev/null
 newbie_helper
 }
