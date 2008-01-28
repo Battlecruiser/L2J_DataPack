11e4f243b0ff3e6fcb365ebd422b3ba9f3421222
DrLecter <DrLecter@users.noreply.github.com>
2008-01-28 13:22:33 +0000

    Several NPC fixes for Kamael village (tnx zhyla and Izzy)

--- datapack_development/data/scripts/village_master/orc_occupation_change_1/__init__.py
+++ datapack_development/data/scripts/village_master/orc_occupation_change_1/__init__.py
@@ -17,7 +17,7 @@ MASK_OF_MEDIUM   = 1631
 #Reward Item
 SHADOW_WEAPON_COUPON_DGRADE = 8869
 #OSBORN,DRIKUS,CASTOR
-NPCS=[30500,30505,30508]
+NPCS=[30500,30505,30508,32150]
 #event:[newclass,req_class,req_race,low_ni,low_i,ok_ni,ok_i,req_item]
 #low_ni : level too low, and you don't have quest item
 #low_i: level too low, despite you have the item
