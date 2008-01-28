9c1686ff39afdb9cdb7ccd252350d80cd171c3cb
yellowperil <yellowperil@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-01-28 07:43:35 +0000

    Transformation:   thanks ahmed  [L1611]
    - Giving skills on transformation, and disabling all character skills.
    - Removing skills on untransformation, and restoring all characters kills.
    - Players cannot switch or change subclasses while transformed.
    - Character transformation ID is stored in character table on transform to recall after a client/server reboot of any kind.
    - Character transformation ID is set to 0 by defualt, on Untransformation the transform ID will be set to 0, this should never be null.
    - Transformation skills do not save anywhere, no skills are ever deleted, or added.
    - Skills list updated on addition/removal of skills due to transformation.
    - If the client crashes, disconnects, etc, while transformed, when character logs in they will still have everything at last second.
    - If cursed weapon is equipped while transformed, player lose previous transform skills.
    - If chaotic player loses chaotic status (not by death), and was transformed before, their transformation and skills are restored.
    - If chaotic player loses chaotic status by death, and if there was a transfom before, that transform is removed, and character goes to normal.
    - Server packet modified to allow display of transformed characters in character selection screen (before was chaotic only).
    - Transform skill handler has been modified to prevent untransformation when already transformed.
    - Transformations in this patch:
    *DragonBomber
    *GolemGuardian
    *GrailApostle
    *Kamael
    *OnyxBeast
    *Unicorn
    - New Skills:
    *Nail Attack
    *Wing Assault
    *Death Beam
    *Power Claw
    *Fast Moving
    *Rolling Attack
    *Dig Attack
    *Sand Cloud
    *Double Slasher
    *Earthquake
    *Steel Cutter
    *Transform Dispel

--- datapack_development/data/scripts/transformations/Kamael.java
+++ datapack_development/data/scripts/transformations/Kamael.java
@@ -1,36 +1,110 @@
 package transformations;
 
-import net.sf.l2j.gameserver.model.L2Transformation;
+import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
+import net.sf.l2j.gameserver.model.L2Skill;
+import net.sf.l2j.gameserver.model.L2Transformation;
 
 /**
- * This is currently only a test of the java script engine
- * Can be used for screenshot purposes :)
+ * Description: <br>
+ * This will handle the transformation, giving the skills, and removing them, when the player logs out and is transformed these skills
+ * do not save. 
+ * When the player logs back in, there will be a call from the enterworld packet that will add all their skills.
+ * The enterworld packet will transform a player. 
+ * The only thing that is missing now is completion of the skill effects and the stat changes of the transformation.<br>
+ * - Ahmed
  * 
  * @author KenM
  *
  */
 public class Kamael extends L2Transformation
 {
-    public Kamael()
-    {
-        // id, duration (secs), colRadius, colHeight
-        super(251, 3600, 9.0, 30.0);
-    }
-    
-    public void onTransform()
-    {
-        this.getPlayer().sendMessage("Proof of concept");
-        //this.getPlayer().sendMessage("You become an mutant evil-chicken");
-    }
-    
-    public void onUntransform()
-    {
-        this.getPlayer().sendMessage("End of transformation");
-    }
-    
-    public static void main(String[] args)
-    {
-        TransformationManager.getInstance().registerTransformation(new Kamael());
-    }
+	public Kamael()
+	{
+		// id, duration (secs), colRadius, colHeight
+		super(251, 3600, 9.0, 30.0);
+	}
+
+	public void onTransform()
+	{
+		// Disable all character skills.
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().removeSkill(sk, false);
+		}
+		if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
+		{
+			// give transformation skills
+			transformedSkills();
+			// Message sent to player after transforming.
+			this.getPlayer().sendMessage("Final Form transformation complete.");
+			return;
+		}
+		// give transformation skills
+		transformedSkills();
+		// Insert Transformation ID
+		this.getPlayer().transformInsertInfo();
+		// Message sent to player after transforming.
+		this.getPlayer().sendMessage("Final Form transformation complete.");
+	}
+
+	public void transformedSkills()
+	{
+		// Nail Attack
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(539, 1), false);
+		// Wing Assault
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(540, 1), false);
+		// Transfrom Dispel
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Soul Sucking
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1471, 1), false);
+		// Death Beam
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1472, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public void onUntransform()
+	{
+		// Enable all character skills
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().addSkill(sk, false);
+		}
+		// Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
+		if (this.getPlayer().isCursedWeaponEquipped())
+		{
+			removeSkills();
+			return;
+		}
+		// Remove transformation skills
+		removeSkills();
+		// Insert Transformation ID, and obj_id of character into character_transform
+		this.getPlayer().transformUpdateInfo();
+		// Message sent to player after transforming.
+		this.getPlayer().sendMessage("Final Form has been dispelled.");
+	}
+
+	public void removeSkills()
+	{
+		// Nail Attack
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(539, 1), false);
+		// Wing Assault
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(540, 1), false);
+		// Transfrom Dispel
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Soul Sucking
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1471, 1), false);
+		// Death Beam
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1472, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new Kamael());
+	}
 }
--- datapack_development/data/scripts/transformations/OnyxBeast.java
+++ datapack_development/data/scripts/transformations/OnyxBeast.java
@@ -1,32 +1,102 @@
 package transformations;
 
-import net.sf.l2j.gameserver.model.L2Transformation;
+import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
+import net.sf.l2j.gameserver.model.L2Skill;
+import net.sf.l2j.gameserver.model.L2Transformation;
 
 /**
- * This is currently only a test of the java script engine
+ * Description: <br>
+ * This will handle the transformation, giving the skills, and removing them, when the player logs out and is transformed these skills
+ * do not save. 
+ * When the player logs back in, there will be a call from the enterworld packet that will add all their skills.
+ * The enterworld packet will transform a player. 
+ * The only thing that is missing now is completion of the skill effects and the stat changes of the transformation.<br>
+ * - Ahmed
  * 
  * @author durgus
  *
  */
 public class OnyxBeast extends L2Transformation
 {
-    public OnyxBeast()
-    {
-        // id, duration (secs), colRadius, colHeight
-        super(1, 3600, 14.0, 14.5);
-    }
-    
-    public void onTransform()
-    {
-    }
-    
-    public void onUntransform()
-    {
-    }
-    
-    public static void main(String[] args)
-    {
-        TransformationManager.getInstance().registerTransformation(new OnyxBeast());
-    }
+	public OnyxBeast()
+	{
+		// id, duration (secs), colRadius, colHeight
+		super(1, 3600, 14.0, 14.5);
+	}
+
+	public void onTransform()
+	{
+		// Disable all character skills.
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().removeSkill(sk, false);
+		}
+		if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
+		{
+			// give transformation skills
+			transformedSkills();
+			// Message sent to player after transforming.
+			this.getPlayer().sendMessage("Onyx Beast transformation complete.");
+			return;
+		}
+		// give transformation skills
+		transformedSkills();
+		// Update Transformation ID
+		this.getPlayer().transformInsertInfo();
+		// Message sent to player after transforming.
+		this.getPlayer().sendMessage("Onyx Beast transformation complete.");
+	}
+
+	public void transformedSkills()
+	{
+		// Power Claw
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(584, 1), false);
+		// Fast Moving
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(585, 1), false);
+		// Transfrom Dispel
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public void onUntransform()
+	{
+		// Enable all character skills
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().addSkill(sk, false);
+		}
+		// Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
+		if (this.getPlayer().isCursedWeaponEquipped())
+		{
+			removeSkills();
+			return;
+		}
+		// Remove transformation skills
+		removeSkills();
+		// Update Transformation ID
+		this.getPlayer().transformUpdateInfo();
+		// Message sent to player after transforming.
+		this.getPlayer().sendMessage("Onyx Beast has been dispelled.");
+	}
+
+	public void removeSkills()
+	{
+		// Power Claw
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(584, 1), false);
+		// Fast Moving
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(585, 1), false);
+		// Transfrom Dispel
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new OnyxBeast());
+	}
 }
