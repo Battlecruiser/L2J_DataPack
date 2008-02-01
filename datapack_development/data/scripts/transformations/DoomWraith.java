18353d79686d7de67db63565c1b5d592bfbee867
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-02-01 12:48:54 +0000

    Commit Comment:
    This patch includes:
    - Corrections to all the custom messages appearing when transformed,
    and untransformed.
    - New skill XML 5400-5499.xml (needs completion)
    - New Passive skill ID: 5491, named: Decrease Bow/Crossbow Attack Speed
    - Skill ID 5491 added to transformations
    - Transformation stat changes have been added to Final Form
    
    More to come later after testing.
    (tnx Ahmed)

--- datapack_development/data/scripts/transformations/DoomWraith.java
+++ datapack_development/data/scripts/transformations/DoomWraith.java
@@ -1,32 +1,103 @@
 package transformations;
 
-import net.sf.l2j.gameserver.model.L2Transformation;
+import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
+import net.sf.l2j.gameserver.model.L2Skill;
+import net.sf.l2j.gameserver.model.L2Transformation;
+import net.sf.l2j.gameserver.network.SystemMessageId;
+import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 
 /**
- * This is currently only a test of the java script engine
+ * Description: <br>
+ * This will handle the transformation, giving the skills, and removing them, when the player logs out and is transformed these skills
+ * do not save. 
+ * When the player logs back in, there will be a call from the enterworld packet that will add all their skills.
+ * The enterworld packet will transform a player. 
+ * The only thing that is missing now is completion of the skill effects and the stat changes of the transformation.<br>
+ * - Ahmed
  * 
  * @author durgus
  *
  */
 public class DoomWraith extends L2Transformation
 {
-    public DoomWraith()
-    {
-        // id, duration (secs), colRadius, colHeight
-        super(2, 3600, 8.0, 22.0);
-    }
-    
-    public void onTransform()
-    {
-    }
-    
-    public void onUntransform()
-    {
-    }
-    
-    public static void main(String[] args)
-    {
-        TransformationManager.getInstance().registerTransformation(new DoomWraith());
-    }
+	public DoomWraith()
+	{
+		// id, duration (secs), colRadius, colHeight
+		super(2, 3600, 8.0, 22.0);
+	}
+
+	public void onTransform()
+	{
+		// Disable all character skills.
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().removeSkill(sk, false);
+		}
+		if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
+		{
+			// give transformation skills
+			transformedSkills();
+			// Message sent to player after transforming.
+			SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
+			this.getPlayer().sendPacket(msg);
+			return;
+		}
+		// give transformation skills
+		transformedSkills();
+		// Update Transformation ID
+		this.getPlayer().transformInsertInfo();
+		// Message sent to player after transforming.
+		SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
+		this.getPlayer().sendPacket(msg);
+	}
+
+	public void transformedSkills()
+	{
+		// Transfrom Dispel
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Decrease Bow/Crossbow Attack Speed
+		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public void onUntransform()
+	{
+		// Enable all character skills
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null)
+				this.getPlayer().addSkill(sk, false);
+		}
+		// Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
+		if (this.getPlayer().isCursedWeaponEquipped())
+		{
+			removeSkills();
+			return;
+		}
+		// Remove transformation skills
+		removeSkills();
+		// Update Transformation ID
+		this.getPlayer().transformUpdateInfo();
+		// Message sent to player when transform has worn off.
+		SystemMessage msg = new SystemMessage(SystemMessageId.S1_HAS_WORN_OFF);
+		this.getPlayer().sendPacket(msg);
+	}
+
+	public void removeSkills()
+	{
+		// Transfrom Dispel
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Decrease Bow/Crossbow Attack Speed
+		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
+		this.getPlayer().sendSkillList();
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new DoomWraith());
+	}
 }
--- datapack_development/data/scripts/transformations/Kamael.java
+++ datapack_development/data/scripts/transformations/Kamael.java
@@ -4,6 +4,8 @@ import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
 import net.sf.l2j.gameserver.model.L2Skill;
 import net.sf.l2j.gameserver.model.L2Transformation;
+import net.sf.l2j.gameserver.network.SystemMessageId;
+import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 
 /**
  * Description: <br>
@@ -19,92 +21,99 @@ import net.sf.l2j.gameserver.model.L2Transformation;
  */
 public class Kamael extends L2Transformation
 {
-	public Kamael()
-	{
-		// id, duration (secs), colRadius, colHeight
-		super(251, 3600, 9.0, 30.0);
-	}
+    public Kamael()
+    {
+        // id, duration (secs), colRadius, colHeight
+        super(251, 3600, 9.0, 30.0);
+    }
 
-	public void onTransform()
-	{
-		// Disable all character skills.
-		for (L2Skill sk : this.getPlayer().getAllSkills())
-		{
-			if (sk != null)
-				this.getPlayer().removeSkill(sk, false);
-		}
-		if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
-		{
-			// give transformation skills
-			transformedSkills();
-			// Message sent to player after transforming.
-			this.getPlayer().sendMessage("Final Form transformation complete.");
-			return;
-		}
-		// give transformation skills
-		transformedSkills();
-		// Insert Transformation ID
-		this.getPlayer().transformInsertInfo();
-		// Message sent to player after transforming.
-		this.getPlayer().sendMessage("Final Form transformation complete.");
-	}
+    public void onTransform()
+    {
+        // Disable all character skills.
+        for (L2Skill sk : this.getPlayer().getAllSkills())
+        {
+            if (sk != null)
+                this.getPlayer().removeSkill(sk, false);
+        }
+        if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
+        {
+            // give transformation skills
+            transformedSkills();
+            // Message sent to player after transforming.
+            SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
+            this.getPlayer().sendPacket(msg);
+            return;
+        }
+        // give transformation skills
+        transformedSkills();
+        // Update Transformation ID
+        this.getPlayer().transformInsertInfo();
+        // Message sent to player after transforming.
+        SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
+        this.getPlayer().sendPacket(msg);
+    }
 
-	public void transformedSkills()
-	{
-		// Nail Attack
-		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(539, 1), false);
-		// Wing Assault
-		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(540, 1), false);
-		// Transfrom Dispel
-		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
-		// Soul Sucking
-		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1471, 1), false);
-		// Death Beam
-		this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1472, 1), false);
-		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
-		this.getPlayer().sendSkillList();
-	}
+    public void transformedSkills()
+    {
+        // Nail Attack
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(539, 1), false);
+        // Wing Assault
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(540, 1), false);
+        // Soul Sucking
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1471, 1), false);
+        // Death Beam
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(1472, 1), false);
+        // Transfrom Dispel
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+        // Decrease Bow/Crossbow Attack Speed
+        this.getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+        // Send a Server->Client packet StatusUpdate to the L2PcInstance.
+        this.getPlayer().sendSkillList();
+    }
 
-	public void onUntransform()
-	{
-		// Enable all character skills
-		for (L2Skill sk : this.getPlayer().getAllSkills())
-		{
-			if (sk != null)
-				this.getPlayer().addSkill(sk, false);
-		}
-		// Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
-		if (this.getPlayer().isCursedWeaponEquipped())
-		{
-			removeSkills();
-			return;
-		}
-		// Remove transformation skills
-		removeSkills();
-		// Insert Transformation ID, and obj_id of character into character_transform
-		this.getPlayer().transformUpdateInfo();
-		// Message sent to player after transforming.
-		this.getPlayer().sendMessage("Final Form has been dispelled.");
-	}
+    public void onUntransform()
+    {
+        // Enable all character skills
+        for (L2Skill sk : this.getPlayer().getAllSkills())
+        {
+            if (sk != null)
+                this.getPlayer().addSkill(sk, false);
+        }
+        // Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
+        if (this.getPlayer().isCursedWeaponEquipped())
+        {
+            removeSkills();
+            return;
+        }
+        // Remove transformation skills
+        removeSkills();
+        // Update Transformation ID
+        this.getPlayer().transformUpdateInfo();
+        // Message sent to player when transform has worn off.
+        SystemMessage msg = new SystemMessage(SystemMessageId.S1_HAS_WORN_OFF);
+        this.getPlayer().sendPacket(msg);
+    }
 
-	public void removeSkills()
-	{
-		// Nail Attack
-		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(539, 1), false);
-		// Wing Assault
-		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(540, 1), false);
-		// Transfrom Dispel
-		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
-		// Soul Sucking
-		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1471, 1), false);
-		// Death Beam
-		this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1472, 1), false);
-		// Send a Server->Client packet StatusUpdate to the L2PcInstance.
-		this.getPlayer().sendSkillList();
-	}
+    public void removeSkills()
+    {
+        // Nail Attack
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(539, 1), false);
+        // Wing Assault
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(540, 1), false);
+        // Soul Sucking
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1471, 1), false);
+        // Death Beam
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(1472, 1), false);
+        // Transfrom Dispel
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+        // Decrease Bow/Crossbow Attack Speed
+        this.getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+        // Send a Server->Client packet StatusUpdate to the L2PcInstance.
+        this.getPlayer().sendSkillList();
+    }
 
-	public static void main(String[] args)
-	{
-		TransformationManager.getInstance().registerTransformation(new Kamael());
-	}
+    public static void main(String[] args)
+    {
+        TransformationManager.getInstance().registerTransformation(new Kamael());
+    }
 }
