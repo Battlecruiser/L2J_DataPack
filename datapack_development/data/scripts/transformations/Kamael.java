f40384d75e72949c158e86ff57e31c284d7fba79
yellowperil <yellowperil@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-02-06 14:42:07 +0000

    Datapack Commit: [L1663] (thanks Ahmed) - Removed System Message for Feel effects/effect worn out because they are done as part of the effect. - Added transformation effects for a time of 30 min. to all transformations and then they all untransform when the time is up.

--- datapack_development/data/scripts/transformations/Kamael.java
+++ datapack_development/data/scripts/transformations/Kamael.java
@@ -4,17 +4,13 @@ import net.sf.l2j.gameserver.datatables.SkillTable;
 import net.sf.l2j.gameserver.instancemanager.TransformationManager;
 import net.sf.l2j.gameserver.model.L2Skill;
 import net.sf.l2j.gameserver.model.L2Transformation;
-import net.sf.l2j.gameserver.network.SystemMessageId;
-import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 
 /**
  * Description: <br>
  * This will handle the transformation, giving the skills, and removing them, when the player logs out and is transformed these skills
  * do not save. 
  * When the player logs back in, there will be a call from the enterworld packet that will add all their skills.
- * The enterworld packet will transform a player. 
- * The only thing that is missing now is completion of the skill effects and the stat changes of the transformation.<br>
- * - Ahmed
+ * The enterworld packet will transform a player.
  * 
  * @author KenM
  *
@@ -28,30 +24,24 @@ public class Kamael extends L2Transformation
     }
 
     public void onTransform()
-    {
-        // Disable all character skills.
-        for (L2Skill sk : this.getPlayer().getAllSkills())
-        {
-            if (sk != null && !sk.isPassive())
-                this.getPlayer().removeSkill(sk, false);
-        }
-        if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
-        {
-            // give transformation skills
-            transformedSkills();
-            // Message sent to player after transforming.
-            SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
-            this.getPlayer().sendPacket(msg);
-            return;
-        }
-        // give transformation skills
-        transformedSkills();
-        // Update Transformation ID
-        this.getPlayer().transformInsertInfo();
-        // Message sent to player after transforming.
-        SystemMessage msg = new SystemMessage(SystemMessageId.YOU_FEEL_S1_EFFECT);
-        this.getPlayer().sendPacket(msg);
-    }
+	{
+		// Disable all character skills.
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null && !sk.isPassive())
+				this.getPlayer().removeSkill(sk, false);
+		}
+		if (this.getPlayer().transformId() > 0 && !this.getPlayer().isCursedWeaponEquipped())
+		{
+			// give transformation skills
+			transformedSkills();
+			return;
+		}
+		// give transformation skills
+		transformedSkills();
+		// Update Transformation ID
+		this.getPlayer().transformInsertInfo();
+	}
 
     public void transformedSkills()
     {
@@ -72,27 +62,24 @@ public class Kamael extends L2Transformation
     }
 
     public void onUntransform()
-    {
-        // Enable all character skills
-        for (L2Skill sk : this.getPlayer().getAllSkills())
-        {
-            if (sk != null && !sk.isPassive())
-                this.getPlayer().addSkill(sk, false);
-        }
-        // Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
-        if (this.getPlayer().isCursedWeaponEquipped())
-        {
-            removeSkills();
-            return;
-        }
-        // Remove transformation skills
-        removeSkills();
-        // Update Transformation ID
-        this.getPlayer().transformUpdateInfo();
-        // Message sent to player when transform has worn off.
-        SystemMessage msg = new SystemMessage(SystemMessageId.S1_HAS_WORN_OFF);
-        this.getPlayer().sendPacket(msg);
-    }
+	{
+		// Enable all character skills
+		for (L2Skill sk : this.getPlayer().getAllSkills())
+		{
+			if (sk != null && !sk.isPassive())
+				this.getPlayer().addSkill(sk, false);
+		}
+		// Only remove transformation skills. Keeps transformation id for restoration after CW is no longer equipped.
+		if (this.getPlayer().isCursedWeaponEquipped())
+		{
+			removeSkills();
+			return;
+		}
+		// Remove transformation skills
+		removeSkills();
+		// Update Transformation ID
+		this.getPlayer().transformUpdateInfo();
+	}
 
     public void removeSkills()
     {
