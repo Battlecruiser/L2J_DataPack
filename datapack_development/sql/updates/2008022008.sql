e20bd6f8bf76492a7f159e4b2295098f6f50a843
Fulminus <Fulminus@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-02-12 16:49:41 +0000

    Clean-up query and fix for friendlist bugs (related to [L1685]).  Thx Ahmed

---
+++ datapack_development/sql/updates/2008022008.sql
@@ -0,0 +1,13 @@
+create table tmp_friends
+(char_id int, friend_id int);
+
+insert into tmp_friends
+(char_id, friend_id)
+select CF1.char_id, CF1.friend_id from character_friends CF1 where CF1.char_id not in (select CF2.friend_id from character_friends CF2 where CF2.char_id = CF1.friend_id);
+
+delete from character_friends using character_friends 
+inner join tmp_friends TF
+On character_friends.char_id = TF.char_id
+and character_friends.friend_id = TF.friend_id;
+
+drop table tmp_friends;
