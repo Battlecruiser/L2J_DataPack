175c3233e0a3730f6105b21f85b3f3fb0635ffb3
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-02-16 00:09:21 +0000

    Quest 226 updates and improvements by Eyeliner. Should fix #1605. Please test it.

--- datapack_development/data/scripts/quests/226_TestOfHealer/__init__.py
+++ datapack_development/data/scripts/quests/226_TestOfHealer/__init__.py
@@ -22,24 +22,45 @@ MARK_OF_HEALER = 2820
 ADENA = 57
 SHADOW_WEAPON_COUPON_CGRADE = 8870
 
-#this handels all dropdata, npcId:[condition,maxcount,item]
+#NPCs
+BANDELLOS = 30473
+PERRIN = 30428
+ALLANA = 30424
+GUPU = 30658
+ORPHAN = 30659
+WINDY = 30660
+SORIUS = 30327
+MYSTERIOUS = 30661
+PIPER = 30662
+SLEIN = 30663
+KEIN = 30664
+KRISTINA = 30665
+DAURIN = 30674
+
+#MOBs
+TATOMA = 27134
+LETO_LEADER = 27123
+LETO_ASSASSIN = 27124
+LETO_SNIPER = 27125
+LETO_WIZARD = 27126
+LETO_LORD = 27127
+
+#this handles all dropdata, npcId:[condition,maxcount,item,next condition]
 DROPLIST={
-27134:[1,1,0],
-27123:[6,1,SECRET_LETTER1],
-27124:[9,1,SECRET_LETTER2],
-27125:[11,1,SECRET_LETTER3],
-27127:[13,1,SECRET_LETTER4]
+TATOMA:[2,1,0,"3"],
+LETO_LEADER:[11,1,SECRET_LETTER1,"12"],
+LETO_ASSASSIN:[14,1,SECRET_LETTER2,"15"],
+LETO_SNIPER:[16,1,SECRET_LETTER3,"17"],
+LETO_LORD:[18,1,SECRET_LETTER4,"19"]
 }
 
-def radar2(st):
-  st.addRadar(-44358,79442,-3634)
-  
-def radar1(st):
-  st.addRadar(-59985,79234,-3502)
-  
-def radar3(st):
-  st.addRadar(-14158,44953,-3556)
-  
+#this handles 3 groups of mobs spawned by Dark Elves, npcId:[html1,html2,html3,html4]
+ELVES={
+PIPER:["30662-01.htm","30662-02.htm","30662-03.htm","30662-04.htm"],
+SLEIN:["30663-01.htm","30663-02.htm","30663-03.htm","30663-04.htm"],
+KEIN:["30664-01.htm","30664-02.htm","30664-03.htm","30664-04.htm"]
+}
+
 
 class Quest (JQuest) :
 
@@ -72,29 +93,36 @@ class Quest (JQuest) :
           st.set("onlyone","1")
     elif event == "30428_1" :
           htmltext = "30428-02.htm"
-          st.addSpawn(27134,-93254,147559,-2679)
+          st.addSpawn(TATOMA)
+          st.set("cond","2")
     elif event == "30658_1" :
           if st.getQuestItemsCount(ADENA) >= 100000 :
             htmltext = "30658-02.htm"
             st.takeItems(ADENA,100000)
             st.giveItems(PICTURE_OF_WINDY,1)
+            st.set("cond","7")
           else:
             htmltext = "30658-05.htm"
     elif event == "30658_2" :
           htmltext = "30658-03.htm"
-          st.set("cond","5")
+          st.set("cond","6")
+    elif event == "30658_06" :
+          htmltext = "30658-07.htm"
+          st.set("cond","9")
     elif event == "30660_1" :
           htmltext = "30660-02.htm"
     elif event == "30660_2" :
           htmltext = "30660-03.htm"
           st.takeItems(PICTURE_OF_WINDY,1)
           st.giveItems(WINDYS_PEBBLES,1)
+          st.set("cond","8")
     elif event == "30674_1" :
           htmltext = "30674-02.htm"
           st.takeItems(ORDER_OF_SORIUS,1)
           st.addSpawn(27122)
           st.addSpawn(27122)
-          st.addSpawn(27123)
+          st.addSpawn(LETO_LEADER)
+          st.set("cond","11")
           st.playSound("Itemsound.quest_before_battle")
     elif event == "30665_1" :
           htmltext = "30665-02.htm"
@@ -103,7 +131,7 @@ class Quest (JQuest) :
           st.takeItems(SECRET_LETTER3,1)
           st.takeItems(SECRET_LETTER4,1)
           st.giveItems(CRISTINAS_LETTER,1)
-          st.set("cond","14")
+          st.set("cond","22")
     return htmltext
 
 
@@ -114,9 +142,9 @@ class Quest (JQuest) :
 
    npcId = npc.getNpcId()
    id = st.getState()
-   if npcId != 30473 and id != State.STARTED : return htmltext
+   if npcId != BANDELLOS and id != State.STARTED : return htmltext
 
-   if npcId == 30473 :
+   if npcId == BANDELLOS :
      if st.getInt("cond")==0 and st.getInt("onlyone")==0 :
         if (player.getClassId().getId() in [0x04, 0x0f, 0x1d, 0x13]) and player.getLevel() > 38 :
           htmltext = "30473-03.htm"
@@ -127,10 +155,9 @@ class Quest (JQuest) :
           st.exitQuest(1)
      elif st.getInt("cond")==0 and st.getInt("onlyone")==1 :
       htmltext = "<html><body>This quest has already been completed.</body></html>"
-
-     elif npcId == 30473 and st.getInt("cond")<10 and st.getInt("cond")>0 :
+     elif st.getInt("cond")<23 and st.getInt("cond")>0 :
       htmltext = "30473-05.htm"
-     elif st.getInt("cond")==15 and st.getQuestItemsCount(GOLDEN_STATUE)==0 :
+     elif st.getInt("cond")==23 and st.getQuestItemsCount(GOLDEN_STATUE)==0 :
       htmltext = "30473-06.htm"
       st.addExpAndSp(32000,4100)
       st.giveItems(MARK_OF_HEALER,1)
@@ -138,146 +165,107 @@ class Quest (JQuest) :
       st.exitQuest(False)
       st.playSound("ItemSound.quest_finish")
       st.set("onlyone","1")
-     elif st.getInt("cond")==15 and st.getQuestItemsCount(GOLDEN_STATUE) :
+     elif st.getInt("cond")==23 and st.getQuestItemsCount(GOLDEN_STATUE) :
       htmltext = "30473-07.htm"
-   elif npcId == 30428:
-     if st.getInt("cond")==1 and st.getQuestItemsCount(REPORT_OF_PERRIN) :
+   elif npcId == PERRIN:
+     if st.getInt("cond")==1 :
       htmltext = "30428-01.htm"
-     elif npcId == 30428 and st.getInt("cond")==2 :
+     elif st.getInt("cond")==3 :
       htmltext = "30428-03.htm"
-      st.set("cond","3")
+      st.set("cond","4")
       st.takeItems(REPORT_OF_PERRIN,1)
-   elif npcId == 30428 and st.getInt("cond")==3 :
+     elif st.getInt("cond")==4 :
       htmltext = "30428-04.htm"
-   elif npcId == 30659 and st.getInt("cond")==4 :
+   elif npcId == ORPHAN and st.getInt("cond")==4 :
         n = st.getRandom(5)
         if n == 0:
           htmltext = "30659-01.htm"
-        if n == 1:
           htmltext = "30659-02.htm"
-        if n == 2:
+        elif n == 2:
           htmltext = "30659-03.htm"
-        if n == 3:
+        elif n == 3:
           htmltext = "30659-04.htm"
-        if n == 4:
+        elif n == 4:
           htmltext = "30659-05.htm"
-   elif npcId == 30424:
-     if st.getInt("cond")==3 :
+   elif npcId == ALLANA:
+     if st.getInt("cond")==4 :
       htmltext = "30424-01.htm"
-      st.set("cond","4")
-     elif npcId == 30424 and st.getInt("cond")==4 :
+      st.set("cond","5")
+     elif st.getInt("cond")==5 :
       htmltext = "30424-02.htm"
-      st.set("cond","4")
-   elif npcId == 30658 :
-     if st.getInt("cond")==4 and st.getQuestItemsCount(PICTURE_OF_WINDY)==0 and st.getQuestItemsCount(WINDYS_PEBBLES)==0 and st.getQuestItemsCount(GOLDEN_STATUE)==0 :
+   elif npcId == GUPU :
+     if st.getInt("cond")==5 :
       htmltext = "30658-01.htm"
-     elif st.getInt("cond")==4 and st.getQuestItemsCount(PICTURE_OF_WINDY) :
+      st.set("cond","6")
+     elif st.getInt("cond")==6 :
+      htmltext = "30658-01.htm"
+     elif st.getInt("cond")==7 :
       htmltext = "30658-04.htm"
-     elif st.getInt("cond")==4 and st.getQuestItemsCount(WINDYS_PEBBLES) :
+     elif st.getInt("cond")==8 and st.getQuestItemsCount(WINDYS_PEBBLES) > 0 :
       htmltext = "30658-06.htm"
       st.giveItems(GOLDEN_STATUE,1)
       st.takeItems(WINDYS_PEBBLES,1)
-      st.set("cond","5")
-     elif st.getInt("cond")==5 :
+     elif st.getInt("cond")==8 :
+      htmltext = "30658-06.htm"
+     elif st.getInt("cond")==9 :
       htmltext = "30658-07.htm"
-   elif npcId == 30660:
-     if st.getInt("cond")==4 and st.getQuestItemsCount(PICTURE_OF_WINDY) :
+   elif npcId == WINDY:
+     if st.getInt("cond")==7:
       htmltext = "30660-01.htm"
-     elif st.getInt("cond")==4 and st.getQuestItemsCount(WINDYS_PEBBLES) :
+     elif st.getInt("cond")==8 :
       htmltext = "30660-04.htm"
-   elif npcId == 30327 :
-     if st.getInt("cond")==5 :
+   elif npcId == SORIUS :
+     if st.getInt("cond")==9 :
       htmltext = "30327-01.htm"
       st.giveItems(ORDER_OF_SORIUS,1)
-      st.set("cond","6")
-     elif st.getInt("cond")>5 and st.getInt("cond")<12 :
+      st.set("cond","10")
+     elif st.getInt("cond")>9 and st.getInt("cond")<22 :
       htmltext = "30327-02.htm"
-     elif st.getInt("cond")==14 :
+     elif st.getInt("cond")==22 :
       htmltext = "30327-03.htm"
       st.takeItems(CRISTINAS_LETTER,1)
-      st.set("cond","15")
-   elif npcId == 30674:
-     if st.getInt("cond")==6 and st.getQuestItemsCount(ORDER_OF_SORIUS) :
+      st.set("cond","23")
+   elif npcId == DAURIN:
+     if st.getInt("cond")==10 and st.getQuestItemsCount(ORDER_OF_SORIUS) :
       htmltext = "30674-01.htm"
-      st.addSpawn(27122,-97547,106503,-3405)
-      st.addSpawn(27122,-97526,106584,-3405)
-      st.addSpawn(27123,-97441,106585,-3405)
-     elif st.getInt("cond")==6 and st.getQuestItemsCount(SECRET_LETTER1) :
+     elif st.getInt("cond")==12 and st.getQuestItemsCount(SECRET_LETTER1) :
       htmltext = "30674-03.htm"
-      st.set("cond","7")
-   elif npcId == 30662 :
-     if st.getInt("cond")==7 or st.getInt("cond")==8:
-      htmltext = "30662-01.htm"
-      st.set("cond","8")
-      radar1(st)
-     elif st.getInt("cond")==9 or st.getInt("cond")==10:
-      htmltext = "30662-03.htm"
-      st.set("cond","10")
-      radar2(st)
-     elif st.getInt("cond")==11 or st.getInt("cond")==12 :
-      htmltext = "30662-02.htm"
-      st.set("cond","12")
-      radar3(st)
-     elif st.getInt("cond")==13:
-      htmltext = "30662-04.htm"
-   elif npcId == 30663:
-     if st.getInt("cond")==7 or st.getInt("cond")==8:
-      htmltext = "30663-01.htm"
-      st.set("cond","8")
-      radar1(st)
-     elif st.getInt("cond")==9 or st.getInt("cond")==10:
-      htmltext = "30663-03.htm"
-      st.set("cond","10")
-      radar2(st)
-     elif st.getInt("cond")==11 or st.getInt("cond")==12:
-      htmltext = "30663-02.htm"
-      st.set("cond","12")
-      radar3(st)
-     elif st.getInt("cond")==13:
-      htmltext = "30663-04.htm"
-   elif npcId == 30664:
-     if st.getInt("cond")==7 or st.getInt("cond")==8:
-      htmltext = "30664-01.htm"
-      st.set("cond","8")
-      radar1(st)
-     elif st.getInt("cond")==9 or st.getInt("cond")==10:
-      htmltext = "30664-03.htm"
-      st.set("cond","10")
-      radar2(st)
-     elif st.getInt("cond")==11 or st.getInt("cond")==12:
-      htmltext = "30664-02.htm"
-      st.set("cond","12")
-      radar3(st)
-     elif st.getInt("cond")==13:
-      htmltext = "30664-04.htm"
-   elif npcId == 30661:
-     if st.getInt("cond")==8 :
-      htmltext = "30661-01.htm"
-      st.addSpawn(27124,-59863,79210,-3521)
-      st.addSpawn(27124,-59848,79300,-3526)
-      st.addSpawn(27124,-59763,79263,-3525)
-      st.playSound("Itemsound.quest_before_battle")
-      st.set("cond","9")
-     elif st.getInt("cond")==10:
-      htmltext = "30661-02.htm"
-      st.addSpawn(27125,-44399,79348,-3713)
-      st.addSpawn(27125,-44458,79481,-3713)
-      st.addSpawn(27125,-44533,79345,-3713)
-      st.playSound("Itemsound.quest_before_battle")
-      st.set("cond","11")
-     elif st.getInt("cond")==12:
-      htmltext = "30661-03.htm"
-      st.addSpawn(27126,-14244,44948,-3593)
-      st.addSpawn(27126,-14316,44844,-3593)
-      st.addSpawn(27127,-14166,44779,-3593)
-      st.playSound("Itemsound.quest_before_battle")
       st.set("cond","13")
-     elif st.getInt("cond") == 13 :
+   elif npcId == MYSTERIOUS:
+     if st.getInt("cond")==13 or st.getInt("cond")==14 :
+      htmltext = "30661-01.htm"
+      st.addSpawn(LETO_ASSASSIN)
+      st.addSpawn(LETO_ASSASSIN)
+      st.addSpawn(LETO_ASSASSIN)
+      st.playSound("Itemsound.quest_before_battle")
+      st.set("cond","14")
+     elif st.getInt("cond")==15 or st.getInt("cond")==16:
+      htmltext = "30661-02.htm"
+      st.addSpawn(LETO_SNIPER)
+      st.addSpawn(LETO_SNIPER)
+      st.addSpawn(LETO_SNIPER)
+      st.playSound("Itemsound.quest_before_battle")
+      st.set("cond","16")
+     elif st.getInt("cond")==17 or st.getInt("cond")==18:
+      htmltext = "30661-03.htm"
+      st.addSpawn(LETO_WIZARD)
+      st.addSpawn(LETO_WIZARD)
+      st.addSpawn(LETO_LORD)
+      st.playSound("Itemsound.quest_before_battle")
+      st.set("cond","18")
+     elif st.getInt("cond") == 19 :
       htmltext = "30661-04.htm"
-   elif npcId == 30665 :
-     if st.getInt("cond")==13 :
+      st.set("cond","20")
+   elif npcId in ELVES.keys() :
+     if st.getInt("cond") == 20:
+      htmltext = ELVES[npcId][2]
+      st.set("cond","21")
+     elif st.getInt("cond") == 21:
+      htmltext = ELVES[npcId][3]
+   elif npcId == KRISTINA :
+     if st.getInt("cond")==21 :
       htmltext = "30665-01.htm"
-     else :
+     elif st.getInt("cond")<21 :
       htmltext = "30665-03.htm"
    return htmltext
 
@@ -287,23 +275,23 @@ class Quest (JQuest) :
    if st.getState() != State.STARTED : return 
    
    npcId = npc.getNpcId()
-   condition,maxcount,item=DROPLIST[npcId] 
+   condition,maxcount,item,next_condition=DROPLIST[npcId] 
    if st.getInt("cond")==condition and st.getQuestItemsCount(item)<maxcount:
      if item != 0:
        st.giveItems(item,1)
-     if npcId == 27134:
-       st.set("cond","2")
+     if next_condition != 0:
+       st.set("cond",next_condition)
      st.playSound("Itemsound.quest_middle")
    return
 
 QUEST       = Quest(226,qn,"Test Of Healer")
 
-QUEST.addStartNpc(30473)
+QUEST.addStartNpc(BANDELLOS)
 
-QUEST.addTalkId(30473)
+QUEST.addTalkId(BANDELLOS)
 
-for npcId in [30327,30424,30428,30658,30659,30660,30661,30662,30663,30664,30665,30674]:
+for npcId in [SORIUS,ALLANA,PERRIN,GUPU,ORPHAN,WINDY,MYSTERIOUS,PIPER,SLEIN,KEIN,KRISTINA,DAURIN]:
   QUEST.addTalkId(npcId)
- 
-for mobId in [20150,27123,27124,27125,27127,27134]:
+
+for mobId in [LETO_LEADER,LETO_ASSASSIN,LETO_SNIPER,LETO_WIZARD,LETO_LORD,TATOMA]:
   QUEST.addKillId(mobId)
