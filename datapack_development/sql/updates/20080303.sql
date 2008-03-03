1f6ce7e100e9fa1849ce27dcbf7f7cea359d4d2a
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-03-03 16:47:38 +0000

    DP side for [L1771]

---
+++ datapack_development/sql/updates/20080303.sql
@@ -0,0 +1,4 @@
+alter table npc add column ss int(4) default 0;
+alter table npc add column bss int(4) default 0;
+alter table npc add column ss_rate int(3) default 0;
+alter table npc add column AI varchar(8) default "fighter";
