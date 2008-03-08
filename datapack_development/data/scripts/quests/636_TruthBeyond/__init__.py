4edb0f65a0ba423a81ad15d538795cfb32a83b26
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-03-08 15:14:48 +0000

    JYTHON: small fix for quest 636, should be repeatable. Thanks zhyla

--- datapack_development/data/scripts/quests/636_TruthBeyond/__init__.py
+++ datapack_development/data/scripts/quests/636_TruthBeyond/__init__.py
@@ -28,7 +28,7 @@ class Quest (JQuest) :
        st.playSound("ItemSound.quest_finish")
        st.giveItems(MARK,1)
        st.unset("cond")
-       st.exitQuest(False)
+       st.exitQuest(1)
     return htmltext
 
  def onTalk (self,npc,player):
