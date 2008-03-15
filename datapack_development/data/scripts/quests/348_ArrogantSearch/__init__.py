ae0894724f088f4da37b2827445c27b3a0111800
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-03-15 02:38:36 +0000

    JYTHON: Committing a few contribs. 	*Adding a new quest, 646 : Signs of Revolt (thx -:edge:- for contributing/sharing) 	*Some updates to quest 348 : An Arrogant Search, by Eyeliner. This update adds the following functionality to the quest: 		- Allows players to gain bloody fabrics by hitting shamans and overlords for first fabric 		- Allows players to gain other white fabrics from Hannelin 		- Allows players to gain bloody fabrics from angels 		- Updates quest rewards htmls still need to be updated in 348, but this is a start.

--- datapack_development/data/scripts/quests/348_ArrogantSearch/__init__.py
+++ datapack_development/data/scripts/quests/348_ArrogantSearch/__init__.py
@@ -6,6 +6,7 @@ import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
+from net.sf.l2j.gameserver.serverpackets import RadarControl
 #Quest info
 QUEST_NUMBER,QUEST_NAME,QUEST_DESCRIPTION = 348,"ArrogantSearch","An Arrogant Search"
 qn = "348_ArrogantSearch"
@@ -19,7 +20,11 @@ ANGEL_KILLER = 27184
 PLATINUM_TRIBE_SHAMAN = 20828
 PLATINUM_TRIBE_OVERLORD = 20829
 LESSER_GIANT_MAGE = 20657
-LESSER_GIANT_ELDER =20658
+LESSER_GIANT_ELDER = 20658
+GUARDIAN_ANGEL_1 = 20830
+GUARDIAN_ANGEL_2 = 20859
+SEAL_ANGEL_1 = 20831
+SEAL_ANGEL_2 = 20860
 
 #NPCS TO TALK TO
 HANELLIN = 30864
@@ -30,6 +35,9 @@ ARK_GUARDIANS_CORPSE = 30980
 HARNE = 30144
 CLAUDIA_ATHEBALT = 31001
 MARTIEN = 30645
+GUSTAV_ATHEBALDT = 30760
+HARDIN = 30832
+HEINE = 30969
 
 #items
 TITANS_POWERSTONE = 4287
@@ -50,8 +58,11 @@ BRANCH_OF_SAINT = 4399 # Ark3 (after fight with Shadowfang)
 WHITE_FABRIC_0 = 4400  #talk to Hanellin to see what to do (for companions)
 WHITE_FABRIC_2 = 5232  #to use on Guardian Angels and Seal Angels
 
-ANTIDOTE = 1831
-HEALING_POTION = 1061
+ANTIDOTE = 1831 #potion
+HEALING_POTION = 1061 #potion
+ANIMAL_BONE = 1872 #reward for bringing the first fabric
+SYNTHETIC_COKES = 1888 #reward for bringing the first fabric
+ADENA = 57 # cheater reward
 
 #ARK: [key, summon, no-key text, openning-with-key text, already-openned text, content item]
 ARKS={
@@ -67,6 +78,13 @@ CLAUDIA_ATHEBALT: [HANELLINS_SECOND_LETTER, BOOK_OF_SAINT, '31001-01.htm', '3100
 MARTIEN: [HANELLINS_THIRD_LETTER, BRANCH_OF_SAINT, '30645-01.htm', '30645-02.htm', '30645-03.htm', [50693,158674,376]]
 }
 
+# npc: fabrics to give, variable to set, ok htm, not ok htm, done htm
+BLOODY_OWNERS={
+GUSTAV_ATHEBALDT: [3, 'athebaldt_delivery', '30760-01.htm', '30760-01a.htm', '30760-01b.htm'],
+HARDIN: [1, 'hardin_delivery', '30832-01.htm', '30832-01a.htm', '30832-01b.htm'],
+HEINE: [6, 'heine_delivery', '30969-01.htm', '30969-01a.htm', '30969-01b.htm']
+}
+
 #mob: cond, giveItem, amount, chance%, takeItem (assumed to take only 1 of it)
 DROPS ={
 LESSER_GIANT_MAGE: [2,TITANS_POWERSTONE,1,10,0],
@@ -76,6 +94,30 @@ ARK_GUARDIAN_ELBEROTH: [5, SECOND_KEY_OF_ARK,1,100,0],
 ARK_GUARDIAN_SHADOWFANG: [5, THIRD_KEY_OF_ARK,1,100,0],
 PLATINUM_TRIBE_SHAMAN: [25,BLOODED_FABRIC,1,10,WHITE_FABRIC_1],
 PLATINUM_TRIBE_OVERLORD: [25,BLOODED_FABRIC,1,10,WHITE_FABRIC_1],
+GUARDIAN_ANGEL_1: [26,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+GUARDIAN_ANGEL_2: [26,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+SEAL_ANGEL_1: [26,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+SEAL_ANGEL_2: [26,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+}
+
+#pathologic situation, mobs in stage 29 drop same items as in stage 26 :/
+#mob: cond, giveItem, amount, chance%, takeItem (assumed to take only 1 of it)
+DROPS_29 ={
+GUARDIAN_ANGEL_1: [29,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+GUARDIAN_ANGEL_2: [29,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+SEAL_ANGEL_1: [29,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+SEAL_ANGEL_2: [29,BLOODED_FABRIC,10,25,WHITE_FABRIC_1],
+}
+
+#mob: cond, giveItem, amount, chance%, takeItem (assumed to take only 1 of it)
+ATTACK_DROPS_24 = {
+PLATINUM_TRIBE_SHAMAN: [24,BLOODED_FABRIC,1,2,WHITE_FABRIC_1],
+PLATINUM_TRIBE_OVERLORD: [24,BLOODED_FABRIC,1,2,WHITE_FABRIC_1],
+}
+
+ATTACK_DROPS_25 = {
+PLATINUM_TRIBE_SHAMAN: [25,BLOODED_FABRIC,1,2,WHITE_FABRIC_1],
+PLATINUM_TRIBE_OVERLORD: [25,BLOODED_FABRIC,1,2,WHITE_FABRIC_1],
 }
 
 """
@@ -98,6 +140,7 @@ class Quest (JQuest) :
  def onEvent (self,event,st) :
     htmltext = event
     if event == "30864_02" :
+        st.setState(State.STARTED)
         st.set("cond","2")
         htmltext = "30864-03.htm"
     elif event == "30864_04a" :  #work alone
@@ -111,6 +154,39 @@ class Quest (JQuest) :
         st.takeItems(TITANS_POWERSTONE,-1)
         htmltext = "not yet implemented"
         #todo: give flowers & handle the multiperson quest...
+    elif event == "30864_07" : #platinum tribe info
+        htmltext = "30864-07b.htm"
+    elif event == "30864_07b" : #platinum tribe info 2
+        htmltext = "30864-07c.htm"
+    elif event == "30864_07c" : #money vs meet emperor
+        htmltext = "30864-07d.htm"
+    elif event == "30864_07meet" : #meet emperor
+        htmltext = "30864-07meet.htm"
+        st.set("cond","24")
+    elif event == "30864_07money" : #want more fabrics
+        htmltext = "30864-07money.htm"
+        st.set("cond","25")
+    elif event == "30864_08" : # 
+        htmltext = "30864-08b.htm"
+    elif event == "30864_08b" : # more about angels blood
+        htmltext = "30864-08c.htm" # more about angels blood
+        st.giveItems(WHITE_FABRIC_1,9)
+        st.set("cond", "26")
+    elif event == "30864_09" : # Who are those people?
+        st.set("cond", "27")
+        htmltext = "30864-09c.htm"
+    elif event == "30864_10continue" : # next round of fabrics
+        htmltext = "30864-08c.htm" # more about angels blood
+        st.giveItems(WHITE_FABRIC_1,10) # next 10 fabrics
+        for i in BLOODY_OWNERS.keys() : # must delivery again
+            st.set(BLOODY_OWNERS[i][1],"0")
+        st.set("cond", "29") # start hunting angels again
+    elif event == "30864_10quit" : # done with fabrics
+        htmltext = "30864-10c.htm" # disappointment
+        st.takeItems(WHITE_FABRIC_1,-1) # just to be sure
+        st.takeItems(BLOODED_FABRIC,-1) # just to be sure
+        st.playSound("ItemSound.quest_finish")
+        st.exitQuest(1)
     return htmltext
 
  def onTalk (self,npc,player):
@@ -123,6 +199,7 @@ class Quest (JQuest) :
     if npcId != HANELLIN and id != State.STARTED : return htmltext
 
     cond = st.getInt("cond")
+    reward1 = st.getInt("reward1")
     if npcId == HANELLIN :
         if id == State.CREATED :
             # if the quest was State.COMPLETED and the player still has a blooded fabric
@@ -137,17 +214,20 @@ class Quest (JQuest) :
                     htmltext = "30864-01.htm"     #not qualified
                     st.exitQuest(1)
                 elif cond==0 :
-                    st.setState(State.STARTED)
                     st.set("cond","1")
+                    st.set("reward1","0")    # for first reward in cond == 25
+                    for i in BLOODY_OWNERS.keys() : # goods are not delivered
+                        st.set(BLOODY_OWNERS[i][1],"0")
                     htmltext = "30864-02.htm"    # Successful start: begin the dialog which will set cond=2
-        # Player abandoned in the middle of last dialog...repeat the dialog.
-        elif cond==1  :
-            htmltext = "30864-02.htm"    # begin the dialog which will set cond=2
+                # Player abandoned in the middle of last dialog...repeat the dialog.
+                elif cond==1  :
+                    htmltext = "30864-02.htm"    # begin the dialog which will set cond=2
         # Has returned before getting the powerstone
-        elif cond==2 and st.getQuestItemsCount(TITANS_POWERSTONE)==0 :
-            htmltext = "30864-03a.htm"    # go get the titan's powerstone
         elif cond==2 :
-            htmltext = "30864-04.htm"    # Ask "work alone or in group?"...only alone is implemented in v0.1
+            if st.getQuestItemsCount(TITANS_POWERSTONE)==0 :
+                htmltext = "30864-03a.htm"    # go get the titan's powerstone
+            else :
+                htmltext = "30864-04.htm"    # Ask "work alone or in group?"...only alone is implemented in v0.1
         elif cond==4 :
             st.set("cond","5")
             st.giveItems(HANELLINS_FIRST_LETTER,1)
@@ -157,28 +237,68 @@ class Quest (JQuest) :
         elif cond == 5 and st.getQuestItemsCount(BOOK_OF_SAINT)+st.getQuestItemsCount(BLOOD_OF_SAINT)+st.getQuestItemsCount(BRANCH_OF_SAINT)<3 :
             htmltext = "30864-05.htm"    # Repeat: Go get the 3 sacred relics
         elif cond == 5 :
-            htmltext = "30864-06.htm"     # All relics collected!...Get me antidotes & greater healing
+            htmltext = "30864-06.htm"     # All relics collected!...Get me antidotes & healing
             st.takeItems(BOOK_OF_SAINT,-1)
             st.takeItems(BLOOD_OF_SAINT,-1)
             st.takeItems(BRANCH_OF_SAINT,-1)
             st.set("cond","22")
         elif cond == 22 and st.getQuestItemsCount(ANTIDOTE)<5 and st.getQuestItemsCount(HEALING_POTION)<1:
             htmltext = "30864-06a.htm"     # where are my antidotes & greater healing
+        elif cond == 22 and st.getQuestItemsCount(WHITE_FABRIC_1)>0 :
+            htmltext = "30864-07c.htm"
         elif cond == 22 :
             st.takeItems(ANTIDOTE,5)
             st.takeItems(HEALING_POTION,1)
             if st.getInt("companions") == 0 :
-                st.set("cond","25")
                 htmltext = "30864-07.htm"    # go get platinum tribe blood...
                 st.giveItems(WHITE_FABRIC_1,1)
             else:
                 st.set("cond","23")
                 htmltext = "not implemented yet"
                 st.giveItems(WHITE_FABRIC_0,3)
+        elif cond == 24 and st.getQuestItemsCount(BLOODED_FABRIC)<1 :
+            htmltext = "30864-07a.htm"
         elif cond == 25 and st.getQuestItemsCount(BLOODED_FABRIC)<1 :
             htmltext = "30864-07a.htm"
-        # the remaining of hanellin's dialogs, including the hunt for Angels which is only done for money,
-        # are NOT implemented.
+        elif cond == 25 and reward1 > 0:
+            htmltext = "30864-08b.htm" # more about angels
+        elif cond == 25 :
+            htmltext = "30864-08.htm" #you want more of this?
+            st.giveItems(ANIMAL_BONE,2)
+            st.giveItems(SYNTHETIC_COKES,2)
+            lowbgrade = st.getRandom(10)+4103 #Low B grade weapon key materials
+            st.giveItems(lowbgrade,1)
+            st.set("reward1","1")
+        elif cond == 26 and st.getQuestItemsCount(WHITE_FABRIC_1)>0 :
+            htmltext = "30864-09a.htm" # 
+        elif cond == 26 and st.getQuestItemsCount(BLOODED_FABRIC)<10 :
+            htmltext = "30864-09b.htm" # Where are other pieces?
+            st.giveItems(ADENA,5000)
+            st.takeItems(BLOODED_FABRIC,-1)
+            st.playSound("ItemSound.quest_finish")
+            st.exitQuest(1)
+        elif cond == 26 :
+            htmltext = "30864-09.htm" # Ah, 10 fabrics.
+        elif cond == 27 and st.getInt(BLOODY_OWNERS[GUSTAV_ATHEBALDT][1]) + st.getInt(BLOODY_OWNERS[HARDIN][1]) + st.getInt(BLOODY_OWNERS[HEINE][1])<3 :
+            htmltext = "30864-10a.htm" # still not delivered?
+        elif cond == 27 :
+            htmltext = "30864-10.htm" # good money
+            st.giveItems(ANIMAL_BONE,5)
+            highbgrade = st.getRandom(8)+4113
+            st.giveItems(highbgrade,1)
+            st.set("cond","28")
+        elif cond == 28:
+            htmltext = "30864-10b.htm" # still not decided to continue or quit
+        elif cond == 29 and st.getQuestItemsCount(WHITE_FABRIC_1)>0 :
+            htmltext = "30864-09a.htm" #
+        elif cond == 29 and st.getQuestItemsCount(BLOODED_FABRIC)<10 :
+            htmltext = "30864-09b.htm" # Where are other pieces?
+            st.giveItems(ADENA,5000)
+            st.takeItems(BLOODED_FABRIC,-1)
+            st.playSound("ItemSound.quest_finish")
+            st.exitQuest(1)
+        elif cond == 29 :
+            htmltext = "30864-09.htm" # Ah, 10 fabrics.
     # Other NPCs follow:
     elif cond == 5:
         if npcId in ARK_OWNERS.keys() :
@@ -197,7 +317,7 @@ class Quest (JQuest) :
             # if you do not have the key (first meeting)
             if st.getQuestItemsCount(ARKS[npcId][0])==0:
                 if ARKS[npcId][1] != 0 :    # spawn the NPC, if appropriate
-                    st.addSpawn(ARKS[npcId][1],player.getClientX(),player.getClientY(),player.getClientZ(),120000)
+                    st.addSpawn(ARKS[npcId][1],120000)
                 return ARKS[npcId][2]
             # if the player already has openned the chest and has its content, show "chest empty"
             elif st.getQuestItemsCount(ARKS[npcId][5])==1:  
@@ -209,14 +329,53 @@ class Quest (JQuest) :
         elif npcId == ARK_GUARDIANS_CORPSE :
             # if you do not have the key (first meeting)
             if st.getQuestItemsCount(FIRST_KEY_OF_ARK)==0 and st.getInt("angelKillerIsDefeated")==0 :
-                st.addSpawn(ANGEL_KILLER,player.getClientX(),player.getClientY(),player.getClientZ(),120000)
+                st.addSpawn(ANGEL_KILLER,120000)
                 htmltext = "30980-01.htm"
             elif st.getQuestItemsCount(FIRST_KEY_OF_ARK)==0 and st.getInt("angelKillerIsDefeated")==1 :
                 st.giveItems(FIRST_KEY_OF_ARK,1)
                 htmltext = "30980-02.htm"
             else :
                 htmltext = "30980-03.htm"
+    elif cond == 27 :
+        if npcId in BLOODY_OWNERS.keys() : 
+            if st.getInt(BLOODY_OWNERS[npcId][1])<1 :
+                if st.getQuestItemsCount(BLOODED_FABRIC)>=BLOODY_OWNERS[npcId][0] : # deliver blood
+                    st.takeItems(BLOODED_FABRIC,BLOODY_OWNERS[npcId][0])
+                    st.set(BLOODY_OWNERS[npcId][1],"1")
+                    htmltext = BLOODY_OWNERS[npcId][2]
+                else : # not enough blood
+                    htmltext = BLOODY_OWNERS[npcId][3] 
+            else : # already delivered
+                htmltext = BLOODY_OWNERS[npcId][4]
     return htmltext
+ 
+ def onAttack (self, npc, player, damage, isPet):
+     st = player.getQuestState(qn)
+     if not st : return
+     if st.getState() != State.STARTED : return
+   
+     npcId = npc.getNpcId()
+     if npcId in ATTACK_DROPS_24.keys() :
+         cond = ATTACK_DROPS_24[npcId][0]
+         chance =  ATTACK_DROPS_24[npcId][3]
+         if st.getInt("cond") == cond and st.getRandom(1000) < ATTACK_DROPS_24[npcId][3] and st.getQuestItemsCount(ATTACK_DROPS_24[npcId][4]) > 0 :  # Attack drops are low chance
+             st.giveItems(ATTACK_DROPS_24[npcId][1],ATTACK_DROPS_24[npcId][2])
+             st.playSound("ItemSound.quest_itemget")
+             st.takeItems(ATTACK_DROPS_24[npcId][4],1)
+             if cond == 24:
+                 st.playSound("ItemSound.quest_finish")
+                 st.exitQuest(1)
+     elif npcId in ATTACK_DROPS_25.keys() :
+         cond = ATTACK_DROPS_25[npcId][0]
+         chance =  ATTACK_DROPS_25[npcId][3]
+         if st.getInt("cond") == cond and st.getRandom(1000) < ATTACK_DROPS_25[npcId][3] st.getQuestItemsCount(ATTACK_DROPS_25[npcId][4]) > 0 :  # Attack drops are low chance
+             st.giveItems(ATTACK_DROPS_25[npcId][1],ATTACK_DROPS_25[npcId][2])
+             st.playSound("ItemSound.quest_itemget")
+             st.takeItems(ATTACK_DROPS_25[npcId][4],1)
+             if cond == 24:
+                 st.playSound("ItemSound.quest_finish")
+                 st.exitQuest(1)
+     return
 
  def onKill(self,npc,player,isPet):
      st = player.getQuestState(qn)
@@ -226,17 +385,23 @@ class Quest (JQuest) :
      npcId = npc.getNpcId()
      if npcId in DROPS.keys() :
          cond = DROPS[npcId][0]
-         chance =  DROPS[npcId][3]
-         if (st.getInt("cond") == cond) and (st.getQuestItemsCount(DROPS[npcId][1]) < DROPS[npcId][2]) and (st.getRandom(100) < DROPS[npcId][3]) :
-             st.giveItems(DROPS[npcId][1],DROPS[npcId][2])
+         if st.getInt("cond") == cond and st.getQuestItemsCount(DROPS[npcId][1]) < DROPS[npcId][2] and st.getRandom(100) < DROPS[npcId][3] and (DROPS[npcId][4] == 0 or st.getQuestItemsCount(DROPS[npcId][4])>0) :
+             st.giveItems(DROPS[npcId][1],1)
              st.playSound("ItemSound.quest_itemget")
              if DROPS[npcId][4] != 0:
                  st.takeItems(DROPS[npcId][4],1)
              # in accordance to http://forum.l2jdp.com/viewtopic.php?t=2974
              # quest ends when you get the blooded fabric
-             if cond == 25:
+             if cond == 24:
                  st.playSound("ItemSound.quest_finish")
                  st.exitQuest(1)
+     if npcId in DROPS_29.keys() :
+         cond = DROPS_29[npcId][0]
+         if st.getInt("cond") == cond and st.getQuestItemsCount(DROPS_29[npcId][1]) < DROPS[npcId][2] and st.getRandom(100) < DROPS[npcId][3] and (DROPS[npcId][4] == 0 or st.getQuestItemsCount(DROPS[npcId][4])>0) :
+             st.giveItems(DROPS_29[npcId][1],1)
+             st.playSound("ItemSound.quest_itemget")
+             if DROPS_29[npcId][4] != 0:
+                 st.takeItems(DROPS_29[npcId][4],1)
      if npcId == ANGEL_KILLER :
          return "Ha, that was fun! If you wish to find the key, search the corpse"
      return
@@ -248,12 +413,17 @@ QUEST       = Quest(QUEST_NUMBER, str(QUEST_NUMBER)+"_"+QUEST_NAME, QUEST_DESCRI
 
 QUEST.addStartNpc(HANELLIN)
 QUEST.addTalkId(HANELLIN)
-
 QUEST.addTalkId(ARK_GUARDIANS_CORPSE)
+
 for i in ARK_OWNERS.keys() :
     QUEST.addTalkId(i)
 for i in ARKS.keys() :
     QUEST.addTalkId(i)
+for i in BLOODY_OWNERS.keys() :
+    QUEST.addTalkId(i)
 
 for i in DROPS.keys():
   QUEST.addKillId(i)
+
+QUEST.addAttackId(PLATINUM_TRIBE_SHAMAN)
+QUEST.addAttackId(PLATINUM_TRIBE_OVERLORD)
