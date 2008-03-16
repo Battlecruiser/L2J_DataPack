24faa0221dd43c513c4a355f9bff3110cfc706ce
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-03-16 13:47:50 +0000

    SQL: Update was required after [4241], so cleaned up core a little to prevent such issues in the future all together. Now the zone names can be changed as many times as needed, just don't change their id. Thus, I changed the structure of grandboss_list a little and this requires [L1822] and the sql update in this commit. Fixes #L2241, thx L2Nemesis for reporting.

--- datapack_development/sql/grandboss_list.sql
+++ datapack_development/sql/grandboss_list.sql
@@ -4,6 +4,6 @@
 
 CREATE TABLE IF NOT EXISTS grandboss_list (
   `player_id` decimal(11,0) NOT NULL,
-  `zone` varchar(40) NOT NULL,
+  `zone` decimal(11,0) NOT NULL,
   PRIMARY KEY  (`player_id`)
 );
