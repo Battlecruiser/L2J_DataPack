4191e0cd9b55a70c44f17d5a6bdefd09a252782a
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-03-30 20:53:58 +0000

    updated nobless ports in kamael village

--- datapack_development/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
+++ datapack_development/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
@@ -6,16 +6,22 @@ from net.sf.l2j.gameserver.model.quest        import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "2000_NoblesseTeleport"
-NPC=[30006,30059,30080,30134,30146,30177,30233,30256,30320,30540,30576,30836,30848,30878,30899,31275,31320,31964]
+NPC=[30006,30059,30080,30134,30146,30177,30233,30256,30320,30540,30576,30836,30848,30878,30899,31275,31320,31964,32163]
 
 class Quest (JQuest) :
 
  def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
- def onTalk (Self,npc,player):
+ def onEvent (self,event,st):
+    return event
+
+ def onTalk (self,npc,player):
     st = player.getQuestState(qn) 
     if player.isNoble() == 1 :
-      htmltext="noble.htm"
+      bypass = "<a action=\"bypass -h Quest 2000_NoblesseTeleport noble-nopass.htm"
+      if st.getQuestItemsCount(6651):
+         bypass = "<a action=\"bypass -h npc_%objectId%_Chat 3"
+      htmltext=st.showHtmlFile("noble.htm").replace("%bypass%",str(bypass))
     else :
       htmltext="nobleteleporter-no.htm"
     st.exitQuest(1)
