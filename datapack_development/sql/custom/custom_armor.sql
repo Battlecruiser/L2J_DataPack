073695aad0aff980fd1552c750712a0700e27fbf
DrLecter <DrLecter@users.noreply.github.com>
2008-04-05 02:15:05 +0000

    Datapack support for easy customization facilities, by Ahmed. It's essentially changes in the database_installer scripts and some sql files reorganized.

---
+++ datapack_development/sql/custom/custom_armor.sql
@@ -0,0 +1,28 @@
+-- --------------------------------
+-- Table structure for custom_armor
+-- --------------------------------
+DROP TABLE IF EXISTS `custom_armor`;
+CREATE TABLE IF NOT EXISTS `custom_armor` (
+  `item_id` int(11) NOT NULL default '0',
+  `name` varchar(70) default NULL,
+  `bodypart` varchar(15) NOT NULL default '',
+  `crystallizable` varchar(5) NOT NULL default '',
+  `armor_type` varchar(5) NOT NULL default '',
+  `weight` int(5) NOT NULL default '0',
+  `material` varchar(15) NOT NULL default '',
+  `crystal_type` varchar(4) NOT NULL default '',
+  `avoid_modify` int(1) NOT NULL default '0',
+  `duration` int(3) NOT NULL default '0',
+  `p_def` int(3) NOT NULL default '0',
+  `m_def` int(2) NOT NULL default '0',
+  `mp_bonus` int(3) NOT NULL default '0',
+  `price` int(11) NOT NULL default '0',
+  `crystal_count` int(4) default NULL,
+  `sellable` varchar(5) default NULL,
+  `dropable` varchar(5) NOT NULL default 'true',
+  `destroyable` varchar(5) NOT NULL default 'true',
+  `tradeable` varchar(5) NOT NULL default 'true',
+  `item_skill_id` decimal(11,0) NOT NULL default '0',
+  `item_skill_lvl` decimal(11,0) NOT NULL default '0',
+  PRIMARY KEY (`item_id`)
+) DEFAULT CHARSET=utf8;
---
+++ datapack_development/sql/custom/custom_notspawned.sql
@@ -0,0 +1,8 @@
+#--------------------------------------
+# Table structure for custom_notspawned
+#--------------------------------------
+CREATE TABLE `custom_notspawned` (
+  `id` int(11) NOT NULL,
+  `isCustom` int(1) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
