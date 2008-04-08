e0853ecab27ac78bb07ca64c67cbb3dd1569989c
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-04-08 09:05:43 +0000

    SQL: fort query fix

--- datapack_development/sql/fort.sql
+++ datapack_development/sql/fort.sql
@@ -14,24 +14,25 @@ CREATE TABLE IF NOT EXISTS `fort` (
 -- ----------------------------
 -- Records 
 -- ----------------------------
-INSERT INTO `fort` VALUES ('101', 'Shanty', '0', '0', '0', '0');
-INSERT INTO `fort` VALUES ('102', 'Southern', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('103', 'Hive', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('104', 'Valley', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('105', 'Ivory', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('106', 'Narsell', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('107', 'Basin', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('108', 'White Sands', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('109', 'Borderland', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('110', 'Marshland', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('111', 'Archaic', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('112', 'Floran', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('113', 'Cloud Mountain', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('114', 'Tanor', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('115', 'Dragonspine', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('116', 'Land Dragon', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('117', 'Western Guard', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('118', 'Hunters', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('119', 'Aaru', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('120', 'Demon', '0', '0', '20', '0');
-INSERT INTO `fort` VALUES ('121', 'Monastic', '0', '0', '20', '0');
+INSERT IGNORE INTO `fort` VALUES 
+('101','Shanty','0','0','0','0'),
+('102','Southern','0','0','20','0'),
+('103','Hive','0','0','20','0'),
+('104','Valley','0','0','20','0'),
+('105','Ivory','0','0','20','0'),
+('106','Narsell','0','0','20','0'),
+('107','Basin','0','0','20','0'),
+('108','White Sands','0','0','20','0'),
+('109','Borderland','0','0','20','0'),
+('110','Marshland','0','0','20','0'),
+('111','Archaic','0','0','20','0'),
+('112','Floran','0','0','20','0'),
+('113','Cloud Mountain','0','0','20','0'),
+('114','Tanor','0','0','20','0'),
+('115','Dragonspine','0','0','20','0'),
+('116','Land Dragon','0','0','20','0'),
+('117','Western Guard','0','0','20','0'),
+('118','Hunters','0','0','20','0'),
+('119','Aaru','0','0','20','0'),
+('120','Demon','0','0','20','0'),
+('121','Monastic','0','0','20','0');
