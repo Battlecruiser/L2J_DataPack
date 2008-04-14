f1b2905b526ce5ec61d96ad42ad791ef560792ca
Emperorc <Emperorc@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-04-14 01:58:59 +0000

    JYTHON: Fix for noble teleports. For some reason, the npcObjectId was not being inserted properly, making it impossible to use the noble pass to teleport, because the link was broken. This will hopefully fix that issue. Thx Zhyla for reporting.

--- datapack_development/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
+++ datapack_development/data/scripts/teleports/2000_NoblesseTeleport/__init__.py
@@ -1,12 +1,15 @@
 # Created by Ham Wong on 2007.02.28
 import sys
-
 from net.sf.l2j.gameserver.model.quest        import State
 from net.sf.l2j.gameserver.model.quest        import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "2000_NoblesseTeleport"
 NPC=[30006,30059,30080,30134,30146,30177,30233,30256,30320,30540,30576,30836,30848,30878,30899,31275,31320,31964,32163]
+
+html = "<html><body>Oh, you're a Noblesse! Why didn't you say so? We offer a special level of service to the Noblesse!<br><br>Use the Noblesse Gate Pass.<br>\
+        <a action=\"bypass -h %bypass%\">Teleport to Hunting Grounds</a><br><br>Do not Use the Noblesse Gate Pass<br>\
+        <a action=\"bypass -h npc_%objectId%_Chat 2\">Teleport to Hunting Grounds</a><br><a action=\"bypass -h npc_%objectId%_Chat 0\">Return</a></body></html>"
 
 class Quest (JQuest) :
 
@@ -16,12 +19,12 @@ class Quest (JQuest) :
     return event
 
  def onTalk (self,npc,player):
-    st = player.getQuestState(qn) 
+    st = player.getQuestState(qn)
     if player.isNoble() == 1 :
-      bypass = "<a action=\"bypass -h Quest 2000_NoblesseTeleport noble-nopass.htm"
+      bypass = "Quest 2000_NoblesseTeleport noble-nopass.htm"
       if st.getQuestItemsCount(6651):
-         bypass = "<a action=\"bypass -h npc_%objectId%_Chat 3"
-      htmltext=st.showHtmlFile("noble.htm").replace("%bypass%",str(bypass))
+         bypass = "npc_%objectId%_Chat 3"
+      htmltext=html.replace("%bypass%",str(bypass))
     else :
       htmltext="nobleteleporter-no.htm"
     st.exitQuest(1)
