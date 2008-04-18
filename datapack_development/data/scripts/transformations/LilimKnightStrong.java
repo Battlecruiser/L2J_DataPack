5f070dbca12c1de725815409a4f92823881a5f51
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-04-18 14:15:15 +0000

    JAVA,HTML: Transformation fixes by Zhyla (thx)

--- datapack_development/data/scripts/transformations/LilimKnightStrong.java
+++ datapack_development/data/scripts/transformations/LilimKnightStrong.java
@@ -20,7 +20,7 @@ public class LilimKnightStrong extends L2Transformation
 	public LilimKnightStrong()
 	{
 		// id, duration (secs), colRadius, colHeight
-		super(207, 3600, 8.0, 22.0);
+		super(207, 3600, 8.0, 24.4);
 	}
 
 	public void onTransform()
