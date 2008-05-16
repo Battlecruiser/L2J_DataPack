9df68f2e0410fca51ea08b773c3ca3963f3b30dc
DrLecter <DrLecter@users.noreply.github.com>
2008-05-16 23:50:38 +0000

    Further changes and reorganize in the access level rewrite (requires [L2110]): * Fields being renamed after the camelCase naming convention. * Field types slightly altered to better contain their data. * Adding documentation for the access level system as comments in the .sql files. * Adding some general SQL documentation. (tnx nbd^)

--- datapack_development/sql/admin_command_access_rights.sql
+++ datapack_development/sql/admin_command_access_rights.sql
@@ -1,10 +1,14 @@
+-- 
+-- For further information on the usage of this table, please refer to the 
+-- documentation comments in the access_levels.sql file
+-- 
 -- -----------------------------------------------
 -- Table structure for admin_command_access_rights
 -- -----------------------------------------------
 CREATE TABLE IF NOT EXISTS `admin_command_access_rights` (
-  `admin_command` varchar(255) NOT NULL default 'admin_',
-  `access_levels` varchar(255) NOT NULL,
-  PRIMARY KEY  (`admin_command`)
+  `adminCommand` varchar(255) NOT NULL default 'admin_',
+  `accessLevels` varchar(255) NOT NULL,
+  PRIMARY KEY  (`adminCommand`)
 ) ;
 
 INSERT IGNORE INTO `admin_command_access_rights` VALUES 
