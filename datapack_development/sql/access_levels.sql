7d1187e005d24ddd4e2d7307f3b254f130b479c0
DrLecter <DrLecter@users.noreply.github.com>
2008-05-16 10:36:14 +0000

    Some SQL cleanup, fixes #2321 and a little reminder about the fact that columns shall be named after cameCase, so I will alter them soon >:[

--- datapack_development/sql/access_levels.sql
+++ datapack_development/sql/access_levels.sql
@@ -7,22 +7,22 @@ CREATE TABLE IF NOT EXISTS `access_levels` (
   `name_color` varchar(6) NOT NULL default 'FFFFFF',
   `title_color` varchar(6) NOT NULL default 'FFFFFF',
   `child_access` varchar(255) NOT NULL default '',
-  `is_gm` tinyint(1) NOT NULL default '0',
-  `allow_peace_attack` tinyint(1) NOT NULL default '0',
-  `allow_fixed_res` tinyint(1) NOT NULL default '0',
-  `allow_transaction` tinyint(1) NOT NULL default '0',
-  `allow_altg` tinyint(1) NOT NULL default '0',
-  `give_damage` tinyint(1) NOT NULL default '0',
-  `take_aggro` tinyint(1) NOT NULL default '0',
-  `gain_exp` tinyint(1) NOT NULL default '0',
+  `is_gm` tinyint(1) NOT NULL default 0,
+  `allow_peace_attack` tinyint(1) NOT NULL default 0,
+  `allow_fixed_res` tinyint(1) NOT NULL default 0,
+  `allow_transaction` tinyint(1) NOT NULL default 0,
+  `allow_altg` tinyint(1) NOT NULL default 0,
+  `give_damage` tinyint(1) NOT NULL default 0,
+  `take_aggro` tinyint(1) NOT NULL default 0,
+  `gain_exp` tinyint(1) NOT NULL default 0,
   PRIMARY KEY  (`access_level`)
 ) ;
 
-INSERT INTO `access_levels` VALUES 
-('1','Admin','0FF000','FFFFFF','2;3;4;5;6','1','1','1','1','1','1','1','1'),
-('2','Head GM','0C0000','FFFFFF','5;6','0','0','1','1','1','1','1','1'),
-('3','Event GM','00C000','FFFFFF','5;6','0','0','1','0','1','0','0','0'),
-('4','Support GM','000C00','FFFFFF','5;6','0','0','1','0','1','0','0','0'),
-('5','General GM','0000C0','FFFFFF','6','0','0','1','0','1','0','0','0'),
-('6','Test GM','FFFFFF','FFFFFF','','0','0','1','0','1','0','0','0');
+INSERT IGNORE INTO `access_levels` VALUES 
+(1,'Admin','0FF000','FFFFFF','2;3;4;5;6',1,1,1,1,1,1,1,1),
+(2,'Head GM','0C0000','FFFFFF','5;6',0,0,1,1,1,1,1,1),
+(3,'Event GM','00C000','FFFFFF','5;6',0,0,1,0,1,0,0,0),
+(4,'Support GM','000C00','FFFFFF','5;6',0,0,1,0,1,0,0,0),
+(5,'General GM','0000C0','FFFFFF','6',0,0,1,0,1,0,0,0),
+(6,'Test GM','FFFFFF','FFFFFF','','0',0,1,0,1,0,0,0);
 
