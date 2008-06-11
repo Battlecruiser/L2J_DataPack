6d138e1512b945dadd3b0068298876bf69b865bc
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-06-11 19:00:40 +0000

    -_-

--- datapack_development/sql/updates/20080611update.sql
+++ datapack_development/sql/updates/20080611update.sql
@@ -1,3 +1,4 @@
-update clanhall set lease=500000 where id in (22,23,24,25,26,27,28,29,30,58,59,60,61);
-update clanhall set lease=200000 where id in (31,32,33);
-update clanhall set lease=1000000 where id in (36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57);
+update clanhall set lease=0 where Grade=0;
+update clanhall set lease=200000 where Grade=1;
+update clanhall set lease=500000 where Grade=2;
+update clanhall set lease=1000000 where Grade=3;
