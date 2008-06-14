2bf64973c747f90b3bd183c9d0556291b6a7096b
DrLecter <DrLecter@users.noreply.github.com>
2008-06-14 13:42:00 +0000

    Some field type optimizations for armor set related tables. Developers ought observe conventions as described in documentation.txt

--- datapack_development/sql/custom/custom_armorsets.sql
+++ datapack_development/sql/custom/custom_armorsets.sql
@@ -4,16 +4,16 @@
 
 DROP TABLE IF EXISTS custom_armorsets;
 CREATE TABLE custom_armorsets (
- id int(3) NOT NULL auto_increment,
- chest decimal(11,0) NOT NULL default '0',
- legs decimal(11,0) NOT NULL default '0',
- head decimal(11,0) NOT NULL default '0',
- gloves decimal(11,0) NOT NULL default '0',
- feet decimal(11,0) NOT NULL default '0',
- skill_id decimal(11,0) NOT NULL default '0',
- skill_lvl decimal(11,0) NOT NULL default '0',
- shield decimal(11,0) NOT NULL default '0',
- shield_skill_id decimal(11,0) NOT NULL default '0',
- enchant6skill decimal(11,0) NOT NULL default '0',
+ id SMALLINT UNSIGNED NOT NULL auto_increment,
+ chest SMALLINT UNSIGNED NOT NULL default 0,
+ legs SMALLINT UNSIGNED NOT NULL default 0,
+ head SMALLINT UNSIGNED NOT NULL default 0,
+ gloves SMALLINT UNSIGNED NOT NULL default 0,
+ feet SMALLINT UNSIGNED NOT NULL default 0,
+ skill_id SMALLINT UNSIGNED NOT NULL default 0,
+ skill_lvl TINYINT UNSIGNED NOT NULL default 0,
+ shield SMALLINT UNSIGNED NOT NULL default 0,
+ shield_skill_id SMALLINT UNSIGNED NOT NULL default 0,
+ enchant6skill SMALLINT UNSIGNED NOT NULL default 0,
  PRIMARY KEY (id,chest)
 ) ENGINE=MyISAM;
