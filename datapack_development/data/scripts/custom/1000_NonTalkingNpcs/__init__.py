38808b27b5aa731263f41d8782fe08bf27d5f386
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-08-06 11:04:29 +0000

    fixed #2422

---
+++ datapack_development/data/scripts/custom/1000_NonTalkingNpcs/__init__.py
@@ -0,0 +1,19 @@
+# Script is used for preventing displaying html for npcs that dont have html on retail
+# Visit http://forum.l2jdp.com for more details.
+import sys
+from net.sf.l2j.gameserver.model.quest import Quest as JQuest
+from net.sf.l2j.gameserver.serverpackets      import ActionFailed
+
+NPCs = [31557,31671,31672,31673,31674,32026,32030,32031,32032]
+
+class Quest (JQuest) :
+    def __init__(self,id,name,descr):
+        JQuest.__init__(self,id,name,descr)
+
+    def onFirstTalk (self,npc,player):
+        player.sendPacket(ActionFailed.STATIC_PACKET)
+        return None
+
+QUEST      = Quest(-1,".","custom")
+for i in NPCs :
+  QUEST.addFirstTalkId(i)
