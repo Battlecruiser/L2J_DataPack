8ea591aa8179e70b9b100cd5125418c310fce5ee
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-08-08 11:03:29 +0000

    update for Quest Little Wings Adventure (strider quest) removed some custom parts and replaced with proper one..now guides on net should be matching =)

---
+++ datapack_development/data/scripts/ai/group_template/fairy_trees.py
@@ -0,0 +1,42 @@
+import sys
+
+from net.sf.l2j.gameserver.ai import CtrlIntention
+from net.sf.l2j.gameserver.datatables import SkillTable
+from net.sf.l2j.gameserver.model.quest import Quest as JQuest
+from net.sf.l2j.gameserver.serverpackets import NpcSay
+from net.sf.l2j.util import Rnd
+
+class trees(JQuest) :
+
+    def __init__(self,id,name,descr):
+        JQuest.__init__(self,id,name,descr)
+
+    def onAdvEvent (self,event,npc,pc) :
+        if npc:
+           npc.deleteMe()
+        return
+
+    def onKill (self,npc,player,isPet):
+        npcId = npc.getNpcId()
+        if npcId in range(27185,27189) :
+           for x in xrange(20):
+               newNpc = self.addSpawn(27189,npc)
+               killer = player
+               if isPet :
+                   killer = player.getPet()
+               newNpc.setRunning()
+               newNpc.addDamageHate(killer,0,999)
+               newNpc.getAI().setIntention(CtrlIntention.AI_INTENTION_ATTACK, killer)
+               self.startQuestTimer("despawn",300000, newNpc, None)
+               if Rnd.get(2) :
+                  skill = SkillTable.getInstance().getInfo(4243,1)
+                  if skill != None :
+                     skill.getEffects(newNpc, killer)
+        return 
+
+
+QUEST		= trees(-2,"fairy trees","ai")
+
+for i in range(27185,27189):
+    QUEST.addKillId(i)
+QUEST.addSpawnId(27189)
