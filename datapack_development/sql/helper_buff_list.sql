38c87e25f39ce699ea8552a4b08c2e27399a3d52
DrLecter <DrLecter@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-08-13 21:38:26 +0000

    SQL: Preemptive support for upcoming features and changes.

--- datapack_development/sql/helper_buff_list.sql
+++ datapack_development/sql/helper_buff_list.sql
@@ -18,18 +18,21 @@ PRIMARY KEY  (`id`)
 #----------------------------
 
 insert  into helper_buff_list values 
-(0, 4322, 'WindWalk', 1, 8, 39, 'false'),
-(1, 4323, 'Shield', 1, 11, 38, 'false'),
+(0, 4322, 'WindWalk', 1, 6, 62, 'false'),
+(1, 4323, 'Shield', 1, 6, 62, 'false'),
 (2, 4338, 'Life Cubic', 1, 16, 34, 'false'), 
-(3, 4324, 'Bless the Body', 1, 12, 37, 'false'),
-(4, 4325, 'Vampiric Rage', 1, 13, 36, 'false'),
-(5, 4326, 'Regeneration', 1, 14, 35, 'false'),
-(6, 4327, 'Haste', 1, 15, 34, 'false'),
-(7, 4322, 'WindWalk', 1, 8, 39, 'true'),
-(8, 4323, 'Shield', 1, 11, 38, 'true'),
+(3, 4324, 'Bless the Body', 1, 6, 62, 'false'),
+(4, 4325, 'Vampiric Rage', 1, 6, 62, 'false'),
+(5, 4326, 'Regeneration', 1, 6, 62, 'false'),
+(6, 4327, 'Haste', 1, 6, 39, 'false'),
+(7, 4322, 'WindWalk', 1, 6, 62, 'true'),
+(8, 4323, 'Shield', 1, 6, 62, 'true'),
 (9, 4338, 'Life Cubic', 1, 16, 34, 'true'),
-(10, 4328, 'Bless the Soul', 1, 12, 37, 'true'),
-(11, 4329, 'Acumen', 1, 13, 36, 'true'),
-(12, 4330, 'Concentration', 1, 14, 35, 'true'),
-(13, 4331, 'Empower', 1, 15, 34, 'true');
+(10, 4328, 'Bless the Soul', 1, 6, 62, 'true'),
+(11, 4329, 'Acumen', 1, 6, 62, 'true'),
+(12, 4330, 'Concentration', 1, 6, 62, 'true'),
+(13, 4331, 'Empower', 1, 6, 62, 'true'),
+(14, 5632, 'Haste', 2, 40, 62, 'false'),
+(15, 5637, 'Magic Barrier', 1, 6, 62, 'false'),
+(16, 5637, 'Magic Barrier', 1, 6, 62, 'true');
 -- (14, 5182, 'Blessing of Protection', 1, 1, 39, 'false'); -- Keeps you safe from an attack by a chaotic character who is more than 10 levels apart from you.
