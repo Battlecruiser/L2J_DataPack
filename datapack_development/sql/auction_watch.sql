fdf5a54db4d120fcbcda7b317ad91eaa5a857ac6
MELERIX <MELERIX@users.noreply.github.com>
2008-08-20 03:23:15 +0000

    Fixes for Ticket #2519

--- datapack_development/sql/auction_watch.sql
+++ datapack_development/sql/auction_watch.sql
@@ -4,5 +4,5 @@
 CREATE TABLE IF NOT EXISTS `auction_watch` (
   `charObjId` INT NOT NULL default 0,
   `auctionId` INT NOT NULL default 0,
-  PRIMARY KEY  (`charObjId`,`auctionId`)
+  PRIMARY KEY (`charObjId`,`auctionId`)
 );
