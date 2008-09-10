aef80c1229a5da9721561d7b29a13376a817c62e
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-09-10 15:56:46 +0000

    adena shouldnt be multiplied in quest Good Works Reward require [L2443]

--- datapack_development/data/scripts/quests/60_GoodWorkReward/__init__.py
+++ datapack_development/data/scripts/quests/60_GoodWorkReward/__init__.py
@@ -128,7 +128,7 @@ class Quest (JQuest) :
       text = BYPASS[player.getClassId().getId()]
       htmltext = "<html><body>Black Marketeer of Mammon:<br>If you are finished thinking, select one. Which class would you like to be?<br>"+text+"</body></html>"
     elif event == "31092-07.htm" :
-      st.giveItems(57,3000000)
+      st.giveAdena(3000000, True)
       st.set("onlyone","1")
     elif event in CLASSES.keys():
          newclass,req_item=CLASSES[event]
@@ -141,7 +141,7 @@ class Quest (JQuest) :
          if adena == 3 :
             return "31092-06.htm"
          if adena > 0 :
-            st.giveItems(57,adena*1000000)
+            st.giveAdena(adena*1000000,True)
          htmltext = "31092-05.htm"
          st.set("onlyone","1")
     return htmltext
