5b37dc9d6717e4d622a722c4f599f369d5da4869
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-09-17 07:14:39 +0000

    fixed #2783

--- datapack_development/data/scripts/custom/1000_NonTalkingNpcs/__init__.py
+++ datapack_development/data/scripts/custom/1000_NonTalkingNpcs/__init__.py
@@ -4,7 +4,7 @@ import sys
 from net.sf.l2j.gameserver.model.quest import Quest as JQuest
 from net.sf.l2j.gameserver.network.serverpackets      import ActionFailed
 
-NPCs = [31557,31671,31672,31673,31674,32026,32030,32031,32032]
+NPCs = [31557,31606,31671,31672,31673,31674,32026,32030,32031,32032]
 
 class Quest (JQuest) :
     def __init__(self,id,name,descr):
