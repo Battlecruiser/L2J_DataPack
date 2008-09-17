cb77ebc5189fffce485d6e5c5b2db76a374d7435
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-09-17 06:08:44 +0000

    SQL: Support for [L2457]

--- datapack_development/sql/transform_skill_trees.sql
+++ datapack_development/sql/transform_skill_trees.sql
@@ -15,8 +15,6 @@ CREATE TABLE `transform_skill_trees` (
 
 insert  into transform_skill_trees values 
 # Human
-(0, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(0, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (0, 541, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (0, 545, 9651, 1, 'Transform Unicorn', 0, 60), 
 (0, 548, 9652, 1, 'Transform Lilim Knight', 0, 60), 
@@ -24,8 +22,6 @@ insert  into transform_skill_trees values
 (0, 554, 9654, 1, 'Transform Inferno Drake', 0, 60), 
 (0, 558, 9655, 1, 'Transform Dragon Bomber', 0, 60), 
 # Elf
-(1, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(1, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (1, 542, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (1, 544, 9651, 1, 'Transform Unicorn', 0, 60), 
 (1, 549, 9652, 1, 'Transform Lilim Knight', 0, 60), 
@@ -33,8 +29,6 @@ insert  into transform_skill_trees values
 (1, 554, 9654, 1, 'Transform Inferno Drake', 0, 60), 
 (1, 557, 9655, 1, 'Transform Dragon Bomber', 0, 60),
 # Delf
-(2, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(2, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (2, 542, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (2, 546, 9651, 1, 'Transform Unicorn', 0, 60), 
 (2, 547, 9652, 1, 'Transform Lilim Knight', 0, 60), 
@@ -42,8 +36,6 @@ insert  into transform_skill_trees values
 (2, 554, 9654, 1, 'Transform Inferno Drake', 0, 60), 
 (2, 557, 9655, 1, 'Transform Dragon Bomber', 0, 60),
 # Dwarf
-(4, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(4, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (4, 542, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (4, 545, 9651, 1, 'Transform Unicorn', 0, 60), 
 (4, 548, 9652, 1, 'Transform Lilim Knight', 0, 60), 
@@ -51,8 +43,6 @@ insert  into transform_skill_trees values
 (4, 555, 9654, 1, 'Transform Inferno Drake', 0, 60), 
 (4, 557, 9655, 1, 'Transform Dragon Bomber', 0, 60),
 # Orc
-(3, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(3, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (3, 542, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (3, 545, 9651, 1, 'Transform Unicorn', 0, 60), 
 (3, 548, 9652, 1, 'Transform Lilim Knight', 0, 60), 
@@ -60,11 +50,24 @@ insert  into transform_skill_trees values
 (3, 553, 9654, 1, 'Transform Inferno Drake', 0, 60), 
 (3, 557, 9655, 1, 'Transform Dragon Bomber', 0, 60),
 # Kamael
-(5, 617, 9648, 1, 'Transform Onyx Beast', 0, 50), 
-(5, 618, 9649, 1, 'Transform Death Blader', 0, 55), 
 (5, 543, 9650, 1, 'Transform Grail Apostle', 0, 60), 
 (5, 545, 9651, 1, 'Transform Unicorn', 0, 60), 
 (5, 548, 9652, 1, 'Transform Lilim Knight', 0, 60), 
 (5, 551, 9653, 1, 'Transform Golem Guardian', 0, 60), 
 (5, 554, 9654, 1, 'Transform Inferno Drake', 0, 60), 
-(5, 556, 9655, 1, 'Transform Dragon Bomber', 0, 60);
+(5, 556, 9655, 1, 'Transform Dragon Bomber', 0, 60),
+# Race independent
+(-1, 617, 9648, 1, 'Transform Onyx Beast', 0, 60),
+(-1, 618, 9649, 1, 'Transform Death Blader', 0, 60),
+(-1, 663, 10295, 1, 'Transfomr Zaken', 0, 60),
+(-1, 664, 10296, 1, 'Transform Anakim', 0, 60),
+(-1, 665, 10297, 1, 'Transform Benom', 0, 60),
+(-1, 666, 10298, 1, 'Transform Gordon', 0, 60),
+(-1, 667, 10299, 1, 'Transform Ranku', 0, 60),
+(-1, 668, 10300, 1, 'Transform Kiyachi', 0, 60),
+(-1, 669, 10301, 1, 'Transform Demon Prince', 0, 60),
+(-1, 670, 10302, 1, 'Transform Heretic', 0, 60),
+(-1, 671, 10303, 1, 'Transform Vale Master', 0, 60),
+(-1, 672, 10304, 1, 'Transform Saber Tooth Tiger', 0, 60),
+(-1, 673, 10305, 1, 'Transform Ol Mahum', 0, 60),
+(-1, 674, 10306, 1, 'Transform Doll Blader', 0, 60);
