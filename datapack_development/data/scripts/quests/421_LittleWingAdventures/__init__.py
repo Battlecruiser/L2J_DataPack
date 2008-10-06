aab36fd68e7216f4b8f20686280311aaf261f216
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-10-06 07:14:08 +0000

    quest for strider update, should be now more retail matching :)

--- datapack_development/data/scripts/quests/421_LittleWingAdventures/__init__.py
+++ datapack_development/data/scripts/quests/421_LittleWingAdventures/__init__.py
@@ -1,70 +1,50 @@
-# Upgrade your Hatchling to Strider version 0.2
+# Upgrade your Hatchling to Strider version 0.3
 # by DrLecter & DraX_
-
-#Quest info
-QUEST_NUMBER      = 421
-QUEST_NAME        = "LittleWingAdventures"
-QUEST_DESCRIPTION = "Little Wing's Big Adventures"
-qn = "421_LittleWingAdventures"
-
-#Configuration
-
-#Minimum pet and player levels required to complete the quest (defaults 55 and 45)
-MIN_PET_LEVEL = 55
-MIN_PLAYER_LEVEL = 45
-# Maximum distance allowed between pet and owner; if it's reached while talking to any NPC, quest is aborted
-MAX_DISTANCE = 100
-
-#Messages
-default = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
-event_1 = "<html><body>Sage Cronos:<br>Then go and see <font color=\"LEVEL\">Fairy Mimyu</font>, she will help you</body></html>"
-error_1 = "<html><body>You're suppossed to own a hatchling and have it summoned to complete this quest.</body></html>"
-error_2 = "<html><body>Hey! What happened with the other hatchling you had? This one is different.</body></html>"
-error_3 = "<html><body>Sage Cronos:<br>You need to be level "+str(MIN_PLAYER_LEVEL)+" to complete this quest.</body></html>"
-error_4 = "<html><body>Sage Cronos:<br>Your pet need to be level "+str(MIN_PET_LEVEL)+" to complete this quest.</body></html>"
-error_5 = "Your pet is not a hatchling. Quest Aborted."
-error_6 = "Your pet should be nearby. Quest aborted"
-qston_1 = "<html><body>Sage Cronos:<br>So, you want to turn your hatchling into a more powerful creature?<br><br><a action=\"bypass -h Quest "+str(QUEST_NUMBER)+"_"+QUEST_NAME+" 16\">Yes, please tell me how</a><br></body></html>"
-qston_2 = "<html><body>Sage Cronos:<br>I've said you need to talk to <font color=\"LEVEL\">Fairy Mimyu</font>!!!. Am i clear???</body></html>"
-qston_3 = "<html><body>Fairy Mimyu:<br>You weren't yet able to find the <font color=\"LEVEL\">Fairy Trees of Wind, Star, Twilight and Abyss</font>? Don't give up! They are all in <font color=\"LEVEL\">Hunter's Valley</font></body></html>"
-order_1 = "<html><body>Fairy Mimyu:<br>Your pet must drink the sap of <font color=\"LEVEL\">Fairy Trees of Wind, Star, Twilight and Abyss</font> to grow up. The trees will probably agree but as we don't want to hurt them, take that leafs to heal any wound your hatchling could cause them</body></html>"
-end_msg = "<html><body>Fairy Mimyu:<br>Great job, your hatchling"
-end_msg2= "has become an strider, enjoy!</body></html>"
-
-#Quest items
-FT_LEAF = 4325
-CONTROL_ITEMS = { 3500:4422, 3501:4423, 3502:4424 }
-
-#NPCs
-SG_CRONOS = 30610
-FY_MYMYU  = 30747
-
-#Mobs
-GUARDIAN = 27189
+# last modify by Kerberos
 
 import sys
+from java.sql import PreparedStatement
+from java.sql import ResultSet
+from java.sql import SQLException
+from net.sf.l2j import L2DatabaseFactory
+from net.sf.l2j.gameserver.datatables import SkillTable
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
+from net.sf.l2j.gameserver.network import SystemMessageId
+from net.sf.l2j.gameserver.network.serverpackets import ItemList;
 from net.sf.l2j.gameserver.network.serverpackets import NpcSay
+from net.sf.l2j.gameserver.network.serverpackets import SystemMessage
 
-def get_control_item(st) :
-  item = st.getPlayer().getPet().getControlItemId()
-  if st.getState() == State.CREATED :
-      st.set("item",str(item))
-  else :
-      if  st.getInt("item") != item : item = 0
-  return item  
+qn = "421_LittleWingAdventures"
 
-def get_distance(player) :
-    is_far = False
-    if abs(player.getPet().getX() - player.getX()) > MAX_DISTANCE :
-        is_far = True
-    if abs(player.getPet().getY() - player.getY()) > MAX_DISTANCE :
-        is_far = True
-    if abs(player.getPet().getZ() - player.getZ()) > MAX_DISTANCE :
-        is_far = True
-    return is_far
+#Quest items
+FT_LEAF = 4325
+
+#NPCs
+CRONOS = 30610
+MIMYU  = 30747
+
+#kinda bugged, missing refresh, works only when player relog so far
+def EvolvePet(player,item,striderControlItem) :
+   con = L2DatabaseFactory.getInstance().getConnection()
+   statement = con.prepareStatement("UPDATE items SET item_id =? WHERE object_id=? AND owner_id=?")
+   statement.setInt(1, striderControlItem)
+   statement.setInt(2, item.getObjectId())
+   statement.setInt(3, player.getObjectId())
+   statement.execute()
+   statement.close()
+   con.close()
+   sm1 = SystemMessage(SystemMessageId.S2_S1_DISAPPEARED)
+   sm1.addItemName(item.getItemId())
+   sm1.addNumber(1)
+   sm2 = SystemMessage(SystemMessageId.YOU_PICKED_UP_A_S1_S2)
+   sm2.addNumber(item.getEnchantLevel())
+   sm2.addItemName(striderControlItem)
+   player.sendPacket(sm1)
+   player.sendPacket(sm2)
+   player.sendPacket(ItemList(player, False))
+   return
 
 class Quest (JQuest) :
 
@@ -73,80 +53,200 @@ class Quest (JQuest) :
    self.questItemIds = [FT_LEAF]
    self.killedTrees = []
 
- def onEvent (self,event,st) :
-    htmltext = event
-    if event == "16" :
-       htmltext = event_1
-       st.setState(State.STARTED)
-       st.set("id","0")
-       st.set("cond","1")
-       st.playSound("ItemSound.quest_accept")
-    return htmltext
-
- def onTalk (self,npc,player):
-   htmltext = default
+ def onAdvEvent (self,event,npc,player):
+   htmltext = event
    st = player.getQuestState(qn)
-   if not st : return htmltext
-   id = st.getState()
-   cond = st.getInt("cond")
-   if id == State.COMPLETED :
-      st.setState(State.CREATED)
-      id = State.CREATED
-   npcId = npc.getNpcId()
-   if player.getPet() == None :
-       htmltext = error_1
-       st.exitQuest(1)
-   elif player.getPet().getTemplate().npcId not in [12311,12312,12313] : #npcIds for hatchlings
-       htmltext = error_5
-       st.exitQuest(1)
-   elif player.getPet().getLevel() < MIN_PET_LEVEL :
-       st.exitQuest(1)
-       htmltext = error_4
-   elif get_distance(player) :
-       st.exitQuest(1)
-       htmltext = error_6
-   elif get_control_item(st) == 0 :
-       st.exitQuest(1)
-       htmltext = error_2
-   elif npcId == SG_CRONOS :
-      if id == State.CREATED :
-         if player.getLevel() < MIN_PLAYER_LEVEL :
-            st.exitQuest(1)
-            htmltext = error_3
-         else :   
-            htmltext = qston_1
+   if not st: return
+   if event == "30610-05.htm" :
+      if ((st.getQuestItemsCount(3500) + st.getQuestItemsCount(3501) + st.getQuestItemsCount(3502)) == 1) :
+         if st.getQuestItemsCount(3500) == 1 :
+            item = player.getInventory().getItemByItemId(3500)
+            if item.getEnchantLevel() < 55 :
+               st.exitQuest(1)
+               htmltext = "30610-06.htm"
+            else :
+               st.setState(State.STARTED)
+               st.set("summonOid",str(item.getObjectId()))
+               st.set("cond","1")
+               st.set("id","1")
+               st.playSound("ItemSound.quest_accept")
+         elif st.getQuestItemsCount(3501) == 1 :
+            item = player.getInventory().getItemByItemId(3501)
+            if item.getEnchantLevel() < 55 :
+               st.exitQuest(1)
+               htmltext = "30610-06.htm"
+            else :
+               st.setState(State.STARTED)
+               st.set("summonOid",str(item.getObjectId()))
+               st.set("cond","1")
+               st.set("id","1")
+               st.playSound("ItemSound.quest_accept")
+         elif st.getQuestItemsCount(3502) == 1 :
+            item = player.getInventory().getItemByItemId(3502)
+            if item.getEnchantLevel() < 55 :
+               st.exitQuest(1)
+               htmltext = "30610-06.htm"
+            else :
+               st.setState(State.STARTED)
+               st.set("summonOid",str(item.getObjectId()))
+               st.set("cond","1")
+               st.set("id","1")
+               st.playSound("ItemSound.quest_accept")
       else :
-         htmltext = qston_2
-   elif npcId == FY_MYMYU :
-     if id == State.STARTED and cond < 3 :
-        if st.getQuestItemsCount(FT_LEAF) == 0 and st.getInt("id") == 0 :
-           st.set("cond","2")
-           st.giveItems(FT_LEAF,4)
-           st.playSound("ItemSound.quest_itemget")
-           htmltext = order_1
-        else :
-            htmltext = qston_3
-     elif id == State.STARTED and cond >= 3:
-        name = player.getPet().getName()
-        if name == None : name = " "
-        else : name = " "+name+" "
-        htmltext = end_msg+name+end_msg2
-        item=CONTROL_ITEMS[player.getInventory().getItemByObjectId(player.getPet().getControlItemId()).getItemId()]
-        player.getPet().deleteMe(player) #both despawn pet and delete controlitem
-        st.giveItems(item,1)
-        st.exitQuest(1)
-        st.playSound("ItemSound.quest_finish")
+         st.exitQuest(1)
+         htmltext = "30610-06.htm"
+   elif event == "30747-02.htm" :
+      summon = player.getPet()
+      if summon :
+         if summon.getControlItem().getObjectId() == st.getInt("summonOid"):
+            htmltext = "30747-04.htm"
+         else :
+            htmltext = "30747-03.htm"
+   elif event == "30747-05.htm" :
+      summon = player.getPet()
+      if summon :
+         if summon.getControlItem().getObjectId() == st.getInt("summonOid"):
+            htmltext = "30747-05.htm"
+            st.giveItems(FT_LEAF,4)
+            st.set("cond","2")
+            st.set("id","0")
+            st.playSound("ItemSound.quest_middle")
+         else :
+            htmltext = "30747-06.htm"
+      else :
+         htmltext = "30747-06.htm"
    return htmltext
 
+ def onTalk (self,npc,player):
+   htmltext = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>" 
+   st = player.getQuestState(qn)
+   if not st: return htmltext
+   id = st.getState()
+   cond = st.getInt("cond")
+   npcId = npc.getNpcId()
+
+   if id == State.CREATED and npcId == CRONOS :
+      if player.getLevel() < 45 and (st.getQuestItemsCount(3500) or st.getQuestItemsCount(3501) or st.getQuestItemsCount(3502)) :
+         st.exitQuest(1)
+         htmltext = "30610-01.htm"
+      elif player.getLevel() >= 45 and ((st.getQuestItemsCount(3500) + st.getQuestItemsCount(3501) + st.getQuestItemsCount(3502)) >= 2) :
+         st.exitQuest(1)
+         htmltext = "30610-02.htm"
+      elif player.getLevel() >= 45 and ((st.getQuestItemsCount(3500) + st.getQuestItemsCount(3501) + st.getQuestItemsCount(3502)) == 1) :
+         if st.getQuestItemsCount(3500) == 1 :
+            item = player.getInventory().getItemByItemId(3500)
+            if item.getEnchantLevel() < 55 :
+               htmltext = "30610-03.htm"
+            else :
+               htmltext = "30610-04.htm"
+         elif st.getQuestItemsCount(3501) == 1 :
+            item = player.getInventory().getItemByItemId(3501)
+            if item.getEnchantLevel() < 55 :
+               htmltext = "30610-03.htm"
+            else :
+               htmltext = "30610-04.htm"
+         elif st.getQuestItemsCount(3502) == 1 :
+            item = player.getInventory().getItemByItemId(3502)
+            if item.getEnchantLevel() < 55 :
+               htmltext = "30610-03.htm"
+            else :
+               htmltext = "30610-04.htm"
+   elif id == State.STARTED :
+      if npcId == CRONOS :
+         htmltext = "30610-07.htm"
+      if npcId == MIMYU :
+         if st.getInt("id") == 1 :
+            st.set("id","2")
+            htmltext = "30747-01.htm"
+         elif st.getInt("id") == 2 :
+            summon = player.getPet()
+            if summon :
+               if summon.getControlItem().getObjectId() == st.getInt("summonOid"):
+                  htmltext = "30747-04.htm"
+               else :
+                  htmltext = "30747-03.htm"
+            else :
+               htmltext = "30747-02.htm"
+         elif st.getInt("id") == 0 :
+            htmltext = "30747-07.htm"
+         elif st.getInt("id") > 0 and st.getInt("id") < 15 and st.getQuestItemsCount(FT_LEAF) >= 1 :
+            htmltext = "30747-11.htm"
+         elif st.getInt("id") == 15 and st.getQuestItemsCount(FT_LEAF) == 0 :
+            summon = player.getPet()
+            if summon :
+               if summon.getControlItem().getObjectId() == st.getInt("summonOid"):
+                  st.set("id","16")
+                  htmltext = "30747-13.htm"
+               else :
+                  htmltext = "30747-14.htm"
+            else :
+               htmltext = "30747-12.htm"
+         elif st.getInt("id") == 16:
+            summon = player.getPet()
+            if summon :
+               htmltext = "30747-15.htm"
+            elif (st.getQuestItemsCount(3500) + st.getQuestItemsCount(3501) + st.getQuestItemsCount(3502)) == 1 :
+               if st.getQuestItemsCount(3500) == 1 :
+                  item = player.getInventory().getItemByItemId(3500)
+                  if item.getObjectId() == st.getInt("summonOid"):
+                     #EvolvePet(player,item,4422)
+                     st.takeItems(3500,1)
+                     st.giveItems(4422,1)
+                     htmltext = "30747-16.htm"
+                     st.exitQuest(1)
+                     st.playSound("ItemSound.quest_finish")
+                  else :
+                     npc.setTarget(player)
+                     skill = SkillTable.getInstance().getInfo(4167,1)
+                     if skill != None:
+                        skill.getEffects(npc, player)
+                     htmltext = "30747-18.htm"
+               elif st.getQuestItemsCount(3501) == 1 :
+                  item = player.getInventory().getItemByItemId(3501)
+                  if item.getObjectId() == st.getInt("summonOid"):
+                     #EvolvePet(player,item,4423)
+                     st.takeItems(3501,1)
+                     st.giveItems(4423,1)
+                     htmltext = "30747-16.htm"
+                     st.exitQuest(1)
+                     st.playSound("ItemSound.quest_finish")
+                  else :
+                     npc.setTarget(player)
+                     skill = SkillTable.getInstance().getInfo(4167,1)
+                     if skill != None:
+                        skill.getEffects(npc, player)
+                     htmltext = "30747-18.htm"
+               elif st.getQuestItemsCount(3502) == 1 :
+                  item = player.getInventory().getItemByItemId(3502)
+                  if item.getObjectId() == st.getInt("summonOid"):
+                     #EvolvePet(player,item,4424)
+                     st.takeItems(3502,1)
+                     st.giveItems(44242,1)
+                     htmltext = "30747-16.htm"
+                     st.exitQuest(1)
+                     st.playSound("ItemSound.quest_finish")
+                  else :
+                     npc.setTarget(player)
+                     skill = SkillTable.getInstance().getInfo(4167,1)
+                     if skill != None:
+                        skill.getEffects(npc, player)
+                     htmltext = "30747-18.htm"
+               else :
+                  htmltext = "30747-18.htm"
+            elif (st.getQuestItemsCount(3500) + st.getQuestItemsCount(3501) + st.getQuestItemsCount(3502)) >= 2 :
+               htmltext = "30747-17.htm"
+   return htmltext
+
+
  def onAttack(self, npc, player, damage, isPet) :
-   st = player.getQuestState(str(QUEST_NUMBER)+"_"+QUEST_NAME)
+   st = player.getQuestState(qn)
    if not st:
      return
    npcId = npc.getNpcId()
    for pc, mobId, in self.killedTrees:
       if pc == player and mobId == npcId:
          return
-   if isPet :
+   if isPet and st.getInt("id") < 16:
       pet = player.getPet()
       if st.getRandom(100) <= 2 and st.getQuestItemsCount(FT_LEAF) >= 0:
          st.takeItems(FT_LEAF,1)
@@ -154,18 +254,19 @@ class Quest (JQuest) :
          npc.broadcastPacket(NpcSay(npc.getNpcId(),0,npcId,"gives me spirit leaf...!"))
          self.killedTrees.append([player,npcId])
          if st.getQuestItemsCount(FT_LEAF) == 0 :
+            st.set("id","15")
             st.set("cond","3")
    return 
 
 # Quest class and state definition
-QUEST       = Quest(QUEST_NUMBER, str(QUEST_NUMBER)+"_"+QUEST_NAME, QUEST_DESCRIPTION)
+QUEST       = Quest(421,qn,"Little Wing's Big Adventures")
 
 # Quest NPC starter initialization
-QUEST.addStartNpc(SG_CRONOS)
-# Quest initialization
-QUEST.addTalkId(SG_CRONOS)
+QUEST.addStartNpc(CRONOS)
 
-QUEST.addTalkId(FY_MYMYU)
+# Quest initialization
+QUEST.addTalkId(CRONOS)
+QUEST.addTalkId(MIMYU)
 
 for i in range(27185,27189):
    QUEST.addAttackId(i)
