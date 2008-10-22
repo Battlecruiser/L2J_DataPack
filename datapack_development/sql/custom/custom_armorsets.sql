992d36d4d65c6cedb517765f4208bdc0d4b2f5f7
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-10-22 21:02:46 +0000

    SQL: Fixes for Ticket #2888

--- datapack_development/sql/custom/custom_armorsets.sql
+++ datapack_development/sql/custom/custom_armorsets.sql
@@ -1,18 +1,17 @@
 -- -------------------------------------------
 -- Table structure for table `custom_armorsets`
 -- -------------------------------------------
-
-CREATE TABLE IF NOT EXISTS custom_armorsets (
- id SMALLINT UNSIGNED NOT NULL auto_increment,
- chest SMALLINT UNSIGNED NOT NULL default 0,
- legs SMALLINT UNSIGNED NOT NULL default 0,
- head SMALLINT UNSIGNED NOT NULL default 0,
- gloves SMALLINT UNSIGNED NOT NULL default 0,
- feet SMALLINT UNSIGNED NOT NULL default 0,
- skill_id SMALLINT UNSIGNED NOT NULL default 0,
- skill_lvl TINYINT UNSIGNED NOT NULL default 0,
- shield SMALLINT UNSIGNED NOT NULL default 0,
- shield_skill_id SMALLINT UNSIGNED NOT NULL default 0,
- enchant6skill SMALLINT UNSIGNED NOT NULL default 0,
- PRIMARY KEY (id,chest)
-) ENGINE=MyISAM;
+CREATE TABLE IF NOT EXISTS `custom_armorsets` (
+  `id` SMALLINT UNSIGNED NOT NULL auto_increment,
+  `chest` SMALLINT UNSIGNED NOT NULL default '0',
+  `legs` SMALLINT UNSIGNED NOT NULL default '0',
+  `head` SMALLINT UNSIGNED NOT NULL default '0',
+  `gloves` SMALLINT UNSIGNED NOT NULL default '0',
+  `feet` SMALLINT UNSIGNED NOT NULL default '0',
+  `skill_id` SMALLINT UNSIGNED NOT NULL default '0',
+  `skill_lvl` TINYINT UNSIGNED NOT NULL default '0',
+  `shield` SMALLINT UNSIGNED NOT NULL default '0',
+  `shield_skill_id` SMALLINT UNSIGNED NOT NULL default '0',
+  `enchant6skill` SMALLINT UNSIGNED NOT NULL default '0',
+  PRIMARY KEY (`id`,`chest`)
+);
