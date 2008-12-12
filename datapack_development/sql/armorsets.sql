7e2657fea7cf5e7367279847c7912f7d56a30d09
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2008-12-12 05:02:11 +0000

    SQL: Armor Sets Passive Skills.

--- datapack_development/sql/armorsets.sql
+++ datapack_development/sql/armorsets.sql
@@ -136,67 +136,67 @@ INSERT INTO armorsets VALUES (76,  10229, 9421, 9422, 9423, 9424, 3637, 1,    94
 INSERT INTO armorsets VALUES (77,  10230, 9421, 9422, 9423, 9424, 3639, 1,    9441, 3417, 3623);   -- Dynasty Platinum Plate - Force Master
 INSERT INTO armorsets VALUES (78,  10231, 9421, 9422, 9423, 9424, 3638, 1,    9441, 3417, 3623);   -- Dynasty Platinum Plate - Bard
 
-INSERT INTO armorsets VALUES (79,  10233, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Mail Dagger Master
-INSERT INTO armorsets VALUES (80,  10234, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Mail Bow Master
+INSERT INTO armorsets VALUES (79,  10233, 9428, 9429, 9430, 9431, 3640, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Mail Dagger Master
+INSERT INTO armorsets VALUES (80,  10234, 9428, 9429, 9430, 9431, 3641, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Mail Bow Master
 
-INSERT INTO armorsets VALUES (81,  10236, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Healer
-INSERT INTO armorsets VALUES (82,  10237, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Enchanter
-INSERT INTO armorsets VALUES (83,  10238, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Summoner
-INSERT INTO armorsets VALUES (84,  10239, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Human Wizard
+INSERT INTO armorsets VALUES (81,  10236, 9437, 9438, 9439, 9440, 3645, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Healer
+INSERT INTO armorsets VALUES (82,  10237, 9437, 9438, 9439, 9440, 3646, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Enchanter
+INSERT INTO armorsets VALUES (83,  10238, 9437, 9438, 9439, 9440, 3647, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Summoner
+INSERT INTO armorsets VALUES (84,  10239, 9437, 9438, 9439, 9440, 3648, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic Human Wizard
 
-INSERT INTO armorsets VALUES (85,  10487, 9421, 9422, 9423, 9424, 0,    0,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Force Master
-INSERT INTO armorsets VALUES (86,  10488, 9421, 9422, 9423, 9424, 0,    0,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Weapon Master
-INSERT INTO armorsets VALUES (87,  10489, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Enchanter
-INSERT INTO armorsets VALUES (88,  10490, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Summoner
+INSERT INTO armorsets VALUES (85,  10487, 9421, 9422, 9423, 9424, 3642, 1,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Force Master
+INSERT INTO armorsets VALUES (86,  10488, 9421, 9422, 9423, 9424, 3643, 1,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Weapon Master
+INSERT INTO armorsets VALUES (87,  10489, 9437, 9438, 9439, 9440, 3805, 1,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Enchanter
+INSERT INTO armorsets VALUES (88,  10490, 9437, 9438, 9439, 9440, 3644, 1,    0,    0,    3624);   -- Dynasty Jeweled Leather Armor Summoner
 
 -- PVP Armor Sets
 -- #########################  id   chest  legs  head  glov  feet  skill sklvl shld  shsk  enchant6
-INSERT INTO armorsets VALUES (89,  10793, 0,    2418, 5771, 5783, 0,    0,    2498, 3551, 3620);   -- Armor of Nightmare - PvP
-INSERT INTO armorsets VALUES (90,  10794, 0,    2419, 5774, 5786, 0,    0,    0,    0,    3620);   -- Majestic Plate Armor - PvP
-INSERT INTO armorsets VALUES (91,  10795, 0,    2418, 5772, 5784, 0,    0,    0,    0,    3621);   -- Leather Armor of Nightmare - PvP
-INSERT INTO armorsets VALUES (92,  10796, 0,    2419, 5775, 5787, 0,    0,    0,    0,    3621);   -- Majestic Leather Mail - PvP
-INSERT INTO armorsets VALUES (93,  10797, 0,    2418, 5773, 5785, 0,    0,    0,    0,    3622);   -- Robe of Nightmare - PvP
-INSERT INTO armorsets VALUES (94,  10798, 0,    2419, 5776, 5788, 0,    0,    0,    0,    3622);   -- Majestic Robe - PvP
+INSERT INTO armorsets VALUES (89,  10793, 0,    2418, 5771, 5783, 8193, 1,    2498, 3551, 3620);   -- Armor of Nightmare - PvP
+INSERT INTO armorsets VALUES (90,  10794, 0,    2419, 5774, 5786, 8194, 1,    0,    0,    3620);   -- Majestic Plate Armor - PvP
+INSERT INTO armorsets VALUES (91,  10795, 0,    2418, 5772, 5784, 8195, 1,    0,    0,    3621);   -- Leather Armor of Nightmare - PvP
+INSERT INTO armorsets VALUES (92,  10796, 0,    2419, 5775, 5787, 8196, 1,    0,    0,    3621);   -- Majestic Leather Mail - PvP
+INSERT INTO armorsets VALUES (93,  10797, 0,    2418, 5773, 5785, 8197, 1,    0,    0,    3622);   -- Robe of Nightmare - PvP
+INSERT INTO armorsets VALUES (94,  10798, 0,    2419, 5776, 5788, 8198, 1,    0,    0,    3622);   -- Majestic Robe - PvP
 
-INSERT INTO armorsets VALUES (95,  10799, 6374, 6378, 6375, 6376, 0,    0,    6377, 3554, 3623);   -- Imperial Crusader Breastplate - PvP
-INSERT INTO armorsets VALUES (96,  10800, 0,    6382, 6380, 6381, 0,    0,    0,    0,    3624);   -- Draconic Leather Armor - PvP
-INSERT INTO armorsets VALUES (97,  10801, 0,    6386, 6384, 6385, 0,    0,    0,    0,    3625);   -- Major Arcana Robe - PvP
+INSERT INTO armorsets VALUES (95,  10799, 6374, 6378, 6375, 6376, 8199, 1,    6377, 3554, 3623);   -- Imperial Crusader Breastplate - PvP
+INSERT INTO armorsets VALUES (96,  10800, 0,    6382, 6380, 6381, 8200, 1,    0,    0,    3624);   -- Draconic Leather Armor - PvP
+INSERT INTO armorsets VALUES (97,  10801, 0,    6386, 6384, 6385, 8201, 1,    0,    0,    3625);   -- Major Arcana Robe - PvP
 
-INSERT INTO armorsets VALUES (98,  10802, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP
-INSERT INTO armorsets VALUES (99,  10803, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Shield Master
-INSERT INTO armorsets VALUES (100, 10804, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Weapon Master
-INSERT INTO armorsets VALUES (101, 10805, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Force Master
-INSERT INTO armorsets VALUES (102, 10806, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Bard
+INSERT INTO armorsets VALUES (98,  10802, 9421, 9422, 9423, 9424, 8202, 1,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP
+INSERT INTO armorsets VALUES (99,  10803, 9421, 9422, 9423, 9424, 8203, 1,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Shield Master
+INSERT INTO armorsets VALUES (100, 10804, 9421, 9422, 9423, 9424, 8204, 1,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Weapon Master
+INSERT INTO armorsets VALUES (101, 10805, 9421, 9422, 9423, 9424, 8205, 1,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Force Master
+INSERT INTO armorsets VALUES (102, 10806, 9421, 9422, 9423, 9424, 8206, 1,    9441, 3417, 3623);   -- Dynasty Breastplate - PvP Bard
 
-INSERT INTO armorsets VALUES (103, 10807, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP
-INSERT INTO armorsets VALUES (104, 10808, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Dagger Master
-INSERT INTO armorsets VALUES (105, 10809, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Bow Master
+INSERT INTO armorsets VALUES (103, 10807, 9428, 9429, 9430, 9431, 8207, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP
+INSERT INTO armorsets VALUES (104, 10808, 9428, 9429, 9430, 9431, 8208, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Dagger Master
+INSERT INTO armorsets VALUES (105, 10809, 9428, 9429, 9430, 9431, 8209, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Bow Master
 
-INSERT INTO armorsets VALUES (106, 10810, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Tunic - PvP
-INSERT INTO armorsets VALUES (107, 10811, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Tunic - PvP Healer
-INSERT INTO armorsets VALUES (108, 10812, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Tunic - PvP Enchanter
-INSERT INTO armorsets VALUES (109, 10813, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Tunic - PvP Summoner
-INSERT INTO armorsets VALUES (110, 10814, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Tunic - PvP Wizard
+INSERT INTO armorsets VALUES (106, 10810, 9437, 9438, 9439, 9440, 8210, 1,    0,    0,    3625);   -- Dynasty Tunic - PvP
+INSERT INTO armorsets VALUES (107, 10811, 9437, 9438, 9439, 9440, 8211, 1,    0,    0,    3625);   -- Dynasty Tunic - PvP Healer
+INSERT INTO armorsets VALUES (108, 10812, 9437, 9438, 9439, 9440, 8212, 1,    0,    0,    3625);   -- Dynasty Tunic - PvP Enchanter
+INSERT INTO armorsets VALUES (109, 10813, 9437, 9438, 9439, 9440, 8213, 1,    0,    0,    3625);   -- Dynasty Tunic - PvP Summoner
+INSERT INTO armorsets VALUES (110, 10814, 9437, 9438, 9439, 9440, 8214, 1,    0,    0,    3625);   -- Dynasty Tunic - PvP Wizard
 
-INSERT INTO armorsets VALUES (111, 10815, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Force Master
-INSERT INTO armorsets VALUES (112, 10816, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Weapon Master
-INSERT INTO armorsets VALUES (113, 10817, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Enchanter
-INSERT INTO armorsets VALUES (114, 10818, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Summoner
+INSERT INTO armorsets VALUES (111, 10815, 9428, 9429, 9430, 9431, 8215, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Force Master
+INSERT INTO armorsets VALUES (112, 10816, 9428, 9429, 9430, 9431, 8216, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Weapon Master
+INSERT INTO armorsets VALUES (113, 10817, 9428, 9429, 9430, 9431, 8217, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Enchanter
+INSERT INTO armorsets VALUES (114, 10818, 9428, 9429, 9430, 9431, 8218, 1,    0,    0,    3624);   -- Dynasty Leather Armor - PvP Summoner
 
-INSERT INTO armorsets VALUES (115, 10820, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Shield Master
-INSERT INTO armorsets VALUES (116, 10821, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Weapon Master
-INSERT INTO armorsets VALUES (117, 10822, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Force Master
-INSERT INTO armorsets VALUES (118, 10823, 9421, 9422, 9423, 9424, 0,    0,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Bard
+INSERT INTO armorsets VALUES (115, 10820, 9421, 9422, 9423, 9424, 8219, 1,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Shield Master
+INSERT INTO armorsets VALUES (116, 10821, 9421, 9422, 9423, 9424, 8220, 1,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Weapon Master
+INSERT INTO armorsets VALUES (117, 10822, 9421, 9422, 9423, 9424, 8222, 1,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Force Master
+INSERT INTO armorsets VALUES (118, 10823, 9421, 9422, 9423, 9424, 8221, 1,    9441, 3417, 3623);   -- Dynasty Platinum Breastplate - PvP Bard
 
-INSERT INTO armorsets VALUES (119, 10825, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP Dagger Master
-INSERT INTO armorsets VALUES (120, 10826, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP Bow Master
+INSERT INTO armorsets VALUES (119, 10825, 9428, 9429, 9430, 9431, 8223, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP Dagger Master
+INSERT INTO armorsets VALUES (120, 10826, 9428, 9429, 9430, 9431, 8224, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP Bow Master
 
-INSERT INTO armorsets VALUES (121, 10828, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Healer
-INSERT INTO armorsets VALUES (122, 10829, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Enchanter
-INSERT INTO armorsets VALUES (123, 10830, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Summoner
-INSERT INTO armorsets VALUES (124, 10831, 9437, 9438, 9439, 9440, 0,    0,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Wizard
+INSERT INTO armorsets VALUES (121, 10828, 9437, 9438, 9439, 9440, 8229, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Healer
+INSERT INTO armorsets VALUES (122, 10829, 9437, 9438, 9439, 9440, 8230, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Enchanter
+INSERT INTO armorsets VALUES (123, 10830, 9437, 9438, 9439, 9440, 8231, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Summoner
+INSERT INTO armorsets VALUES (124, 10831, 9437, 9438, 9439, 9440, 8232, 1,    0,    0,    3625);   -- Dynasty Silver Satin Tunic - PvP Wizard
 
-INSERT INTO armorsets VALUES (125, 10832, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Force Master
-INSERT INTO armorsets VALUES (126, 10833, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Weapon Master
-INSERT INTO armorsets VALUES (127, 10834, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Enchanter
-INSERT INTO armorsets VALUES (128, 10835, 9428, 9429, 9430, 9431, 0,    0,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Summoner
+INSERT INTO armorsets VALUES (125, 10832, 9428, 9429, 9430, 9431, 8225, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Force Master
+INSERT INTO armorsets VALUES (126, 10833, 9428, 9429, 9430, 9431, 8226, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Weapon Master
+INSERT INTO armorsets VALUES (127, 10834, 9428, 9429, 9430, 9431, 8228, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Enchanter
+INSERT INTO armorsets VALUES (128, 10835, 9428, 9429, 9430, 9431, 8227, 1,    0,    0,    3624);   -- Dynasty Jewel Leather Armor - PvP	Summoner
