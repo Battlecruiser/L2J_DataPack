2c728adb0e8b075197b2e1ddf871bae8814a2d16
MELERIX <MELERIX@users.noreply.github.com>
2009-01-08 18:56:59 +0000

    Moving 20090107update.sql to deprecated folder, so if a server admin needs update, he can run this update manually, also removing UPDATE lines in 20080920update.sql & 20081227update.sql leaving only DELETE lines. Players will lose only the incorrect skills.

--- datapack_development/sql/updates/20080920update.sql
+++ datapack_development/sql/updates/20080920update.sql
@@ -1,15 +1,3 @@
--- UPDATE DEATH SPIKE SKILLS
-UPDATE IGNORE character_skills SET skill_id = 1530 WHERE skill_id = 1148 AND class_index = 0 AND charid IN ( SELECT charid FROM characters WHERE base_class IN (41, 111));
-UPDATE IGNORE character_skills SET skill_id = 1530 WHERE skill_id = 1148 AND class_index = 1 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 1);
-UPDATE IGNORE character_skills SET skill_id = 1530 WHERE skill_id = 1148 AND class_index = 2 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 2);
-UPDATE IGNORE character_skills SET skill_id = 1530 WHERE skill_id = 1148 AND class_index = 3 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 3);
-
--- UPDATE DEATH SPIKE SHORTCUTS
-UPDATE IGNORE character_shortcuts SET shortcut_id = 1530 WHERE shortcut_id = 1148 AND class_index = 0 AND charid IN ( SELECT charid FROM characters WHERE base_class IN (41, 111));
-UPDATE IGNORE character_shortcuts SET shortcut_id = 1530 WHERE shortcut_id = 1148 AND class_index = 1 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 1);
-UPDATE IGNORE character_shortcuts SET shortcut_id = 1530 WHERE shortcut_id = 1148 AND class_index = 2 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 2);
-UPDATE IGNORE character_shortcuts SET shortcut_id = 1530 WHERE shortcut_id = 1148 AND class_index = 3 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 3);
-
 -- DELETE INCORRECT DEATH SPIKE SKILLS
 DELETE FROM character_skills WHERE skill_id = 1148 AND class_index = 0 AND charid IN ( SELECT charid FROM characters WHERE base_class IN (41, 111));
 DELETE FROM character_skills WHERE skill_id = 1148 AND class_index = 1 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 1);
