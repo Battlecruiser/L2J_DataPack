d10a1987af2edd98ca992e286eed82a6507bf069
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-01-17 12:05:54 +0000

    fixed #3061

--- datapack_development/data/scripts/quests/101_SwordOfSolidarity/__init__.py
+++ datapack_development/data/scripts/quests/101_SwordOfSolidarity/__init__.py
@@ -1,6 +1,7 @@
 # Made by Mr. Have fun! - Version 0.3 by DrLecter
 
 import sys
+from net.sf.l2j import Config
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
