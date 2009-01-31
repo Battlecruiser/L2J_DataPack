5eccba3ea3560afd35af708eeeb2c0404b507a9f
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-01-31 15:46:56 +0000

    update for update

--- datapack_development/sql/updates/20090131update.sql
+++ datapack_development/sql/updates/20090131update.sql
@@ -4,3 +4,5 @@ ALTER TABLE `fort` ADD COLUMN `castleId` int(1) NOT NULL default 0;
 UPDATE `fort` SET `fortType` = '1' WHERE id IN (102,104,107,109,110,112,113,116,117,118);
 ALTER TABLE `fortsiege_clans` DROP `type`, DROP `fort_owner`;
 DROP TABLE IF EXISTS `fort_door`;
+UPDATE `fort` SET `name` = 'Swamp' WHERE id = 110;
+UPDATE `fort` SET `name` = 'Cloud Mountain' WHERE id = 113;
