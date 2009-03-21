50f3ea5fc5470e9ca6798fd7d154d390918e97b7
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-03-21 13:53:18 +0000

    few skills update also purple maned horse (pet) became unused

--- datapack_development/data/scripts/transformations/Teleporter2.java
+++ datapack_development/data/scripts/transformations/Teleporter2.java
@@ -24,11 +24,11 @@ public class Teleporter2 extends L2Transformation
 	public void transformedSkills()
 	{
 		// Transfrom Dispel
-		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(8248, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
 
-		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+		getPlayer().setTransformAllowedSkills(new int[]{8248,5491});
 	}
 
 	public void onUntransform()
@@ -40,7 +40,7 @@ public class Teleporter2 extends L2Transformation
 	public void removeSkills()
 	{
 		// Transfrom Dispel
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false, false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(8248, 1), false, false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false, false);
 
