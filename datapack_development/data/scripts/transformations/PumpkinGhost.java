fd52619364817344800f433414df67ce2b6f450a
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-03-21 13:32:09 +0000

    few new transformations

---
+++ datapack_development/data/scripts/transformations/PumpkinGhost.java
@@ -0,0 +1,54 @@
+package transformations;
+
+import net.sf.l2j.gameserver.datatables.SkillTable;
+import net.sf.l2j.gameserver.instancemanager.TransformationManager;
+import net.sf.l2j.gameserver.model.L2Transformation;
+
+public class PumpkinGhost extends L2Transformation
+{
+	public PumpkinGhost()
+	{
+		// id, duration (secs), colRadius, colHeight
+		super(108, 1800, 18.0, 32.0);
+	}
+
+	public void onTransform()
+	{
+		if (getPlayer().getTransformationId() != 108 || getPlayer().isCursedWeaponEquipped())
+			return;
+
+		// give transformation skills
+		transformedSkills();
+	}
+
+	public void transformedSkills()
+	{
+		// Transfrom Dispel
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+
+		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+	}
+
+	public void onUntransform()
+	{
+		// remove transformation skills
+		removeSkills();
+	}
+
+	public void removeSkills()
+	{
+		// Transfrom Dispel
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false, false);
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false, false);
+
+		getPlayer().setTransformAllowedSkills(new int[]{});
+	}
+
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new PumpkinGhost());
+	}
+}
