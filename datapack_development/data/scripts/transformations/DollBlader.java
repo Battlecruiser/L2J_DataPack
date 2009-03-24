5213b461207f30aa876058ccdd7a946446db4d08
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-03-24 19:25:13 +0000

    few transformation skills update

--- datapack_development/data/scripts/transformations/DollBlader.java
+++ datapack_development/data/scripts/transformations/DollBlader.java
@@ -37,8 +37,26 @@ public class DollBlader extends L2Transformation
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		if (getPlayer().getLevel() >= 76)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(754, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(752, 3), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(753, 3), false);
+		}
+		else if (getPlayer().getLevel() >= 73)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(754, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(752, 2), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(753, 2), false);
+		}
+		else if (getPlayer().getLevel() >= 70)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(754, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(752, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(753, 1), false);
+		}
 
-		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+		getPlayer().setTransformAllowedSkills(new int[]{619,5491,752,753,754});
 	}
 
 	public void onUntransform()
@@ -53,6 +71,22 @@ public class DollBlader extends L2Transformation
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false, false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false, false);
+		if (getPlayer().getLevel() >= 76)
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(752, 3), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(753, 3), false, false);
+		}
+		else if (getPlayer().getLevel() >= 73)
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(752, 2), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(753, 2), false, false);
+		}
+		else
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(752, 1), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(753, 1), false, false);
+		}
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(754, 1), false, false);
 
 		getPlayer().setTransformAllowedSkills(new int[]{});
 	}
--- datapack_development/data/scripts/transformations/ValeMaster.java
+++ datapack_development/data/scripts/transformations/ValeMaster.java
@@ -37,8 +37,29 @@ public class ValeMaster extends L2Transformation
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		if (getPlayer().getLevel() >= 76)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(742, 3), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(743, 3), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(744, 3), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(745, 3), false);
+		}
+		else if (getPlayer().getLevel() >= 73)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(742, 2), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(743, 2), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(744, 2), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(745, 2), false);
+		}
+		else if (getPlayer().getLevel() >= 70)
+		{
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(742, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(743, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(744, 1), false);
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(745, 1), false);
+		}
 
-		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+		getPlayer().setTransformAllowedSkills(new int[]{619,5491,742,743,744,745});
 	}
 
 	public void onUntransform()
@@ -53,6 +74,27 @@ public class ValeMaster extends L2Transformation
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false, false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false, false);
+		if (getPlayer().getLevel() >= 76)
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(742, 3), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(743, 3), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(744, 3), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(745, 3), false, false);
+		}
+		else if (getPlayer().getLevel() >= 73)
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(742, 2), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(743, 2), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(744, 2), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(745, 2), false, false);
+		}
+		else
+		{
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(742, 1), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(743, 1), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(744, 1), false, false);
+			getPlayer().removeSkill(SkillTable.getInstance().getInfo(745, 1), false, false);
+		}
 
 		getPlayer().setTransformAllowedSkills(new int[]{});
 	}
