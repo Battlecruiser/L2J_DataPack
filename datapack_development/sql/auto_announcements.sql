f4e6bb7ddec7b592891867c4371eb325b12c2ae1
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-03-30 02:22:15 +0000

    SQL: Minor fix for "auto_announcements.sql", thanks Tukune & Janiii.

--- datapack_development/sql/auto_announcements.sql
+++ datapack_development/sql/auto_announcements.sql
@@ -1,12 +1,11 @@
-
 -- ----------------------------
 -- Table structure for auto_announcements
 -- ----------------------------
-CREATE TABLE `auto_announcements` (
+CREATE TABLE IF NOT EXISTS `auto_announcements` (
   `id` INT(11) NOT NULL,
   `initial` BIGINT(20) NOT NULL,
   `delay` BIGINT(20) NOT NULL,
   `cycle` INT(11) NOT NULL,
   `memo` TEXT DEFAULT NULL,
   PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8;
+);
