17ca8f9d82b66671d5e0025751f5aa295cdf6f3b
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-04-10 10:11:10 +0000

    dp side for [L2907]

--- datapack_development/data/scripts/handlers/admincommandhandlers/AdminAnnouncements.java
+++ datapack_development/data/scripts/handlers/admincommandhandlers/AdminAnnouncements.java
@@ -17,7 +17,7 @@ package handlers.admincommandhandlers;
 import java.util.Collection;
 
 import net.sf.l2j.gameserver.Announcements;
-import net.sf.l2j.gameserver.taskmanager.tasks.AutoAnnounceTaskManager; 
+import net.sf.l2j.gameserver.taskmanager.AutoAnnounceTaskManager; 
 import net.sf.l2j.gameserver.handler.IAdminCommandHandler;
 import net.sf.l2j.gameserver.model.L2World;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
