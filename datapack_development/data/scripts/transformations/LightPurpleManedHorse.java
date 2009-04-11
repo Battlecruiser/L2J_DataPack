0b961fca9e9a424583628f1050a6681f8a09bda1
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-04-11 08:08:52 +0000

    small fixes for horse, thx toastgodsupreme and some small cleanup

--- datapack_development/data/scripts/transformations/LightPurpleManedHorse.java
+++ datapack_development/data/scripts/transformations/LightPurpleManedHorse.java
@@ -24,12 +24,12 @@ public class LightPurpleManedHorse extends L2Transformation
 
 	public void transformedSkills()
 	{
-		// Transfrom Dispel
-		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Dismount
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
 
-		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+		getPlayer().setTransformAllowedSkills(new int[]{839,5491});
 	}
 
 	public void onUntransform()
@@ -40,8 +40,8 @@ public class LightPurpleManedHorse extends L2Transformation
 
 	public void removeSkills()
 	{
-		// Transfrom Dispel
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Dismount
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
 
