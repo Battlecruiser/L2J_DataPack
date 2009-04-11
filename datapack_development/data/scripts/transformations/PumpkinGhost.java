adfcb7211f72920dd408ae0d515df7b5c53b12b4
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-04-11 12:23:35 +0000

    pumpkin ghost update

--- datapack_development/data/scripts/transformations/PumpkinGhost.java
+++ datapack_development/data/scripts/transformations/PumpkinGhost.java
@@ -9,7 +9,7 @@ public class PumpkinGhost extends L2Transformation
 	public PumpkinGhost()
 	{
 		// id, duration (secs), colRadius, colHeight
-		super(108, 1800, 18.0, 32.0);
+		super(108, 3600, 18.0, 32.0);
 	}
 
 	public void onTransform()
@@ -26,9 +26,9 @@ public class PumpkinGhost extends L2Transformation
 		// Transfrom Dispel
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
-		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(5437, 2), false);
 
-		getPlayer().setTransformAllowedSkills(new int[]{619,5491});
+		getPlayer().setTransformAllowedSkills(new int[]{619,5437});
 	}
 
 	public void onUntransform()
@@ -42,7 +42,7 @@ public class PumpkinGhost extends L2Transformation
 		// Transfrom Dispel
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		// Decrease Bow/Crossbow Attack Speed
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5437, 2), false);
 
 		getPlayer().setTransformAllowedSkills(new int[]{});
 	}
