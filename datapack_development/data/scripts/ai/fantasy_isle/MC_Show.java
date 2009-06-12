3b1b1240d2473b8f322c7148ae52978498827acc
Kerberos <Kerberos@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-06-12 05:24:04 +0000

    typo

--- datapack_development/data/scripts/ai/fantasy_isle/MC_Show.java
+++ datapack_development/data/scripts/ai/fantasy_isle/MC_Show.java
@@ -395,7 +395,7 @@ public class MC_Show extends Quest
 					int y = (Integer) walks.get(event)[1];
 					int z = (Integer) walks.get(event)[2];
 					String nextEvent = (String) walks.get(event)[3];
-					int time = Integer.valueOf((String) walks.get(event)[4]);
+					int time = (Integer) walks.get(event)[4];
 					npc.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO,new L2CharPosition(x,y,z,0));
 					startQuestTimer(nextEvent,time, npc, null);
 				}
