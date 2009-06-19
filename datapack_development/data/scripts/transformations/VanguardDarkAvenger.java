6364b9bba8fe3b58ca6464989582f38c8c9f5efc
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-06-19 14:53:35 +0000

    JAVA: Missing passives by Vanguards

--- datapack_development/data/scripts/transformations/VanguardDarkAvenger.java
+++ datapack_development/data/scripts/transformations/VanguardDarkAvenger.java
@@ -29,7 +29,8 @@ public class VanguardDarkAvenger extends L2Transformation
 			getPlayer().addSkill(SkillTable.getInstance().getInfo(817, getPlayer().getLevel() - 43), false);
 			// Blade Hurricane
 			getPlayer().addSkill(SkillTable.getInstance().getInfo(815, getPlayer().getLevel() - 43), false);
-			
+			// Dual Weapon Mastery
+			getPlayer().addSkill(SkillTable.getInstance().getInfo(144, getPlayer().getLevel() - 43), false);			
 			getPlayer().setTransformAllowedSkills(new int[]{838,5491,817,815,28,18,283,65,401,86});
 		}
 		else
@@ -52,6 +53,8 @@ public class VanguardDarkAvenger extends L2Transformation
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(817, getPlayer().getLevel() - 43), false);
 		// Blade Hurricane
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(815, getPlayer().getLevel() - 43), false);
+		// Dual Weapon Mastery
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(144, getPlayer().getLevel() - 43), false);
 		// Decrease Bow/Crossbow Attack Speed
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false); 
 		// Switch Stance
