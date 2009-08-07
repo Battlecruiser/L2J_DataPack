869cff71766be460afd49375c8187222c3e599c8
MELERIX <MELERIX@users.noreply.github.com>
2009-08-07 08:15:52 +0000

    SQL: DP part for [L3369] by _DS_ (UPDATE REQUIRED!).

--- datapack_development/sql/character_skills.sql
+++ datapack_development/sql/character_skills.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS `character_skills` (
   `charId` INT UNSIGNED NOT NULL default 0,
   `skill_id` INT NOT NULL default 0,
   `skill_level` INT(3) NOT NULL default 1,
-  `skill_name` varchar(40),
   `class_index` INT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`charId`,`skill_id`,`class_index`)
 );
--- datapack_development/sql/heroes.sql
+++ datapack_development/sql/heroes.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `heroes` (
   `charId` INT UNSIGNED NOT NULL default 0,
-  `char_name` varchar(35) NOT NULL default '',
   `class_id` decimal(3,0) NOT NULL default 0,
   `count` decimal(3,0) NOT NULL default 0,
   `played` decimal(1,0) NOT NULL default 0,
