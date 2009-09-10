4e016a2f5bb7845b92207aed886d75591b149135
MELERIX <MELERIX@users.noreply.github.com>
2009-09-10 21:16:37 +0000

    Bin folder to svn:ignore

--- datapack_development/sql/updates/20090424update.sql
+++ datapack_development/sql/updates/20090424update.sql
@@ -1,4 +1,2 @@
-ALTER TABLE `items`
-CHANGE count count BIGINT UNSIGNED NOT NULL default 0;
-ALTER TABLE `itemsonground`
-CHANGE count count BIGINT UNSIGNED NOT NULL default 0;
+ALTER TABLE `items` CHANGE count count BIGINT UNSIGNED NOT NULL default 0;
+ALTER TABLE `itemsonground` CHANGE count count BIGINT UNSIGNED NOT NULL default 0;
