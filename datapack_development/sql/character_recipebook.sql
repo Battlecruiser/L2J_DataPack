dd1f2f5174fb44fb5f7be5fe071fd0908aba73a7
nBd <nBd@users.noreply.github.com>
2009-09-16 11:26:18 +0000

    fix for recipes conflict by JIV

--- datapack_development/sql/character_recipebook.sql
+++ datapack_development/sql/character_recipebook.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS `character_recipebook` (
   `id` decimal(11) NOT NULL default 0,
   `classIndex` TINYINT NOT NULL DEFAULT 0,
   `type` INT NOT NULL default 0,
-  PRIMARY KEY (`id`,`charId`)
+  PRIMARY KEY (`id`,`charId`,`classIndex`)
 );
