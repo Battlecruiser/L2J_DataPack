4811e362e04ad137d66cf709df14c17cf79a0155
MELERIX <MELERIX@users.noreply.github.com>
2009-10-08 20:39:32 +0000

    SQL: Fix for Ticket #3687.

--- datapack_development/sql/updates/20091007update.sql
+++ datapack_development/sql/updates/20091007update.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `custom_armor` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `dropable`;
-ALTER TABLE `custom_etcitem` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `dropable`;
-ALTER TABLE `custom_weapon` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `dropable`;
+ALTER TABLE `custom_armor` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `tradeable`;
+ALTER TABLE `custom_etcitem` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `tradeable`;
+ALTER TABLE `custom_weapon` ADD `depositable` varchar(5) NOT NULL default 'false' AFTER `tradeable`;
