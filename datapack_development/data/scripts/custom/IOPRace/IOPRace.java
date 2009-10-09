7595e0a2102ddd3282c964d15929cb84b43a5e90
Gnacik <Gnacik@users.noreply.github.com>
2009-10-09 11:09:25 +0000

    Gigiikun: IOP race update.

--- datapack_development/data/scripts/custom/IOPRace/IOPRace.java
+++ datapack_development/data/scripts/custom/IOPRace/IOPRace.java
@@ -16,6 +16,7 @@ package custom.IOPRace;
 
 import net.sf.l2j.gameserver.model.actor.L2Npc;
 import net.sf.l2j.gameserver.datatables.SkillTable;
+import net.sf.l2j.gameserver.model.L2Skill;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.quest.Quest;
 import net.sf.l2j.gameserver.model.quest.QuestState;
@@ -25,73 +26,85 @@ public class IOPRace extends Quest
 	final private static int RIGNOS = 32349;
 	final private static int STAMP = 10013;
 	final private static int KEY = 9694;
-	final private static String qn = "IOPRace";
+
 	private int _player = -1;
 
-	public IOPRace(int id,String name,String descr)
+	public IOPRace(int id, String name, String descr)
 	{
-		super(id,name,descr);
+		super(id, name, descr);
 		addStartNpc(RIGNOS);
 		addTalkId(RIGNOS);
 		addFirstTalkId(RIGNOS);
 	}
-	
-	public String onFirstTalk(L2Npc npc,L2PcInstance player)
+
+	public String onFirstTalk(L2Npc npc, L2PcInstance player)
 	{
-		QuestState st = player.getQuestState(qn);
+		QuestState st = player.getQuestState(getName());
 		if (st == null)
 			st = newQuestState(player);
-		int npcId = npc.getNpcId();
-		if (npcId == RIGNOS)
-		{
-			if (_player != -1 && _player == player.getObjectId())
-				return "32349-return.htm"; // retail text missing
-			else if (_player != -1)
-				return "32349-notavailable.htm"; // retail text missing
-		}
-		npc.showChatWindow(player);
-		return null;
-	}
-	
-	public String onTalk(L2Npc npc,L2PcInstance player)
-	{
-		QuestState st = player.getQuestState(qn);
-		if (st == null)
-			st = newQuestState(player);
+
 		int npcId = npc.getNpcId();
 		if (npcId == RIGNOS)
 		{
 			if (player.getLevel() < 78)
-				return "32349-lolevel.htm"; // retail text missing
-			else if (_player == -1)
+				return "32349-notavailable.htm";
+			else if ((_player != -1) && (_player == player.getObjectId()) && (st.getQuestItemsCount(STAMP) == 4))
+				return "32349-return.htm"; // retail text missing
+			else if (_player != -1)
+				return "32349-notavailable.htm";
+		}
+
+		npc.showChatWindow(player);
+		return null;
+	}
+
+	public String onTalk(L2Npc npc, L2PcInstance player)
+	{
+		QuestState st = player.getQuestState(getName());
+		if (st == null)
+			st = newQuestState(player);
+
+		int npcId = npc.getNpcId();
+		if (npcId == RIGNOS)
+		{
+			if (_player == -1)
 			{
 				// clean old data
 				player.stopSkillEffects(5239);
 				if (player.getPet() != null)
 					player.getPet().stopSkillEffects(5239);
-				st.takeItems(STAMP,-1);
+
+				st.takeItems(STAMP, -1);
 				st.set("1st", "0");
 				st.set("2nd", "0");
 				st.set("3rd", "0");
 				st.set("4th", "0");
 
-				npc.setTarget(player);
-				npc.doCast(SkillTable.getInstance().getInfo(5239, 5));
-				if (player.getPet() != null)
+				L2Skill skill = SkillTable.getInstance().getInfo(5239, 5);
+				if (skill != null)
 				{
-					npc.setTarget(player.getPet());
-					npc.doCast(SkillTable.getInstance().getInfo(5239, 5));
+					npc.setTarget(player);
+					npc.doCast(skill);
+					if (player.getPet() != null)
+					{
+						npc.setTarget(player.getPet());
+						npc.doCast(skill);
+					}
 				}
+
 				startQuestTimer("timer", 1800000, null, null); // 30 min
 				_player = player.getObjectId();
-				return "32349-started.htm"; // retail text missing
 			}
 		}
-		return super.onTalk(npc,player);
+
+		return null;
 	}
-	
-	public String onAdvEvent (String event, L2Npc npc, L2PcInstance player)
+
+	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
+		String htmltext = "";
+		QuestState st = player.getQuestState(getName());
+
 		if (event.equalsIgnoreCase("timer"))
 		{
 			_player = -1;
@@ -102,40 +115,23 @@ public class IOPRace extends Quest
 			if (_player == player.getObjectId())
 			{
 				_player = -1;
-				QuestState st = player.getQuestState(qn);
-				if (st.getQuestItemsCount(STAMP) < 4)
-				{
-					// not done message missing
-					return "32349-nofinish.htm";
-				}
+
 				st.takeItems(STAMP, -1);
 				st.giveItems(KEY, 3);
 				player.stopSkillEffects(5239);
 				if (player.getPet() != null)
 					player.getPet().stopSkillEffects(5239);
 				cancelQuestTimers("timer");
-				return "32349-finish.htm"; // need better message
+				htmltext = "32349-finish.htm"; // need better message
+				st.exitQuest(true);
 			}
 		}
-		else if (event.equalsIgnoreCase("break")) // this is probably custom
-		{
-			if (_player == player.getObjectId())
-			{
-				_player = -1;
-				QuestState st = player.getQuestState(qn);
-				st.takeItems(STAMP, -1);
-				player.stopSkillEffects(5239);
-				if (player.getPet() != null)
-					player.getPet().stopSkillEffects(5239);
-				cancelQuestTimers("timer");
-				return "32349-cancel.htm";
-			}
-		}
-		return super.onAdvEvent(event,npc,player);
+
+		return htmltext;
 	}
 
 	public static void main(String[] args)
 	{
-		new IOPRace(-1,qn,"custom");
+		new IOPRace(-1, "IOPRace", "custom");
 	}
 }
