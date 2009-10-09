fe10fe4cc3c39693781959f466687c9cd57f155e
Aikimaniac <Aikimaniac@5288da4f-0efc-0310-bd40-8d490a2659fa>
2009-10-09 11:44:24 +0000

    SQL: Lets use the same helpers buff as on retail

--- datapack_development/sql/helper_buff_list.sql
+++ datapack_development/sql/helper_buff_list.sql
@@ -12,21 +12,21 @@ CREATE TABLE `helper_buff_list` (
 );
 
 INSERT INTO `helper_buff_list` VALUES
-(0, 4322, 'WindWalk', 1, 6, 62, 'false', 'true'),
-(1, 4323, 'Shield', 1, 6, 62, 'false', 'true'),
-(2, 4338, 'Life Cubic', 1, 16, 34, 'false', 'false'), 
-(3, 4324, 'Bless the Body', 1, 6, 62, 'false', 'true'),
-(4, 4325, 'Vampiric Rage', 1, 6, 62, 'false', 'true'),
-(5, 4326, 'Regeneration', 1, 6, 62, 'false', 'true'),
-(6, 4327, 'Haste', 1, 6, 39, 'false', 'true'),
-(7, 4322, 'WindWalk', 1, 6, 62, 'true', 'false'),
-(8, 4323, 'Shield', 1, 6, 62, 'true', 'false'),
-(9, 4338, 'Life Cubic', 1, 16, 34, 'true', 'false'),
-(10, 4328, 'Bless the Soul', 1, 6, 62, 'true', 'true'),
-(11, 4329, 'Acumen', 1, 6, 62, 'true', 'true'),
-(12, 4330, 'Concentration', 1, 6, 62, 'true', 'true'),
-(13, 4331, 'Empower', 1, 6, 62, 'true', 'true'),
-(14, 5632, 'Haste', 1, 40, 62, 'false', 'true'),
-(15, 5637, 'Magic Barrier', 1, 6, 62, 'false', 'true'),
-(16, 5637, 'Magic Barrier', 1, 6, 62, 'true', 'false');
--- (14, 5182, 'Blessing of Protection', 1, 1, 39, 'false'); -- Keeps you safe from an attack by a chaotic character who is more than 10 levels apart from you.
+(0,5627,'WindWalk',1,6,62,'false','true'),
+(1,5628,'Shield',1,6,62,'false','true'),
+(2,4338,'Life Cubic',1,16,34,'false','false'),
+(3,5629,'Bless the Body',1,6,62,'false','true'),
+(4,5630,'Vampiric Rage',1,6,62,'false','true'),
+(5,5631,'Regeneration',1,6,62,'false','true'),
+(6,5632,'Haste',1,6,39,'false','true'),
+(7,5627,'WindWalk',1,6,62,'true','false'),
+(8,5628,'Shield',1,6,62,'true','false'),
+(9,4338,'Life Cubic',1,16,34,'true','false'),
+(10,5633,'Bless the Soul',1,6,62,'true','true'),
+(11,5634,'Acumen',1,6,62,'true','true'),
+(12,5635,'Concentration',1,6,62,'true','true'),
+(13,5636,'Empower',1,6,62,'true','true'),
+(14,5632,'Haste',1,40,62,'false','true'),
+(15,5637,'Magic Barrier',1,6,62,'false','true'),
+(16,5637,'Magic Barrier',1,6,62,'true','false');
+-- (14,5182,'Blessing of Protection',1,1,39,'false'); -- Keeps you safe from an attack by a chaotic character who is more than 10 levels apart from you.
