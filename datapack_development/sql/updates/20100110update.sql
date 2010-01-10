bc5dd0c1677b4b5aede8d7cb1ee002754c998f93
MELERIX <MELERIX@users.noreply.github.com>
2010-01-10 19:45:41 +0000

    SQL: Fix for #L4342

--- datapack_development/sql/updates/20100110update.sql
+++ datapack_development/sql/updates/20100110update.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `seven_signs_status` CHANGE `date` decimal(20,0) NOT NULL default 0;
+ALTER TABLE `seven_signs_status` CHANGE `date` `date` decimal(20,0) NOT NULL default 0;
 -- UPDATE `seven_signs_status` SET `date` = UNIX_TIMESTAMP() * 1000;
