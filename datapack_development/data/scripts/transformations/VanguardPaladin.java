992a787a04b417cba70776b9bb5cb1b95487a908
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2010-02-04 05:44:13 +0000

    JAVA: Vanguard "Boost Morale" should stay after untransform.

--- datapack_development/data/scripts/transformations/VanguardPaladin.java
+++ datapack_development/data/scripts/transformations/VanguardPaladin.java
@@ -58,7 +58,7 @@ public class VanguardPaladin extends L2Transformation
 		// Switch Stance
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(838, 1), false);
 		// Boost Morale
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(956, getPlayer().getLevel() - 43), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(956, getPlayer().getLevel() - 43), false, false);
 		// Guillotine Attack
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(957, getPlayer().getLevel() - 43), false);
 
