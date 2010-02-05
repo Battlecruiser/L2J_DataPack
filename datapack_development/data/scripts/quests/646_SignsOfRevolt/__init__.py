bb36501c3f7706291d108ab400b6da3931a88609
Gnacik <Gnacik@5288da4f-0efc-0310-bd40-8d490a2659fa>
2010-02-05 07:53:40 +0000

    Quest ''Signs Of Revolt'' is no longer available in Gracia Epilog. Also fix for some spawns and html little update

--- datapack_development/data/scripts/quests/646_SignsOfRevolt/__init__.py
+++ datapack_development/data/scripts/quests/646_SignsOfRevolt/__init__.py
@@ -7,89 +7,21 @@ from com.l2jserver.gameserver.model.quest.jython import QuestJython as JQuest
 
 qn = "646_SignsOfRevolt"
 
-#NPC
-TORRANT = 32016
-#Drop rate
-DROP_CHANCE = 75
-#Mobs
-MOBS = range(22029,22046) + [22047,22049]
-#Item
-CURSED_DOLL = 8087
-#REWARDS
-REWARDS = {
-    "1" : [1880 , 9 ], #Steel
-    "2" : [1881 , 12 ], #CBP
-    "3" : [1882 , 20], #Leather
-    "4" : [57 , 21600], #Adena
-    }
-
 class Quest (JQuest) :
  def __init__(self,id,name,descr):
     JQuest.__init__(self,id,name,descr)
-    self.questItemIds = [CURSED_DOLL]
-
- def onEvent (self,event,st) :
-   htmltext = event
-   if event == "32016-03.htm" :
-       st.set("cond","1")
-       st.setState(State.STARTED)
-       st.playSound("ItemSound.quest_accept")
-   elif event in REWARDS.keys() :
-       item, amount = REWARDS[event]
-       st.takeItems(CURSED_DOLL,-1)
-       st.rewardItems(item, amount)
-       st.playSound("ItemSound.quest_finish")
-       st.exitQuest(1)
-       htmltext = "32016-07.htm"
-   return htmltext
+    self.questItemIds = [8087]
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
+   htmltext = "<html><body>x You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
    st = player.getQuestState(qn)
    if st :
-     id = st.getState()
-     cond = st.getInt("cond")
-     if id == State.CREATED :
-      if player.getLevel() < 40 :
-         htmltext = "32016-02.htm"
-         st.exitQuest(1)
-      else :
-         htmltext = "32016-01.htm"
-     elif cond == 1 :
-         htmltext = "32016-04.htm"
-     elif cond == 2 :
-         if st.getQuestItemsCount(CURSED_DOLL) >= 180 :
-             htmltext = "32016-05.htm"
-         else :
-             htmltext = "32016-04.htm"
-   return htmltext
-
- def onKill (self,npc,player,isPet):
-   partyMember = self.getRandomPartyMemberState(player,State.STARTED)
-   if not partyMember: return
-   st = partyMember.getQuestState(qn)
-   if st :
-      if st.getState() == State.STARTED :
-         count = st.getQuestItemsCount(CURSED_DOLL)
-         if st.getInt("cond") == 1 and count < 180 :
-            chance = DROP_CHANCE * Config.RATE_QUEST_DROP
-            numItems, chance = divmod(chance,100)
-            if st.getRandom(100) < chance :
-               numItems += 1
-            if numItems :
-               if count + numItems >= 180 :
-                  numItems = 180 - count
-                  st.playSound("ItemSound.quest_middle")
-                  st.set("cond","2")
-               else:
-                  st.playSound("ItemSound.quest_itemget")
-               st.giveItems(CURSED_DOLL,int(numItems))
-   return
+      # Quest is no longer available
+      st.unset("cond")
+      st.exitQuest(1);
+   return "32016-00.htm"
 
 QUEST       = Quest(646, qn, "Signs of Revolt")
 
-QUEST.addStartNpc(TORRANT)
-QUEST.addTalkId(TORRANT)
-
-for i in MOBS :
-  QUEST.addKillId(i)
+QUEST.addStartNpc(32016)
+QUEST.addTalkId(32016)
