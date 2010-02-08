d7800856f79e06fa064376f7d8bb055158354a27
Gnacik <Gnacik@5288da4f-0efc-0310-bd40-8d490a2659fa>
2010-02-08 20:35:15 +0000

    SQL: User IP restriction. If that field constains IP number - login to account is possible only from that IP. Leave NULL to be able to login from any IP. Usable when users have static IP, increasing security.
    '''Database update is required'''
    
    Also some updates from JiV

--- datapack_development/sql/accounts.sql
+++ datapack_development/sql/accounts.sql
@@ -5,5 +5,11 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `accessLevel` TINYINT NOT NULL DEFAULT 0,
   `lastIP` CHAR(15) NULL DEFAULT NULL,
   `lastServer` TINYINT DEFAULT 1,
+  `userIP` char(15) DEFAULT NULL,
+  `pcIp` char(15) DEFAULT NULL,
+  `hop1` char(15) DEFAULT NULL,
+  `hop2` char(15) DEFAULT NULL,
+  `hop3` char(15) DEFAULT NULL,
+  `hop4` char(15) DEFAULT NULL,
   PRIMARY KEY (`login`)
 );
