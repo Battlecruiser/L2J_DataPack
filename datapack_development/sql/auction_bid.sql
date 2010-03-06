341b7c717cef99b8c930c4d87623fa422b817613
Gnacik <Gnacik@users.noreply.github.com>
2010-03-06 10:07:26 +0000

    SQL: Auctions tables update, thanks _DS_

--- datapack_development/sql/auction_bid.sql
+++ datapack_development/sql/auction_bid.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `auction_bid` (
   `bidderId` INT NOT NULL default 0,
   `bidderName` varchar(50) NOT NULL,
   `clan_name` varchar(50) NOT NULL,
-  `maxBid` int(11) NOT NULL default 0,
+  `maxBid` BIGINT UNSIGNED NOT NULL default 0,
   `time_bid` decimal(20,0) NOT NULL default '0',
   PRIMARY KEY  (`auctionId`,`bidderId`),
   KEY `id` (`id`)
