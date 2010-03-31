fce03b5185a018e1cafebabf9617b95431efef11
MELERIX <MELERIX@users.noreply.github.com>
2010-03-31 22:57:48 +0000

    SQL: Fix for Ticket #3946.

--- datapack_development/tools/login_install.sql
+++ datapack_development/tools/login_install.sql
@@ -2,8 +2,9 @@
 -- previous L2J installations.
 --
 -- Queries that create these tables and/or populate them apropiately are
--- available in the ../sql/ folder. See the documentation.txt file 
+-- available in the ../sql/ folder. See the documentation.txt file
 -- for more details.
 
+DROP TABLE IF EXISTS account_data;
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS gameservers;
