248c5ad680dc44af379c3c52d5a8d99f0018f677
Gnacik <Gnacik@users.noreply.github.com>
2010-05-28 09:49:16 +0000

    [7440] update

--- datapack_development/sql/updates/20100528update.sql
+++ datapack_development/sql/updates/20100528update.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `teleport` ADD `itemId` decimal(11,0) NOT NULL default '57' AFTER `fornoble`;
 ALTER TABLE `custom_teleport` ADD `itemId` decimal(11,0) NOT NULL default '57' AFTER `fornoble`;
 
-UPDATE `teleport` SET itemId = 13722 WHERE fornoble = 1;
+UPDATE `teleport` SET itemId = 13722 WHERE id BETWEEN 9900 AND 10027;
 UPDATE `custom_teleport` SET itemId = 13722 WHERE fornoble = 1;
