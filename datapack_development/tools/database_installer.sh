d47c530b687809056912ec53a1eda0264a8cc21e
Gnacik <Gnacik@users.noreply.github.com>
2010-07-20 06:57:12 +0000

    SQL: Delete not used merchant tables, thanks janiii

--- datapack_development/tools/database_installer.sh
+++ datapack_development/tools/database_installer.sh
@@ -423,11 +423,9 @@ $MYG < ../sql/itemsonground.sql &> /dev/null
 $MYG < ../sql/locations.sql &> /dev/null
 $MYG < ../sql/lvlupgain.sql &> /dev/null
 $MYG < ../sql/mapregion.sql &> /dev/null
-$MYG < ../sql/merchant_areas_list.sql &> /dev/null
 $MYG < ../sql/merchant_buylists.sql &> /dev/null
 $MYG < ../sql/merchant_lease.sql &> /dev/null
 $MYG < ../sql/merchant_shopids.sql &> /dev/null
-$MYG < ../sql/merchants.sql &> /dev/null
 $MYG < ../sql/messages.sql &> /dev/null
 $MYG < ../sql/minions.sql &> /dev/null
 $MYG < ../sql/npc.sql &> /dev/null
