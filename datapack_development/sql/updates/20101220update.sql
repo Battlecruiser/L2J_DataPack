01a8ff3d0bf9ea4e451a8738d4f16ef0d228f957
MELERIX <MELERIX@users.noreply.github.com>
2010-12-21 10:36:19 +0000

    SQL: spawnlist.sql table is cleaned automatically when is imported.

--- datapack_development/sql/updates/20101220update.sql
+++ datapack_development/sql/updates/20101220update.sql
@@ -1,4 +1,2 @@
-ALTER TABLE `spawnlist` DROP COLUMN `id`;
-ALTER TABLE `spawnlist` DROP INDEX `key_npc_templateid`;
 ALTER TABLE `custom_spawnlist` DROP COLUMN `id`;
 ALTER TABLE `custom_spawnlist` DROP INDEX `key_npc_templateid`;
