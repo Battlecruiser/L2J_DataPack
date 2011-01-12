b734b4fff27aec44a0e261c1c2d6c1e32047827c
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2011-01-12 04:30:21 +0000

    XML: Added some skills that are not used in game yet, but are used by some custom servers (two of they will be used in High Five).

--- datapack_development/sql/character_offline_trade_items.sql
+++ datapack_development/sql/character_offline_trade_items.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `character_offline_trade_items` (
-  `charId` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `item` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `count` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
-  `price` bigint(20) UNSIGNED NOT NULL DEFAULT '0'
+  `charId` int(10) unsigned NOT NULL DEFAULT '0',
+  `item` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `price` bigint(20) unsigned NOT NULL DEFAULT '0'
 );
