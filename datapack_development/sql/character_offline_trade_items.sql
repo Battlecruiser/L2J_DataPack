c8b8b803482d365e22bd56ad3aec067ce11a1681
MELERIX <MELERIX@users.noreply.github.com>
2011-01-17 18:46:31 +0000

    HTM: Admin Skill Menu update, thanks alextaicho.

--- datapack_development/sql/character_offline_trade_items.sql
+++ datapack_development/sql/character_offline_trade_items.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `character_offline_trade_items` (
   `charId` int(10) unsigned NOT NULL DEFAULT '0',
-  `item` int(10) unsigned NOT NULL DEFAULT '0', -- ObjectId
+  `item` int(10) unsigned NOT NULL DEFAULT '0', -- itemId(for buy) & ObjectId(for sell)
   `count` bigint(20) unsigned NOT NULL DEFAULT '0',
   `price` bigint(20) unsigned NOT NULL DEFAULT '0'
 );
