12ad067093ffd2ad594884dbdeea6f7c4ee4d695
MELERIX <MELERIX@users.noreply.github.com>
2011-02-04 04:50:42 +0000

    minor cleanup

--- datapack_development/sql/updates/20080920update.sql
+++ datapack_development/sql/updates/20080920update.sql
@@ -9,3 +9,5 @@ DELETE FROM character_shortcuts WHERE shortcut_id = 1148 AND class_index = 0 AND
 DELETE FROM character_shortcuts WHERE shortcut_id = 1148 AND class_index = 1 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 1);
 DELETE FROM character_shortcuts WHERE shortcut_id = 1148 AND class_index = 2 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 2);
 DELETE FROM character_shortcuts WHERE shortcut_id = 1148 AND class_index = 3 AND charid IN ( SELECT charid FROM character_subclasses WHERE class_id IN (41, 111) AND class_index = 3);
+
+update `character_quests` set `value`='Start' WHERE `name`='637_ThroughOnceMore' AND `var`='<state>' AND `value`='Completed';
--- datapack_development/sql/updates/20080920update_1.sql
+++
@@ -1,1 +0,0 @@
-update `character_quests` set `value`='Start' WHERE `name`='637_ThroughOnceMore' AND `var`='<state>' AND `value`='Completed';
