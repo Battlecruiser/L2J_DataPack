ed30c32b79c10440ece37c22ec00574620294e39
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2011-02-10 19:05:46 +0000

    SQl: Fix for Tickets #L5315 & #L5339.

--- L2J_DataPack/sql/character_offline_trade.sql
+++ L2J_DataPack/sql/character_offline_trade.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `character_offline_trade` (
-  `charId` int(11) NOT NULL,
+  `charId` int(10) unsigned NOT NULL,
   `time` bigint(20) unsigned NOT NULL DEFAULT '0',
   `type` tinyint(4) NOT NULL DEFAULT '0',
   `title` varchar(50) DEFAULT NULL,
