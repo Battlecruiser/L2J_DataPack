f4de1dbea4787ae9372560a269d693b8efb724eb
MELERIX <MELERIX@users.noreply.github.com>
2011-03-31 19:14:57 +0000

    SQL: Cleanup due [L4535].

--- L2J_DataPack/sql/server/zone_vertices.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `zone_vertices`;
-CREATE TABLE `zone_vertices` (
-  `id` mediumint(5) unsigned NOT NULL,
-  `order` tinyint(2) unsigned NOT NULL,
-  `x` mediumint(6) NOT NULL,
-  `y` mediumint(6) NOT NULL,
-  PRIMARY KEY (`id`,`order`)
-);
