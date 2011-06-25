161185f0b12a2877d37c3fc0a8f9004069ea1c03
MELERIX <MELERIX@users.noreply.github.com>
2011-06-25 21:07:23 +0000

    BETA/STABLE: Retail Like '''"Shadow Weapons Buy System"''' by Adry_85.

--- L2J_DataPack/data/scripts/handlers/bypasshandlers/BuyShadowItem.java
+++ L2J_DataPack/data/scripts/handlers/bypasshandlers/BuyShadowItem.java
@@ -34,10 +34,14 @@ public class BuyShadowItem implements IBypassHandler
 			return false;
 		
 		NpcHtmlMessage html = new NpcHtmlMessage(((L2Npc)target).getObjectId());
-		if (activeChar.getLevel() >= 40)
-			html.setFile(activeChar.getHtmlPrefix(), "data/html/merchant/shadow_item.htm");
-		else
-			html.setFile(activeChar.getHtmlPrefix(), "data/html/merchant/shadow_item-lowlevel.htm");
+		if (activeChar.getLevel() < 40)
+			html.setFile(activeChar.getHtmlPrefix(), "data/html/common/shadow_item-lowlevel.htm");
+		else if ((activeChar.getLevel() >= 40) && (activeChar.getLevel() < 46))
+			html.setFile(activeChar.getHtmlPrefix(), "data/html/common/shadow_item_d.htm");
+		else if ((activeChar.getLevel() >= 46) && (activeChar.getLevel() < 52))
+			html.setFile(activeChar.getHtmlPrefix(), "data/html/common/shadow_item_c.htm");
+		else if (activeChar.getLevel() >= 52)
+			html.setFile(activeChar.getHtmlPrefix(), "data/html/common/shadow_item_b.htm");
 		html.replace("%objectId%", String.valueOf(((L2Npc)target).getObjectId()));
 		activeChar.sendPacket(html);
 		
