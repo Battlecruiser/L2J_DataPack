32c28da5e849f8ded07377f12602a57420f2fe25
MELERIX <MELERIX@users.noreply.github.com>
2011-09-13 04:13:23 +0000

    BETA: Missing "IF NOT EXISTS" in dynamic SCH tables, thanks Raikkon35.

--- L2J_DataPack_BETA/dist/sql/game/rainbowsprings_attacker_list.sql
+++ L2J_DataPack_BETA/dist/sql/game/rainbowsprings_attacker_list.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `rainbowsprings_attacker_list` (
+CREATE TABLE IF NOT EXISTS `rainbowsprings_attacker_list` (
   `clanId` int(10) DEFAULT NULL,
   `war_decrees_count` double(20,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
