430368675858efb37c672f93426318c3faa73b17
Tan <Tan@5288da4f-0efc-0310-bd40-8d490a2659fa>
2011-09-25 23:13:00 +0000

    BETA: skills ID : 22183,22184 Thanks Nayran, SQL update Thanks Melerix, 2 New Transformations, Some Skills :P

---
+++ L2J_DataPack_BETA/data/scripts/transformations/DragonMasterKarin.java
@@ -0,0 +1,67 @@
+package transformations;
+
+import com.l2jserver.gameserver.datatables.SkillTable;
+import com.l2jserver.gameserver.instancemanager.TransformationManager;
+import com.l2jserver.gameserver.model.L2Transformation;
+
+public class DragonMasterKarin extends L2Transformation
+{
+	private static final int[] SKILLS = {5491,619,20003,20004,20005};
+	public DragonMasterKarin()
+	{
+		// id, colRadius, colHeight
+		super(20006, 8, 18.6);
+	}
+	
+	@Override
+	public void onTransform()
+	{
+		if (getPlayer().getTransformationId() != 20006 || getPlayer().isCursedWeaponEquipped())
+			return;
+		
+		transformedSkills();
+	}
+	
+	public void transformedSkills()
+	{
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Transform Dispel
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Dragon Slash
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(20003, 1), false);
+		// Dragon Dash
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(20004, 1), false);
+		// Dragon Aura
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(20005, 1), false);
+		
+		getPlayer().setTransformAllowedSkills(SKILLS);
+	}
+	
+	@Override
+	public void onUntransform()
+	{
+		removeSkills();
+	}
+	
+	public void removeSkills()
+	{
+		// Decrease Bow/Crossbow Attack Speed
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Transform Dispel
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
+		// Dragon Slash
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(20003, 1), false);
+		// Dragon Dash
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(20004, 1), false);
+		// Dragon Aura
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(20005, 1), false);
+		
+		getPlayer().setTransformAllowedSkills(EMPTY_ARRAY);
+	}
+	
+	public static void main(String[] args)
+	{
+		TransformationManager.getInstance().registerTransformation(new DragonMasterKarin());
+	}
+}
