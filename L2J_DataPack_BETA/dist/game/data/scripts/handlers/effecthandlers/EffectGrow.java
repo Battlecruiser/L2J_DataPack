dee277227d6c19fd3446b9dd677b233c2dd1857c
Zoey76 <Zoey76@users.noreply.github.com>
2011-10-15 02:06:04 +0000

    BETA: Datapack part for [L5010].

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/EffectGrow.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/EffectGrow.java
@@ -64,7 +64,7 @@ public class EffectGrow extends L2Effect
 			L2Npc npc = (L2Npc) getEffected();
 			//TODO: Uncomment line when fix for mobs falling underground is found
 			//npc.setCollisionHeight(npc.getTemplate().collisionHeight);
-			npc.setCollisionRadius(npc.getTemplate().fCollisionRadius);
+			npc.setCollisionRadius(npc.getTemplate().getfCollisionRadius());
 			
 			getEffected().stopAbnormalEffect(AbnormalEffect.GROW);
 		}
