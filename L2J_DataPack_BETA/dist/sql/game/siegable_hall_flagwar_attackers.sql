e65148762eec660d564bbbb65ec11856dfc97cb4
MELERIX <MELERIX@users.noreply.github.com>
2011-10-18 04:30:30 +0000

    BETA: More Missing SQL Updates for [8450].

--- L2J_DataPack_BETA/dist/sql/game/siegable_hall_flagwar_attackers.sql
+++ L2J_DataPack_BETA/dist/sql/game/siegable_hall_flagwar_attackers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `siegable_hall_flagwar_attackers` (
-`hall_id` int(2) UNSIGNED NOT NULL DEFAULT '0',
-`flag` int(10) UNSIGNED NOT NULL DEFAULT '0',
-`npc` int(10) UNSIGNED NOT NULL DEFAULT '0',
-`clan_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+`hall_id` tinyint(2) unsigned NOT NULL DEFAULT '0',
+`flag` int(10) unsigned NOT NULL DEFAULT '0',
+`npc` int(10) unsigned NOT NULL DEFAULT '0',
+`clan_id` int(10) unsigned NOT NULL DEFAULT '0',
 PRIMARY KEY (`flag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
