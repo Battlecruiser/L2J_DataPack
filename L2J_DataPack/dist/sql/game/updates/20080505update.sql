aa4f450d91d50cc07e6afb4d27b3af13f8adffdb
MELERIX <MELERIX@users.noreply.github.com>
2011-10-20 05:46:28 +0000

    BETA/STABLE: Making SQL Updates line by line.

--- L2J_DataPack/dist/sql/game/updates/20080505update.sql
+++ L2J_DataPack/dist/sql/game/updates/20080505update.sql
@@ -8,9 +8,8 @@ ALTER TABLE `character_subclasses` CHANGE `char_obj_id` `charId` INT UNSIGNED NO
 ALTER TABLE `character_skills` CHANGE `char_obj_id` `charId` INT UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE `character_skills_save` CHANGE `char_obj_id` `charId` INT UNSIGNED NOT NULL DEFAULT 0;
 
-ALTER TABLE `character_friends`
-CHANGE `char_id` `charId` INT UNSIGNED NOT NULL DEFAULT 0,
-CHANGE `friend_id` `friendId` INT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `character_friends` CHANGE `char_id` `charId` INT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `character_friends` CHANGE `friend_id` `friendId` INT UNSIGNED NOT NULL DEFAULT 0;
 
 ALTER TABLE `cursed_weapons` CHANGE `playerId` `charId` INT UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE `heroes` CHANGE `char_id` `charId` INT UNSIGNED NOT NULL DEFAULT 0;
