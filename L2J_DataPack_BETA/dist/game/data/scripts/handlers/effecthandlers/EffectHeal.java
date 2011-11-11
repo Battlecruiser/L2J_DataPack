86544dc4919d760c4359c4b6eefb28f90aa25193
Rumen Nikiforov <unafraid89@gmail.com>
2011-11-11 00:39:24 +0000

    BETA: DP-Part [L5072] and fix for Hero's Potion

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/EffectHeal.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package handlers.effecthandlers;
+
+import com.l2jserver.gameserver.model.L2Effect;
+import com.l2jserver.gameserver.model.actor.L2Character;
+import com.l2jserver.gameserver.model.actor.instance.L2DoorInstance;
+import com.l2jserver.gameserver.network.SystemMessageId;
+import com.l2jserver.gameserver.network.serverpackets.StatusUpdate;
+import com.l2jserver.gameserver.network.serverpackets.SystemMessage;
+import com.l2jserver.gameserver.skills.Env;
+import com.l2jserver.gameserver.templates.effects.EffectTemplate;
+import com.l2jserver.gameserver.templates.skills.L2EffectType;
+
+/**
+ * @author UnAfraid
+ *
+ */
+public class EffectHeal extends L2Effect
+{
+	public EffectHeal(Env env, EffectTemplate template)
+	{
+		super(env, template);
+	}
+
+	
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.HEAL;
+	}
+	
+	@Override
+	public boolean onStart()
+	{
+		L2Character target = getEffected();
+		if (target == null || target.isDead() || target instanceof L2DoorInstance)
+			return false;
+		
+		StatusUpdate su = new StatusUpdate(target);
+		
+		double amount = calc();
+		
+		amount = Math.min(amount, target.getMaxRecoverableHp() - target.getCurrentHp());
+		
+		// Prevent negative amounts
+		if (amount < 0)
+			amount = 0;
+		
+		// To prevent -value heals, set the value only if current hp is less than max recoverable.
+		if (target.getCurrentHp() < target.getMaxRecoverableHp())
+			target.setCurrentHp(amount + target.getCurrentHp());
+		
+		SystemMessage sm;
+		if (getEffector().getObjectId() != target.getObjectId())
+		{
+			sm = SystemMessage.getSystemMessage(SystemMessageId.S2_HP_RESTORED_BY_C1);
+			sm.addCharName(getEffector());
+		}
+		else
+			sm = SystemMessage.getSystemMessage(SystemMessageId.S1_HP_RESTORED);
+		
+		sm.addNumber((int)amount);
+		target.sendPacket(sm);
+		su.addAttribute(StatusUpdate.CUR_HP, (int) target.getCurrentHp());
+		target.sendPacket(su);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onActionTime()
+	{
+		return false;
+	}
+}
---
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/EffectManaHeal.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package handlers.effecthandlers;
+
+import com.l2jserver.gameserver.model.L2Effect;
+import com.l2jserver.gameserver.model.actor.L2Character;
+import com.l2jserver.gameserver.model.actor.instance.L2DoorInstance;
+import com.l2jserver.gameserver.network.SystemMessageId;
+import com.l2jserver.gameserver.network.serverpackets.StatusUpdate;
+import com.l2jserver.gameserver.network.serverpackets.SystemMessage;
+import com.l2jserver.gameserver.skills.Env;
+import com.l2jserver.gameserver.templates.effects.EffectTemplate;
+import com.l2jserver.gameserver.templates.skills.L2EffectType;
+
+/**
+ * @author UnAfraid
+ *
+ */
+public class EffectManaHeal extends L2Effect
+{
+	public EffectManaHeal(Env env, EffectTemplate template)
+	{
+		super(env, template);
+	}
+	
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.MANAHEAL;
+	}
+	
+	@Override
+	public boolean onStart()
+	{
+		L2Character target = getEffected();
+		if (target == null || target.isDead() || target instanceof L2DoorInstance)
+			return false;
+	
+		StatusUpdate su = new StatusUpdate(target);
+		
+		double amount = calc();
+		
+		amount = Math.min(amount, target.getMaxRecoverableMp() - target.getCurrentMp());
+		
+		// Prevent negative amounts
+		if (amount < 0)
+			amount = 0;
+		
+		// To prevent -value heals, set the value only if current mp is less than max recoverable.
+		if (target.getCurrentMp() < target.getMaxRecoverableMp())
+			target.setCurrentMp(amount + target.getCurrentMp());
+		
+		SystemMessage sm;
+		if (getEffector().getObjectId() != target.getObjectId())
+		{
+			sm = SystemMessage.getSystemMessage(SystemMessageId.S2_MP_RESTORED_BY_C1);
+			sm.addCharName(getEffector());
+		}
+		else
+			sm = SystemMessage.getSystemMessage(SystemMessageId.S1_MP_RESTORED);
+		sm.addNumber((int) amount);
+		target.sendPacket(sm);
+		su.addAttribute(StatusUpdate.CUR_MP, (int) target.getCurrentMp());
+		target.sendPacket(su);
+		
+		return true;
+	}
+	
+	@Override
+	public boolean onActionTime()
+	{
+		return false;
+	}
+}
