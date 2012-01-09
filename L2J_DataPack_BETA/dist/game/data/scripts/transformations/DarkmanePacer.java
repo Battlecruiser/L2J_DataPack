372a0ecd0b3ae723d9be3d884f6e32f87d671a94
Zoey76 <Zoey76@users.noreply.github.com>
2012-01-09 01:16:46 +0000

    BETA: Datapack fixes: 	* [8615] Improvements, thanks jurchiks for noticing it/fix contribution. 	* Treykan Transformation fixes, based on thecast/nonom contribution. 	* Darkamane Peacer Transformation fixes. 	* Dissonance(5437) skill fixes. 	* Cursed Body(967) skill pre-implemented, proper effects will come in next updates.

--- L2J_DataPack_BETA/dist/game/data/scripts/transformations/DarkmanePacer.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/transformations/DarkmanePacer.java
@@ -4,10 +4,14 @@ import com.l2jserver.gameserver.datatables.SkillTable;
 import com.l2jserver.gameserver.instancemanager.TransformationManager;
 import com.l2jserver.gameserver.model.L2Transformation;
 
-public class LightPurpleManedHorse extends L2Transformation
+public class DarkmanePacer extends L2Transformation
 {
-	private static final int[] SKILLS = {5491,839};
-	public LightPurpleManedHorse()
+	private static final int[] SKILLS =
+	{
+		5437, 839
+	};
+	
+	public DarkmanePacer()
 	{
 		// id, colRadius, colHeight
 		super(106, 31, 32.5);
@@ -16,16 +20,18 @@ public class LightPurpleManedHorse extends L2Transformation
 	@Override
 	public void onTransform()
 	{
-		if (getPlayer().getTransformationId() != 106 || getPlayer().isCursedWeaponEquipped())
+		if ((getPlayer().getTransformationId() != 106) || getPlayer().isCursedWeaponEquipped())
+		{
 			return;
+		}
 		
 		transformedSkills();
 	}
 	
 	public void transformedSkills()
 	{
-		// Decrease Bow/Crossbow Attack Speed
-		getPlayer().addSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dissonance
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(5437, 2), false);
 		// Dismount
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		
@@ -40,8 +46,8 @@ public class LightPurpleManedHorse extends L2Transformation
 	
 	public void removeSkills()
 	{
-		// Decrease Bow/Crossbow Attack Speed
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5491, 1), false);
+		// Dissonance
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5437, 2), false);
 		// Dismount
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(839, 1), false);
 		
@@ -50,6 +56,6 @@ public class LightPurpleManedHorse extends L2Transformation
 	
 	public static void main(String[] args)
 	{
-		TransformationManager.getInstance().registerTransformation(new LightPurpleManedHorse());
+		TransformationManager.getInstance().registerTransformation(new DarkmanePacer());
 	}
 }
