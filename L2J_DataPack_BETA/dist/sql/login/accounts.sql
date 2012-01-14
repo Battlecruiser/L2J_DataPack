83c046c6b43d47e49b1800332a16efe859acdbfd
MELERIX <MELERIX@users.noreply.github.com>
2012-01-14 06:44:49 +0000

    BETA/STABLE: Added `email` & `created_time` columns to accounts.sql (used by some tools like ACM and Drupal).

--- L2J_DataPack_BETA/dist/sql/login/accounts.sql
+++ L2J_DataPack_BETA/dist/sql/login/accounts.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `accounts` (
   `login` VARCHAR(45) NOT NULL default '',
-  `password` VARCHAR(45) ,
+  `password` VARCHAR(45),
+  `email` varchar(255) DEFAULT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastactive` bigint(13) unsigned NOT NULL DEFAULT '0',
   `accessLevel` TINYINT NOT NULL DEFAULT 0,
   `lastIP` CHAR(15) NULL DEFAULT NULL,
