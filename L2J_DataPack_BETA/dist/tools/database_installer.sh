4940ac56175991172db609e33c223d1b7a300e7f
MELERIX <MELERIX@users.noreply.github.com>
2012-05-16 01:47:03 +0000

    BETA: Removed "update" boolean from Ant Zip Task, because it doesn't build the Zip file properly (doesn't remove the old files), and few other changes included for database_installers.

--- L2J_DataPack_BETA/dist/tools/database_installer.sh
+++ L2J_DataPack_BETA/dist/tools/database_installer.sh
@@ -241,6 +241,7 @@ ls_install
 }
 
 ls_upgrade(){
+clear
 echo ""
 echo "Upgrading structure of Community Server tables."
 echo ""
@@ -326,6 +327,7 @@ cs_install
 }
 
 cs_upgrade(){
+clear
 echo ""
 echo "Upgrading structure of Game Server tables."
 echo ""
@@ -411,6 +413,7 @@ gs_install
 }
 
 gs_upgrade(){
+clear
 echo ""
 echo "Upgrading structure of Game Server tables (this could take awhile, be patient)"
 echo ""
