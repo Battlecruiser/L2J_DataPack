685425fef28d80fd66ac21ee6844d052667fb0d8
MELERIX <MELERIX@users.noreply.github.com>
2012-05-20 03:42:06 +0000

    BETA: Minor fix for L2DoorInstanceActionShift to show min/max XYZ properly.

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/actionhandlers/L2DoorInstanceActionShift.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/actionhandlers/L2DoorInstanceActionShift.java
@@ -33,24 +33,23 @@ public class L2DoorInstanceActionShift implements IActionHandler
 			activeChar.setTarget(target);
 			activeChar.sendPacket(new MyTargetSelected(target.getObjectId(), activeChar.getLevel()));
 			
-			L2DoorInstance door = (L2DoorInstance)target;
+			L2DoorInstance door = (L2DoorInstance) target;
 			StaticObject su = new StaticObject(door, activeChar.isGM());
 			activeChar.sendPacket(su);
 			
 			NpcHtmlMessage html = new NpcHtmlMessage(0);
 			html.setFile(activeChar.getHtmlPrefix(), "data/html/admin/doorinfo.htm");
 			html.replace("%class%", target.getClass().getSimpleName());
-			html.replace("%hp%",    String.valueOf((int)door.getCurrentHp()));
+			html.replace("%hp%", String.valueOf((int) door.getCurrentHp()));
 			html.replace("%hpmax%", String.valueOf(door.getMaxHp()));
 			html.replace("%objid%", String.valueOf(target.getObjectId()));
-			html.replace("%doorid%",  String.valueOf(door.getDoorId()));
+			html.replace("%doorid%", String.valueOf(door.getDoorId()));
 			
-			// TODO: Change minx/y & maxx/y to use zone bounds
 			html.replace("%minx%", String.valueOf(door.getX(0)));
-			html.replace("%miny%", String.valueOf(door.getY(1)));
+			html.replace("%miny%", String.valueOf(door.getY(0)));
 			html.replace("%minz%", String.valueOf(door.getZMin()));
 			
-			html.replace("%maxx%", String.valueOf(door.getX(1)));
+			html.replace("%maxx%", String.valueOf(door.getX(2)));
 			html.replace("%maxy%", String.valueOf(door.getY(2)));
 			html.replace("%maxz%", String.valueOf(door.getZMax()));
 			html.replace("%unlock%", door.isOpenableBySkill() ? "<font color=00FF00>YES<font>" : "<font color=FF0000>NO</font>");
