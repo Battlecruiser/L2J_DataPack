5da7050c663fe0b2f206d4df06ea15a40efc11a4
Rumen Nikiforov <unafraid89@gmail.com>
2012-05-21 21:19:43 +0000

    BETA: DP-Part for [L5373]

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/itemhandlers/SoulShots.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/itemhandlers/SoulShots.java
@@ -14,12 +14,15 @@
  */
 package handlers.itemhandlers;
 
+import java.util.logging.Level;
+
 import com.l2jserver.gameserver.handler.IItemHandler;
 import com.l2jserver.gameserver.model.actor.L2Playable;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.items.L2Item;
+import com.l2jserver.gameserver.model.holders.SkillHolder;
 import com.l2jserver.gameserver.model.items.L2Weapon;
 import com.l2jserver.gameserver.model.items.instance.L2ItemInstance;
+import com.l2jserver.gameserver.model.items.type.L2ActionType;
 import com.l2jserver.gameserver.network.SystemMessageId;
 import com.l2jserver.gameserver.network.serverpackets.MagicSkillUse;
 import com.l2jserver.gameserver.util.Broadcast;
@@ -30,7 +33,7 @@ public class SoulShots implements IItemHandler
 	@Override
 	public boolean useItem(L2Playable playable, L2ItemInstance item, boolean forceUse)
 	{
-		if (!(playable instanceof L2PcInstance))
+		if (!playable.isPlayer())
 		{
 			return false;
 		}
@@ -38,7 +41,15 @@ public class SoulShots implements IItemHandler
 		final L2PcInstance activeChar = playable.getActingPlayer();
 		final L2ItemInstance weaponInst = activeChar.getActiveWeaponInstance();
 		final L2Weapon weaponItem = activeChar.getActiveWeaponItem();
-		final int itemId = item.getItemId();
+		final SkillHolder[] skills = item.getItem().getSkills();
+		
+		int itemId = item.getItemId();
+		
+		if (skills == null)
+		{
+			_log.log(Level.WARNING, getClass().getSimpleName() + ": is missing skills!");
+			return false;
+		}
 		
 		// Check if Soul shot can be used
 		if ((weaponInst == null) || (weaponItem.getSoulShotCount() == 0))
@@ -50,49 +61,7 @@ public class SoulShots implements IItemHandler
 			return false;
 		}
 		
-		boolean gradeCheck = true;
-		final int weaponGrade = weaponItem.getCrystalType();
-		switch (weaponGrade)
-		{
-			case L2Item.CRYSTAL_NONE:
-				if ((itemId != 5789) && (itemId != 1835))
-				{
-					gradeCheck = false;
-				}
-				break;
-			case L2Item.CRYSTAL_D:
-				if ((itemId != 1463) && (itemId != 22082))
-				{
-					gradeCheck = false;
-				}
-				break;
-			case L2Item.CRYSTAL_C:
-				if ((itemId != 1464) && (itemId != 22083))
-				{
-					gradeCheck = false;
-				}
-				break;
-			case L2Item.CRYSTAL_B:
-				if ((itemId != 1465) && (itemId != 22084))
-				{
-					gradeCheck = false;
-				}
-				break;
-			case L2Item.CRYSTAL_A:
-				if ((itemId != 1466) && (itemId != 22085))
-				{
-					gradeCheck = false;
-				}
-				break;
-			case L2Item.CRYSTAL_S:
-			case L2Item.CRYSTAL_S80:
-			case L2Item.CRYSTAL_S84:
-				if ((itemId != 1467) && (itemId != 22086))
-				{
-					gradeCheck = false;
-				}
-				break;
-		}
+		boolean gradeCheck = item.isEtcItem() && item.getEtcItem().getDefaultAction() == L2ActionType.soulshot && weaponInst.getItem().getItemGradeSPlus() == item.getItem().getItemGradeSPlus();
 		
 		if (!gradeCheck)
 		{
@@ -134,47 +103,10 @@ public class SoulShots implements IItemHandler
 		{
 			activeChar.soulShotLock.unlock();
 		}
-		int skillId = 0;
-		switch (itemId)
-		{
-			case 1835:
-			case 5789:
-				skillId = 2039;
-				break;
-			case 1463:
-				skillId = 2150;
-				break;
-			case 1464:
-				skillId = 2151;
-				break;
-			case 1465:
-				skillId = 2152;
-				break;
-			case 1466:
-				skillId = 2153;
-				break;
-			case 1467:
-				skillId = 2154;
-				break;
-			case 22082:
-				skillId = 26060;
-				break;
-			case 22083:
-				skillId = 26061;
-				break;
-			case 22084:
-				skillId = 26062;
-				break;
-			case 22085:
-				skillId = 26063;
-				break;
-			case 22086:
-				skillId = 26064;
-				break;
-		}
+		
 		// Send message to client
 		activeChar.sendPacket(SystemMessageId.ENABLED_SOULSHOT);
-		Broadcast.toSelfAndKnownPlayersInRadius(activeChar, new MagicSkillUse(activeChar, activeChar, skillId, 1, 0, 0), 360000);
+		Broadcast.toSelfAndKnownPlayersInRadius(activeChar, new MagicSkillUse(activeChar, activeChar, skills[0].getSkillId(), skills[0].getSkillLvl(), 0, 0), 360000);
 		return true;
 	}
 }
