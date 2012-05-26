30fd2a8436225ae17a2b2f21a9ff72efb507e46b
MELERIX <MELERIX@users.noreply.github.com>
2012-05-26 06:49:59 +0000

    BETA: Small Improvement for [8870].

--- L2J_DataPack_BETA/dist/sql/game/updates/20110831update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20110831update.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `characters` ADD `createDate` date NOT NULL DEFAULT '0000-00-00' AFTER `createTime`;
-UPDATE `characters` SET `createDate`=CURDATE() WHERE `createDate`='0000-00-00';
+UPDATE `characters` SET `createDate`=CURDATE() WHERE `createDate`<'2004-00-00';
 ALTER TABLE `characters` DROP `createTime`;
 
 ALTER TABLE `messages` CHANGE `isFourStars` `sendBySystem` tinyint(1) unsigned NOT NULL DEFAULT '0' AFTER `isLocked`;
