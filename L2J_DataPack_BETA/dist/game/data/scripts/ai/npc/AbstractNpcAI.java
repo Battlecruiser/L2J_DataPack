09a69e3d5ac3612ba6bfc59f016e4bae613d10c2
Zoey76 <zoey_76@msn.com>
2012-08-12 17:04:01 +0000

    BETA: Unardcoding instance of Territory Managers: 	* Using retail htmls. 		* Removed all the duplicated htmls, same way as retail does, we send Gludio's Npc htmls. 	* Added all retail conditions to become noblesse. 		* Inspired by malyelfik's work. 		* Now noblesse quest are completed and related items are removed. 		* Subclass quest is completed, depending if player's race is Kamael or not. 		* If the player is under level 40 or hasn't completed the second class transfer, the Territory Manager will send a different html. 		* Added level 75 missing check. 		* Reworked the way item Ids and territory Ids are generated, bypass won't be exploited. 	* Implementing AbstractNpcAI by UnAfraid and me. 		* Sets a new standard to make NPC AIs, outside the core. 		* Extends L2Script, you can use Listeners! 		* Keeps the script and the htmls together, easy to modify and easy to disable/enable. 			* Features removed in upcoming versions, could be retained as L2J customs.

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/npc/AbstractNpcAI.java
@@ -0,0 +1,40 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.npc;
+
+import com.l2jserver.gameserver.model.actor.L2Npc;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.scripting.scriptengine.impl.L2Script;
+
+/**
+ * Abstract NPC AI class for datapack based AIs.
+ * @author UnAfraid, Zoey76
+ */
+public abstract class AbstractNpcAI extends L2Script
+{
+	/**
+	 * Simple on first talk event handler.
+	 */
+	@Override
+	public String onFirstTalk(L2Npc npc, L2PcInstance player)
+	{
+		return npc.getNpcId() + ".html";
+	}
+	
+	public AbstractNpcAI(String name, String descr)
+	{
+		super(-1, name, descr);
+	}
+}
