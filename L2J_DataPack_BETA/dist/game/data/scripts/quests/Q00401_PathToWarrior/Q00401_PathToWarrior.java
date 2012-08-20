e9e6ab00e16cfadc9e2720e2fe05ff1f59017370
Rumen Nikiforov <unafraid89@gmail.com>
2012-08-20 01:08:21 +0000

    BETA: Updating Quest 401 PathToWarrior: 	* Added the author. 	* Updated code to last standards. 	* Cleanup.

--- L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00401_PathToWarrior/Q00401_PathToWarrior.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00401_PathToWarrior/Q00401_PathToWarrior.java
@@ -16,35 +16,33 @@ package quests.Q00401_PathToWarrior;
 
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.base.ClassId;
 import com.l2jserver.gameserver.model.itemcontainer.Inventory;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
-import com.l2jserver.gameserver.model.quest.State;
 import com.l2jserver.gameserver.network.serverpackets.SocialAction;
 
 /**
- * Path of the Warrior (401)
- * @author jython script by ElgarL, Mr.
+ * Path of the Warrior (401)<br>
+ * Jython script by ElgarL, Mr
+ * @author malyelfik
  * @version 2010-12-29 (Freya)
  */
 public class Q00401_PathToWarrior extends Quest
 {
-	
 	// Item
-	private static final int AuronsLetter = 1138;
-	private static final int WarriorGuildMark = 1139;
-	private static final int RustedBronzeSword1 = 1140;
-	private static final int RustedBronzeSword2 = 1141;
-	private static final int RustedBronzeSword3 = 1142;
-	private static final int SimplonsLetter = 1143;
-	private static final int PoisonSpiderLeg = 1144;
-	private static final int MedallionOfWarrior = 1145;
+	private static final int AURONSLETTER = 1138;
+	private static final int WARRIORGUILDMARK = 1139;
+	private static final int RUSTEDBRONZESWORD1 = 1140;
+	private static final int RUSTEDBRONZESWORD2 = 1141;
+	private static final int RUSTEDBRONZESWORD3 = 1142;
+	private static final int SIMPLONSLETTER = 1143;
+	private static final int POISONSPIDERLEG = 1144;
+	private static final int MEDALLIONOFWARRIOR = 1145;
 	
 	// Npc
-	private static final int Auron = 30010;
-	private static final int Simplon = 30253;
-	private static final int[] Monsters =
+	private static final int AURON = 30010;
+	private static final int SIMPLON = 30253;
+	private static final int[] MONSTERS =
 	{
 		20035,
 		20038,
@@ -65,47 +63,49 @@ public class Q00401_PathToWarrior extends Quest
 		
 		if (event.equalsIgnoreCase("401_1"))
 		{
-			if (player.getClassId().getId() == 0x00)
+			switch (player.getClassId())
 			{
-				if (player.getLevel() >= 18)
+				case fighter:
 				{
-					if (st.getQuestItemsCount(MedallionOfWarrior) == 1)
+					if (player.getLevel() >= 18)
 					{
-						htmltext = "30010-04.htm";
+						if (st.getQuestItemsCount(MEDALLIONOFWARRIOR) == 1)
+						{
+							htmltext = "30010-04.htm";
+						}
+						else
+						{
+							htmltext = "30010-05.htm";
+						}
 					}
 					else
 					{
-						htmltext = "30010-05.htm";
+						htmltext = "30010-02.htm";
 					}
+					break;
 				}
-				else
+				case warrior:
 				{
-					htmltext = "30010-02.htm";
+					htmltext = "30010-03.htm";
+					break;
 				}
-			}
-			else if (player.getClassId() == ClassId.warrior)
-			{
-				htmltext = "30010-03.htm";
-			}
-			else
-			{
-				htmltext = "30010-02b.htm";
+				default:
+				{
+					htmltext = "30010-02b.htm";
+				}
 			}
 		}
 		else if (event.equalsIgnoreCase("401_accept"))
 		{
-			st.setState(State.STARTED);
-			st.set("cond", "1");
-			st.giveItems(AuronsLetter, 1);
-			st.playSound("ItemSound.quest_accept");
+			st.startQuest();
+			st.giveItems(AURONSLETTER, 1);
 			htmltext = "30010-06.htm";
 		}
 		else if (event.equalsIgnoreCase("30253_1"))
 		{
-			st.set("cond", "2");
-			st.playSound("ItemSound.quest_middle");
-			st.takeItems(AuronsLetter, 1);
-			st.giveItems(WarriorGuildMark, 1);
+			st.setCond(2, true);
+			st.takeItems(AURONSLETTER, 1);
+			st.giveItems(WARRIORGUILDMARK, 1);
 			htmltext = "30253-02.html";
 		}
 		else if (event.equalsIgnoreCase("401_2"))
@@ -114,11 +114,10 @@ public class Q00401_PathToWarrior extends Quest
 		}
 		else if (event.equalsIgnoreCase("401_3"))
 		{
-			st.set("cond", "5");
-			st.takeItems(RustedBronzeSword2, 1);
-			st.giveItems(RustedBronzeSword3, 1);
-			st.takeItems(SimplonsLetter, 1);
-			st.playSound("ItemSound.quest_middle");
+			st.setCond(5, true);
+			st.takeItems(RUSTEDBRONZESWORD2, 1);
+			st.giveItems(RUSTEDBRONZESWORD3, 1);
+			st.takeItems(SIMPLONSLETTER, 1);
 			htmltext = "30010-11.html";
 		}
 		return htmltext;
@@ -134,147 +133,172 @@ public class Q00401_PathToWarrior extends Quest
 			return htmltext;
 		}
 		
-		if (npc.getNpcId() == Auron)
+		switch (npc.getNpcId())
 		{
-			if (st.getInt("cond") == 0)
+			case AURON:
 			{
-				htmltext = "30010-01.htm";
-			}
-			else if (st.getInt("cond") == 1)
-			{
-				htmltext = "30010-07.html";
-			}
-			else if ((st.getInt("cond") == 2) || (st.getInt("cond") == 3))
-			{
-				htmltext = "30010-08.html";
-			}
-			else if (st.getInt("cond") == 4)
-			{
-				htmltext = "30010-09.html";
-			}
-			else if (st.getInt("cond") == 5)
-			{
-				htmltext = "30010-12.html";
-			}
-			else if (st.getInt("cond") == 6)
-			{
-				st.takeItems(RustedBronzeSword3, 1);
-				st.takeItems(PoisonSpiderLeg, -1);
-				if (player.getLevel() >= 20)
+				switch (st.getCond())
 				{
-					st.addExpAndSp(320534, 21012);
+					case 0:
+					{
+						htmltext = "30010-01.htm";
+						break;
+					}
+					case 1:
+					{
+						htmltext = "30010-07.html";
+						break;
+					}
+					case 2:
+					case 3:
+					{
+						htmltext = "30010-08.html";
+						break;
+					}
+					case 4:
+					{
+						htmltext = "30010-09.html";
+						break;
+					}
+					case 5:
+					{
+						htmltext = "30010-12.html";
+						break;
+					}
+					case 6:
+					{
+						st.takeItems(RUSTEDBRONZESWORD3, 1);
+						st.takeItems(POISONSPIDERLEG, -1);
+						
+						if (player.getLevel() >= 20)
+						{
+							st.addExpAndSp(320534, 21012);
+						}
+						else if (player.getLevel() == 19)
+						{
+							st.addExpAndSp(456128, 27710);
+						}
+						else
+						{
+							st.addExpAndSp(160267, 34408);
+						}
+						
+						st.giveAdena(163800, true);
+						st.giveItems(MEDALLIONOFWARRIOR, 1);
+						player.sendPacket(new SocialAction(player.getObjectId(), 3));
+						st.saveGlobalQuestVar("1ClassQuestFinished", "1");
+						st.exitQuest(false, true);
+						htmltext = "30010-13.html";
+						break;
+					}
 				}
-				else if (player.getLevel() == 19)
+				break;
+			}
+			case SIMPLON:
+			{
+				switch (st.getCond())
 				{
-					st.addExpAndSp(456128, 27710);
+					case 1:
+					{
+						htmltext = "30253-01.html";
+						break;
+					}
+					case 2:
+					{
+						htmltext = "30253-03.html";
+						break;
+					}
+					case 3:
+					{
+						st.setCond(4, true);
+						st.takeItems(WARRIORGUILDMARK, 1);
+						st.takeItems(RUSTEDBRONZESWORD1, 10);
+						st.giveItems(RUSTEDBRONZESWORD2, 1);
+						st.giveItems(SIMPLONSLETTER, 1);
+						htmltext = "30253-04.html";
+						break;
+					}
+					case 4:
+					{
+						htmltext = "30253-05.html";
+						break;
+					}
 				}
-				else
-				{
-					st.addExpAndSp(160267, 34408);
-				}
-				st.giveItems(57, 163800);
-				st.giveItems(MedallionOfWarrior, 1);
-				st.playSound("ItemSound.quest_finish");
-				player.sendPacket(new SocialAction(player.getObjectId(), 3));
-				st.saveGlobalQuestVar("1ClassQuestFinished", "1");
-				st.exitQuest(false);
-				htmltext = "30010-13.html";
-			}
-		}
-		else if (npc.getNpcId() == Simplon)
-		{
-			if (st.getInt("cond") == 1)
-			{
-				htmltext = "30253-01.html";
-			}
-			else if (st.getInt("cond") == 2)
-			{
-				htmltext = "30253-03.html";
-			}
-			else if (st.getInt("cond") == 3)
-			{
-				st.set("cond", "4");
-				st.playSound("ItemSound.quest_middle");
-				st.takeItems(WarriorGuildMark, 1);
-				st.takeItems(RustedBronzeSword1, 10);
-				st.giveItems(RustedBronzeSword2, 1);
-				st.giveItems(SimplonsLetter, 1);
-				htmltext = "30253-04.html";
-			}
-			else if (st.getInt("cond") == 4)
-			{
-				htmltext = "30253-05.html";
+				break;
 			}
 		}
 		return htmltext;
 	}
 	
 	@Override
-	public String onKill(L2Npc npc, L2PcInstance player, boolean isPet)
+	public String onKill(L2Npc npc, L2PcInstance killer, boolean isPet)
 	{
-		QuestState st = player.getQuestState(getName());
+		QuestState st = killer.getQuestState(getName());
 		if (st == null)
 		{
 			return null;
 		}
 		
-		if (st.getInt("cond") == 2)
+		switch (st.getCond())
 		{
-			if ((npc.getNpcId() == Monsters[0]) || (npc.getNpcId() == Monsters[2]))
+			case 2:
 			{
-				if (st.getQuestItemsCount(RustedBronzeSword1) < 10)
+				if ((npc.getNpcId() == MONSTERS[0]) || (npc.getNpcId() == MONSTERS[2]))
 				{
-					if (getRandom(10) < 4)
+					if (st.getQuestItemsCount(RUSTEDBRONZESWORD1) < 10)
 					{
-						st.giveItems(RustedBronzeSword1, 1);
-						st.playSound("ItemSound.quest_itemget");
+						if (getRandom(10) < 4)
+						{
+							st.giveItems(RUSTEDBRONZESWORD1, 1);
+							st.playSound("ItemSound.quest_itemget");
+						}
+					}
+					if (st.getQuestItemsCount(RUSTEDBRONZESWORD1) == 10)
+					{
+						st.setCond(3, true);
 					}
 				}
-				if (st.getQuestItemsCount(RustedBronzeSword1) == 10)
-				{
-					st.playSound("ItemSound.quest_middle");
-					st.set("cond", "3");
-				}
+				break;
 			}
-		}
-		else if ((st.getInt("cond") == 5) && (st.getItemEquipped(Inventory.PAPERDOLL_RHAND) == RustedBronzeSword3))
-		{
-			if ((npc.getNpcId() == Monsters[1]) || (npc.getNpcId() == Monsters[3]))
+			case 5:
 			{
-				if (st.getQuestItemsCount(PoisonSpiderLeg) < 20)
+				if ((st.getItemEquipped(Inventory.PAPERDOLL_RHAND) == RUSTEDBRONZESWORD3) && (npc.getNpcId() == MONSTERS[1]) || (npc.getNpcId() == MONSTERS[3]))
 				{
-					st.giveItems(PoisonSpiderLeg, 1);
-					st.playSound("ItemSound.quest_itemget");
-				}
-				if (st.getQuestItemsCount(PoisonSpiderLeg) == 20)
-				{
-					st.playSound("ItemSound.quest_middle");
-					st.set("cond", "6");
+					if (st.getQuestItemsCount(POISONSPIDERLEG) < 20)
+					{
+						st.giveItems(POISONSPIDERLEG, 1);
+						st.playSound("ItemSound.quest_itemget");
+					}
+					
+					if (st.getQuestItemsCount(POISONSPIDERLEG) == 20)
+					{
+						st.setCond(6, true);
+					}
 				}
 			}
 		}
-		return null;
+		
+		return super.onKill(npc, killer, isPet);
 	}
 	
 	public Q00401_PathToWarrior(int questId, String name, String descr)
 	{
 		super(questId, name, descr);
-		addStartNpc(Auron);
-		addTalkId(Auron);
-		addTalkId(Simplon);
-		for (int i : Monsters)
-		{
-			addKillId(i);
-		}
+		
+		addStartNpc(AURON);
+		addTalkId(AURON);
+		addTalkId(SIMPLON);
+		addKillId(MONSTERS);
+		
 		questItemIds = new int[]
 		{
-			AuronsLetter,
-			WarriorGuildMark,
-			RustedBronzeSword1,
-			RustedBronzeSword2,
-			RustedBronzeSword3,
-			SimplonsLetter,
-			PoisonSpiderLeg
+			AURONSLETTER,
+			WARRIORGUILDMARK,
+			RUSTEDBRONZESWORD1,
+			RUSTEDBRONZESWORD2,
+			RUSTEDBRONZESWORD3,
+			SIMPLONSLETTER,
+			POISONSPIDERLEG
 		};
 	}
 	
