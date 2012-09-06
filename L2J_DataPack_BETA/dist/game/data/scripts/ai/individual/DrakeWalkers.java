b52e8a0a8d3a40036261d9c72f23c5a767fd222d
Adry_85 <Adrya85@hotmail.it>
2012-09-06 18:05:02 +0000

    BETA: '''Dragon Valley''' and '''Antharas' Lair''' base implementation:
    	- Antharas' Lair and Dragon Valley have been revamped. New Separated Soul NPCs live in Hunter's Village, Dragon Valley, and Antharas' Lair that can teleport characters level 80 and above between each of these areas.
    	- Separated Soul NPCs can synthesize certain items into Antharas Blood Crystals:
    		- Will of Antharas: Can be obtained from the Raid Boss monsters in Antharas' Lair.
        	- Sealed Blood Crystal: Can be obtained from the raid boss monster in Dragon Valley.
    	- Certain older monsters in Dragon Valley have been moved to Watcher's Tomb.
    	- The Large Dragon Bone item, which you can obtain in this zone, summons particular raid boss monsters. Use the item on a Dragon Vortex found in four places inside Dragon Valley to summon and hunt the following raid boss monsters:
    	   	- Emerald Horn
        	- Bleeding Fly
        	- Blackdagger Wing
        	- Shadow Summoner
        	- Spike Slasher
        	- Muscle Bomber
        	- Dust Rider
        - Antharas’ overall stats have been reduced.
        - Some monsters in Dragon Valley are running called Trackers(Howl, Mesmer Drake, Dragon Tracker of the Valley, etc).
        - Antharas' Lair new RBs and Knoriks can see you through Silent Move.
    	
    	'''NOTE:''' Remember to run the SQL update queries to delete old raid bosses. 
        
        Patch by: Adry_85
        Review by: Adry_85, nonom, !UnAfraid
        Tested by: Adry_85, !MELERIX, nonom, !UnAfraid
        
        Special thanks for corbin12, goncafa, !MELERIX, Michalis, Micr0, u3games and !UnAfraid for the other support and features.

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/individual/DrakeWalkers.java
@@ -0,0 +1,76 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.individual;
+
+import java.util.Arrays;
+
+import javolution.util.FastSet;
+import ai.npc.AbstractNpcAI;
+
+import com.l2jserver.gameserver.datatables.SpawnTable;
+import com.l2jserver.gameserver.instancemanager.WalkingManager;
+import com.l2jserver.gameserver.model.L2Spawn;
+import com.l2jserver.gameserver.model.actor.L2Npc;
+import com.l2jserver.util.Rnd;
+
+/**
+ * Drake Walkers AI.
+ * @author UnAfraid
+ */
+public class DrakeWalkers extends AbstractNpcAI
+{
+	private static final int[] DRAKES =
+	{
+		22848,
+		22849,
+		22850,
+		22851,
+		22857
+	};
+	
+	private static final int[] ROUTE_ID =
+	{
+		11,
+		12,
+		13
+	};
+	
+	private DrakeWalkers(String name, String descr)
+	{
+		super(name, descr);
+		addSpawnId(DRAKES);
+		
+		FastSet<L2Spawn> spawns = SpawnTable.getInstance().getSpawnTable();
+		for (L2Spawn spawn : spawns)
+		{
+			if (Arrays.binarySearch(DRAKES, spawn.getNpcid()) >= 0)
+			{
+				onSpawn(spawn.getLastSpawn());
+			}
+		}
+	}
+	
+	@Override
+	public String onSpawn(L2Npc npc)
+	{
+		WalkingManager.getInstance().startMoving(npc, ROUTE_ID[Rnd.get(ROUTE_ID.length)]);
+		return super.onSpawn(npc);
+	}
+	
+	public static void main(String[] args)
+	{
+		new DrakeWalkers(DrakeWalkers.class.getSimpleName(), "ai");
+	}
+}
---
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/individual/SandDrake.java
@@ -0,0 +1,69 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.individual;
+
+import javolution.util.FastSet;
+import ai.npc.AbstractNpcAI;
+
+import com.l2jserver.gameserver.datatables.SpawnTable;
+import com.l2jserver.gameserver.instancemanager.WalkingManager;
+import com.l2jserver.gameserver.model.L2Spawn;
+import com.l2jserver.gameserver.model.actor.L2Npc;
+
+/**
+ * Sand Drake AI.
+ * @author Adry_85
+ */
+public class SandDrake extends AbstractNpcAI
+{
+	private static final int SAND_DRAKE_ID = 22833;
+	
+	private static final int[] ROUTE_ID =
+	{
+		28,
+		29
+	};
+	
+	private SandDrake(String name, String descr)
+	{
+		super(name, descr);
+		addSpawnId(SAND_DRAKE_ID);
+		
+		FastSet<L2Spawn> spawns = SpawnTable.getInstance().getSpawnTable();
+		for (L2Spawn spawn : spawns)
+		{
+			if (spawn.getNpcid() == SAND_DRAKE_ID)
+			{
+				onSpawn(spawn.getLastSpawn());
+			}
+		}
+	}
+	
+	@Override
+	public String onSpawn(L2Npc npc)
+	{
+		for (int element : ROUTE_ID)
+		{
+			WalkingManager.getInstance().startMoving(npc, element);
+		}
+		
+		return super.onSpawn(npc);
+	}
+	
+	public static void main(String[] args)
+	{
+		new SandDrake(SandDrake.class.getSimpleName(), "ai");
+	}
+}
