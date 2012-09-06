4a7e2ec13b1eb7b29c46cf274dc3842b3a501fdd
MELERIX <MELERIX@users.noreply.github.com>
2012-09-06 05:50:38 +0000

    BETA: oops it was accidentally deleted in [9148] -.-

--- L2J_DataPack_BETA/dist/tools/gs_cleanup.sql
+++ L2J_DataPack_BETA/dist/tools/gs_cleanup.sql
@@ -1,3 +1,12 @@
+-- These queries are meant to delete any gameserver table you may have from previous
+-- L2J installations.
+--
+-- Queries that create these tables and/or populate them apropiately are available in
+-- the ../sql/ folder.
+--
+-- See the documentation.txt file for more details.  
+
+DROP TABLE IF EXISTS 
 account_gsdata,
 airships,
 auction,
