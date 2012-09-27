5b12b5a1bd87647c76933c675ea7f487b7e90136
malyelfik <malyelfik@users.noreply.github.com>
2012-09-27 19:32:15 +0000

    BETA: Improved quest '''Control Device of the Giants (307) '''  * Now Hekaton respawn isn't stored only for players who killed him

--- L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00307_ControlDeviceOfTheGiants/Q00307_ControlDeviceOfTheGiants.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00307_ControlDeviceOfTheGiants/Q00307_ControlDeviceOfTheGiants.java
@@ -29,91 +29,79 @@ public class Q00307_ControlDeviceOfTheGiants extends Quest
 {
 	// NPC
 	private final static int DROPH = 32711;
+	
 	// RB
 	private final static int GORGOLOS = 25681;
 	private final static int LAST_TITAN_UTENUS = 25684;
 	private final static int GIANT_MARPANAK = 25680;
 	private final static int HEKATON_PRIME = 25687;
+	
 	// Items
 	private final static int SUPPORT_ITEMS = 14850;
 	private final static int CET_1_SHEET = 14851;
 	private final static int CET_2_SHEET = 14852;
 	private final static int CET_3_SHEET = 14853;
 	
-	private final static int respawnDelay = 3600000; // 1 hour
+	// Misc
+	private final static int RESPAWN_DELAY = 3600000; // 1 hour
+	private static L2Npc hekaton;
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		QuestState st = player.getQuestState(getName());
-		String htmltext = event;
-		
+		final QuestState st = player.getQuestState(getName());
 		if (st == null)
 		{
 			return null;
 		}
 		
-		if (event.equalsIgnoreCase("32711-04.htm"))
+		String htmltext = event;
+		switch (event)
 		{
-			if (st.getPlayer().getLevel() >= 79)
-			{
-				st.setState(State.STARTED);
-				st.set("cond", "1");
-				st.playSound("ItemSound.quest_accept");
-				if (!st.hasQuestItems(CET_1_SHEET) || !st.hasQuestItems(CET_2_SHEET) || !st.hasQuestItems(CET_3_SHEET))
+			case "32711-04.html":
+				if (player.getLevel() >= 79)
 				{
-					htmltext = "32711-04.htm";
+					st.startQuest();
+					htmltext = (hasQuestItems(player, CET_1_SHEET, CET_2_SHEET, CET_3_SHEET)) ? "32711-04a.html" : "32711-04.html";
 				}
-				else if (st.hasQuestItems(CET_1_SHEET) && st.hasQuestItems(CET_2_SHEET) && st.hasQuestItems(CET_3_SHEET))
+				break;
+			case "32711-05a.html":
+				player.sendPacket(new RadarControl(0, 2, 186214, 61591, -4152));
+				break;
+			case "32711-05b.html":
+				player.sendPacket(new RadarControl(0, 2, 187554, 60800, -4984));
+				break;
+			case "32711-05c.html":
+				player.sendPacket(new RadarControl(0, 2, 193432, 53922, -4368));
+				break;
+			case "spawn":
+				if (!hasQuestItems(player, CET_1_SHEET, CET_2_SHEET, CET_3_SHEET))
 				{
-					htmltext = "32711-04a.htm";
+					return getNoQuestMsg(player);
 				}
-			}
-		}
-		else if (event.equalsIgnoreCase("32711-05a.htm"))
-		{
-			player.sendPacket(new RadarControl(0, 2, 186214, 61591, -4152));
-		}
-		else if (event.equalsIgnoreCase("32711-05b.htm"))
-		{
-			player.sendPacket(new RadarControl(0, 2, 187554, 60800, -4984));
-		}
-		else if (event.equalsIgnoreCase("32711-05c.htm"))
-		{
-			player.sendPacket(new RadarControl(0, 2, 193432, 53922, -4368));
-		}
-		// Hekaton Prime spawn
-		else if (event.equalsIgnoreCase("spawn"))
-		{
-			String test = st.getGlobalQuestVar("Hekaton respawn");
-			
-			if (test.isEmpty())
-			{
+				else if ((hekaton != null) && !hekaton.isDead())
+				{
+					return "32711-09.html";
+				}
+				String respawn = loadGlobalQuestVar("Respawn");
+				long remain = (!respawn.isEmpty()) ? Long.parseLong(respawn) - System.currentTimeMillis() : 0;
+				if (remain > 0)
+				{
+					return "32711-09a.html";
+				}
 				st.takeItems(CET_1_SHEET, 1);
 				st.takeItems(CET_2_SHEET, 1);
 				st.takeItems(CET_3_SHEET, 1);
-				addSpawn(HEKATON_PRIME, 191887, 56405, -7626, 1000, false, 0);
-				st.set("spawned", "1");
-				htmltext = "32711-09.htm";
-			}
-			else
-			{
-				long remain = Long.parseLong(test) - System.currentTimeMillis();
-				
-				if (remain > 0)
-				{
-					htmltext = "32711-09a.htm";
-				}
-				else
-				{
-					st.takeItems(CET_1_SHEET, 1);
-					st.takeItems(CET_2_SHEET, 1);
-					st.takeItems(CET_3_SHEET, 1);
-					addSpawn(HEKATON_PRIME, 192062, 57357, -7650, 1000, false, 0);
-					st.set("spawned", "1");
-					htmltext = "32711-09.htm";
-				}
-			}
+				hekaton = addSpawn(HEKATON_PRIME, 191777, 56197, -7624, 0, false, 0);
+				htmltext = "32711-09.html";
+				break;
+			case "32711-03.htm":
+			case "32711-05.html":
+			case "32711-06.html":
+				break;
+			default:
+				htmltext = null;
+				break;
 		}
 		return htmltext;
 	}
@@ -122,7 +110,7 @@ public class Q00307_ControlDeviceOfTheGiants extends Quest
 	public String onTalk(L2Npc npc, L2PcInstance player)
 	{
 		String htmltext = getNoQuestMsg(player);
-		QuestState st = player.getQuestState(getName());
+		final QuestState st = player.getQuestState(getName());
 		if (st == null)
 		{
 			return htmltext;
@@ -132,39 +120,24 @@ public class Q00307_ControlDeviceOfTheGiants extends Quest
 		{
 			case State.CREATED:
 			{
-				if (player.getLevel() >= 79)
-				{
-					htmltext = "32711-01.htm";
-				}
-				else
-				{
-					htmltext = "32711-02.htm";
-				}
+				htmltext = (player.getLevel() >= 79) ? "32711-01.htm" : "32711-02.htm";
 				break;
 			}
 			case State.STARTED:
 			{
-				if (st.getInt("spawned") == 1)
+				if ((hekaton != null) && !hekaton.isDead())
 				{
-					htmltext = "32711-09.htm";
+					htmltext = "32711-09.html";
 				}
-				else if (st.getInt("cond") == 1)
+				else if (st.isCond(1))
 				{
-					if (!st.hasQuestItems(CET_1_SHEET) || !st.hasQuestItems(CET_2_SHEET) || !st.hasQuestItems(CET_3_SHEET))
-					{
-						htmltext = "32711-07.htm";
-					}
-					else
-					{
-						htmltext = "32711-08.htm";
-					}
+					htmltext = (!hasQuestItems(player, CET_1_SHEET, CET_2_SHEET, CET_3_SHEET)) ? "32711-07.html" : "32711-08.html";
 				}
-				else if (st.getInt("cond") == 2)
+				else if (st.isCond(2))
 				{
 					st.giveItems(SUPPORT_ITEMS, 1);
-					st.playSound("ItemSound.quest_finish");
-					st.exitQuest(true);
-					htmltext = "32711-10.htm";
+					st.exitQuest(true, true);
+					htmltext = "32711-10.html";
 				}
 				break;
 			}
@@ -175,10 +148,10 @@ public class Q00307_ControlDeviceOfTheGiants extends Quest
 	@Override
 	public String onKill(L2Npc npc, L2PcInstance player, boolean isPet)
 	{
-		L2PcInstance partyMember = getRandomPartyMember(player, "1");
+		final L2PcInstance partyMember = getRandomPartyMember(player, "1");
 		if (partyMember == null)
 		{
-			return null;
+			return super.onKill(npc, player, isPet);
 		}
 		final QuestState st = partyMember.getQuestState(getName());
 		
@@ -187,67 +160,52 @@ public class Q00307_ControlDeviceOfTheGiants extends Quest
 			case GORGOLOS:
 			{
 				st.giveItems(CET_1_SHEET, 1);
-				st.playSound("ItemSound.quest_itemget");
+				st.playSound(QuestSound.ITEMSOUND_QUEST_ITEMGET);
 				break;
 			}
 			case LAST_TITAN_UTENUS:
 			{
 				st.giveItems(CET_2_SHEET, 1);
-				st.playSound("ItemSound.quest_itemget");
+				st.playSound(QuestSound.ITEMSOUND_QUEST_ITEMGET);
 				break;
 			}
 			case GIANT_MARPANAK:
 			{
 				st.giveItems(CET_3_SHEET, 1);
-				st.playSound("ItemSound.quest_itemget");
+				st.playSound(QuestSound.ITEMSOUND_QUEST_ITEMGET);
 				break;
 			}
 			case HEKATON_PRIME:
 			{
-				if (player.getParty() != null)
+				if (player.isInParty())
 				{
-					for (L2PcInstance party : player.getParty().getMembers())
+					for (L2PcInstance pl : player.getParty().getMembers())
 					{
-						rewardPlayer(party);
+						final QuestState qs = pl.getQuestState(getName());
+						
+						if ((qs != null) && qs.isCond(1))
+						{
+							qs.setCond(2, true);
+						}
 					}
-				}
-				else
-				{
-					rewardPlayer(player);
+					saveGlobalQuestVar("Respawn", Long.toString(System.currentTimeMillis() + RESPAWN_DELAY));
 				}
 				break;
 			}
 		}
-		return null;
-	}
-	
-	private void rewardPlayer(L2PcInstance player)
-	{
-		QuestState st = player.getQuestState(getName());
-		
-		if ((st != null) && (st.getInt("spawned") == 1))
-		{
-			st.playSound("ItemSound.quest_middle");
-			st.unset("spawned");
-			st.set("cond", "2");
-			st.saveGlobalQuestVar("Hekaton respawn", Long.toString(System.currentTimeMillis() + respawnDelay));
-		}
+		return super.onKill(npc, player, isPet);
 	}
 	
 	public Q00307_ControlDeviceOfTheGiants(int id, String name, String descr)
 	{
 		super(id, name, descr);
-		
 		addStartNpc(DROPH);
 		addTalkId(DROPH);
-		addKillId(GORGOLOS);
-		addKillId(LAST_TITAN_UTENUS);
-		addKillId(GIANT_MARPANAK);
-		addKillId(HEKATON_PRIME);
+		addKillId(GORGOLOS, LAST_TITAN_UTENUS, GIANT_MARPANAK, HEKATON_PRIME);
 	}
 	
 	public static void main(String[] args)
 	{
-		new Q00307_ControlDeviceOfTheGiants(307, Q00307_ControlDeviceOfTheGiants.class.getSimpleName(), "Control Device Of The Giants");
+		new Q00307_ControlDeviceOfTheGiants(307, Q00307_ControlDeviceOfTheGiants.class.getSimpleName(), "Control Device of the Giants");
 	}
 }
