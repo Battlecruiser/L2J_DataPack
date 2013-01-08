c74aea25daaec9227bb1694786e17235a54fe8c8
Adry_85 <Adry_85@users.noreply.github.com>
2013-01-08 19:55:14 +0000

    BETA: Missing from [9373].

--- L2J_DataPack_BETA/dist/sql/game/character_quests.sql
+++ L2J_DataPack_BETA/dist/sql/game/character_quests.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `character_quests` (
   `charId` INT UNSIGNED NOT NULL DEFAULT 0,
-  `name` VARCHAR(40) NOT NULL DEFAULT '',
+  `name` VARCHAR(50) NOT NULL DEFAULT '',
   `var`  VARCHAR(20) NOT NULL DEFAULT '',
   `value` VARCHAR(255) ,
   `class_index` int(1) NOT NULL DEFAULT '0',
