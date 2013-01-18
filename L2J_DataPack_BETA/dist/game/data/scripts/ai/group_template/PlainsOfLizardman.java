88e183243f59e2f6c65d4a958db32bba843043fc
malyelfik <malyelfik@users.noreply.github.com>
2013-01-18 18:40:34 +0000

    BETA: Updating '''Plains of Lizardman''' * Also updating Czech language for SQLAccountManager

--- L2J_DataPack_BETA/dist/game/data/scripts/ai/group_template/PlainsOfLizardman.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/group_template/PlainsOfLizardman.java
@@ -21,178 +21,227 @@ package ai.group_template;
 import ai.npc.AbstractNpcAI;
 
 import com.l2jserver.gameserver.ai.CtrlIntention;
-import com.l2jserver.gameserver.datatables.SkillTable;
-import com.l2jserver.gameserver.handler.ISkillHandler;
-import com.l2jserver.gameserver.handler.SkillHandler;
 import com.l2jserver.gameserver.model.L2CharPosition;
 import com.l2jserver.gameserver.model.actor.L2Attackable;
 import com.l2jserver.gameserver.model.actor.L2Character;
 import com.l2jserver.gameserver.model.actor.L2Npc;
-import com.l2jserver.gameserver.model.actor.L2Playable;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.skills.L2Skill;
-import com.l2jserver.gameserver.network.serverpackets.MagicSkillUse;
+import com.l2jserver.gameserver.model.holders.SkillHolder;
 
 /**
- * Plains Of Lizardman AI.
- * @author Gnacik
+ * Plains of Lizardman AI
+ * @author Gnacik, malyelfik
  */
-public class PlainsOfLizardman extends AbstractNpcAI
+public final class PlainsOfLizardman extends AbstractNpcAI
 {
-	private static final int[] _MOBS =
+	// NPCs
+	private static final int INVISIBLE_NPC = 18919;
+	private static final int TANTA_GUARD = 18862;
+	private static final int FANTASY_MUSHROOM = 18864;
+	private static final int STICKY_MUSHROOM = 18865;
+	private static final int RAINBOW_FROG = 18866;
+	private static final int ENERGY_PLANT = 18868;
+	private static final int TANTA_SCOUT = 22768;
+	private static final int TANTA_MAGICIAN = 22773;
+	private static final int TANTA_SUMMONER = 22774;
+	private static final int[] MOBS =
 	{
-		18864,
-		18865,
-		18866,
-		18868
+		22768, // Tanta Lizardman Scout
+		22769, // Tanta Lizardman Warrior
+		22770, // Tanta Lizardman Soldier
+		22771, // Tanta Lizardman Berserker
+		22772, // Tanta Lizardman Archer
+		22773, // Tanta Lizardman Magician
+		22774, // Tanta Lizardman Summoner
 	};
 	
-	private static final int FANTASY_MUSHROOM = 18864;
-	private static final int FANTASY_MUSHROOM_SKILL = 6427;
+	// Skills
+	private static final SkillHolder STUN_EFFECT = new SkillHolder(6622, 1);
+	private static final SkillHolder DEMOTIVATION_HEX = new SkillHolder(6425, 1);
+	private static final SkillHolder FANTASY_MUSHROOM_SKILL = new SkillHolder(6427, 1);
+	private static final SkillHolder RAINBOW_FROG_SKILL = new SkillHolder(6429, 1);
+	private static final SkillHolder STICKY_MUSHROOM_SKILL = new SkillHolder(6428, 1);
+	private static final SkillHolder ENERGY_PLANT_SKILL = new SkillHolder(6430, 1);
 	
-	private static final int RAINBOW_FROG = 18866;
-	private static final int RAINBOW_FROG_SKILL = 6429;
+	// Buffs
+	private static final SkillHolder[] BUFFS =
+	{
+		new SkillHolder(6625, 1), // Energy of Life
+		new SkillHolder(6626, 2), // Energy of Life's Power
+		new SkillHolder(6627, 3), // Energy of Life's Highest Power
+		new SkillHolder(6628, 1), // Energy of Mana
+		new SkillHolder(6629, 2), // Energy of Mana's Power
+		new SkillHolder(6630, 3), // Energy of Mana's Highest Power
+		new SkillHolder(6631, 1), // Energy of Power
+		new SkillHolder(6633, 1), // Energy of Attack Speed
+		new SkillHolder(6635, 1), // Energy of Crt Rate
+		new SkillHolder(6636, 1), // Energy of Moving Speed
+		new SkillHolder(6638, 1), // Aura of Mystery
+		new SkillHolder(6639, 1), // Bane of Auras - Damage
+		new SkillHolder(6640, 1), // Energizing Aura
+		new SkillHolder(6674, 1), // Energy of Range Increment
+	};
 	
-	private static final int STICKY_MUSHROOM = 18865;
-	private static final int STICKY_MUSHROOM_SKILL = 6428;
-	
-	private static final int ENERGY_PLANT = 18868;
-	private static final int ENERGY_PLANT_SKILL = 6430;
+	// Misc
+	private static final int[] BUFF_LIST =
+	{
+		6,
+		7,
+		8,
+		11,
+		13
+	};
 	
 	private PlainsOfLizardman(String name, String descr)
 	{
 		super(name, descr);
-		registerMobs(_MOBS, QuestEventType.ON_ATTACK);
+		addAttackId(FANTASY_MUSHROOM, RAINBOW_FROG, STICKY_MUSHROOM, ENERGY_PLANT, TANTA_SUMMONER);
+		addKillId(MOBS);
 	}
 	
 	@Override
 	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
 	{
-		if ((player != null) && !player.isAlikeDead())
+		if (event.equals("fantasy_mushroom") && (npc != null) && (player != null))
 		{
-			boolean isPet = false;
-			if (event.endsWith("_pet") && player.hasSummon() && !player.getSummon().isDead())
+			npc.doCast(FANTASY_MUSHROOM_SKILL.getSkill());
+			for (L2Character target : npc.getKnownList().getKnownCharactersInRadius(200))
 			{
-				isPet = true;
-			}
-			
-			if (event.startsWith("rainbow_frog"))
-			{
-				triggerSkill(npc, isPet ? player.getSummon() : player, RAINBOW_FROG_SKILL, 1);
-			}
-			else if (event.startsWith("energy_plant"))
-			{
-				triggerSkill(npc, isPet ? player.getSummon() : player, ENERGY_PLANT_SKILL, 1);
-			}
-			else if (event.startsWith("sticky_mushroom"))
-			{
-				triggerSkill(npc, isPet ? player.getSummon() : player, STICKY_MUSHROOM_SKILL, 1);
-			}
-			else if (event.startsWith("fantasy_mushroom"))
-			{
-				L2Skill skill = SkillTable.getInstance().getInfo(FANTASY_MUSHROOM_SKILL, 1);
-				npc.doCast(skill);
-				for (L2Character target : npc.getKnownList().getKnownCharactersInRadius(200))
+				if ((target != null) && target.isL2Attackable())
 				{
-					if ((target != null) && (target instanceof L2Attackable) && (target.getAI() != null))
-					{
-						skill.getEffects(npc, target);
-						attackPlayer((L2Attackable) target, isPet ? player.getSummon() : player);
-					}
+					final L2Attackable monster = (L2Attackable) target;
+					npc.setTarget(monster);
+					npc.doCast(STUN_EFFECT.getSkill());
+					attackPlayer(monster, player);
 				}
-				npc.doDie(player);
 			}
+			npc.doDie(player);
 		}
-		return super.onAdvEvent(event, npc, player);
+		return null;
 	}
 	
 	@Override
 	public String onAttack(L2Npc npc, L2PcInstance attacker, int damage, boolean isPet)
 	{
-		if (npc.isDead())
+		switch (npc.getNpcId())
 		{
-			return null;
-		}
-		
-		if (npc.getNpcId() == RAINBOW_FROG)
-		{
-			if (isPet)
-			{
-				startQuestTimer("rainbow_frog_pet", 2000, npc, attacker);
-			}
-			else
-			{
-				startQuestTimer("rainbow_frog", 2000, npc, attacker);
-			}
-			npc.doDie(attacker);
-		}
-		else if (npc.getNpcId() == STICKY_MUSHROOM)
-		{
-			if (isPet)
-			{
-				startQuestTimer("sticky_mushroom_pet", 2000, npc, attacker);
-			}
-			else
-			{
-				startQuestTimer("sticky_mushroom", 2000, npc, attacker);
-			}
-			npc.doDie(attacker);
-		}
-		else if (npc.getNpcId() == ENERGY_PLANT)
-		{
-			if (isPet)
-			{
-				startQuestTimer("energy_plant_pet", 2000, npc, attacker);
-			}
-			else
-			{
-				startQuestTimer("energy_plant", 2000, npc, attacker);
-			}
-			npc.doDie(attacker);
-		}
-		else if (npc.getNpcId() == FANTASY_MUSHROOM)
-		{
-			for (L2Character target : npc.getKnownList().getKnownCharactersInRadius(1000))
-			{
-				if ((target != null) && (target instanceof L2Attackable) && (target.getAI() != null))
+			case TANTA_SUMMONER:
+				if (npc.getFirstEffect(DEMOTIVATION_HEX.getSkillId()) == null)
 				{
-					target.setIsRunning(true);
-					target.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, new L2CharPosition(npc.getX(), npc.getY(), npc.getZ(), 0));
+					npc.doCast(DEMOTIVATION_HEX.getSkill());
 				}
-			}
-			if (isPet)
-			{
-				startQuestTimer("fantasy_mushroom_pet", 3000, npc, attacker);
-			}
-			else
-			{
-				startQuestTimer("fantasy_mushroom", 3000, npc, attacker);
-			}
+				break;
+			case RAINBOW_FROG:
+				castSkill(npc, attacker, RAINBOW_FROG_SKILL);
+				break;
+			case ENERGY_PLANT:
+				castSkill(npc, attacker, ENERGY_PLANT_SKILL);
+				break;
+			case STICKY_MUSHROOM:
+				castSkill(npc, attacker, STICKY_MUSHROOM_SKILL);
+				break;
+			case FANTASY_MUSHROOM:
+				if (npc.isScriptValue(0))
+				{
+					npc.setScriptValue(1);
+					npc.setIsInvul(true);
+					for (L2Character target : npc.getKnownList().getKnownCharactersInRadius(1000))
+					{
+						if ((target != null) && target.isL2Attackable())
+						{
+							final L2Attackable monster = (L2Attackable) target;
+							if ((monster.getNpcId() == TANTA_MAGICIAN) || (monster.getNpcId() == TANTA_SCOUT))
+							{
+								target.setIsRunning(true);
+								target.getAI().setIntention(CtrlIntention.AI_INTENTION_MOVE_TO, new L2CharPosition(npc.getX(), npc.getY(), npc.getZ(), 0));
+							}
+						}
+					}
+					startQuestTimer("fantasy_mushroom", 4000, npc, attacker);
+				}
+				break;
 		}
 		return super.onAttack(npc, attacker, damage, isPet);
 	}
 	
-	private static void triggerSkill(L2Character caster, L2Playable playable, int skill_id, int skill_level)
+	@Override
+	public String onKill(L2Npc npc, L2PcInstance killer, boolean isPet)
 	{
-		L2Character[] targets = new L2Character[1];
-		targets[0] = playable;
-		
-		L2Skill trigger = SkillTable.getInstance().getInfo(skill_id, skill_level);
-		
-		if ((trigger != null) && playable.isInsideRadius(caster, trigger.getCastRange(), true, false) && (playable.getInstanceId() == caster.getInstanceId()))
+		// Tanta Guard
+		if (getRandom(1000) == 0)
 		{
-			playable.broadcastPacket(new MagicSkillUse(playable, playable, skill_id, skill_level, 0, 0));
-			
-			ISkillHandler handler = SkillHandler.getInstance().getHandler(trigger.getSkillType());
-			if (handler != null)
+			final L2Npc guard = addSpawn(TANTA_GUARD, npc);
+			attackPlayer((L2Attackable) guard, killer);
+		}
+		
+		// Invisible buff npc
+		final int random = getRandom(100);
+		final L2Npc buffer = addSpawn(INVISIBLE_NPC, npc.getLocation(), false, 6000);
+		buffer.setTarget(killer);
+		
+		if (random <= 42)
+		{
+			castRandomBuff(buffer, 7, 45, BUFFS[0], BUFFS[1], BUFFS[2]);
+		}
+		if (random <= 11)
+		{
+			castRandomBuff(buffer, 8, 60, BUFFS[3], BUFFS[4], BUFFS[5]);
+			castRandomBuff(buffer, 3, 6, BUFFS[9], BUFFS[10], BUFFS[12]);
+		}
+		if (random <= 25)
+		{
+			buffer.doCast(BUFFS[BUFF_LIST[getRandom(BUFF_LIST.length)]].getSkill());
+		}
+		if (random <= 10)
+		{
+			buffer.doCast(BUFFS[13].getSkill());
+		}
+		if (random <= 1)
+		{
+			final int i = getRandom(100);
+			if (i <= 34)
 			{
-				handler.useSkill(playable, trigger, targets);
+				buffer.doCast(BUFFS[6].getSkill());
+				buffer.doCast(BUFFS[7].getSkill());
+				buffer.doCast(BUFFS[8].getSkill());
+			}
+			else if (i < 67)
+			{
+				buffer.doCast(BUFFS[13].getSkill());
 			}
 			else
 			{
-				trigger.useSkill(playable, targets);
+				buffer.doCast(BUFFS[2].getSkill());
+				buffer.doCast(BUFFS[5].getSkill());
 			}
 		}
+		return super.onKill(npc, killer, isPet);
+	}
+	
+	private void castRandomBuff(L2Npc npc, int chance1, int chance2, SkillHolder... buffs)
+	{
+		final int rand = getRandom(100);
+		if (rand <= chance1)
+		{
+			npc.doCast(buffs[2].getSkill());
+		}
+		else if (rand <= chance2)
+		{
+			npc.doCast(buffs[1].getSkill());
+		}
+		else
+		{
+			npc.doCast(buffs[0].getSkill());
+		}
+	}
+	
+	private void castSkill(L2Npc npc, L2Character target, SkillHolder skill)
+	{
+		npc.doDie(target);
+		
+		final L2Npc buffer = addSpawn(INVISIBLE_NPC, npc.getLocation(), false, 6000);
+		buffer.setTarget(target);
+		buffer.doCast(skill.getSkill());
 	}
 	
 	public static void main(String[] args)
