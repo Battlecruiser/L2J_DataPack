c1b44e7f2ddc21f8d79b30150b719b12a9017672
malyelfik <malyelfik@users.noreply.github.com>
2013-02-15 16:34:17 +0000

    BETA: Quest Wedding March (431) Patch by: eyjine Reviewed by: malyelfik

--- L2J_DataPack_BETA/dist/sql/game/updates/20130215update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130215update.sql
@@ -5,4 +5,5 @@ UPDATE character_quests SET name='Q00140_ShadowFoxPart2' WHERE name='140_ShadowF
 UPDATE character_quests SET name='Q00141_ShadowFoxPart3' WHERE name='141_ShadowFoxPart3';
 UPDATE character_quests SET name='Q00142_FallenAngelRequestOfDawn' WHERE name='142_FallenAngelRequestOfDawn';
 UPDATE character_quests SET name='Q00143_FallenAngelRequestOfDusk' WHERE name='143_FallenAngelRequestOfDusk';
+UPDATE character_quests SET name='Q00431_WeddingMarch' WHERE name='431_WeddingMarch';
 UPDATE character_quests SET name='Q00998_FallenAngelSelect' WHERE name='998_FallenAngelSelect';
