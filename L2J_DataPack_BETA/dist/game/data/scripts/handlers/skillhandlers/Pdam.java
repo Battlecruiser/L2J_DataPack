ec568c271f42a8d0edd25308877d6ba4ba408f73
Adry_85 <Adrya85@hotmail.it>
2013-02-23 19:07:19 +0000

    BETA: Updated souls formula, added check if player's souls are more than skill's max consume.
    	Reviewed by: !MELERIX

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/skillhandlers/Pdam.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/skillhandlers/Pdam.java
@@ -99,7 +99,8 @@ public class Pdam implements ISkillHandler
 			if (!skill.isStaticDamage() && (skill.getMaxSoulConsumeCount() > 0) && activeChar.isPlayer())
 			{
 				// Souls Formula (each soul increase +4%)
-				damage *= ((activeChar.getActingPlayer().getSouls() * 0.04) + 1);
+				int chargedSouls = (activeChar.getActingPlayer().getSouls() <= skill.getMaxSoulConsumeCount()) ? activeChar.getActingPlayer().getSouls() : skill.getMaxSoulConsumeCount();
+				damage *= 1 + (chargedSouls * 0.04);
 			}
 			if (crit)
 			{
