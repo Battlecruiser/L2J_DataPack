19a93ab8a932a6296f144174da85a0da7e7b1eba
Zoey76 <Zoey76@users.noreply.github.com>
2013-03-16 00:48:19 +0000

    BETA: Quest Arrow for Vengeance (331) in Java. 	* SQL queries to update previous quest names.
    Patch by: xban1x
    Reviewed by: Zoey76

---
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130315update.sql
@@ -0,0 +1,4 @@
+UPDATE character_quests SET name='Q00324_SweetestVenom' WHERE name='324_SweetestVenom';
+UPDATE character_quests SET name='Q00328_SenseForBusiness' WHERE name='328_SenseForBusiness';
+UPDATE character_quests SET name='Q00331_ArrowOfVengeance' WHERE name='331_ArrowForVengeance';
+UPDATE character_quests SET name='Q00341_HuntingForWildBeasts' WHERE name='341_HuntingForWildBeasts';
