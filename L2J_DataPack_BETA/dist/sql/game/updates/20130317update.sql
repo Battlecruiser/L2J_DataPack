74e4627f5d11c1c7272f55bb9b30b17dd048fae3
malyelfik <malyelfik@users.noreply.github.com>
2013-03-17 19:11:58 +0000

    BETA: Quest '''Find Sir Windawood''' (155)

--- L2J_DataPack_BETA/dist/sql/game/updates/20130317update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130317update.sql
@@ -1,3 +1,4 @@
+UPDATE character_quests SET name='Q00155_FindSirWindawood' WHERE name='155_FindSirWindawood';
 UPDATE character_quests SET name='Q00635_IntoTheDimensionalRift' WHERE name='RiftQuest';
 UPDATE character_quests SET name='Q00639_GuardiansOfTheHolyGrail' WHERE name='639_GuardiansOfTheHolyGrail';
 UPDATE character_quests SET name='Q00646_SignsOfRevolt' WHERE name='646_SignsOfRevolt';
