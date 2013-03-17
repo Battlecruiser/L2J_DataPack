7445d7fd2a91577554cbe5beb4d0b5191bb8d29c
malyelfik <malyelfik@users.noreply.github.com>
2013-03-17 09:38:41 +0000

    BETA: Updating some quests to High Five * Guardians of the Holy Grail (639)  * Patch by: corbin12
    * Signs of Revolt (646)
    * Unhardcoded Into the Dimensional Rift (635)
    * Removed The Zero Hour (640)

---
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130317update.sql
@@ -0,0 +1,3 @@
+UPDATE character_quests SET name='Q00635_IntoTheDimensionalRift' WHERE name='RiftQuest';
+UPDATE character_quests SET name='Q00639_GuardiansOfTheHolyGrail' WHERE name='639_GuardiansOfTheHolyGrail';
+UPDATE character_quests SET name='Q00646_SignsOfRevolt' WHERE name='646_SignsOfRevolt';
