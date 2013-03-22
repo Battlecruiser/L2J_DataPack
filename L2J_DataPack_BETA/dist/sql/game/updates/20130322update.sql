73558cfb11d96f26f6f922cee1ac94a5059208c5
malyelfik <malyelfik@users.noreply.github.com>
2013-03-22 19:40:03 +0000

    BETA: Quest '''Seed of Evil''' (158)

--- L2J_DataPack_BETA/dist/sql/game/updates/20130322update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130322update.sql
@@ -1,3 +1,4 @@
+UPDATE character_quests SET name='Q00158_SeedOfEvil' WHERE name='158_SeedOfEvil';
 UPDATE character_quests SET name='Q00162_CurseOfTheUndergroundFortress' WHERE name='162_CurseOfFortress';
 UPDATE character_quests SET name='Q00163_LegacyOfThePoet' WHERE name='163_LegacyOfPoet';
 UPDATE character_quests SET name='Q00166_MassOfDarkness' WHERE name='166_DarkMass';
