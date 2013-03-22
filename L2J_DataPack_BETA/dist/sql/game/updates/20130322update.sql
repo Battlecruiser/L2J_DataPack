93aea22439074045980ef994ab58ecce2d8b7fe8
malyelfik <malyelfik@users.noreply.github.com>
2013-03-22 16:26:15 +0000

    BETA: Quests in Java * Curse of the Underground Fortress (162) * Legacy of the Poet (163) * Mass of Darkness (166) * Dwarven Kinship (167) Patch by: xban1x Reviewed by: malyelfik

---
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130322update.sql
@@ -0,0 +1,4 @@
+UPDATE character_quests SET name='Q00162_CurseOfTheUndergroundFortress' WHERE name='162_CurseOfFortress';
+UPDATE character_quests SET name='Q00163_LegacyOfThePoet' WHERE name='163_LegacyOfPoet';
+UPDATE character_quests SET name='Q00166_MassOfDarkness' WHERE name='166_DarkMass';
+UPDATE character_quests SET name='Q00167_DwarvenKinship' WHERE name='167_DwarvenKinship';
