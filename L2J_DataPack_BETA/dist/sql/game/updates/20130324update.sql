e927145a171a67e98ff48bf7b35d565e7286ab0e
malyelfik <malyelfik@users.noreply.github.com>
2013-03-24 12:52:52 +0000

    BETA: Quest '''Offspring of Nightmares''' (169) Patch by: xban1x Reviewed by: malyelfik

--- L2J_DataPack_BETA/dist/sql/game/updates/20130324update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130324update.sql
@@ -1,1 +1,2 @@
 UPDATE character_quests SET name='Q00168_DeliverSupplies' WHERE name='168_DeliverSupplies';
+UPDATE character_quests SET name='Q00169_OffspringOfNightmares' WHERE name='169_NightmareChildren';
