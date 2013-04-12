47dae935e9ca2100e9000e3039fca5c40b94bdc3
MELERIX <MELERIX@users.noreply.github.com>
2013-04-12 23:05:51 +0000

    BETA: SQL Fixes for [9616].  * Formatted SQL tables.  * Fixed SQL Update (it should be line by line, otherwise if one change fail the others are not executed).

--- L2J_DataPack_BETA/dist/sql/game/castle_doorupgrade.sql
+++ L2J_DataPack_BETA/dist/sql/game/castle_doorupgrade.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `castle_doorupgrade` (
-	`doorId` INT(11) NOT NULL DEFAULT 0,
-	`ratio` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	`castleId` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	PRIMARY KEY (`doorId`)
+  `doorId` int(8) unsigned NOT NULL DEFAULT '0',
+  `ratio` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `castleId` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`doorId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- L2J_DataPack_BETA/dist/sql/game/castle_trapupgrade.sql
+++ L2J_DataPack_BETA/dist/sql/game/castle_trapupgrade.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `castle_trapupgrade` (
-	`castleId` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	`towerIndex` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	`level` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	PRIMARY KEY (`towerIndex`, `castleId`)
+  `castleId` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `towerIndex` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`towerIndex`,`castleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
