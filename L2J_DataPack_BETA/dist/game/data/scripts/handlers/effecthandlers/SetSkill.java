8083775039d33a82153ac5f928426831b864ba73
Zoey76 <Zoey76@users.noreply.github.com>
2013-05-12 03:52:21 +0000

    BETA: Datapack part for [L5989]: 	* Implementing Set Skill effect. 	* Implementing few missing skills for event items: 		* Fighter's Will (9090) 		* Archer's Will (9091) 		* Magician's Will (9092) 		* Protection of Rune (9093) 		* Protection of Elemental (9094) 		* Protection of Alignment (9095)

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/SetSkill.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2004-2013 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package handlers.effecthandlers;
+
+import com.l2jserver.gameserver.datatables.SkillTable;
+import com.l2jserver.gameserver.model.effects.EffectTemplate;
+import com.l2jserver.gameserver.model.effects.L2Effect;
+import com.l2jserver.gameserver.model.effects.L2EffectType;
+import com.l2jserver.gameserver.model.skills.L2Skill;
+import com.l2jserver.gameserver.model.stats.Env;
+
+/**
+ * Set Skill effect implementation.
+ * @author Zoey76
+ */
+public class SetSkill extends L2Effect
+{
+	private final int _skillId;
+	private final int _skillLvl;
+	
+	public SetSkill(Env env, EffectTemplate template)
+	{
+		super(env, template);
+		_skillId = template.getParameters().getInteger("skillId", 0);
+		_skillLvl = template.getParameters().getInteger("skillLvl", 1);
+	}
+	
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.NONE;
+	}
+	
+	@Override
+	public boolean onStart()
+	{
+		if ((getEffected() == null) || !getEffected().isPlayer())
+		{
+			return false;
+		}
+		
+		final L2Skill skill = SkillTable.getInstance().getInfo(_skillId, _skillLvl);
+		if (skill == null)
+		{
+			return false;
+		}
+		getEffected().getActingPlayer().addSkill(skill, true);
+		return true;
+	}
+}
