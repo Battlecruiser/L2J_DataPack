baaca11cb56fcf0c4ede13ed44e3f55b63ba0121
Rumen Nikiforov <unafraid89@gmail.com>
2013-05-17 18:32:01 +0000

    BETA: Quest Invention Ambition (269) in Java. 	* SQL queries to update previous quest names.
    	* Patch by: xBan1x
    	* Reviewed by: UnAfraid, Zoey76, jurchiks

--- L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
@@ -1,1 +1,2 @@
+UPDATE character_quests SET name='Q00269_InventionAmbition' WHERE name='269_InventionAmbition';
 UPDATE character_quests SET name='Q00271_ProofOfValor' WHERE name='271_ProofOfValor';
