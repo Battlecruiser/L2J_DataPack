a9f0e2939109db8b2e87a20a282833bc120e7398
Rumen Nikiforov <unafraid89@gmail.com>
2013-05-17 18:42:28 +0000

    BETA: Quest Wrath of Ancestors (272) in Java. 	* SQL queries to update previous quest names.
    	* Patch by: xBan1x
    	* Reviewed by: UnAfraid, Zoey76, jurchiks

--- L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
@@ -1,3 +1,4 @@
 UPDATE character_quests SET name='Q00267_WrathOfVerdure' WHERE name='267_WrathOfVerdure';
 UPDATE character_quests SET name='Q00269_InventionAmbition' WHERE name='269_InventionAmbition';
 UPDATE character_quests SET name='Q00271_ProofOfValor' WHERE name='271_ProofOfValor';
+UPDATE character_quests SET name='Q00272_WrathOfAncestors' WHERE name='272_WrathOfAncestors';
