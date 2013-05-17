bf7573b7fcef263e1a90e8d5abf1f3483f2756d4
Rumen Nikiforov <unafraid89@gmail.com>
2013-05-17 18:35:08 +0000

    BETA: Quest Wrath of Verdure (267) in Java. 	* SQL queries to update previous quest names.
    	* Patch by: xBan1x
    	* Reviewed by: UnAfraid, Zoey76, jurchiks

--- L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
@@ -1,2 +1,3 @@
+UPDATE character_quests SET name='Q00267_WrathOfVerdure' WHERE name='267_WrathOfVerdure';
 UPDATE character_quests SET name='Q00269_InventionAmbition' WHERE name='269_InventionAmbition';
 UPDATE character_quests SET name='Q00271_ProofOfValor' WHERE name='271_ProofOfValor';
