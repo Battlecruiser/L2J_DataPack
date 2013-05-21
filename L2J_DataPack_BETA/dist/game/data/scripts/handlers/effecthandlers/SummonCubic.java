361b25ceae5e89a5b9d53b068188994f8c176a6e
Zoey76 <zoey_76@msn.com>
2013-05-21 04:11:52 +0000

    BETA: Skill fixes: 	* Only passive Buff effect can't be stolen. 	* Passive Buff effect last indefinitely. 	* Implemented Cubic Mastery effect. 		* Fixed Cubic Mastery skill to use effect, please no more COREDONE skills! 	* Implemented Summon Cubic effect. 		* Removed custom system messages and restrictions. 		* If cubic is already present it's replaced. 		* If maximum cubic count is reached (related to Cubic Mastery effect), '''a random cubic is removed and the new cubic is added last, retail like'''. 			* No more custom restrictions. 		* Fixed all cubic power enchant broken in [9230]. 	* Passive skills with stats need to be wrapped into Buff effect. 		* Fixed some skills, the rest will be fixed soon. 			* Added missing MP regeneration to Armor Mastery. 	* Fixed High Five Arena buffs, they shouldn't stack with normal buffs! 		* Reported by: nBd 	* Fixing Petrify (6882) with missing abnormal time. 		* Reported by: blacksea

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/SummonCubic.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2004-2013 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package handlers.effecthandlers;
+
+import com.l2jserver.gameserver.model.actor.instance.L2CubicInstance;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.model.effects.EffectTemplate;
+import com.l2jserver.gameserver.model.effects.L2Effect;
+import com.l2jserver.gameserver.model.effects.L2EffectType;
+import com.l2jserver.gameserver.model.stats.Env;
+import com.l2jserver.util.Rnd;
+
+/**
+ * Summon Cubic effect implementation.
+ * @author Zoey76
+ */
+public class SummonCubic extends L2Effect
+{
+	private final int _npcId;
+	/** Cubic power. */
+	private final int _cubicPower;
+	/** Cubic duration. */
+	private final int _cubicDuration;
+	/** Cubic activation delay. */
+	private final int _cubicDelay;
+	/** Cubic maximum casts before going idle. */
+	private final int _cubicMaxCount;
+	/** Cubic activation chance. */
+	private final int _cubicSkillChance;
+	
+	public SummonCubic(Env env, EffectTemplate template)
+	{
+		super(env, template);
+		_npcId = template.getParameters().getInteger("npcId", 0);
+		// Custom AI data.
+		_cubicPower = template.getParameters().getInteger("cubicPower", 0);
+		_cubicDuration = template.getParameters().getInteger("cubicDuration", 0);
+		_cubicDelay = template.getParameters().getInteger("cubicDelay", 0);
+		_cubicMaxCount = template.getParameters().getInteger("cubicMaxCount", -1);
+		_cubicSkillChance = template.getParameters().getInteger("cubicSkillChance", 0);
+	}
+	
+	@Override
+	public L2EffectType getEffectType()
+	{
+		return L2EffectType.NONE;
+	}
+	
+	@Override
+	public boolean isInstant()
+	{
+		return true;
+	}
+	
+	@Override
+	public boolean onStart()
+	{
+		if ((getEffected() == null) || !getEffected().isPlayer() || getEffected().isAlikeDead() || getEffected().getActingPlayer().inObserverMode())
+		{
+			return false;
+		}
+		
+		if (_npcId <= 0)
+		{
+			_log.warning(SummonCubic.class.getSimpleName() + ": Invalid NPC Id:" + _npcId + " in skill Id: " + getSkill().getId());
+			return false;
+		}
+		
+		final L2PcInstance player = getEffected().getActingPlayer();
+		if (player.inObserverMode() || player.isMounted())
+		{
+			return false;
+		}
+		
+		// Gnacik: TODO: Make better method of calculation.
+		// If skill is enchanted calculate cubic skill level based on enchant
+		// 8 at 101 (+1 Power)
+		// 12 at 130 (+30 Power)
+		// Because 12 is max 5115-5117 skills
+		int _cubicSkillLevel = getSkill().getLevel();
+		if (_cubicSkillLevel > 100)
+		{
+			_cubicSkillLevel = ((getSkill().getLevel() - 100) / 7) + 8;
+		}
+		
+		// If cubic is already present, it's replaced.
+		final L2CubicInstance cubic = player.getCubicById(_npcId);
+		if (cubic != null)
+		{
+			cubic.stopAction();
+			cubic.cancelDisappear();
+			player.getCubics().remove(cubic);
+		}
+		else
+		{
+			// If maximum amount is reached, random cubic is removed.
+			final L2Effect cubicMastery = player.getFirstPassiveEffect(L2EffectType.CUBIC_MASTERY);
+			final int cubicCount = (int) (cubicMastery != null ? cubicMastery.calc() : 0);
+			if (player.getCubics().size() >= cubicCount)
+			{
+				player.getCubics().remove(Rnd.get(player.getCubics().size()));
+			}
+		}
+		player.addCubic(_npcId, _cubicSkillLevel, _cubicPower, _cubicDelay, _cubicSkillChance, _cubicMaxCount, _cubicDuration, getEffected() != getEffector());
+		player.broadcastUserInfo();
+		return true;
+	}
+}
