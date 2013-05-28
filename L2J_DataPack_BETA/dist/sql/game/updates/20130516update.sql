a733aeecfb9adfe29c16870b6eb4d4bd9a198461
Rumen Nikiforov <unafraid89@gmail.com>
2013-05-28 22:46:40 +0000

    BETA: Quest Invaders of the Holy Land (273) in Java. 	* SQL queries to update previous quest names
    	* Patch by: xban1x
    	* Reviewed by: UnAfraid, Zoey76, jurchiks

--- L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
@@ -2,3 +2,4 @@ UPDATE character_quests SET name='Q00267_WrathOfVerdure' WHERE name='267_WrathOf
 UPDATE character_quests SET name='Q00269_InventionAmbition' WHERE name='269_InventionAmbition';
 UPDATE character_quests SET name='Q00271_ProofOfValor' WHERE name='271_ProofOfValor';
 UPDATE character_quests SET name='Q00272_WrathOfAncestors' WHERE name='272_WrathOfAncestors';
+UPDATE character_quests SET name='Q00273_InvadersOfTheHolyLand' WHERE name='273_InvadersOfHolyLand';
