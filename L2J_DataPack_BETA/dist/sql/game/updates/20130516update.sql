026e8dbd24354aa14317c48bee9ace41fed1d4e4
Rumen Nikiforov <unafraid89@gmail.com>
2013-05-29 20:07:49 +0000

    BETA: Quest Relic Exploration (183) in Java. 	* SQL queries to update previous quest names
    	* Patch by: IvanTotov
    	* Tested by: xban1x
    	* Reviewed by: xban1x, jurchiks, UnAfraid

--- L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130516update.sql
@@ -3,3 +3,4 @@ UPDATE character_quests SET name='Q00269_InventionAmbition' WHERE name='269_Inve
 UPDATE character_quests SET name='Q00271_ProofOfValor' WHERE name='271_ProofOfValor';
 UPDATE character_quests SET name='Q00272_WrathOfAncestors' WHERE name='272_WrathOfAncestors';
 UPDATE character_quests SET name='Q00273_InvadersOfTheHolyLand' WHERE name='273_InvadersOfHolyLand';
+UPDATE character_quests SET name='Q00183_RelicExploration' WHERE name='183_Relic_Exploration';
