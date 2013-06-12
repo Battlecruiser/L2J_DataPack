c78a7db1afb2722d4fc8ed4d6dced3eb855a9bd7
xban1x <urban.lavbic@gmail.com>
2013-06-12 17:31:35 +0000

    BETA: Fixing typo in SQL query.
    	* Reported by: lucan

--- L2J_DataPack_BETA/dist/sql/game/updates/20130605update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20130605update.sql
@@ -1,2 +1,2 @@
 UPDATE character_quests SET name='Q00260_OrcHunting' WHERE name='260_HuntForOrcs1';
-UPDATE character_variables SET name='GUIDE_MISSION' WHERE name='guide_mission';
+UPDATE character_variables SET var='GUIDE_MISSION' WHERE var='guide_mission';
