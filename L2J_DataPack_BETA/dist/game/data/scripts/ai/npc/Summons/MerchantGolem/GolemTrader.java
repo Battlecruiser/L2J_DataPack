ad8dfa8ce853b67aebe2cfdd5fc9f6fab21f9f35
Zoey76 <zoey_76@msn.com>
2013-08-25 03:47:27 +0000

    BETA: Fixing Golem Trader summon. 	* Removed empty folder. 	* Added Golem Trader retail AI.
    Reported by: pandragon

---
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/npc/Summons/MerchantGolem/GolemTrader.java
@@ -0,0 +1,33 @@
+package ai.npc.Summons.MerchantGolem;
+
+import ai.npc.AbstractNpcAI;
+
+import com.l2jserver.gameserver.model.actor.L2Npc;
+
+/**
+ * Golem Trader AI.
+ * @author Zoey76
+ */
+public class GolemTrader extends AbstractNpcAI
+{
+	// NPC
+	private static final int GOLEM_TRADER = 13128;
+	
+	public GolemTrader()
+	{
+		super(GolemTrader.class.getSimpleName(), "ai/npc/Summons");
+		addSpawnId(GOLEM_TRADER);
+	}
+	
+	@Override
+	public String onSpawn(L2Npc npc)
+	{
+		npc.scheduleDespawn(180000);
+		return super.onSpawn(npc);
+	}
+	
+	public static void main(String[] args)
+	{
+		new GolemTrader();
+	}
+}
