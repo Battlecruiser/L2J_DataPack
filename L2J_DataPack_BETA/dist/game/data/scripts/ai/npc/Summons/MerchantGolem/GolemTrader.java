519436be11cc1afeb62aa25d96eea7eb9fab3940
xban1x <urban.lavbic@gmail.com>
2013-10-04 08:01:00 +0000

    BETA: Missing license for GolemTrader.java 	* Reported by: u3games 	* Patch by: u3games 	* Reviewed by: xban1x

--- L2J_DataPack_BETA/dist/game/data/scripts/ai/npc/Summons/MerchantGolem/GolemTrader.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/npc/Summons/MerchantGolem/GolemTrader.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2004-2013 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package ai.npc.Summons.MerchantGolem;
 
 import ai.npc.AbstractNpcAI;
@@ -8,12 +26,12 @@ import com.l2jserver.gameserver.model.actor.L2Npc;
  * Golem Trader AI.
  * @author Zoey76
  */
-public class GolemTrader extends AbstractNpcAI
+public final class GolemTrader extends AbstractNpcAI
 {
 	// NPC
 	private static final int GOLEM_TRADER = 13128;
 	
-	public GolemTrader()
+	private GolemTrader()
 	{
 		super(GolemTrader.class.getSimpleName(), "ai/npc/Summons");
 		addSpawnId(GOLEM_TRADER);
