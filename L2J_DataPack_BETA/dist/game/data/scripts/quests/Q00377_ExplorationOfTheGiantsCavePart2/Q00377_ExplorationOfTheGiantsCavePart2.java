1d4344f1c03b28babdc2c5f24346f398da9d9817
Adry_85 <Adrya85@hotmail.it>
2013-10-27 22:13:24 +0000

    BETA: Updating reward system for quests '''Exploration of the Giants' Cave'''. 	Reported by: lion, !UnAfraid 	Reviewed by: jurchiks 	Thanks to: lion

--- L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00377_ExplorationOfTheGiantsCavePart2/Q00377_ExplorationOfTheGiantsCavePart2.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00377_ExplorationOfTheGiantsCavePart2/Q00377_ExplorationOfTheGiantsCavePart2.java
@@ -18,20 +18,16 @@
  */
 package quests.Q00377_ExplorationOfTheGiantsCavePart2;
 
-import com.l2jserver.gameserver.enums.QuestSound;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
-import com.l2jserver.gameserver.model.quest.State;
 import com.l2jserver.gameserver.network.serverpackets.RadarControl;
-import com.l2jserver.gameserver.util.Util;
 
 /**
  * Exploration of the Giants' Cave Part 2 (377)<br>
  * Original Jython script by Gnacik.
  * @author nonom
- * @version 2010-02-17 based on official Franz server
  */
 public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 {
@@ -44,8 +40,6 @@ public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 	private static final int BOOK3 = 14844;
 	private static final int BOOK4 = 14845;
 	private static final int BOOK5 = 14846;
-	// Drop Chance
-	private static final int DROP_CHANCE = 50;
 	// Mobs
 	private static final int[] MOBS =
 	{
@@ -59,16 +53,10 @@ public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 		22668,
 		22669
 	};
-	// Rewards
-	private static final int OBLIVION = 9625;
-	private static final int DISCIPLINE = 9626;
-	private static final int LEONARD = 9628;
-	private static final int ADAMANTINE = 9629;
-	private static final int ORICHALCUM = 9630;
 	
-	public Q00377_ExplorationOfTheGiantsCavePart2(int id, String name, String descr)
+	private Q00377_ExplorationOfTheGiantsCavePart2()
 	{
-		super(id, name, descr);
+		super(377, Q00377_ExplorationOfTheGiantsCavePart2.class.getSimpleName(), "Exploration of the Giants' Cave - Part 2");
 		addStartNpc(SOBLING);
 		addTalkId(SOBLING);
 		addKillId(MOBS);
@@ -85,35 +73,18 @@ public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 			return htmltext;
 		}
 		
-		if (event.equalsIgnoreCase("31147-02.htm"))
+		switch (event)
 		{
-			st.startQuest();
-			player.sendPacket(new RadarControl(0, 2, -113360, -244676, -15536));
-		}
-		else if (event.equalsIgnoreCase("31147-quit.html"))
-		{
-			st.exitQuest(true, true);
-		}
-		else if (Util.isDigit(event))
-		{
-			final int val = Integer.parseInt(event);
-			switch (val)
+			case "31147-02.htm":
 			{
-				case OBLIVION:
-					htmltext = exchangeRequest(st, val, 1, 5); // Giant's Codex - Oblivion
-					break;
-				case DISCIPLINE:
-					htmltext = exchangeRequest(st, val, 1, 5); // Giant's Codex - Discipline
-					break;
-				case LEONARD:
-					htmltext = exchangeRequest(st, val, 6, 1); // Leonard
-					break;
-				case ADAMANTINE:
-					htmltext = exchangeRequest(st, val, 3, 1); // Adamantine
-					break;
-				case ORICHALCUM:
-					htmltext = exchangeRequest(st, val, 4, 1); // Orichalcum
-					break;
+				st.startQuest();
+				player.sendPacket(new RadarControl(0, 2, -113360, -244676, -15536));
+				break;
+			}
+			case "31147-quit.html":
+			{
+				st.exitQuest(true, true);
+				break;
 			}
 		}
 		return htmltext;
@@ -122,16 +93,10 @@ public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 	@Override
 	public String onKill(L2Npc npc, L2PcInstance player, boolean isSummon)
 	{
-		final QuestState st = player.getQuestState(getName());
-		if (st == null)
+		final QuestState st = getRandomPartyMemberState(player, -1, 3, npc);
+		if (st != null)
 		{
-			return null;
-		}
-		
-		if ((st.isCond(1)) && (getRandom(100) < DROP_CHANCE))
-		{
-			st.giveItems(TITAN_ANCIENT_BOOK, 1);
-			st.playSound(QuestSound.ITEMSOUND_QUEST_ITEMGET);
+			giveItemRandomly(player, npc, TITAN_ANCIENT_BOOK, 1, 0, 0.5, true);
 		}
 		return super.onKill(npc, player, isSummon);
 	}
@@ -146,40 +111,19 @@ public class Q00377_ExplorationOfTheGiantsCavePart2 extends Quest
 			return htmltext;
 		}
 		
-		if (npc.getId() == SOBLING)
+		if (st.isCreated())
 		{
-			switch (st.getState())
-			{
-				case State.CREATED:
-					htmltext = (player.getLevel() >= 79) ? "31147-01.htm" : "31147-00.html";
-					break;
-				case State.STARTED:
-					htmltext = (st.hasQuestItems(BOOK1) && st.hasQuestItems(BOOK2) && st.hasQuestItems(BOOK3) && st.hasQuestItems(BOOK4) && st.hasQuestItems(BOOK5)) ? "31147-03.html" : "31147-02a.html";
-					break;
-			}
-			
+			htmltext = (player.getLevel() >= 79 ? "31147-01.htm" : "31147-00.html");
+		}
+		else if (st.isStarted())
+		{
+			htmltext = (st.hasQuestItems(BOOK1, BOOK2, BOOK3, BOOK4, BOOK5) ? "31147-03.html" : "31147-02a.html");
 		}
 		return htmltext;
 	}
 	
-	private static String exchangeRequest(QuestState st, int giveid, int qty, int rem)
-	{
-		if ((st.getQuestItemsCount(BOOK1) >= rem) && (st.getQuestItemsCount(BOOK2) >= rem) && (st.getQuestItemsCount(BOOK3) >= rem) && (st.getQuestItemsCount(BOOK4) >= rem) && (st.getQuestItemsCount(BOOK5) >= rem))
-		{
-			st.takeItems(BOOK1, rem);
-			st.takeItems(BOOK2, rem);
-			st.takeItems(BOOK3, rem);
-			st.takeItems(BOOK4, rem);
-			st.takeItems(BOOK5, rem);
-			st.giveItems(giveid, qty);
-			st.playSound(QuestSound.ITEMSOUND_QUEST_FINISH);
-			return "31147-ok.html";
-		}
-		return "31147-no.html";
-	}
-	
 	public static void main(String[] args)
 	{
-		new Q00377_ExplorationOfTheGiantsCavePart2(377, Q00377_ExplorationOfTheGiantsCavePart2.class.getSimpleName(), "Exploration of the Giants' Cave - Part 2");
+		new Q00377_ExplorationOfTheGiantsCavePart2();
 	}
 }
