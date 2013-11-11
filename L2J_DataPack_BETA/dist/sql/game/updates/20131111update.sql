c2c23f72bb2dc9a70f4f96af408103c704473b8e
xban1x <xban1x@users.noreply.github.com>
2013-11-11 11:07:57 +0000

    BETA: Quest Q00286_FabulousFeathers in Java. 	* Reviewed by: Zoey76, UnAfraid

--- L2J_DataPack_BETA/dist/sql/game/updates/20131111update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20131111update.sql
@@ -1,1 +1,2 @@
 UPDATE character_quests SET name='Q00284_MuertosFeather' WHERE name='284_MuertosFeather';
+UPDATE character_quests SET name='Q00286_FabulousFeathers' WHERE name='286_FabulousFeathers';
