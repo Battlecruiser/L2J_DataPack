daa08159e00e1cac716b24a8de3657c4eec5d2f5
Rumen Nikiforov <unafraid89@gmail.com>
2013-11-30 19:32:51 +0000

    BETA: Fixing minor typo in AdminScan.js. 	* Reported by: FinalDestination 	* Patch by: malyelfik

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/admincommandhandlers/AdminScan.js
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/admincommandhandlers/AdminScan.js
@@ -63,7 +63,7 @@ AdminCommandHandler.getInstance().registerHandler(new JavaAdapter(IAdminCommandH
 						sb.append("<tr>");
 						sb.append("<td width=\"54\">" + character.getId() + "</td>");
 						sb.append("<td width=\"54\">" + character.getName() + "</td>");
-						sb.append("<td width=\"54\">" + Math.round(Util.calculateDistance(player, character, false)) + "</td>");
+						sb.append("<td width=\"54\">" + Math.round(player.calculateDistance(character, false, false)) + "</td>");
 						sb.append("<td width=\"54\"><a action=\"bypass -h admin_deleteNpcByObjectId " + character.getObjectId() + "\"><font color=\"LEVEL\">Delete</font></a></td>");
 						sb.append("<td width=\"54\"><a action=\"bypass -h admin_move_to " + character.getX() + " " + character.getY() + " " + character.getZ() + "\"><font color=\"LEVEL\">Go to</font></a></td>");
 						sb.append("</tr>");
