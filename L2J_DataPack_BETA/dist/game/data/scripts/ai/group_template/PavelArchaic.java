b26f6e7b9ac663b193391a193af6d343f635697b
Zoey76 <zoey_76@msn.com>
2013-12-07 22:26:50 +0000

    BETA: AIs updates. 	* Update for Giant's Cave AI. 		* Reviewed by: jurchiks, Zoey76 		* Tested by: St3eT 	* Update for Pavel Archaic AI. 		* Reviewed by: Zoey76 		* Tested by: St3eT, FinalDestination

--- L2J_DataPack_BETA/dist/game/data/scripts/ai/group_template/PavelArchaic.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/ai/group_template/PavelArchaic.java
@@ -26,41 +26,33 @@ import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 
 /**
  * Pavel Archaic AI.
- * @author Gnacik
+ * @author Gnacik, St3eT
  */
-public class PavelArchaic extends AbstractNpcAI
+public final class PavelArchaic extends AbstractNpcAI
 {
+	private static final int SAFETY_DEVICE = 18917; // Pavel Safety Device
+	private static final int PINCER_GOLEM = 22801; // Cruel Pincer Golem
+	private static final int PINCER_GOLEM2 = 22802; // Cruel Pincer Golem
+	private static final int PINCER_GOLEM3 = 22803; // Cruel Pincer Golem
+	private static final int JACKHAMMER_GOLEM = 22804; // Horrifying Jackhammer Golem
+	
 	private PavelArchaic()
 	{
 		super(PavelArchaic.class.getSimpleName(), "ai/group_template");
-		addKillId(22801, 22804);
-		addAttackId(18917);
-	}
-	
-	@Override
-	public String onAttack(L2Npc npc, L2PcInstance attacker, int damage, boolean isSummon)
-	{
-		if (!npc.isDead())
-		{
-			npc.doDie(attacker);
-			
-			if (getRandom(100) < 40)
-			{
-				L2Attackable golem1 = (L2Attackable) addSpawn(22801, npc.getLocation(), false, 0);
-				attackPlayer(golem1, attacker);
-				
-				L2Attackable golem2 = (L2Attackable) addSpawn(22804, npc.getLocation(), false, 0);
-				attackPlayer(golem2, attacker);
-			}
-		}
-		return super.onAttack(npc, attacker, damage, isSummon);
+		addKillId(SAFETY_DEVICE, PINCER_GOLEM, JACKHAMMER_GOLEM);
 	}
 	
 	@Override
 	public String onKill(L2Npc npc, L2PcInstance killer, boolean isSummon)
 	{
-		L2Attackable golem = (L2Attackable) addSpawn(npc.getId() + 1, npc.getLocation(), false, 0);
-		attackPlayer(golem, killer);
+		if (getRandom(100) < 70)
+		{
+			final L2Attackable golem1 = (L2Attackable) addSpawn(PINCER_GOLEM2, npc.getX(), npc.getY(), npc.getZ() + 10, npc.getHeading(), false, 0, false);
+			attackPlayer(golem1, killer);
+			
+			final L2Attackable golem2 = (L2Attackable) addSpawn(PINCER_GOLEM3, npc.getX(), npc.getY(), npc.getZ() + 10, npc.getHeading(), false, 0, false);
+			attackPlayer(golem2, killer);
+		}
 		return super.onKill(npc, killer, isSummon);
 	}
 	
