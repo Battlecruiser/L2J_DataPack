6dad5317f2b7322c86cc3bdffd44896b91d12a17
Adry_85 <Adrya85@hotmail.it>
2013-12-15 21:33:24 +0000

    BETA: Minor fix for [10076].

--- L2J_DataPack_BETA/dist/sql/game/updates/20131215update.sql
+++ L2J_DataPack_BETA/dist/sql/game/updates/20131215update.sql
@@ -1,3 +1,2 @@
-UPDATE character_quests SET name='Q00362_BardsMandolin' WHERE name='362_BardsMandolin';
-UPDATE character_quests SET name='Q00363_SorrowfulSoundOfFlute' WHERE name='363_SorrowfulSoundofFlute';
-UPDATE character_quests SET name='Q00364_JovialAccordion' WHERE name='364_JovialAccordion';
+ALTER TABLE `characters` ALTER `title_color` SET DEFAULT 0xECF9A2; 
+UPDATE `characters` SET `title_color` = 0xECF9A2 WHERE `title_color` = 0xFFFFFF;
