0dd04147a2d395e4c66469737d6030513caf423d
Nos <NosBit@users.noreply.github.com>
2013-12-20 23:59:24 +0000

    BETA: DP-Part for [L6319]

--- L2J_DataPack_BETA/dist/sql/game/character_norestart_zone_time.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `character_norestart_zone_time` (
-  `charId` int(10) unsigned NOT NULL DEFAULT '0',
-  `time_limit` bigint(13) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY  (`charId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
