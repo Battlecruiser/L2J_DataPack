91777c1bfbbe40bafea593b9911182ab87f8fb7f
Adry_85 <Adrya85@hotmail.it>
2013-12-22 17:26:22 +0000

    BETA: Retail-like quests in Java. 	* '''Silver Haired Shaman''' (366) 	* '''Electrifying Recharge! ''' (367) Patch by: Adry_85, jurchiks

---
+++ L2J_DataPack_BETA/dist/sql/game/updates/20131222update.sql
@@ -0,0 +1,2 @@
+UPDATE character_quests SET name='Q00366_SilverHairedShaman' WHERE name='366_SilverHairedShaman';
+UPDATE character_quests SET name='Q00367_ElectrifyingRecharge' WHERE name='367_ElectrifyingRecharge';
