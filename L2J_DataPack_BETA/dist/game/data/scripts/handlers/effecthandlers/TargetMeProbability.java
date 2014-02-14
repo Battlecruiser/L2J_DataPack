c3e8cfe47dd7d3eb889c5301675200c1d0f5841b
Nos <NosBit@users.noreply.github.com>
2014-02-14 23:18:58 +0000

    BETA: DP-Part for [L6397].

--- L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/TargetMeProbability.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/handlers/effecthandlers/TargetMeProbability.java
@@ -20,7 +20,6 @@ package handlers.effecthandlers;
 
 import com.l2jserver.gameserver.model.StatsSet;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
-import com.l2jserver.gameserver.model.actor.instance.L2SiegeSummonInstance;
 import com.l2jserver.gameserver.model.conditions.Condition;
 import com.l2jserver.gameserver.model.effects.AbstractEffect;
 import com.l2jserver.gameserver.model.skills.BuffInfo;
@@ -57,11 +56,6 @@ public final class TargetMeProbability extends AbstractEffect
 	{
 		if (info.getEffected().isPlayable())
 		{
-			if (info.getEffected() instanceof L2SiegeSummonInstance)
-			{
-				return;
-			}
-			
 			if (info.getEffected().getTarget() != info.getEffector())
 			{
 				L2PcInstance effector = info.getEffector().getActingPlayer();
