24a21499f2069b5709a0b42ee72a8fd41f1fba2e
St3eT <St3eT@users.noreply.github.com>
2014-03-04 23:08:17 +0000

    BETA: Implementing '''Klemis''' AI, also fixed typo from [10229]. * Reviewed by: Nos, Adry_85, Zoey76

--- L2J_DataPack_BETA/dist/sql/game/teleport.sql
+++ L2J_DataPack_BETA/dist/sql/game/teleport.sql
@@ -796,7 +796,6 @@ INSERT INTO `teleport` VALUES
 ('Oren -> Sel Mahum Training Grounds (South Gate)',15001,79414,71496,-3448,3700,0,57),
 ('Oren -> Sel Mahum Training Grounds (Center)',15002,87448,61460,-3664,1800,0,57),
 ('Schuttgart -> Mithril Mines',15003,171946,-173352,3440,5300,0,57),
-('Entrance SOA -> Enter the Seed of Annihilation',15004,-180218,185923,-10576,0,0,57),
 ('Enter the Seed of Annihilation -> Entrance SOA',15005,-178262,153430,2472,0,0,57),
 ('Seed of Infinity Dock',15006,-212843,209695,4280,150000,0,57), -- retail
 ('Seed of Destruction Dock',15007,-248535,250273,4336,150000,0,57), -- retail
