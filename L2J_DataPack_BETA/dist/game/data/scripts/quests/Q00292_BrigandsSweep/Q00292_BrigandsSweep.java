de9f6cd602052f74ce606182b09147d55be84536
Zoey76 <Zoey76@users.noreply.github.com>
2014-08-11 03:15:41 +0000

    BETA: Fixing minor typos.

--- L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00292_BrigandsSweep/Q00292_BrigandsSweep.java
+++ L2J_DataPack_BETA/dist/game/data/scripts/quests/Q00292_BrigandsSweep/Q00292_BrigandsSweep.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2013 L2J DataPack
+ * Copyright (C) 2004-2014 L2J DataPack
  *
  * This file is part of L2J DataPack.
  *
@@ -21,8 +21,8 @@ package quests.Q00292_BrigandsSweep;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.l2jserver.gameserver.enums.Race;
 import com.l2jserver.gameserver.enums.QuestSound;
+import com.l2jserver.gameserver.enums.Race;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.quest.Quest;
