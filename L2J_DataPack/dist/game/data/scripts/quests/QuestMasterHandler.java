99d6a3c4a164f8a3578255e4b82822a591d4170a
ivantotov <gmtotov@gmail.com>
2014-11-20 23:05:47 +0200

    Quest: Test Of Sagittarius (224) Reviewed by: @Zoey76

--- L2J_DataPack/dist/game/data/scripts/quests/QuestMasterHandler.java
+++ L2J_DataPack/dist/game/data/scripts/quests/QuestMasterHandler.java
@@ -200,6 +200,7 @@ import quests.Q00219_TestimonyOfFate.Q00219_TestimonyOfFate;
 import quests.Q00220_TestimonyOfGlory.Q00220_TestimonyOfGlory;
 import quests.Q00221_TestimonyOfProsperity.Q00221_TestimonyOfProsperity;
 import quests.Q00223_TestOfTheChampion.Q00223_TestOfTheChampion;
+import quests.Q00224_TestOfSagittarius.Q00224_TestOfSagittarius;
 import quests.Q00235_MimirsElixir.Q00235_MimirsElixir;
 import quests.Q00237_WindsOfChange.Q00237_WindsOfChange;
 import quests.Q00238_SuccessFailureOfBusiness.Q00238_SuccessFailureOfBusiness;
@@ -642,6 +643,7 @@ public class QuestMasterHandler
 		Q00220_TestimonyOfGlory.class,
 		Q00221_TestimonyOfProsperity.class,
 		Q00223_TestOfTheChampion.class,
+		Q00224_TestOfSagittarius.class,
 		Q00235_MimirsElixir.class,
 		Q00237_WindsOfChange.class,
 		Q00238_SuccessFailureOfBusiness.class,
--- L2J_DataPack/dist/sql/game/updates/20141120update.sql
+++ L2J_DataPack/dist/sql/game/updates/20141120update.sql
@@ -1,2 +1,3 @@
 UPDATE character_quests SET name='Q00220_TestimonyOfGlory' WHERE name='220_TestimonyOfGlory';
 UPDATE character_quests SET name='Q00223_TestOfTheChampion' WHERE name='223_TestOfChampion';
+UPDATE character_quests SET name='Q00224_TestOfSagittarius' WHERE name='224_TestOfSagittarius'; 
