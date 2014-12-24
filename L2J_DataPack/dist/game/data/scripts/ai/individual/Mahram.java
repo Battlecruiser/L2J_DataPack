4fd301548b76242efd714935b96aadda1f8e20b4
St3eT <St3eT@seznam.cz>
2014-12-24 13:56:31 +0200

    Adding few AIs: Adding ai for Drill Sergeant and Mahram Patch by

---
+++ L2J_DataPack/dist/game/data/scripts/ai/individual/Mahram.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2004-2014 L2J DataPack
+ * 
+ * This file is part of L2J DataPack.
+ * 
+ * L2J DataPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J DataPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package ai.individual;
+
+import ai.npc.AbstractNpcAI;
+
+import com.l2jserver.gameserver.model.actor.L2Npc;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.network.NpcStringId;
+import com.l2jserver.gameserver.network.clientpackets.Say2;
+
+/**
+ * Mahram AI.
+ * @author St3eT
+ */
+public final class Mahram extends AbstractNpcAI
+{
+	// NPCs
+	private static final int MAHRAM = 33271;
+	
+	private Mahram()
+	{
+		super(Mahram.class.getSimpleName(), "ai/individual");
+		addSpawnId(MAHRAM);
+	}
+	
+	@Override
+	public String onAdvEvent(String event, L2Npc npc, L2PcInstance player)
+	{
+		if (event.equals("SPAM_TEXT") && (npc != null))
+		{
+			broadcastNpcSay(npc, Say2.ALL, NpcStringId.ALCHEMY_IS_A_SCIENCE_AND_AN_ART);
+		}
+		return super.onAdvEvent(event, npc, player);
+	}
+	
+	@Override
+	public String onSpawn(L2Npc npc)
+	{
+		startQuestTimer("SPAM_TEXT", 10000, npc, null, true);
+		return super.onSpawn(npc);
+	}
+	
+	public static void main(String[] args)
+	{
+		new Mahram();
+	}
+}
