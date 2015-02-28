f83f916a7f1f56067279c2107b3c3b8517b80a7b
Zealar <Zealar@users.noreply.github.com>
2015-02-28 15:37:23 +0200

    2 New effects * MaxHp * MaxCp Both support diff and per Fix problem while increase and then regenerate hp Require core!

--- L2J_DataPack/dist/game/data/scripts/handlers/EffectMasterHandler.java
+++ L2J_DataPack/dist/game/data/scripts/handlers/EffectMasterHandler.java
@@ -119,6 +119,8 @@ public final class EffectMasterHandler
 		ManaHealByLevel.class,
 		ManaHealOverTime.class,
 		ManaHealPercent.class,
+		MaxCp.class,
+		MaxHp.class,
 		MpConsumePerLevel.class,
 		Mute.class,
 		NoblesseBless.class,
